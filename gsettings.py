9284fe35e54ae95ce41bab855d4fdb4766533700
mbs-affiliate <mbs.affiliate@gmail.com>
2015-02-16 19:30:38 +0300

    Create gsettings.py

---
+++ gsettings.py
@@ -0,0 +1,23 @@
+{
+    "mobile":
+[
+    [["US"], ["http://trkur.com/178649/17311"]],
+    [["ZZ"], [
+              "http://trkur.com/178649/17310", 
+              "http://trkur.com/178649/17311"
+             ]]
+
+],
+ 
+    "desktop":
+[
+
+    [["ZZ"], [
+              "http://trkur4.com/178649/17882", 
+              "http://trkur.com/178649/17310", 
+              "http://trkur.com/178649/17311", 
+              "http://chaturbate.com/affiliates/in/R2Xc/Xbh8t/?track=male"
+             ]]
+]
+}
+
