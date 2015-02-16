7b49d596736ea2747e6133c723f5267da81be2d5
mbs-affiliate <mbs.affiliate@gmail.com>
2015-02-16 19:26:55 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,48 @@
+{
+    "mobile":
+[
+    [["US"], [
+              # Hookup Cloud mobile
+              "http://trkur3.com/178649/17886"
+             ]],
+    [["IT"], [
+               # Joliess Adult Mobile
+               "http://trkur.com/178649/17609", 
+               "http://mobile.bitterstrawberry.org/?id=170"
+             ]],
+    [["AE", "AF", "AL", "AM", "AO", "AR", "AT", "AU", "AW", "AZ", "BA", "BB", "BD", "BE", "BG", "BH", "BN", "BO", "BR", "BT", "CA", "CH", "CL", "CM", "CN", "CO", "CR", "CY", "CZ", "DE", "DK", "DO", "DZ", "EC", "EE", "EG", "ES", "ET", "FI", "FR", "GE", "GH", "GM", "GR", "GT", "GU", "HK", "HN", "HR", "HU", "ID", "IE", "IL", "IN", "IQ", "IR", "JM", "JO", "JP", "KE", "KH", "KR", "KW", "KZ", "LB", "LK", "LT", "LU", "LV", "LY", "MA", "MD", "ME", "MK", "MM", "MN", "MU", "MV", "MX", "MY", "NA", "NG", "NI", "NL", "NO", "NP", "NZ", "OM", "PA", "PE", "PH", "PK", "PL", "PR", "PS", "PT", "PY", "QA", "RE", "RO", "RS", "RU", "SA", "SD", "SE", "SG", "SI", "SK", "SS", "SV", "TH", "TM", "TN", "TR", "TW", "TZ", "UA", "UG", "UK", "UY", "UZ", "VE", "VN", "YE","ZA","ZW"], [
+              "http://mobile.bitterstrawberry.org/?id=170"
+             ]],
+    [["ZZ"], [
+              "http://rd.mobiteasy.com/mr/?id=alex1306"
+             ]]
+
+
+],
+ 
+    "desktop":
+[
+    [["US", 
+      "AU", 
+      "GB", 
+      "CA"], [
+              "http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=default",
+              # Sexier PPS
+              "http://trkur.com/178649/17307", 
+              # Phone mates PPS  
+              "http://trkur.com/178649/16873"   
+             ]],
+    [["DE", 
+      "AT", 
+      "CH"], [
+              # My Secretary Girls PPS
+              "http://trkur4.com/178649/18043",
+              # Sexy.com
+              "http://crtracklink.com/go/caRG9_bBBLR/DEFAULT"
+             ]],
+    [["ZZ"], [
+              # Sexy.com
+              "http://crtracklink.com/go/caRG9_bBBLR/DEFAULT"
+             ]]
+]
+}
