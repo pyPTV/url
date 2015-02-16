e68be94f0114f7c1233fad9e45fe9deb831343db
mbs-affiliate <mbs.affiliate@gmail.com>
2015-02-16 22:25:36 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@
               "http://rd.mobiteasy.com/mr/?id=alex1306"
              ]],
     [["IT"], [
-               "http://trkur.com/178649/17609", 
+               
                "http://mobile.bitterstrawberry.org/?id=170"
              ]],
     [["AE", "AF", "AL", "AM", "AO", "AR", "AT", "AU", "AW", "AZ", "BA", "BB", "BD", "BE", "BG", "BH", "BN", "BO", "BR", "BT", "CA", "CH", "CL", "CM", "CN", "CO", "CR", "CY", "CZ", "DE", "DK", "DO", "DZ", "EC", "EE", "EG", "ES", "ET", "FI", "FR", "GE", "GH", "GM", "GR", "GT", "GU", "HK", "HN", "HR", "HU", "ID", "IE", "IL", "IN", "IQ", "IR", "JM", "JO", "JP", "KE", "KH", "KR", "KW", "KZ", "LB", "LK", "LT", "LU", "LV", "LY", "MA", "MD", "ME", "MK", "MM", "MN", "MU", "MV", "MX", "MY", "NA", "NG", "NI", "NL", "NO", "NP", "NZ", "OM", "PA", "PE", "PH", "PK", "PL", "PR", "PS", "PT", "PY", "QA", "RE", "RO", "RS", "RU", "SA", "SD", "SE", "SG", "SI", "SK", "SS", "SV", "TH", "TM", "TN", "TR", "TW", "TZ", "UA", "UG", "UK", "UY", "UZ", "VE", "VN", "YE","ZA","ZW"], [
@@ -24,14 +24,12 @@
       "AU", 
       "GB", 
       "CA"], [
-              "http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=default",
-              "http://trkur.com/178649/17307", 
-              "http://trkur.com/178649/16873"   
+              "http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=default"
              ]],
     [["DE", 
       "AT", 
       "CH"], [
-              "http://trkur4.com/178649/18043",
+
               "http://crtracklink.com/go/caRG9_bBBLR/DEFAULT"
              ]],
     [["ZZ"], [
