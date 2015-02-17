78a93a5c6f2a66ff7ce0c89481292e819c5fbd7c
mbs-affiliate <mbs.affiliate@gmail.com>
2015-02-17 03:03:10 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,14 +1,22 @@
 {
     "mobile":
 [
-    [["US"], [
-              "http://rd.mobiteasy.com/mr/?id=alex1306"
-             ]],
-    [["IT"], [
-               
-               "http://mobile.bitterstrawberry.org/?id=170"
-             ]],
-    [["AE", "AF", "AL", "AM", "AO", "AR", "AT", "AU", "AW", "AZ", "BA", "BB", "BD", "BE", "BG", "BH", "BN", "BO", "BR", "BT", "CA", "CH", "CL", "CM", "CN", "CO", "CR", "CY", "CZ", "DE", "DK", "DO", "DZ", "EC", "EE", "EG", "ES", "ET", "FI", "FR", "GE", "GH", "GM", "GR", "GT", "GU", "HK", "HN", "HR", "HU", "ID", "IE", "IL", "IN", "IQ", "IR", "JM", "JO", "JP", "KE", "KH", "KR", "KW", "KZ", "LB", "LK", "LT", "LU", "LV", "LY", "MA", "MD", "ME", "MK", "MM", "MN", "MU", "MV", "MX", "MY", "NA", "NG", "NI", "NL", "NO", "NP", "NZ", "OM", "PA", "PE", "PH", "PK", "PL", "PR", "PS", "PT", "PY", "QA", "RE", "RO", "RS", "RU", "SA", "SD", "SE", "SG", "SI", "SK", "SS", "SV", "TH", "TM", "TN", "TR", "TW", "TZ", "UA", "UG", "UK", "UY", "UZ", "VE", "VN", "YE","ZA","ZW"], [
+
+    [["AE", "AF", "AL", "AM", "AO", "AR", "AT", "AU", "AW", 
+      "AZ", "BA", "BB", "BD", "BE", "BG", "BH", "BN", "BO", 
+      "BR", "BT", "CA", "CH", "CL", "CM", "CN", "CO", "CR", 
+      "CY", "CZ", "DE", "DK", "DO", "DZ", "EC", "EE", "EG", 
+      "ES", "ET", "FI", "FR", "GE", "GH", "GM", "GR", "GT", 
+      "GU", "HK", "HN", "HR", "HU", "ID", "IE", "IL", "IN", 
+      "IQ", "IR", "JM", "JO", "JP", "KE", "KH", "KR", "KW", 
+      "KZ", "LB", "LK", "LT", "LU", "LV", "LY", "MA", "MD", 
+      "ME", "MK", "MM", "MN", "MU", "MV", "MX", "MY", "NA", 
+      "NG", "NI", "NL", "NO", "NP", "NZ", "OM", "PA", "PE", 
+      "PH", "PK", "PL", "PR", "PS", "PT", "PY", "QA", "RE", 
+      "RO", "RS", "RU", "SA", "SD", "SE", "SG", "SI", "SK", 
+      "SS", "SV", "TH", "TM", "TN", "TR", "TW", "TZ", "UA", 
+      "UG", "UK", "UY", "UZ", "VE", "VN", "YE","ZA","ZW", "IT"], 
+             [
               "http://mobile.bitterstrawberry.org/?id=170"
              ]],
     [["ZZ"], [
@@ -24,27 +32,11 @@
       "AU", 
       "GB", 
       "CA"], [
-              
               "http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=default"
              ]],
 
-    [["LU", 
-      "NO", 
-      "DK", 
-      "PA", 
-      "DE", 
-      "IS", 
-      "NL", 
-      "SE", 
-      "AT"],
-             [
-              "http://crtracklink.com/go/caRG9_bBBLR/DEFAULT"
-             ]],
     [["ZZ"], [
-              "http://enter.gangav.com/track/MTkyMjguMi4xMC4zNi4wLjAuMC4wLjA",
-              "http://enter.javhd.com/track/MTkyMjguMi4yLjY0LjAuMC4wLjAuMA",
-              "http://enter.av69.tv/track/MTkyMjguMi40LjQuMC4wLjAuMC4w",
-              "http://enter.ferame.com/track/MTkyMjguMi42LjE0LjAuMC4wLjAuMA"
+              "http://crtracklink.com/go/caRG9_bBBLR/DEFAULT"
              ]]
 ]
 }
