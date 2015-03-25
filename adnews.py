bdba47edfcd56cdb4d759ab43b4bae7f76aa3051
mbs-affiliate <mbs.affiliate@gmail.com>
2015-03-25 10:52:15 +0300

    Create adnews.py

---
+++ adnews.py
@@ -0,0 +1,39 @@
+{
+    "mobile":
+[
+
+    [["US"], [
+              "http://mobile.us", "Title for mobile.us"
+             ]],
+
+    [["ZZ"], [
+              "http://mobile.zz", "Title for mobile.zz"
+              "http://mobile-1.zz", "Title for mobile-1.zz"
+             ]]
+
+
+],
+ 
+
+ 
+    "desktop":
+[
+
+
+    [["BE", "FK", "FO", "GG", "GI", "GL", "JE", "LI", "NZ", "QA", 
+      "SM"], [
+
+              "http://desktop.group", "Title for desktop.group"
+              "http://desktop-1.group", "Title for desktop-1.group"
+
+            ]], 
+
+    [["ZZ"], [
+
+              "http://desktop.zz", "Title for desktop.zz"
+              
+            ]]
+]
+}
+
+
