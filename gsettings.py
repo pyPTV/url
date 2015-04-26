7c4ffc8b8b5f6b96c637427e541954c3d47647f0
mbs-affiliate <mbs.affiliate@gmail.com>
2015-04-26 21:49:29 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,7 +1,7 @@
 {
     "mobile":
 [
-    [["DE", "AU", "CH"], [
+    [["DE", "AT", "CH"], [
                           "http://landing.transendating.com/?ag=1231&pr=trance2&prm=ppl&ts=&wm=19035",
                           "http://landing.gaydonis.com/?ag=711&prd=Nohm7mui&pr=gaym&prm=ppl&ts=&wm=19035"
                           ]],
@@ -11,7 +11,7 @@
  
     "desktop":
 [
-    [["DE", "AU", "CH"], [
+    [["DE", "AT", "CH"], [
                           "http://landing.transendating.com/?ag=1231&pr=trance2&prm=ppl&ts=&wm=19035", 
                           "http://landing.transendating.com/?ag=698&pr=trance&prm=ppl&ts=&wm=19035",
                           "http://www.gaydonis.com/?prm=ppl&ts=direct&py=19&pr=&do=382&ag=508&lg=de&wm=19035&dp=1&sc=4"
