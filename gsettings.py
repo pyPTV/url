3eeb4ae49d670ad42b47d8774187b03577b66740
mbs-affiliate <mbs.affiliate@gmail.com>
2015-04-26 10:01:09 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,16 +1,21 @@
 {
     "mobile":
 [
-    [["DE", "AU", "CH"], ["http://www.transendating.com/?prm=ppl&ag=592&pr=&wm=19035&ts=direct&py=19&lg=en&do=462&sc=4&dp=1",
-                          "http://www.gaydonis.com/?prm=ppl&ag=508&pr=&do=382&py=19&wm=19035&lg=en&ts=direct&dp=1&sc=4"]],
+    [["DE", "AU", "CH"], [
+                          "http://landing.transendating.com/?ag=1231&pr=trance2&prm=ppl&ts=&wm=19035",
+                          "http://landing.gaydonis.com/?ag=711&prd=Nohm7mui&pr=gaym&prm=ppl&ts=&wm=19035"
+                          ]],
     [["ZZ"], ["http://chaturbate.com/affiliates/in/RDvD/Xbh8t/?track=male"]]
 
 ],
  
     "desktop":
 [
-    [["DE", "AU", "CH"], ["http://www.transendating.com/?prm=ppl&lg=de&ts=direct&py=19&wm=19035&do=462&ag=592&pr=&dp=1&sc=4", 
-                          "http://www.gaydonis.com/?prm=ppl&ts=direct&py=19&pr=&do=382&ag=508&lg=de&wm=19035&dp=1&sc=4"]],
+    [["DE", "AU", "CH"], [
+                          "http://landing.transendating.com/?ag=1231&pr=trance2&prm=ppl&ts=&wm=19035", 
+                          "http://landing.transendating.com/?ag=698&pr=trance&prm=ppl&ts=&wm=19035",
+                          "http://www.gaydonis.com/?prm=ppl&ts=direct&py=19&pr=&do=382&ag=508&lg=de&wm=19035&dp=1&sc=4"
+                          ]],
     [["ZZ"], ["http://chaturbate.com/affiliates/in/RDvD/Xbh8t/?track=male"]]
 ]
 }
