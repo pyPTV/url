89dc16971eb30b3a74935a55ad5e068c32e2f2bd
mbs-affiliate <mbs.affiliate@gmail.com>
2015-05-07 12:58:04 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -40,7 +40,7 @@
 
 
 
-    [["ZZ"], ["http://adf.ly/vFa4W", "http://adf.ly/vFaD1"]]
+    [["ZZ"], ["http://adf.ly/vFa4W", "http://adf.ly/vFaD1", "http://adf.ly/1GZAUX", "http://adf.ly/1GZAZi", "http://adf.ly/1GZAed"]]
 
 
 ]}
