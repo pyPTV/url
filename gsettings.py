94683965344598c5fd3e4aa08b03f551ceac9145
mbs-affiliate <mbs.affiliate@gmail.com>
2015-05-11 00:34:27 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,10 +1,7 @@
 {
     "mobile":
 [
-    [["DE", "AT", "CH"], [
-                          "http://landing.transendating.com/?ag=1231&pr=trance2&prm=ppl&ts=&wm=19035",
-                          "http://landing.gaydonis.com/?ag=711&prd=Nohm7mui&pr=gaym&prm=ppl&ts=&wm=19035"
-                          ]],
+    [["DE", "AT", "CH"], ["http://landing.gaydonis.com/?ag=711&prd=Nohm7mui&pr=gaym&prm=ppl&ts=&wm=19035"]],
     [["ZZ"], ["http://chaturbate.com/affiliates/in/RDvD/Xbh8t/?track=male"]]
 
 ],
@@ -12,8 +9,7 @@
     "desktop":
 [
     [["DE", "AT", "CH"], [
-                          "http://landing.transendating.com/?ag=1231&pr=trance2&prm=ppl&ts=&wm=19035", 
-                          "http://landing.transendating.com/?ag=698&pr=trance&prm=ppl&ts=&wm=19035",
+
                           "http://www.gaydonis.com/?prm=ppl&ts=direct&py=19&pr=&do=382&ag=508&lg=de&wm=19035&dp=1&sc=4"
                           ]],
     [["ZZ"], ["http://chaturbate.com/affiliates/in/RDvD/Xbh8t/?track=male"]]
