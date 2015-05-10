87ffda11e47bc465cc4e22029474825df153d22f
mbs-affiliate <mbs.affiliate@gmail.com>
2015-05-10 20:29:16 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,40 +13,31 @@
 [
 
 
-    [["US", "AU", "CA", "GB", "NZ"], 
-     ["http://chaturbate.com/affiliates/in/ZmU7/Xbh8t/?track=world", "http://www.camplacecash.com/delivery/?w=1385&b=&c=prem"]],
+    [["US", "AU", "CA", "GB", "NZ"], [
+      "http://chaturbate.com/affiliates/in/ZmU7/Xbh8t/?track=world", 
+      "http://www.camplacecash.com/delivery/?w=1385&b=&c=prem"]],
 
 
 
-    [["FK", "FO", "GG", "GI", "GL", "JE", "LI", "LU", "QA", 
-      "SG", "SM", "AW", "BN", "BS", "KW", "MO", "PR"], 
-     ["http://chaturbate.com/affiliates/in/ZmU7/Xbh8t/?track=world"]],
+    [["FK", "FO", "GG", "GI", "GL", "JE", "LI", "LU", "QA", "SG", "SM", "AW", "BN", "BS", "KW", "MO", "PR"], [
+      "http://chaturbate.com/affiliates/in/ZmU7/Xbh8t/?track=world"]],
 
 
+    [["DE", "AT", "CH"], [
+      "http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035", 
+      "http://landing.parkplatzkartei.com/?ag=771&pr=&prm=ppl&ts=xxx&wm=19035&tsd=atcrm57"]],
 
 
-
-    [["DE", "AT", "CH"], 
-     ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035", "http://landing.parkplatzkartei.com/?ag=771&pr=&prm=ppl&ts=xxx&wm=19035&tsd=atcrm57"]],
-
-
-    [["FR"], ["http://www.camplacecash.com/delivery/show/p12/?w=1385&b=204685&l=fr&c=fr&muted=1"]],
-    [["SE"], ["http://www.camplacecash.com/delivery/show/p12/?w=1385&b=204685&l=se&c=se&muted=1"]],
-    [["DK", "FI", "NO", "IE", "NL", "EE", "CZ", "SK", "BE", "IS"], ["http://www.camplacecash.com/delivery/?w=1385&b=&c=eu"]], 
+    [["FR", "SE", "DK", "FI", "NO", "IE", "NL", "EE", "CZ", "SK", "BE", "IS"], [
+      "http://www.camplacecash.com/delivery/?w=1385&b=&c=eu"]], 
 
 
-
-    [["ES", "LV", "GR", "SI", "JP", "IT", "HR", "RS", "IL", "PT", "AE", 
-      "MK", "HU", "BG", "RO", "KR", "BR", "ZA", "CY", "PL", "LT", "TR", "BH", "JO"],
-     ["http://bongacams.com/track?c=20944"]],
+    [["ES", "LV", "GR", "SI", "JP", "IT", "HR", "RS", "IL", "PT", "AE", "MK", "HU", "BG", "RO", "KR", "BR", "ZA", "CY", "PL", "LT", "TR", "BH", "JO"], [
+      "http://bongacams.com/track?c=20944"]],
 
 
-
-
-    [["RU", "UA", "KZ", "AZ", "GE", "AM", "BY", "MD", "KG", "UZ", "TM"], 
-     ["http://runetki.com/?id=7423&cmp=&lng=ru&tp=5"]],
-
-
+    [["RU", "UA", "KZ", "AZ", "GE", "AM", "BY", "MD", "KG", "UZ", "TM"], [
+      "http://runetki.com/?id=7423&cmp=&lng=ru&tp=5"]],
 
 
     [["ZZ"], ["http://adf.ly/1Gid8n"]]
