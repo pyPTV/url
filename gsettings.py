2501ba8de44f39f0dfa7d67a23ff8824d5602db7
mbs-affiliate <mbs.affiliate@gmail.com>
2015-06-01 21:34:40 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -2,7 +2,7 @@
     "mobile":
 [
     [["DE", "AT", "CH"], ["http://landing.gaydonis.com/?ag=711&prd=Nohm7mui&pr=gaym&prm=ppl&ts=&wm=19035"]],
-    [["ZZ"], ["http://chaturbate.com/affiliates/in/RDvD/Xbh8t/?track=male"]]
+    [["ZZ"], ["http://chaturbate.com/affiliates/in/R2Xc/Xbh8t/?track=male"]]
 
 ],
  
@@ -12,7 +12,7 @@
 
                           "http://www.gaydonis.com/?prm=ppl&ts=direct&py=19&pr=&do=382&ag=508&lg=de&wm=19035&dp=1&sc=4"
                           ]],
-    [["ZZ"], ["http://chaturbate.com/affiliates/in/RDvD/Xbh8t/?track=male"]]
+    [["ZZ"], ["http://chaturbate.com/affiliates/in/R2Xc/Xbh8t/?track=male"]]
 ]
 }
 
