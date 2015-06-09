2f57be799b089e8666eab46f0d4879297e487f5f
mbs-affiliate <mbs.affiliate@gmail.com>
2015-06-09 13:34:43 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 {
     "mobile":
 [
-    [["US"], ["http://rd.mobiteasy.com/mr/?id=alex1306"]],
+    [["US"], ["http://rd.mobiteasy.com/mr/?id=alex1306&tr=xxx"]],
     [["ZZ"], ["http://mobile.bitterstrawberry.org/?id=170"]]
 
 ],
