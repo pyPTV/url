04bd74e2fa1ac493e565a258d053395c8cf5542e
mbs-affiliate <mbs.affiliate@gmail.com>
2015-06-12 13:03:58 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,44 +13,29 @@
 [
 
 
-    [["US", "AU", "CA", "GB", "NZ"], [
-      "http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=premium"]],
-
-
+    [["US"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=us"]],
+    [["AU"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=au"]],
+    [["CA"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=ca"]],
+    [["GB"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=gb"]],
+    [["NZ"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=nz"]],  
+    [["FR"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=fr"]],
+    [["BE"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=be"]],
+    [["IT"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=it"]],
+    [["NL"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=nl"]],
+    [["ES"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=es"]],
+    [["AT"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=at"]],
+    [["CH"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=ch"]],
+    [["NO"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=no"]],
+    [["SE"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=se"]],
+    [["DK"], ["http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=dk"]],
 
     [["FK", "FO", "GG", "GI", "GL", "JE", "LI", "QA", "SM", "AW", "BN", "BS", 
-      "KW", "MO", "PR"], [
+      "KW", "MO", "PR", "IE", "LU", "IS", "FI"], [
       "http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=minor"]],
 
 
-    [["DE"], [
-      "http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
+    [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
 
-
-    [["NO", "SE", "FI", "DK", "IE", "LU", "IS"], [
-      "http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=neurope"]],
-
-
-    [["AT", "CH"], [
-      "http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=atch"]],
-
-
-    [["FR", "BE"], [
-      "http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=frbe"]],
-
-
-    [["IT"], [
-      "http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=it"]],
-
-
-    [["NL"], [
-      "http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=nl"]],
-
-
-    [["ES"], [
-      "http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=es"]],
-
-      
     [["BR", "IN", "MX", "CO", "NG", "AR", "NA", "VE", 
       "GR", "PA", "MK"], [
       "http://ertya.com/go/caRG9_bBBLR/DEFAULT"]], 
@@ -61,17 +46,9 @@
       "http://bongacams.com/track?c=20943"]],
 
 
-    [["RU", "UA", "KZ", "AZ", "GE", "AM", "BY", "MD", "KG", "UZ", "TM"], [
-      "http://runetki.com/?id=7423&cmp=&lng=ru&tp=5"]],
-
-    [["JP", "TW"], [
-      "http://click.dtiserv2.com/Direct/9354999-354-us9126/jp/japanese-webcam-girls.shtml"]],
-
-
-    [["CN", "HK"], [
-     "http://click.dtiserv2.com/Direct/9354999-354-us9126/cn/japanese-webcam-girls.shtml"]],
-
-
+    [["RU", "UA", "KZ", "AZ", "GE", "AM", "BY", "MD", "KG", "UZ", "TM"], ["http://runetki.com/?id=7423&cmp=&lng=ru&tp=5"]],
+    [["JP", "TW"], ["http://click.dtiserv2.com/Direct/9354999-354-us9126/jp/japanese-webcam-girls.shtml"]],
+    [["CN", "HK"], ["http://click.dtiserv2.com/Direct/9354999-354-us9126/cn/japanese-webcam-girls.shtml"]],
     [["ZZ"], ["http://adf.ly/vFa4W", "http://adf.ly/vFaD1", "http://adf.ly/1GZAUX", "http://adf.ly/1GZAZi", "http://adf.ly/1GZAed"]]
 
 
