a720cf56ee602fbe267221e3487905c7abb56a40
mbs-affiliate <mbs.affiliate@gmail.com>
2015-06-18 09:53:15 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -32,6 +32,8 @@
       "KW", "MO", "PR", "IE", "LU", "IS", "FI"], [
       "http://chaturbate.com/affiliates/in/hr8m/Xbh8t/?track=minor"]],
 
+    [["JP", "TW"], ["http://adf.ly/1Ix9A3"]],
+    [["CN", "HK"], ["http://adf.ly/1Ix9EA"]],
 
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035",
               "http://landing.amateurseite.com/?ag=768&fsk=18&pr=tubelp&prm=rev&ts=&wm=19035",
@@ -42,12 +44,12 @@
 
     [["EE", "SK", "LV", "SI", "HR", "RS", "IL", "PT", "AE", "HU", "BG", "RO", "MT", "ME",
       "KR", "ZA", "CY", "PL", "LT", "TR", "BH", "JO", "CZ", "SG", "UA", "GR", "MK", "AD"], [
-      "http://adf.ly/1Ix8mZ"]],
+      "http://adf.ly/vFa4W", "http://adf.ly/vFaD1", "http://adf.ly/1GZAUX", "http://adf.ly/1GZAZi", "http://adf.ly/1GZAed"]],
 
 
     [["RU", "KZ", "AZ", "GE", "AM", "BY", "MD", "KG", "UZ", "TM"], ["http://adf.ly/lvii7"]],
-    [["JP", "TW"], ["http://adf.ly/1Ix9A3"]],
-    [["CN", "HK"], ["http://adf.ly/1Ix9EA"]],
+
+
     [["ZZ"], ["http://adf.ly/vFa4W", "http://adf.ly/vFaD1", "http://adf.ly/1GZAUX", "http://adf.ly/1GZAZi", "http://adf.ly/1GZAed"]]
 
 
