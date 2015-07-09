16044d246923ca367c6fb91c509b35499052a444
mbs-affiliate <mbs.affiliate@gmail.com>
2015-07-09 13:27:31 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,17 +14,11 @@
 
 
     [["US", "AU", "GB"], ["http://adf.ly/1Ix9A3"]],
-
-
-
-    [["JP", "TW"], ["http://adf.ly/1Ix9A3"]],
-    [["CN", "HK"], ["http://adf.ly/1Ix9EA"]],
-    [["RU", "KZ"], ["http://adf.ly/lvii7"]],
-
-    [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-
-
-    [["ZZ"], ["http://adf.ly/vFa4W", "http://adf.ly/vFaD1", "http://adf.ly/1GZAUX", "http://adf.ly/1GZAZi", "http://adf.ly/1GZAed"]]
+    [["JP", "TW"],       ["http://adf.ly/1Ix9A3"]],
+    [["CN", "HK"],       ["http://adf.ly/1Ix9EA"]],
+    [["RU", "KZ"],       ["http://adf.ly/lvii7"]],
+    [["DE"],       ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
+    [["ZZ"],       ["http://adf.ly/vFa4W", "http://adf.ly/vFaD1", "http://adf.ly/1GZAUX", "http://adf.ly/1GZAZi", "http://adf.ly/1GZAed"]]
 
 
 ]}
