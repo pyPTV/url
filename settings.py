4f0a3e5503ba50b4f3a6961bc7971796a87d5aed
mbs-affiliate <mbs.affiliate@gmail.com>
2015-07-11 16:05:57 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@
      ["http://imlive.com/wmaster.ashx?WID=125977836270&LinkID=1220&queryid=1&perlead=1220&promocode=BCODE_00000"]],
 
     [["GB", "IE", "BE", "DK", "FI", "FR", "GI", "IS", "IT", "MC", "NL", "NO", "PT", "ES", "SE", "CH", "AT", "LU", "HK", "SM"], 
-     ["http://adf.ly/1Kao5M", "http://adf.ly/1KaoEF"]],
+     ["http://promotools.biz/cp/text_link/index.php?ur=aHR0cDovL3d3dy54Y2Ftcy5jb20%2FY29tZnJvbT04ODA5MjUmY2YyPWhwYWdlJmNmc2EyPSZjZjA9cHBz&promo=xcams&subid1=hpage&subid2=&brand=xcams"]],
 
     [["RU", "KZ"],       ["http://adf.ly/lvii7"]],
 
