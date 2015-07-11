a17b7e3d61d59215abeddf353ab4a21ccb517e9c
mbs-affiliate <mbs.affiliate@gmail.com>
2015-07-11 11:32:32 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,10 +13,15 @@
 [
 
 
-    [["US"], ["http://imlive.com/wmaster.ashx?WID=125977836270&LinkID=1220&queryid=1&perlead=1220&promocode=BCODE_00000"]],
+    [["US"], 
+     ["http://imlive.com/wmaster.ashx?WID=125977836270&LinkID=1220&queryid=1&perlead=1220&promocode=BCODE_00000"]],
+
+    [["GB", "IE", "BE", "DK", "FI", "FR", "GI", "IS", "IT", "MC", "NL", "NO", "PT", "ES", "SE", "CH", "AT", "LU", "HK", "SM"], 
+     ["http://adf.ly/1Kao5M", "http://adf.ly/1KaoEF"]],
+
     [["RU", "KZ"],       ["http://adf.ly/lvii7"]],
-    [["DE"],       ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-    [["ZZ"],       ["http://adf.ly/vFa4W", "http://adf.ly/vFaD1", "http://adf.ly/1GZAUX", "http://adf.ly/1GZAZi", "http://adf.ly/1GZAed"]]
+    [["DE"],             ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
+    [["ZZ"],             ["http://adf.ly/vFa4W", "http://adf.ly/vFaD1", "http://adf.ly/1GZAUX", "http://adf.ly/1GZAZi", "http://adf.ly/1GZAed"]]
 
 
 ]}
