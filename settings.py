da481fe885cf6124472d76455d116c7f1a6dae6d
mbs-affiliate <mbs.affiliate@gmail.com>
2015-07-11 11:39:01 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -21,7 +21,7 @@
 
     [["RU", "KZ"],       ["http://adf.ly/lvii7"]],
     [["DE"],             ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-    [["ZZ"],             ["http://adf.ly/vFa4W", "http://adf.ly/vFaD1", "http://adf.ly/1GZAUX", "http://adf.ly/1GZAZi", "http://adf.ly/1GZAed"]]
+    [["ZZ"],             ["http://adf.ly/1Kapei"]]
 
 
 ]}
