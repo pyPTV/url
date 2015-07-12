0da4b286e73fc0881076e613d68c04418fc3e264
mbs-affiliate <mbs.affiliate@gmail.com>
2015-07-12 13:22:55 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,7 +14,8 @@
 
 
     [["US", "CA", "NZ", "AU"], 
-     ["http://imlive.com/wmaster.ashx?WID=125977836270&LinkID=1220&queryid=1&perlead=1220&promocode=BCODE_00000"]],
+     ["http://imlive.com/wmaster.ashx?WID=125977836270&LinkID=1220&queryid=1&perlead=1220&promocode=BCODE_00000",
+      "http://www.seekingmilf.com?ainfo=NDA0MTh8MTM1OXww&skin=203&i=1"]],
 
     [["GB", "IE", "BE", "DK", "FI", "FR", "GI", "IS", "IT", "MC", "NL", "NO", "PT", "ES", "SE", "CH", "AT", "LU", "HK", "SM"], 
      ["http://promotools.biz/cp/text_link/index.php?ur=aHR0cDovL3d3dy54Y2Ftcy5jb20%2FY29tZnJvbT04ODA5MjUmY2YyPWhwYWdlJmNmc2EyPSZjZjA9cHBz&promo=xcams&subid1=hpage&subid2=&brand=xcams"]],
