24ecf7f73246d874c31b677f0440c809ea206bfa
mbs-affiliate <mbs.affiliate@gmail.com>
2015-07-18 09:32:41 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,11 +13,11 @@
 [
 
 
-    [["US", "CA", "NZ", "AU"], 
-     ["http://imlive.com/wmaster.ashx?WID=125977836270&LinkID=1220&queryid=1&perlead=1220&promocode=BCODE_00000",
+    [["US"], 
+     ["http://www.localmilfselfies.com/?page=x1&wm_login=alex1306&ps=p",
       "http://www.seekingmilf.com?ainfo=NDA0MTh8MTM1OXww&skin=203&i=1"]],
 
-    [["GB", "IE", "BE", "DK", "FI", "FR", "GI", "IS", "IT", "MC", "NL", "NO", "PT", "ES", "SE", "CH", "AT", "LU", "HK", "SM"], 
+    [["CA", "NZ", "AU", "GB", "IE", "BE", "DK", "FI", "FR", "GI", "IS", "IT", "MC", "NL", "NO", "PT", "ES", "SE", "CH", "AT", "LU", "HK", "SM"], 
      ["http://frtya.com/go/caRG9_bBBLR/DEFAULT"]],
 
     [["RU", "KZ"],       ["http://adf.ly/lvii7"]],
