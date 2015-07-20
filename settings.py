64e1af3f0ea1ddbfc5c689ee604870ab98484120
mbs-affiliate <mbs.affiliate@gmail.com>
2015-07-21 00:00:10 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,21 +13,18 @@
 [
 
 
-    [["US", "CA", "AU"], 
-     ["http://www.localmilfselfies.com/?page=x1&wm_login=alex1306&ps=p",
-      "http://www.seekingmilf.com?ainfo=NDA0MTh8MTM1OXww&skin=203&i=1"]],
+    [["US", "CA", "AU"], ["http://www.localmilfselfies.com/?page=x1&wm_login=alex1306&ps=p", "http://www.seekingmilf.com?ainfo=NDA0MTh8MTM1OXww&skin=203&i=1"]],
 
-    [["NZ", "GB", "IE", "BE", "DK", "FI", "FR", "GI", "IS", "IT", "MC", "NL", "NO", "PT", "ES", "SE", "CH", "AT", "LU", "HK", "SM"], 
-     ["http://frtya.com/go/caRG9_bBBLR/DEFAULT"]],
+    [["GB", "IE", "NZ", "NL", "DK", "SE", "NO", "IS", "CH", "BE"], ["http://frtya.com/go/fxyag_bBBLR/DEFAULT", "http://frtya.com/go/caRG9_bBBLR/DEFAULT", "http://frtya.com/go/wHl_bBBLR/DEFAULT"]],
 
-    [["RU", "KZ"],       ["http://adf.ly/lvii7"]],
+    [["FI", "IT", "PT", "ES", "AT", "HK", "SM", "GI"], ["http://frtya.com/go/fxyag_bBBLR/DEFAULT", "http://frtya.com/go/caRG9_bBBLR/DEFAULT"]],
 
+    [[AD, "FR", "LU", "MC", "RE"], ["http://frtya.com/go/lW8l_bBBLR/DEFAULT", "http://frtya.com/go/fxyag_bBBLR/DEFAULT", "http://frtya.com/go/wHl_bBBLR/DEFAULT", "http://frtya.com/go/hsL_bBBLR/DEFAULT"]],
 
-    [["JP", "TW"],       
-     ["http://click.dtiserv2.com/Direct/9173999-173-us9126/home/index.html", 
-      "http://click.dtiserv2.com/Direct/9354999-354-us9126/jp/japanese-webcam-girls.shtml"]],
+    [["RU", "KZ"],  ["http://adf.ly/lvii7"]],
 
 
+    [["JP", "TW"], ["http://click.dtiserv2.com/Direct/9173999-173-us9126/home/index.html", "http://click.dtiserv2.com/Direct/9354999-354-us9126/jp/japanese-webcam-girls.shtml"]],
     [["CN", "PH"],       ["http://click.dtiserv2.com/Direct/9354999-354-us9126/cn/japanese-webcam-girls.shtml"]],
     [["DE"],             ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
     [["ZZ"],             ["http://adf.ly/1Kapei"]]
