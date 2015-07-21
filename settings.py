194691d4c28b52085e4a6fb152aa970f7528acc8
mbs-affiliate <mbs.affiliate@gmail.com>
2015-07-21 05:18:17 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -15,7 +15,8 @@
 
     [["US", "CA", "AU"], [
                          "http://wcjoin.com/osuog", "http://wcjoin.com/ysuob",
-                          "http://wcjoin.com/jtuoz", "http://wcjoin.com/euuoh",
+                         "http://wcjoin.com/jtuoz", "http://wcjoin.com/euuoh",
+                         "http://livechat.yourcamsite.com/livechat/?reseller=alex1306&rev=0&type=2&misc1=livechat&misc2=&theme_id=1",
                          "http://www.seekingmilf.com?ainfo=NDA0MTh8MTM1OXww&skin=203&i=1"]],
 
     [["GB", "IE", "NZ", "NL", "DK", "SE", "NO", "IS", "CH", "BE"], [
