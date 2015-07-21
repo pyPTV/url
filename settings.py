bc9021d34f05a3793a2462b369494b113d219f51
mbs-affiliate <mbs.affiliate@gmail.com>
2015-07-21 04:25:27 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@
 
 
     [["US", "CA", "AU"], [
-                         "http://www.localmilfselfies.com/?page=x1&wm_login=alex1306&ps=p", 
+                         "http://wcjoin.com/osuog", 
                          "http://www.seekingmilf.com?ainfo=NDA0MTh8MTM1OXww&skin=203&i=1"]],
 
     [["GB", "IE", "NZ", "NL", "DK", "SE", "NO", "IS", "CH", "BE"], [
