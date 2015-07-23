85fdf47af76122028733301f3612d7634c921329
mbs-affiliate <mbs.affiliate@gmail.com>
2015-07-23 16:55:34 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -18,25 +18,23 @@
                          "http://www.seekingmilf.com?ainfo=NDA0MTh8MTM1OXww&skin=203&i=1"]],
 
     [["GB", "IE", "NZ", "NL", "DK", "SE", "NO", "IS", "CH", "BE"], [
-                         "http://frtya.com/go/fxyag_bBBLR/DEFAULT", 
-                         "http://frtya.com/go/caRG9_bBBLR/DEFAULT", 
-                         "http://frtya.com/go/wHl_bBBLR/DEFAULT"]],
+                         "http://frtya.com/go/caRG9_bBBLR/sexy", 
+                         "http://frtya.com/go/wHl_bBBLR/exposed"]],
 
-    [["FI", "IT", "PT", "AT", "SG", "TW", "HK", "SM", "GI"], [
-                         "http://frtya.com/go/fxyag_bBBLR/DEFAULT", 
-                         "http://frtya.com/go/caRG9_bBBLR/DEFAULT"]],
+    [["FI", "PT", "AT", "SG", "TW", "HK", "SM", "GI", "LU"], [
+                         "http://frtya.com/go/caRG9_bBBLR/sexy"]],
 
-    [["AD", "FR", "LU", "MC", "RE"], [
-                         "http://frtya.com/go/fxyag_bBBLR/DEFAULT",
-                         "http://frtya.com/go/lW8l_bBBLR/DEFAULT",                           
-                         "http://frtya.com/go/wHl_bBBLR/DEFAULT", 
-                         "http://frtya.com/go/hsL_bBBLR/DEFAULT"]],
+    [["FR"], [
+                         "http://frtya.com/go/lW8l_bBBLR/gratuite",                           
+                         "http://frtya.com/go/wHl_bBBLR/exposed", 
+                         "http://frtya.com/go/hsL_bBBLR/dusexe",
+                         "http://bongacams.com/track?c=20944"]],
 
     [["DE"],             ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
     [["RU", "KZ"],       ["http://adf.ly/lvii7"]],    
-    [["JP"],             ["http://frtya.com/go/bVrGI_bBBLR/DEFAULT"]],
+    [["JP"],             ["http://frtya.com/go/bVrGI_bBBLR/jpcams"]],
     [["CN", "PH"],       ["http://click.dtiserv2.com/Direct/9354999-354-us9126/cn/japanese-webcam-girls.shtml"]],    
-    [["ZZ"],             ["http://adf.ly/1Kapei"]]
+    [["ZZ"],             ["http://bongacams.com/track?c=20944"]]
 
 
 
