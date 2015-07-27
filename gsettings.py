5e0a61a83269162d51a366eed596a575ee26d7c1
mbs-affiliate <mbs.affiliate@gmail.com>
2015-07-27 21:55:52 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,8 +4,7 @@
     
     [["ZZ"], ["http://join.bukkakeboysmobile.com/track/MTAwNTI4LjIxNS42OC42OC4wLjAuMC4wLjA",
               "http://www.buddylead.com/fistingcentral/go.php?pr=9&su=2&si=52&ad=250878&pa=index&ar=&buffer=",
-              "http://www.buddylead.com/nextdoorebony/go.php?pr=9&su=2&si=51&ad=250878&pa=index&ar=&buffer=",
-              "http://www.buddylead.com/scaryfuckers/go.php?pr=9&su=2&si=43&ad=250878&pa=index&ar=&buffer="]]
+              "http://www.buddylead.com/nextdoorebony/go.php?pr=9&su=2&si=51&ad=250878&pa=index&ar=&buffer="]]
 
 ],
  
@@ -14,8 +13,7 @@
 
     [["ZZ"], ["http://join.bukkakeboys.com/track/MTAwNTI4LjI2OC42Mi4zMDguMC4wLjAuMC4w",
               "http://www.buddylead.com/fistingcentral/go.php?pr=9&su=2&si=52&ad=250878&pa=index&ar=&buffer=",
-              "http://www.buddylead.com/nextdoorebony/go.php?pr=9&su=2&si=51&ad=250878&pa=index&ar=&buffer=",
-              "http://www.buddylead.com/scaryfuckers/go.php?pr=9&su=2&si=43&ad=250878&pa=index&ar=&buffer="]]
+              "http://www.buddylead.com/nextdoorebony/go.php?pr=9&su=2&si=51&ad=250878&pa=index&ar=&buffer="]]
 
 
 
