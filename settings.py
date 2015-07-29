30480f4e0489b2be738efae2bda86b115c192a66
mbs-affiliate <mbs.affiliate@gmail.com>
2015-07-29 17:33:04 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -16,19 +16,18 @@
 
 
     [["US", "CA", "AU"], ["http://www.seekingmilf.com?ainfo=NDA0MTh8MTM1OXww&skin=203&i=1", "http://go.milfder.com/SHM"]],
-    [["GB", "IE", "NZ", "DK", "SE", "NO", "IS", "CH"], ["http://frtya.com/go/caRG9_bBBLR/sexy", "http://frtya.com/go/wHl_bBBLR/exposed"]],
-    [["FI", "PT", "AT", "SG", "TW", "HK", "SM", "GI", "LU"], ["http://frtya.com/go/caRG9_bBBLR/sexy"]],
-    [["NL", "BE", "IT", "ES"], ["http://promotools.biz/cp/text_link/index.php?ur=aHR0cDovL3d3dy54Y2Ftcy5jb20%2FY29tZnJvbT04ODA5MjUmY2YyPSZjZnNhMj0mY2YwPXBjMg%3D%3D&promo=xcams&subid1=&subid2=&brand=xcams"]],
-    
+    [["GB", "IE", "NZ", "DK", "SE", "NO", "IS", "CH"], ["http://frtya.com/go/caRG9_bBBLR/sexy"]],
+    [["LU", "PA", "AT", "TW", "SG"], ["http://frtya.com/go/caRG9_bBBLR/sexy"]],
+    [["NL", "BE", "IT", "ES", "FI", "PT"], ["http://promotools.biz/cp/text_link/index.php?ur=aHR0cDovL3d3dy54Y2Ftcy5jb20%2FY29tZnJvbT04ODA5MjUmY2YyPSZjZnNhMj0mY2YwPXBjMg%3D%3D&promo=xcams&subid1=&subid2=&brand=xcams"]],
+    [["JP"], ["http://frtya.com/go/bVrGI_bBBLR/jpcams"]],
     [["FR"], [
                          "http://frtya.com/go/lW8l_bBBLR/gratuite",
                          "http://frtya.com/go/hsL_bBBLR/dusexe",
                          "http://promotools.biz/cp/text_link/index.php?ur=aHR0cDovL3d3dy54Y2Ftcy5jb20%2FY29tZnJvbT04ODA5MjUmY2YyPSZjZnNhMj0mY2YwPXBjMg%3D%3D&promo=xcams&subid1=&subid2=&brand=xcams"]],
 
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-    [["RU", "KZ", "BY", "EE", "LV", "LT"], ["http://runetki.com/?id=7423&cmp=&lng=ru&tp=5"]],    
-    [["JP"], ["http://frtya.com/go/bVrGI_bBBLR/jpcams"]],
-    [["ZZ"], ["http://click.dtiserv2.com/Direct/9354999-354-us9126/cn/japanese-webcam-girls.shtml"]]
+    [["RU", "KZ", "BY", "EE", "LV", "LT"], ["http://runetki.com/?id=7423&cmp=&lng=ru&tp=5"]],   
+    [["ZZ"], ["http://www.derangeddollars.com/hit.php?s=1&p=1&a=100161&t=0&c="]]
 
 
 
