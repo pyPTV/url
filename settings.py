548c132fa5e9f51e15d1c4dcc39fab83bc754624
mbs-affiliate <mbs.affiliate@gmail.com>
2015-07-29 15:10:28 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -16,22 +16,8 @@
 
 
     [["US", "CA", "AU"], ["http://www.seekingmilf.com?ainfo=NDA0MTh8MTM1OXww&skin=203&i=1"]],
-
-    [["GB", "IE", "NZ", "DK", "SE", "NO", "IS", "CH"], [
-                         "http://frtya.com/go/caRG9_bBBLR/sexy", 
-                         "http://frtya.com/go/wHl_bBBLR/exposed"]],
-
-    [["FI", "PT", "AT", "SG", "TW", "HK", "SM", "GI", "LU"], [
-                         "http://frtya.com/go/caRG9_bBBLR/sexy"]],
-
-    [["CZ", 
-      "SK", "SI", 
-      "GR", 
-      "IL", 
-      "HR", "AE", "RO", "TR", "BG", "PL", "HU", "MK", "CY", 
-      "KR", "ZA", "UA", "BH", "JO", "RS"], [
-                         "http://frtya.com/go/caRG9_bBBLR/sexy"]],
-
+    [["GB", "IE", "NZ", "DK", "SE", "NO", "IS", "CH"], ["http://frtya.com/go/caRG9_bBBLR/sexy", "http://frtya.com/go/wHl_bBBLR/exposed"]],
+    [["FI", "PT", "AT", "SG", "TW", "HK", "SM", "GI", "LU"], ["http://frtya.com/go/caRG9_bBBLR/sexy"]],
     [["NL", "BE", "IT", "ES"], ["http://promotools.biz/cp/text_link/index.php?ur=aHR0cDovL3d3dy54Y2Ftcy5jb20%2FY29tZnJvbT04ODA5MjUmY2YyPSZjZnNhMj0mY2YwPXBjMg%3D%3D&promo=xcams&subid1=&subid2=&brand=xcams"]],
     
     [["FR"], [
@@ -40,11 +26,10 @@
                          "http://frtya.com/go/hsL_bBBLR/dusexe",
                          "http://frtya.com/go/bQlyF_bBBLR/sondage"]],
 
-    [["DE"],             ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-    [["RU", "KZ", "BY", 
-      "EE", "LV", "LT"], ["http://runetki.com/?id=7423&cmp=&lng=ru&tp=5"]],    
-    [["JP"],             ["http://frtya.com/go/bVrGI_bBBLR/jpcams"]],
-    [["ZZ"],             ["http://click.dtiserv2.com/Direct/9354999-354-us9126/cn/japanese-webcam-girls.shtml"]]
+    [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
+    [["RU", "KZ", "BY", "EE", "LV", "LT"], ["http://runetki.com/?id=7423&cmp=&lng=ru&tp=5"]],    
+    [["JP"], ["http://frtya.com/go/bVrGI_bBBLR/jpcams"]],
+    [["ZZ"], ["http://click.dtiserv2.com/Direct/9354999-354-us9126/cn/japanese-webcam-girls.shtml"]]
 
 
 
