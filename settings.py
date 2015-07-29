e02a068dae5d7f860da26599e9c636c93b780de2
mbs-affiliate <mbs.affiliate@gmail.com>
2015-07-30 00:18:01 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,7 +14,11 @@
 
 
 
-    [["US", "CA", "AU"], ["http://www.seekingmilf.com?ainfo=NDA0MTh8MTM1OXww&skin=203&i=1"]],
+    [["US", "CA", "AU"], ["http://www.seekingmilf.com?ainfo=NDA0MTh8MTM1OXww&skin=203&i=1",
+                          "http://www.120mg.com/catalog/Bestsellers.htm?refid=898",
+                          "http://www.world-pharmacy-24h.com/catalog/Bestsellers.htm?refid=898",
+                          "http://www.first-meds.net/catalog/Bestsellers.htm?refid=898",
+                          "http://www.online-drugs-store.com/catalog/Bestsellers.htm?refid=898"]],
     [["GB", "IE", "NZ", "DK", "SE", "NO", "IS", "CH"], ["http://frtya.com/go/caRG9_bBBLR/sexy"]],
     [["LU", "PA", "AT", "TW", "SG"], ["http://frtya.com/go/caRG9_bBBLR/sexy"]],
     [["NL", "BE", "IT", "ES", "FI", "PT"], ["http://promotools.biz/cp/text_link/index.php?ur=aHR0cDovL3d3dy54Y2Ftcy5jb20%2FY29tZnJvbT04ODA5MjUmY2YyPSZjZnNhMj0mY2YwPXBjMg%3D%3D&promo=xcams&subid1=&subid2=&brand=xcams"]],
@@ -25,7 +29,11 @@
 
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
     [["RU", "KZ", "BY", "EE", "LV", "LT"], ["http://runetki.com/?id=7423&cmp=&lng=ru&tp=5"]],   
-    [["ZZ"], ["http://www.derangeddollars.com/hit.php?s=1&p=1&a=100161&t=0&c="]]
+    [["ZZ"], ["http://www.derangeddollars.com/hit.php?s=1&p=1&a=100161&t=0&c=",
+              "http://www.120mg.com/catalog/Bestsellers.htm?refid=898",
+              "http://www.world-pharmacy-24h.com/catalog/Bestsellers.htm?refid=898",
+              "http://www.first-meds.net/catalog/Bestsellers.htm?refid=898",
+              "http://www.online-drugs-store.com/catalog/Bestsellers.htm?refid=898"]]
 
 
 
