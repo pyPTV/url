892718dcb2f5c835dcec144e2f11d4a44bac321a
mbs-affiliate <mbs.affiliate@gmail.com>
2015-08-02 08:26:44 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -18,13 +18,11 @@
     [["GB", "IE", "NZ", "DK", "SE", "NO", "IS", "CH"], ["http://frtya.com/go/caRG9_bBBLR/sexy"]],
     [["LU", "PA", "AT", "TW", "SG"], ["http://frtya.com/go/caRG9_bBBLR/sexy"]],
     [["NL", "BE", "IT", "ES", "FI", "PT"], ["http://promotools.biz/cp/text_link/index.php?ur=aHR0cDovL3d3dy54Y2Ftcy5jb20%2FY29tZnJvbT04ODA5MjUmY2YyPSZjZnNhMj0mY2YwPXBjMg%3D%3D&promo=xcams&subid1=&subid2=&brand=xcams"]],
-    [["FR"], [
-                         "http://frtya.com/go/lW8l_bBBLR/gratuite",
-                         "http://promotools.biz/cp/text_link/index.php?ur=aHR0cDovL3d3dy54Y2Ftcy5jb20%2FY29tZnJvbT04ODA5MjUmY2YyPSZjZnNhMj0mY2YwPXBjMg%3D%3D&promo=xcams&subid1=&subid2=&brand=xcams"]],
+    [["FR"], ["http://frtya.com/go/lW8l_bBBLR/gratuite"]],
 
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-    [["RU", "KZ", "BY", "EE", "LV", "LT"], ["http://runetki.com/?id=7423&cmp=&lng=ru&tp=5"]],   
-    [["ZZ"], ["http://www.derangeddollars.com/hit.php?s=1&p=1&a=100161&t=0&c="]]
+    [["RU", "KZ", "BY", "EE", "LV", "LT"], ["http://runetki.com/432914/"]],   
+    [["ZZ"], ["http://runetki.com/160851/"]]
 
 
 
