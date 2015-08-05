7fa5eadf5dbfd4aa6251d203ff531c52752a1d98
mbs-affiliate <mbs.affiliate@gmail.com>
2015-08-05 13:01:24 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -2,8 +2,7 @@
     "mobile":
 [
     
-    [["ZZ"], ["http://join.bukkakeboysmobile.com/track/MTAwNTI4LjIxNS42OC42OC4wLjAuMC4wLjA",
-              "http://www.buddylead.com/nextdoorebony/go.php?pr=9&su=2&si=51&ad=250878&pa=index&ar=&buffer="]]
+    [["ZZ"], ["http://1empiredirect.com/redirect?aff_id=1621&at=2"]]
 
 ],
  
