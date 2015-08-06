89c5c1a372b178fee3dbdc678aeaf6c274fe1b54
mbs-affiliate <mbs.affiliate@gmail.com>
2015-08-07 01:03:18 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -2,15 +2,13 @@
     "mobile":
 [
     
-    [["ZZ"], ["http://join.bukkakeboysmobile.com/track/MTAwNTI4LjIxNS42OC42OC4wLjAuMC4wLjA",
-              "http://www.buddylead.com/nextdoorebony/go.php?pr=9&su=2&si=51&ad=250878&pa=index&ar=&buffer="]]
+    [["ZZ"], ["http://join.bukkakeboysmobile.com/track/MTAwNTI4LjIxNS42OC42OC4wLjAuMC4wLjA"]]
 ],
  
     "desktop":
 [
 
-    [["ZZ"], ["http://join.bukkakeboys.com/track/MTAwNTI4LjI2OC42Mi4zMDguMC4wLjAuMC4w",
-              "http://www.buddylead.com/nextdoorebony/go.php?pr=9&su=2&si=51&ad=250878&pa=index&ar=&buffer="]]
+    [["ZZ"], ["http://join.bukkakeboys.com/track/MTAwNTI4LjI2OC42Mi4zMDguMC4wLjAuMC4w"]]
 
 
 
