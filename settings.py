2974ac74a5a28895ed7c50e330a7d6fcb77ac6b6
mbs-affiliate <mbs.affiliate@gmail.com>
2015-08-08 02:14:02 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,15 +14,15 @@
 
 
 
-    [["US", "CA", "AU"], ["http://runetki.com/432914/"]],
-    [["GB", "IE", "NZ", "DK", "SE", "NO", "IS", "CH"], ["http://runetki.com/432914/"]],
-    [["LU", "PA", "AT", "TW", "SG"], ["http://runetki.com/432914/"]],
-    [["NL", "BE", "IT", "ES", "FI", "PT"], ["http://runetki.com/160851/"]],
-    [["FR"], ["http://runetki.com/432914/", "http://runetki.com/160851/"]],
+    [["US", "CA", "AU"], ["http://www.120mg.com/catalog/Bestsellers.htm?refid=898"]],
+    [["GB", "IE", "NZ", "DK", "SE", "NO", "IS", "CH"], ["http://www.120mg.com/catalog/Bestsellers.htm?refid=898"]],
+    [["LU", "PA", "AT", "TW", "SG"], ["http://www.120mg.com/catalog/Bestsellers.htm?refid=898"]],
+    [["NL", "BE", "IT", "ES", "FI", "PT"], ["http://www.120mg.com/catalog/Bestsellers.htm?refid=898"]],
+    [["FR"], ["http://www.120mg.com/catalog/Bestsellers.htm?refid=898"]],
 
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-    [["RU", "KZ", "BY", "EE", "LV", "LT"], ["http://runetki.com/432914/"]],   
-    [["ZZ"], ["http://runetki.com/160851/"]]
+    [["RU", "KZ", "BY", "EE", "LV", "LT"], ["http://www.120mg.com/catalog/Bestsellers.htm?refid=898"]],   
+    [["ZZ"], ["http://www.120mg.com/catalog/Bestsellers.htm?refid=898"]]
 
 
 
