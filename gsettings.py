45be11bdbb08b203bb1bdad1b19455f02c7599d2
mbs-affiliate <mbs.affiliate@gmail.com>
2015-08-07 16:32:23 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,17 +1,52 @@
 {
     "mobile":
 [
-    
+    [["US", "CA", "AU"], [
+
+              "http://www.buddylead.com/nextdoorebony/go.php?pr=9&su=2&si=51&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/boyzparty/go.php?pr=9&su=2&si=68&ad=250878&pa=index&ar=&buffer=",
+              "http://www.buddylead.com/circlejerkboys/go.php?pr=9&su=2&si=65&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/clubinfernodungeon/go.php?pr=9&su=2&si=42&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/extrabigdicks/go.php?pr=9&su=2&si=66&ad=250878&cs=ma&ar=&buffer=",
+              "http://www.buddylead.com/fistingcentral/go.php?pr=9&su=2&si=52&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/highperformancemen/go.php?pr=9&su=2&si=50&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/hothouse/go.php?pr=9&su=2&si=41&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/malereality/go.php?pr=9&su=2&si=73&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/nextdoorbuddies/go.php?pr=9&su=2&si=3&ad=250878&cs=tour3&ar=&buffer=",
+              "http://www.buddylead.com/nextdoortwink/go.php?pr=9&su=2&si=35&ad=250878&cs=tour3&ar=&buffer="
+                
+              ]],
+
+
     [["ZZ"], ["http://join.bukkakeboysmobile.com/track/MTAwNTI4LjIxNS42OC42OC4wLjAuMC4wLjA"]]
+
 ],
  
     "desktop":
 [
 
-    [["ZZ"], ["http://join.bukkakeboys.com/track/MTAwNTI4LjI2OC42Mi4zMDguMC4wLjAuMC4w"]]
 
+    [["US", "CA", "AU"], [
+
+              "http://www.buddylead.com/nextdoorebony/go.php?pr=9&su=2&si=51&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/boyzparty/go.php?pr=9&su=2&si=68&ad=250878&pa=index&ar=&buffer=",
+              "http://www.buddylead.com/circlejerkboys/go.php?pr=9&su=2&si=65&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/clubinfernodungeon/go.php?pr=9&su=2&si=42&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/extrabigdicks/go.php?pr=9&su=2&si=66&ad=250878&cs=ma&ar=&buffer=",
+              "http://www.buddylead.com/fistingcentral/go.php?pr=9&su=2&si=52&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/highperformancemen/go.php?pr=9&su=2&si=50&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/hothouse/go.php?pr=9&su=2&si=41&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/malereality/go.php?pr=9&su=2&si=73&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/nextdoorbuddies/go.php?pr=9&su=2&si=3&ad=250878&cs=tour3&ar=&buffer=",
+              "http://www.buddylead.com/nextdoortwink/go.php?pr=9&su=2&si=35&ad=250878&cs=tour3&ar=&buffer="
+                
+              ]],
+
+    [["ZZ"], ["http://join.bukkakeboys.com/track/MTAwNTI4LjI2OC42Mi4zMDguMC4wLjAuMC4w"]]
 
 
 ]
 }
+
+
 
