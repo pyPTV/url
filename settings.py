944ed7eee68e59078f95919e597800dbb7cd68a5
mbs-affiliate <mbs.affiliate@gmail.com>
2015-08-09 15:43:04 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 {
     "mobile":
 [
-    [["US"], ["http://rd.mobiteasy.com/mr/?id=alex1306&tr=xxx", "http://traffic.trafficcompany.com/?p=2478&media_type=adult&product_type=video&pi=sraight&source_type=redirect"]], 
-    [["ZZ"], ["http://rd.mobiteasy.com/mr/?id=alex1306&tr=xxx", "http://traffic.trafficcompany.com/?p=2478&media_type=adult&product_type=video&pi=sraight&source_type=redirect"]]
+    [["US"], ["http://rd.mobiteasy.com/mr/?id=alex1306&tr=xxx", "http://traffic.trafficcompany.com/?p=2478&media_type=adult&product_type=video&pi=sraight"]], 
+    [["ZZ"], ["http://rd.mobiteasy.com/mr/?id=alex1306&tr=xxx", "http://traffic.trafficcompany.com/?p=2478&media_type=adult&product_type=video&pi=sraight"]]
 
 ],
  
