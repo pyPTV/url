9b6949358a4a5ab3367f14311b4d93304ca80bb6
mbs-affiliate <mbs.affiliate@gmail.com>
2015-08-09 15:41:42 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,24 +1,8 @@
 {
     "mobile":
 [
-    [["US", "CA", "AU"], [
-
-              "http://www.buddylead.com/nextdoorebony/go.php?pr=9&su=2&si=51&ad=250878&cs=tour1&ar=&buffer=",
-              "http://www.buddylead.com/boyzparty/go.php?pr=9&su=2&si=68&ad=250878&pa=index&ar=&buffer=",
-              "http://www.buddylead.com/circlejerkboys/go.php?pr=9&su=2&si=65&ad=250878&cs=tour1&ar=&buffer=",
-              "http://www.buddylead.com/clubinfernodungeon/go.php?pr=9&su=2&si=42&ad=250878&cs=tour1&ar=&buffer=",
-              "http://www.buddylead.com/extrabigdicks/go.php?pr=9&su=2&si=66&ad=250878&cs=ma&ar=&buffer=",
-              "http://www.buddylead.com/fistingcentral/go.php?pr=9&su=2&si=52&ad=250878&cs=tour1&ar=&buffer=",
-              "http://www.buddylead.com/highperformancemen/go.php?pr=9&su=2&si=50&ad=250878&cs=tour1&ar=&buffer=",
-              "http://www.buddylead.com/hothouse/go.php?pr=9&su=2&si=41&ad=250878&cs=tour1&ar=&buffer=",
-              "http://www.buddylead.com/malereality/go.php?pr=9&su=2&si=73&ad=250878&cs=tour1&ar=&buffer=",
-              "http://www.buddylead.com/nextdoorbuddies/go.php?pr=9&su=2&si=3&ad=250878&cs=tour3&ar=&buffer=",
-              "http://www.buddylead.com/nextdoortwink/go.php?pr=9&su=2&si=35&ad=250878&cs=tour3&ar=&buffer="
-                
-              ]],
-
-
-    [["ZZ"], ["http://www.buddylead.com/nextdoorebony/go.php?pr=9&su=2&si=51&ad=250878&cs=tour1&ar=&buffer="]]
+    [["US", "CA", "AU"], ["http://traffic.trafficcompany.com/?p=2478&media_type=adult&product_type=video&gay=1&pi=gay"]],
+    [["ZZ"],             ["http://traffic.trafficcompany.com/?p=2478&media_type=adult&product_type=video&gay=1&pi=gay"]]
 
 ],
  
