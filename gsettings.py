27558a680ff0e215f9693ca5e04d691f57d4b916
mbs-affiliate <mbs.affiliate@gmail.com>
2015-08-11 21:19:05 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -26,7 +26,21 @@
                 
               ]],
 
-    [["ZZ"], ["http://www.buddylead.com/nextdoorebony/go.php?pr=9&su=2&si=51&ad=250878&cs=tour1&ar=&buffer="]]
+    [["ZZ"], [
+
+              "http://www.buddylead.com/nextdoorebony/go.php?pr=9&su=2&si=51&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/boyzparty/go.php?pr=9&su=2&si=68&ad=250878&pa=index&ar=&buffer=",
+              "http://www.buddylead.com/circlejerkboys/go.php?pr=9&su=2&si=65&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/clubinfernodungeon/go.php?pr=9&su=2&si=42&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/extrabigdicks/go.php?pr=9&su=2&si=66&ad=250878&cs=ma&ar=&buffer=",
+              "http://www.buddylead.com/fistingcentral/go.php?pr=9&su=2&si=52&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/highperformancemen/go.php?pr=9&su=2&si=50&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/hothouse/go.php?pr=9&su=2&si=41&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/malereality/go.php?pr=9&su=2&si=73&ad=250878&cs=tour1&ar=&buffer=",
+              "http://www.buddylead.com/nextdoorbuddies/go.php?pr=9&su=2&si=3&ad=250878&cs=tour3&ar=&buffer=",
+              "http://www.buddylead.com/nextdoortwink/go.php?pr=9&su=2&si=35&ad=250878&cs=tour3&ar=&buffer="
+                
+              ]]
 
 
 ]
