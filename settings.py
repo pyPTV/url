19cbd5dec83bed004b2938096c2918298c181ed6
mbs-affiliate <mbs.affiliate@gmail.com>
2015-08-13 18:00:20 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 {
     "mobile":
 [
-    [["US"], ["http://m.idhad.com/?codeaff=aff_affil4you_6722"]], 
+    [["US"], ["http://rd.mobiteasy.com/mr/?id=alex1306&tr=xxx"]], 
     [["ZZ"], ["http://rd.mobiteasy.com/mr/?id=alex1306&tr=xxx", "http://rd.mobiteasy.com/mr/?id=alex1306&tr=xxx", "http://traffic.trafficcompany.com/?p=2478&media_type=adult&product_type=video&pi=sraight"]]
 
 ],
