f2cb2803d1afbbd2acc8c2193ed3651ba720a913
mbs-affiliate <mbs.affiliate@gmail.com>
2015-08-22 14:32:26 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -12,17 +12,8 @@
   "desktop":
 [
 
-
-
-    [["US", "CA", "AU"], ["http://runetki.com/160851/"]],
-    [["GB", "IE", "NZ", "DK", "SE", "NO", "IS", "CH"], ["http://runetki.com/160851/"]],
-    [["LU", "PA", "AT", "TW", "SG"], ["http://runetki.com/160851/"]],
-    [["NL", "BE", "IT", "ES", "FI", "PT"], ["http://runetki.com/160851/"]],
-    [["FR"], ["http://runetki.com/160851/"]],
-
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-    [["RU", "KZ", "BY", "EE", "LV", "LT"], ["http://runetki.com/432914/"]],   
-    [["ZZ"], ["http://runetki.com/160851/"]]
+    [["ZZ"], ["http://www.fpcTraffic.com/raw/click.cgi?account=ppubros"]]
 
 
 
