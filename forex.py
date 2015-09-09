8b10d5f497639988da70a47b4719758193c2108e
mbs-affiliate <mbs.affiliate@gmail.com>
2015-09-09 20:02:43 +0300

    Update forex.py

--- forex.py
+++ forex.py
@@ -2,8 +2,8 @@
 {
     "mobile":
 [
-    [["US"], ["http://rd.mobiteasy.com/mr/?id=alex1306&tr=xxx"]], 
-    [["ZZ"], ["http://rd.mobiteasy.com/mr/?id=alex1306&tr=xxx"]]
+    [["US"], ["http://mobile.com"]], 
+    [["ZZ"], ["http://mobile.com"]]
 
 ],
  
@@ -13,9 +13,9 @@
   "desktop":
 [
 
-    [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-    [["GB", "DK", "FR", "AT", "NO", "BR", "CH", "BE"], ["http://frtya.com/go/caRG9_bBBLR/DEFAULT"]],
-    [["ZZ"], ["http://runetki.com/160851/"]]
+    [["DE"], ["http://germany.com"]],
+    [["GB", "DK", "FR", "AT", "NO", "BR", "CH", "BE"], ["http://norway.com"]],
+    [["ZZ"], ["http://site.com"]]
 
 
 
