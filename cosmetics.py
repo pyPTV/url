ebc007fba2efea23c59d900f04bffd921df8dc1e
mbs-affiliate <mbs.affiliate@gmail.com>
2015-09-10 16:12:21 +0300

    Update cosmetics.py

--- cosmetics.py
+++ cosmetics.py
@@ -1,1 +1,48 @@
-11
+
+{
+    "mobile":
+[
+    [["RU"], [
+                "http://numberonelash.ru/?stid=585", 
+                "http://magic-lash.ru/?stid=585", 
+                "http://magic-glance.com/catalog/ukhod-za-resnitsami/rost/magic-glance-intensive/?stid=585", 
+                "http://magic-glance.com/catalog/ukhod-za-volosami/dlya-rosta/?stid=585", 
+                "http://bloginwoman.ru/?stid=585",
+                "http://sexybeautylash.ru/?stid=585"
+              ]], 
+    [["ZZ"], [
+                "http://numberonelash.ru/?stid=585", 
+                "http://magic-lash.ru/?stid=585", 
+                "http://magic-glance.com/catalog/ukhod-za-resnitsami/rost/magic-glance-intensive/?stid=585", 
+                "http://magic-glance.com/catalog/ukhod-za-volosami/dlya-rosta/?stid=585", 
+                "http://bloginwoman.ru/?stid=585",
+                "http://sexybeautylash.ru/?stid=585"
+              ]]
+
+],
+ 
+
+ 
+
+  "desktop":
+[
+
+    [["RU"], [
+                "http://numberonelash.ru/?stid=585", 
+                "http://magic-lash.ru/?stid=585", 
+                "http://magic-glance.com/catalog/ukhod-za-resnitsami/rost/magic-glance-intensive/?stid=585", 
+                "http://magic-glance.com/catalog/ukhod-za-volosami/dlya-rosta/?stid=585", 
+                "http://bloginwoman.ru/?stid=585",
+                "http://sexybeautylash.ru/?stid=585"
+              ]], 
+    [["ZZ"], [
+                "http://numberonelash.ru/?stid=585", 
+                "http://magic-lash.ru/?stid=585", 
+                "http://magic-glance.com/catalog/ukhod-za-resnitsami/rost/magic-glance-intensive/?stid=585", 
+                "http://magic-glance.com/catalog/ukhod-za-volosami/dlya-rosta/?stid=585", 
+                "http://bloginwoman.ru/?stid=585",
+                "http://sexybeautylash.ru/?stid=585"
+              ]]
+
+]}
+
