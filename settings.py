afd253cc17adbf166bc8031f850c7a73002fca52
mbs-affiliate <mbs.affiliate@gmail.com>
2015-09-16 13:24:45 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@
 [
 
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-    [["GB", "DK", "FR", "AT", "NO", "BR", "CH", "BE"], ["http://frtya.com/go/caRG9_bBBLR/DEFAULT"]],
+    [["GB", "DK", "FR", "AT", "NO", "BR", "CH", "BE"], ["http://cam4.com"]],
     [["ZZ"], ["http://cam4.com"]]
 
 
