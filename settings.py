897db96d3e7d2e78cd6451581437e116fc7c927e
mbs-affiliate <mbs.affiliate@gmail.com>
2015-09-16 13:12:59 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 {
     "mobile":
 [
-    [["US"], ["http://runetki.com/160851/"]], 
-    [["ZZ"], ["http://runetki.com/160851/"]]
+    [["US"], ["http://cam4.com"]], 
+    [["ZZ"], ["http://cam4.com"]]
 
 ],
  
@@ -14,7 +14,7 @@
 
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
     [["GB", "DK", "FR", "AT", "NO", "BR", "CH", "BE"], ["http://frtya.com/go/caRG9_bBBLR/DEFAULT"]],
-    [["ZZ"], ["http://runetki.com/160851/"]]
+    [["ZZ"], ["http://cam4.com"]]
 
 
 
