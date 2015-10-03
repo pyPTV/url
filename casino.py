6ace6f17a5596c33600b9042b20b934afd091d4a
mbs-affiliate <mbs.affiliate@gmail.com>
2015-10-04 00:30:32 +0300

    Update casino.py

--- casino.py
+++ casino.py
@@ -1,22 +1,8 @@
 {
     "mobile":
 [
-    [["RU"], [
-                "http://numberonelash.ru/?stid=585", 
-                "http://magic-lash.ru/?stid=585", 
-                "http://magic-glance.com/catalog/ukhod-za-resnitsami/rost/magic-glance-intensive/?stid=585", 
-                "http://magic-glance.com/catalog/ukhod-za-volosami/dlya-rosta/?stid=585", 
-                "http://bloginwoman.ru/?stid=585",
-                "http://sexybeautylash.ru/?stid=585"
-              ]], 
-    [["ZZ"], [
-                "http://numberonelash.ru/?stid=585", 
-                "http://magic-lash.ru/?stid=585", 
-                "http://magic-glance.com/catalog/ukhod-za-resnitsami/rost/magic-glance-intensive/?stid=585", 
-                "http://magic-glance.com/catalog/ukhod-za-volosami/dlya-rosta/?stid=585", 
-                "http://bloginwoman.ru/?stid=585",
-                "http://sexybeautylash.ru/?stid=585"
-              ]]
+    [["US"], ["http://runetki.com/160851/"]], 
+    [["ZZ"], ["http://mobile.bitterstrawberry.org/?id=170"]]
 
 ],
  
@@ -25,22 +11,13 @@
 
   "desktop":
 [
+    [["US", "AU", "CA"], ["http://chaturbate.com/affiliates/in/hr8m/iWhVu/?track=default&disable_sound=True"]],
+    [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
+    [["GB", "DK", "FR", "AT", "NO", "BR", "CH", "BE"], ["http://runetki.com/160851/"]],
+    [["ZZ"], ["http://runetki.com/160851/"]]
 
-    [["RU"], [
-                "http://numberonelash.ru/?stid=585", 
-                "http://magic-lash.ru/?stid=585", 
-                "http://magic-glance.com/catalog/ukhod-za-resnitsami/rost/magic-glance-intensive/?stid=585", 
-                "http://magic-glance.com/catalog/ukhod-za-volosami/dlya-rosta/?stid=585", 
-                "http://bloginwoman.ru/?stid=585",
-                "http://sexybeautylash.ru/?stid=585"
-              ]], 
-    [["ZZ"], [
-                "http://numberonelash.ru/?stid=585", 
-                "http://magic-lash.ru/?stid=585", 
-                "http://magic-glance.com/catalog/ukhod-za-resnitsami/rost/magic-glance-intensive/?stid=585", 
-                "http://magic-glance.com/catalog/ukhod-za-volosami/dlya-rosta/?stid=585", 
-                "http://bloginwoman.ru/?stid=585",
-                "http://sexybeautylash.ru/?stid=585"
-              ]]
+
+
 
 ]}
+
