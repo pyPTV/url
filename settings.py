6b406419fc18cc8ddb5ba85e2eab7dfadfedb269
mbs-affiliate <mbs.affiliate@gmail.com>
2015-10-12 15:10:52 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -11,9 +11,9 @@
 
   "desktop":
 [
-    [["US", "AU", "CA"], ["http://chaturbate.com/affiliates/in/hr8m/wljQg/?track=default&disable_sound=True"]],
+    [["US", "AU", "CA", "GB", "NO", "FR", "CH"], ["http://chaturbate.com/affiliates/in/hr8m/wljQg/?track=default&disable_sound=True"]],
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-    [["GB", "DK", "FR", "AT", "NO", "BR", "CH", "BE"], ["http://runetki.com/160851/"]],
+    [["DK", "AT", "BR", "BE"], ["http://runetki.com/160851/"]],
     [["ZZ"], ["http://runetki.com/160851/"]]
 
 
