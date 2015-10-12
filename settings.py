409be353b2e0b8590afd5a70b3ef600710f9dfce
mbs-affiliate <mbs.affiliate@gmail.com>
2015-10-12 15:53:14 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 {
     "mobile":
 [
-    [["US"], ["http://runetki.com/160851/"]], 
+    [["US"], ["http://chaturbate.com/affiliates/in/hr8m/wljQg/?track=default&disable_sound=True"]], 
     [["ZZ"], ["http://mobile.bitterstrawberry.org/?id=170"]]
 
 ],
@@ -11,10 +11,8 @@
 
   "desktop":
 [
-    [["US", "AU", "CA", "GB", "NO", "FR", "CH"], ["http://chaturbate.com/affiliates/in/hr8m/wljQg/?track=default&disable_sound=True"]],
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-    [["DK", "AT", "BR", "BE"], ["http://runetki.com/160851/"]],
-    [["ZZ"], ["http://runetki.com/160851/"]]
+    [["ZZ"], ["http://chaturbate.com/affiliates/in/hr8m/wljQg/?track=default&disable_sound=True"]]
 
 
 
