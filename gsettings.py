3e7f617ea246f51295ec7dd6f1b312cc5d8b2bfb
mbs-affiliate <mbs.affiliate@gmail.com>
2015-10-20 15:47:52 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,7 +1,7 @@
 {
     "mobile":
 [
-    [["US", "CA", "AU"], ["http://chaturbate.com/affiliates/in/9rL0/wljQg/?track=default&disable_sound=True"]],
+    [["US", "CA", "AU"], ["http://join.bukkakeboysmobile.com/track/affmasterico.268.68.68.0.0.0.0.0"]],
     [["ZZ"],             ["http://join.bukkakeboysmobile.com/track/affmasterico.268.68.68.0.0.0.0.0"]]
 
 ],
