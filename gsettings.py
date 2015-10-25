8b4fba4b0930ede7928004cc1f48edabce00620e
mbs-affiliate <mbs.affiliate@gmail.com>
2015-10-25 10:29:33 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,15 +1,15 @@
 {
     "mobile":
 [
-    [["US", "CA", "AU"], ["http://join.bukkakeboysmobile.com/track/affmasterico.268.68.68.0.0.0.0.0"]],
-    [["ZZ"],             ["http://join.bukkakeboysmobile.com/track/affmasterico.268.68.68.0.0.0.0.0"]]
+    [["US"], ["http://frtya.com/go/F6pX_bBBLR/DEFAULT"]],
+    [["ZZ"], ["http://join.bukkakeboysmobile.com/track/affmasterico.268.68.68.0.0.0.0.0"]]
 
 ],
  
     "desktop":
 [
 
-    [["US", "CA", "AU"], ["http://chaturbate.com/affiliates/in/9rL0/wljQg/?track=default&disable_sound=True"]],
+    [["US"], ["http://frtya.com/go/uFYX_bBBLR/DEFAULT"]],
     [["ZZ"], ["http://join.bukkakeboys.com/track/affmasterico.212.62.612.0.0.0.0.0"]]
 
 
