6d7a9a64b9365db9ca80c1b961243c07bf44827a
mbs-affiliate <mbs.affiliate@gmail.com>
2015-10-26 19:58:40 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@
   "desktop":
 [
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-    [["ZZ"], ["http://chaturbate.com/affiliates/in/hr8m/wZqzy/?track=default", "http://www.allporncollection.com"]]
+    [["ZZ"], ["http://chaturbate.com/affiliates/in/hr8m/wZqzy/?track=default"]]
 
 
 
