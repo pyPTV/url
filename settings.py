69bde5ac2ea81e109094d0500e2f1d3d8540d052
mbs-affiliate <mbs.affiliate@gmail.com>
2015-11-04 16:52:00 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -12,6 +12,7 @@
   "desktop":
 [
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
+    [["RU"], ["http://runetki.com/432914/"]],
     [["ZZ"], ["http://chaturbate.com/affiliates/in/hr8m/wZqzy/?track=default"]]
 
 
