51a3c450ecc878a784dfc29647e73a6558659a52
mbs-affiliate <mbs.affiliate@gmail.com>
2015-11-08 21:24:23 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,6 +13,8 @@
 [
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
     [["RU"], ["http://runetki.com/432914/"]],
+    [["EE", "CZ", "AT", "BE", "SK", "IT", "SI", "JP", "ES", "LV", "IL",
+      "HR", "PT", "LT", "RO", "TR", "BG", "PL", "HU", "CY", "FI", "IS"], ["http://bongacams.com/track?c=302460"],
     [["ZZ"], ["http://chaturbate.com/affiliates/in/hr8m/wZqzy/?track=default"]]
 
 
