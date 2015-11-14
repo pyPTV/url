b44fbca464098fcf846bd4d567c2f93e6d20169e
mbs-affiliate <mbs.affiliate@gmail.com>
2015-11-14 21:56:31 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
     "mobile":
 [
     [["AU", "CA", "GB", "IE", "NZ", "US", "NL", "DK", 
-      "BE", "FR", "IS", "NO", "SE"], ["http://frtyb.com/go/eWVN_bBBLR/DEFAULT"]], 
+      "BE", "FR", "IS", "NO", "SE"], ["http://frtyb.com/go/eWVN_bBBLR/FreeCamsExposedMobilePPL"]], 
     [["ZZ"], ["http://traffic.trafficcompany.com/?p=2478&media_type=adult"]]
 
 ],
@@ -13,16 +13,17 @@
   "desktop":
 [
     
-    [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035", "http://frtyb.com/go/wHl_bBBLR/DEFAULT"]],
-    [["AU", "CA", "IE", "NZ", "DK", "SE", "NO", "IS", "CH", "BE", "FR"], ["http://frtyb.com/go/wHl_bBBLR/DEFAULT"]], 
-
+    
+    [["AU", "CA", "GB", "IE", "NZ", "NL", 
+      "DK", "SE", "NO", "IS", "FR" "CH", "BE"], ["http://frtyb.com/go/wHl_bBBLR/FreeCamsExposedPPL"]], 
+    [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035", "http://frtyb.com/go/wHl_bBBLR/FreeCamsExposedPPL"]],
 
 
 
     [["RU"], ["http://runetki.com/432914/"]],
     [["EE", "CZ", "AT", "SK", "IT", "SI", "JP", "ES", "LV", "IL", 
       "HR", "PT", "LT", "RO", "TR", "BG", "PL", "HU", "CY", "FI"], ["http://bongacams.com/track?c=302460"]],
-    [["US", "NL", "GB"], ["http://chaturbate.com/affiliates/in/hr8m/XSQ5K/?track=default"]],
+    [["US"], ["http://chaturbate.com/affiliates/in/hr8m/XSQ5K/?track=default"]],
     [["ZZ"], ["http://runetki.com/160851/"]]
 
 
