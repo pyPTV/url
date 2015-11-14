334ff0d310966b786aaf297f531d34c8c1b6df8c
mbs-affiliate <mbs.affiliate@gmail.com>
2015-11-14 21:33:46 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -12,13 +12,17 @@
 
   "desktop":
 [
-    [["US", "NL", "GB"], ["http://chaturbate.com/affiliates/in/hr8m/XSQ5K/?track=default"]],
+    
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035", "http://frtyb.com/go/wHl_bBBLR/DEFAULT"]],
     [["AU", "CA", "IE", "NZ", "DK", "SE", "NO", "IS", "CH", "BE", "FR"], ["http://frtyb.com/go/wHl_bBBLR/DEFAULT"]], 
+
+
+
+
     [["RU"], ["http://runetki.com/432914/"]],
-    [["EE", "CZ", "AT", "SK", "IT", "SI", "JP", "ES", "LV", "IL",
+    [["EE", "CZ", "AT", "SK", "IT", "SI", "JP", "ES", "LV", "IL", 
       "HR", "PT", "LT", "RO", "TR", "BG", "PL", "HU", "CY", "FI"], ["http://bongacams.com/track?c=302460"]],
-    
+    [["US", "NL", "GB"], ["http://chaturbate.com/affiliates/in/hr8m/XSQ5K/?track=default"]],
     [["ZZ"], ["http://runetki.com/160851/"]]
 
 
