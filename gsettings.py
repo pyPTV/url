c0b2f656cf12808d8c9e7a88b0597464c111d4d5
mbs-affiliate <mbs.affiliate@gmail.com>
2015-11-14 21:43:13 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,7 +1,10 @@
 {
     "mobile":
 [
-    [["US"], ["http://frtyb.com/go/F6pX_bBBLR/DEFAULT"]],
+    [["AU", "CA", "GB", "NZ", "US", "SG" 
+      "DK", "FR", "IE", "IS", "IT", "NO", 
+      "PR", "SE", "BE", "CH", "ES", "FI", 
+      "HK", "JP", "KR", "PT", "ZA"], ["http://frtyb.com/go/F6pX_bBBLR/DEFAULT"]],
     [["ZZ"], ["http://traffic.trafficcompany.com/?p=2478&media_type=adult&gay=1"]]
 
 ],
@@ -9,9 +12,10 @@
     "desktop":
 [
 
-    [["BE", "CH", "ES", "FI", "HK", "KR", "PT", "ZA", 
-      "DK", "FR", "IE", "IS", "IT", "NO", "PR", "SE",
-      "CA", "NZ", "SG", "AU"], ["http://frtyb.com/go/uFYX_bBBLR/DEFAULT"]],
+    [["BE", "CH", "ES", "FI", "HK", "KR", 
+      "PT", "ZA", "DK", "FR", "IE", "IS", 
+      "IT", "NO", "PR", "SE", "CA", "NZ", 
+      "SG", "AU"], ["http://frtyb.com/go/uFYX_bBBLR/DEFAULT"]],
     [["ZZ"], ["http://join.bukkakeboys.com/track/affmasterico.212.62.612.0.0.0.0.0"]]
 
 
