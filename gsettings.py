37ac981ee364407fef320b3112b10adac5a15106
mbs-affiliate <mbs.affiliate@gmail.com>
2015-11-14 21:50:03 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,7 +4,7 @@
     [["AU", "CA", "GB", "NZ", "US", "SG" 
       "DK", "FR", "IE", "IS", "IT", "NO", 
       "PR", "SE", "BE", "CH", "ES", "FI", 
-      "HK", "JP", "KR", "PT", "ZA"], ["http://frtyb.com/go/F6pX_bBBLR/DEFAULT"]],
+      "HK", "JP", "KR", "PT", "ZA"], ["http://frtyb.com/go/F6pX_bBBLR/JizzRouletteMobilePPL"]],
     [["ZZ"], ["http://traffic.trafficcompany.com/?p=2478&media_type=adult&gay=1"]]
 
 ],
@@ -12,10 +12,10 @@
     "desktop":
 [
 
-    [["BE", "CH", "ES", "FI", "HK", "KR", 
-      "PT", "ZA", "DK", "FR", "IE", "IS", 
-      "IT", "NO", "PR", "SE", "CA", "NZ", 
-      "SG", "AU"], ["http://frtyb.com/go/uFYX_bBBLR/DEFAULT"]],
+    [["AU", "CA", "GB", "NZ", "SG" 
+      "DK", "FR", "IE", "IS", "IT", "NO", 
+      "PR", "SE", "BE", "CH", "ES", "FI", 
+      "HK", "KR", "PT", "ZA"], ["http://frtyb.com/go/uFYX_bBBLR/GayCamsExposed"]],
     [["ZZ"], ["http://join.bukkakeboys.com/track/affmasterico.212.62.612.0.0.0.0.0"]]
 
 
