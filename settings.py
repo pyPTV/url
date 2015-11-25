33d718e50a7b1bce0caafa3a87d43f75d848fc68
mbs-affiliate <mbs.affiliate@gmail.com>
2015-11-25 13:47:33 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@
     
     
     [["AU", "CA", "GB", "IE", "NZ", "NL", "DK", "SE", "NL", "NO", "IS", "CH"], ["http://frtyb.com/go/caRG9_bBBLR/sexy", "http://frtyb.com/go/wHl_bBBLR/FreeCamsExposedPPL"]], 
-    [["DE"], ["http://frtyb.com/go/caRG9_bBBLR/sexy", "http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
+    [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
     [["RU"], ["http://runetki.com/432914/"]],
     [["BR", "IN", "CO", "IT", "MX", "VE", "ES", "AR", "AT", "LU", "CZ", "FI", "FR", "BE"], ["http://frtyb.com/go/caRG9_bBBLR/sexy"]], 
     
