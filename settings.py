e956cd23a29a02da67935e6ed6857b1267cf03e4
mbs-affiliate <mbs.affiliate@gmail.com>
2015-12-12 17:56:37 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 {
     "mobile":
 [
-    [["AU", "CA", "GB", "IE", "NZ", "US", "NL", "DK", "IS", "NO", "SE"], ["http://runetki.com"]], 
+    [["US"], ["http://runetki.com"]], 
     [["ZZ"], ["http://traffic.trafficcompany.com/?p=2478&media_type=adult"]]
 
 ],
@@ -13,11 +13,8 @@
 [
     
     
-    [["AU", "CA", "GB", "IE", "NZ", "NL", "DK", "SE", "NL", "NO", "IS", "CH"], ["http://runetki.com"]], 
+
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-    [["RU"], ["http://runetki.com/432914/"]],
-    [["BR", "IN", "CO", "IT", "MX", "VE", "ES", "AR", "AT", "LU", "CZ", "FI", "FR", "BE"], ["http://runetki.com"]], 
-    
     [["ZZ"], ["http://runetki.com"]]
    
 
