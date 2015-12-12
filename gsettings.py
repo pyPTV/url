21f092582f3d19820f0d47461364e8f6a98a31c6
mbs-affiliate <mbs.affiliate@gmail.com>
2015-12-12 17:55:28 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,10 +1,7 @@
 {
     "mobile":
 [
-    [["AU", "CA", "GB", "NZ", "US", "SG", 
-      "DK", "FR", "IE", "IS", "IT", "NO", 
-      "PR", "SE", "BE", "CH", "ES", "FI", 
-      "HK", "JP", "KR", "PT", "ZA"], ["http://runetki.com"]],
+    [["US"], ["http://runetki.com"]],
     [["ZZ"], ["http://traffic.trafficcompany.com/?p=2478&media_type=adult&gay=1"]]
 
 ],
@@ -12,10 +9,7 @@
     "desktop":
 [
 
-    [["AU", "CA", "GB", "NZ", "SG", 
-      "DK", "FR", "IE", "IS", "IT", "NO", 
-      "PR", "SE", "BE", "CH", "ES", "FI", 
-      "HK", "KR", "PT", "ZA"], ["http://runetki.com"]],
+    [["US"], ["http://runetki.com"]],
     [["ZZ"], ["http://join.bukkakeboys.com/track/affmasterico.212.62.612.0.0.0.0.0"]]
 
 
