a0df7745280e49da8d08a167a533ffe18de16b7e
mbs-affiliate <mbs.affiliate@gmail.com>
2015-12-12 17:43:02 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,7 +4,7 @@
     [["AU", "CA", "GB", "NZ", "US", "SG", 
       "DK", "FR", "IE", "IS", "IT", "NO", 
       "PR", "SE", "BE", "CH", "ES", "FI", 
-      "HK", "JP", "KR", "PT", "ZA"], ["http://frtyb.com/go/F6pX_bBBLR/JizzRouletteMobilePPL"]],
+      "HK", "JP", "KR", "PT", "ZA"], ["http://runetki.com"]],
     [["ZZ"], ["http://traffic.trafficcompany.com/?p=2478&media_type=adult&gay=1"]]
 
 ],
@@ -15,7 +15,7 @@
     [["AU", "CA", "GB", "NZ", "SG", 
       "DK", "FR", "IE", "IS", "IT", "NO", 
       "PR", "SE", "BE", "CH", "ES", "FI", 
-      "HK", "KR", "PT", "ZA"], ["http://frtyb.com/go/uFYX_bBBLR/GayCamsExposed"]],
+      "HK", "KR", "PT", "ZA"], ["http://runetki.com"]],
     [["ZZ"], ["http://join.bukkakeboys.com/track/affmasterico.212.62.612.0.0.0.0.0"]]
 
 
