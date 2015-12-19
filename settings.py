8afad3f10d7913d0058b5b0b8f8acee144d7a1f8
mbs-affiliate <mbs.affiliate@gmail.com>
2015-12-19 12:25:19 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 {
     "mobile":
 [
-    [["US"], ["http://chaturbate.com/affiliates/in/ZmU7/XSQ5K/?track=default"]], 
+    [["US"], ["http://runetki.com"]], 
     [["ZZ"], ["http://traffic.trafficcompany.com/?p=2478&media_type=adult"]]
 
 ],
@@ -15,7 +15,10 @@
     
 
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
-    [["ZZ"], ["http://chaturbate.com/affiliates/in/ZmU7/XSQ5K/?track=default"]]
+    [["NL", "AT", "AU", "BE", "CA", "CH", "DK", "FI", "FR", 
+      "GB", "IE", "IS", "JP", "LI", "LU", "NO", "NZ", "SE", 
+      "SG"], ["http://chaturbate.com/affiliates/in/ZmU7/XSQ5K/?track=default"]],
+    [["ZZ"], ["http://runetki.com"]]
    
 
 
