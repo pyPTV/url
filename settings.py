21cd2a024e5cf306f9791ea1b71ed6cfd4b9bd66
mbs-affiliate <mbs.affiliate@gmail.com>
2015-12-24 14:30:46 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
     [["NL", "AT", "AU", "BE", "CA", "CH", "DK", "FI", "FR", 
       "GB", "IE", "IS", "JP", "LI", "LU", "NO", "NZ", "SE", 
-      "SG"], ["http://chaturbate.com/affiliates/in/ZmU7/XSQ5K/?track=default"]],
+      "SG"], ["http://runetki.com"]],
     [["ZZ"], ["http://runetki.com"]]
    
 
