dcda2f401cf94b5f628906c6e11614a6cc98e5b2
mbs-affiliate <mbs.affiliate@gmail.com>
2016-01-29 15:09:43 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 {
     "mobile":
 [
-    [["US"], ["http://runetki.com"]], 
+    [["US"], ["http://www.tgtrak.com/SHht"]], 
     [["ZZ"], ["http://t0pm0b1l3.com/?id=170"]]
 
 ],
@@ -17,8 +17,8 @@
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
     [["NL", "AT", "AU", "BE", "CA", "CH", "DK", "FI", "FR", 
       "GB", "IE", "IS", "JP", "LI", "LU", "NO", "NZ", "SE", 
-      "SG"], ["http://runetki.com"]],
-    [["ZZ"], ["http://runetki.com"]]
+      "SG"], ["http://www.tgtrak.com/SHht"]],
+    [["ZZ"], ["http://www.tgtrak.com/SHht"]]
    
 
 
