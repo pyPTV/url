54155bd01a360bc8339b5ac4e330fa1c5440d5a2
mbs-affiliate <mbs.affiliate@gmail.com>
2016-01-31 20:36:06 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
     [["NL", "AT", "AU", "BE", "CA", "CH", "DK", "FI", "FR", 
       "GB", "IE", "IS", "JP", "LI", "LU", "NO", "NZ", "SE", 
-      "SG"], ["http://www.tgtrak.com/SHht", "http://www.tgtrak.com/SHhv"]],
+      "SG"], ["http://bongacams.com/track?c=302460"]],
     [["ZZ"], ["http://www.tgtrak.com/SHht", "http://www.tgtrak.com/SHhv"]]
    
 
