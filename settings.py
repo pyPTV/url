d941d3b7ad3b1244810d3270ce78ab4e5f1e4d90
mbs-affiliate <mbs.affiliate@gmail.com>
2016-02-02 13:28:36 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -18,6 +18,7 @@
     [["IN"], ["http://secure.babesofindia.com/track/affmasterico.1.8.14.0.0.0.0.0"]],
     [["RU"], ["http://runetki.com/?id=28053&cmp=&lng=ru&tp=5"]],
     [["JP"], ["http://click.dtiserv2.com/Direct/9103999-103-us9126/"]],
+    [["CN"], ["http://click.dtiserv2.com/Direct/9354999-354-us9126/cn/japanese-webcam-girls.shtml"]],
     [["ZZ"], ["http://bongacams.com/track?c=302460",
               "http://www.flirt4free.com/live/girls/?mp_code=ag65c",  
               "http://promotools.biz/cp/text_link/index.php?ur=aHR0cDovL3d3dy54Y2Ftcy5jb20%2FY29tZnJvbT04ODA5MjUmY2YyPSZjZnNhMj0mY2YwPXBjMg%3D%3D&promo=xcams&subid1=&subid2=&brand=xcams"]]
