63b4d20ce78e786e6270384e654be2593d1f4ad3
mbs-affiliate <mbs.affiliate@gmail.com>
2016-02-02 13:03:11 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,7 +17,9 @@
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
     [["IN"], ["http://secure.babesofindia.com/track/affmasterico.1.8.14.0.0.0.0.0"]],
     [["RU"], ["http://runetki.com/?id=28053&cmp=&lng=ru&tp=5"]],
-    [["ZZ"], ["http://tubecamgirl.com/?affil=132450"]]
+    [["ZZ"], ["http://bongacams.com/track?c=302460",
+              "http://www.flirt4free.com/live/girls/?mp_code=ag65c",  
+              "http://promotools.biz/cp/text_link/index.php?ur=aHR0cDovL3d3dy54Y2Ftcy5jb20%2FY29tZnJvbT04ODA5MjUmY2YyPSZjZnNhMj0mY2YwPXBjMg%3D%3D&promo=xcams&subid1=&subid2=&brand=xcams"]]
    
 
 
