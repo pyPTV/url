e01bf3e6561a6deaf775a0d4144255af05d27c7d
mbs-affiliate <mbs.affiliate@gmail.com>
2016-02-02 18:41:05 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@
     [["US"], ["http://tubecamgirl.com/?affil=223714"]],
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
     [["IN"], ["http://secure.babesofindia.com/track/affmasterico.1.8.14.0.0.0.0.0"]],
-    [["RU", "KZ", "BY"], ["http://runetki.com/?id=28053&cmp=&lng=ru&tp=5"]],
+    [["RU", "KZ", "BY", "AM", "AZ", "MD"], ["http://runetki.com/?id=28053&cmp=&lng=ru&tp=5"]],
     [["JP"], ["http://click.dtiserv2.com/Direct/9103999-103-us9126/"]],
     [["CN"], ["http://click.dtiserv2.com/Direct/9354999-354-us9126/cn/japanese-webcam-girls.shtml"]],
     [["NO", "CH", "GB",
