4d5923cac19f5acb87b9e519b9d0099162c2707a
mbs-affiliate <mbs.affiliate@gmail.com>
2016-02-02 10:52:38 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,7 +1,7 @@
 {
     "mobile":
 [
-    [["US"], ["http://track.wapempire.com/SH1xb"]],
+    [["US"], ["http://runetki.com"]],
     [["ZZ"], ["http://join.bukkakeboys.com/track/affmasterico.212.62.612.0.0.0.0.0"]]
 
 ],
