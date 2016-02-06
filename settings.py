8c63ecf2bd5a50b33bd43a2b90a539d2fbb844d3
mbs-affiliate <mbs.affiliate@gmail.com>
2016-02-06 09:51:23 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@
 [
     
     
-    [["US"], ["http://tubecamgirl.com/?affil=223714"]],
+    [["US"], ["http://tubecamgirl.com"]],
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
     [["IN"], ["http://secure.babesofindia.com/track/affmasterico.1.8.14.0.0.0.0.0"]],
     [["RU"], ["http://runetki.com/?id=28053&cmp=&lng=ru&tp=5"]],
