735b121f9678ca4a37b076591a0125faca9e94b4
mbs-affiliate <mbs.affiliate@gmail.com>
2016-02-10 08:18:44 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -19,15 +19,6 @@
     [["RU"], ["http://runetki.com/?id=28053&cmp=&lng=ru&tp=5"]],
     [["JP"], ["http://enter.javhd.com/track/MjIwOTIuMi4yLjgzLjAuMC4wLjAuMA"]],
     [["CN"], ["http://enter.javhd.com/track/MjIwOTIuMi4yLjY4LjAuMC4wLjAuMA"]],
-    [["NO", "CH", "GB",
-      "DK", "FI", "SE",
-      "EE", "CZ", "AT", 
-      "BE", "NL", "FR",
-      "IE", "SK", "IS", 
-      "IT", "SI", "ES", 
-      "LT", "IL", "LU", 
-      "AE", "LV", "TR", 
-      "HU", "MK", "CY"], ["http://bongacams.com/track?c=302460"]],
     [["ZZ"], ["http://ads.itmcash.com/a/14758"]]
    
 
