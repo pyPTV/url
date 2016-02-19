1ed9e9aa86fc6e2f176d8d6a4a4c67f41ad8f547
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-02-19 15:13:07 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@
     [["US", "CA", "GB"], ["http://chaturbate.com/affiliates/in/hr8m/YBq6s/?track=default"]],
     [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=first&prm=ppl&ts=&wm=19035"]],
     [["IN"], ["http://secure.babesofindia.com/track/affmasterico.1.8.14.0.0.0.0.0"]],
-    [["RU"], ["http://runetki.com/?id=28053&cmp=&lng=ru&tp=5"]],
+    [["RU"], ["http://runetki.com/627737/"]],
     [["JP"], ["http://enter.javhd.com/track/MjIwOTIuMi4yLjgzLjAuMC4wLjAuMA"]],
     [["CN"], ["http://enter.javhd.com/track/MjIwOTIuMi4yLjY4LjAuMC4wLjAuMA"]],
     [["ZZ"], ["http://ads.itmcash.com/a/14758"]]
