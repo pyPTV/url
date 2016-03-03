5881cfb69f89d6a5504fab40e66cfa3a7fdae563
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-03-03 08:30:51 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,7 +1,7 @@
 {
     "mobile":
 [
-    [["US"], ["http://join.bukkakeboysmobile.com/track/MTAwNjg0LjIxNC42OC42OC4wLjAuMC4wLjA"]],
+    [["US"], ["http://join.bukkakeboysmobile.com/track/MTAwNjA1LjIxNC42OC42OC4wLjAuMC4wLjA"]],
     [["ZZ"], ["http://traffic.trafficcompany.com/?p=2478&media_type=adult&gay=1&source_type=redirect"]]
 
 ],
@@ -9,10 +9,10 @@
     "desktop":
 [
 
-    [["US"], ["http://join.bukkakeboys.com/track/MTAwNjg0LjIxMS42Mi42MTIuMC4wLjAuMC4w",
-              "http://join.shemalefuckfest.com/track/MTAwNjg0LjIxMS4zMy4zMy4wLjAuMC4wLjA"]],
-    [["ZZ"], ["http://join.bukkakeboys.com/track/MTAwNjg0LjIxMS42Mi42MTIuMC4wLjAuMC4w",
-              "http://join.shemalefuckfest.com/track/MTAwNjg0LjIxMS4zMy4zMy4wLjAuMC4wLjA"]]
+    [["US"], ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w",
+              "http://join.shemalefuckfest.com/track/MTAwNjA1LjIxMS4zMy4zMy4wLjAuMC4wLjA"]],
+    [["ZZ"], ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w",
+              "http://join.shemalefuckfest.com/track/MTAwNjA1LjIxMS4zMy4zMy4wLjAuMC4wLjA"]]
 
 
 ]
