b988c06396c5dc3a7c61c27aecaf67ce6d2ffa32
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-03-14 14:44:34 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,7 +1,7 @@
 {
     "mobile":
 [
-    [["US"], ["http://join.bukkakeboysmobile.com/track/MTAwNjA1LjIxNC42OC42OC4wLjAuMC4wLjA"]],
+    [["US"], ["http://www.cam4.com/male"]],
     [["ZZ"], ["http://traffic.trafficcompany.com/?p=2478&media_type=adult&gay=1&source_type=redirect"]]
 
 ],
@@ -9,8 +9,7 @@
     "desktop":
 [
 
-    [["US"], ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w",
-              "http://join.shemalefuckfest.com/track/MTAwNjA1LjIxMS4zMy4zMy4wLjAuMC4wLjA"]],
+    [["US"], ["http://www.cam4.com/male"]],
 
     [["AT", "AU", "BE", "CA", "CH", "DK",
       "FI", "FR", "GB", "IE", "IS", "JP",
