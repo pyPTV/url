f2383c89659e8efc76dce227c6692d3d0cf4b00d
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-03-14 07:59:42 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -22,9 +22,13 @@
       "GQ", "HK", "IL", "IM", "KR", "KW", 
       "KY", "MO", "PF", "PR"], ["http://chaturbate.com/affiliates/in/hr8m/AkxtL/?track=default"]],
 
-    [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=&prm=ppl&ts=&wm=19035",
-              "http://landing.rotlichtkartei.com/?ag=724&prd=Nohm7mui&pr=&prm=ppl&ts=&wm=19035",
-              "http://www.robins-sexblog.com/?ag=742&pr=&prm=ppl&ts=&wm=19035"]],
+    [["DE"], ["http://www.saboom.com/?ag=758&pr=saboom&prm=ppl&ts=&wm=19035",
+              "http://www.robins-sexblog.com/?ag=742&pr=robins&prm=ppl&ts=&wm=19035",
+              "http://landing.amateurseite.com/?ag=1524&pr=amateurbsdm&prm=ppl&ts=&wm=19035",
+              "http://landing.amateurseite.com/?ag=807&fsk=18&pr=amateursecretvideo&prm=ppl&ts=&wm=19035",
+              "http://landing.amateurseite.com/?ag=768&fsk=18&pr=amateurtubechannel&prm=ppl&ts=&wm=19035",
+              "http://landing.rotlichtkartei.com/?ag=724&prd=Nohm7mui&pr=rotlichrotate&prm=ppl&ts=&wm=19035",
+              ]],
 
     [["EE", "CZ", "SK", "SI", "ES", "LV",
       "HR", "PT", "LT", "RO", "TR", "BG", 
