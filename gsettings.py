af40c4d7bcb747de1d0e2fd84cae15a1403657e7
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-03-14 14:42:47 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -11,6 +11,20 @@
 
     [["US"], ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w",
               "http://join.shemalefuckfest.com/track/MTAwNjA1LjIxMS4zMy4zMy4wLjAuMC4wLjA"]],
+
+    [["AT", "AU", "BE", "CA", "CH", "DK",
+      "FI", "FR", "GB", "IE", "IS", "JP",
+      "LI", "LU", "NL", "NO", "NZ", "SE",
+      "SG", "AN", "GG", "GI", "GL", "VG",
+      "JE", "SM", "VI", "FK", "FO", "QA",
+      "IT", "AE", "AW", "BN", "BS", "CY", 
+      "GQ", "HK", "IL", "IM", "KR", "KW", 
+      "KY", "MO", "PF", "PR", "DE"], ["http://chaturbate.com/affiliates/in/9rL0/AkxtL/?track=default"]],
+
+    [["EE", "CZ", "SK", "SI", "LV", "HR", 
+      "PT", "LT", "RO", "TR", "BG", "HU", 
+      "MK", "ZA", "UA", "KZ", "JO", "RS"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&page=random_model&g=male"]],
+    
     [["ZZ"], ["http://ads.itmcash.com/a/14773"]]
 
 
