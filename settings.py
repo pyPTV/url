ab00310aa14787d7203cc57b78543fad74829f44
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-03-17 17:10:56 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,7 @@
 {
     "mobile":
 [
-    [["US"], ["http://enter.javhd.com/track/MjIxNTIuMi4yLjkuMC4wLjAuMC4w",
-              "http://enter.javhd.com/track/MjIxNTIuMi4yLjg1LjAuMC4wLjAuMA",
-              "http://enter.shiofuky.com/track/MjIxNTIuMi4zLjExLjAuMC4wLjAuMA"]], 
+    [["US"], ["http://runetki.com/886392/"]], 
     [["ZZ"], ["http://traffic.trafficcompany.com/?p=2478&media_type=adult&source_type=redirect"]]
 
 ],
@@ -32,6 +30,7 @@
       "PT", "LT", "RO", "TR", "BG", "HU", 
       "MK", "ZA", "UA", "KZ", "JO", "RS"], ["http://bongacams.com/track?c=332288"]],
 
+    [["US"], ["http://runetki.com/886392/"]],
     [["ZZ"], ["http://enter.javhd.com/track/MjIxNTIuMi4yLjIuMC4wLjAuMC4w",
               "http://enter.javhd.com/track/MjIxNTIuMi4yLjI2LjAuMC4wLjAuMA",
               "http://enter.shiofuky.com/track/MjIxNTIuMi4zLjI3LjAuMC4wLjAuMA"]]
