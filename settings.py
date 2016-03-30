30ac6200de3e548e167f2f7b6415f5434a740f4c
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-03-30 10:31:57 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -25,7 +25,6 @@
     [["DE"], ["http://landing.amateurseite.com/?ag=1524&pr=amateurbsdm&prm=ppl&ts=&wm=19035",
               "http://landing.amateurseite.com/?ag=807&fsk=18&pr=amateursecretvideo&prm=ppl&ts=&wm=19035",
               "http://landing.amateurseite.com/?ag=768&fsk=18&pr=amateurtubechannel&prm=ppl&ts=&wm=19035",
-              "http://landing.amateurseite.com/?ag=1527&fsk=18&pr=teen&prm=ppl&ts=&wm=19035",
               "http://landing.amateurseite.com/?ag=1526&pr=milf&prm=ppl&ts=&wm=19035",
               "http://landing.amateurseite.com/?ag=1523&fsk=18&pr=asia&prm=ppl&ts=&wm=19035"]],
 
@@ -34,7 +33,11 @@
       "MK", "ZA", "UA", "KZ", "JO", "RS"], ["http://bongacams.com/track?c=332288"]],
     [["US"], ["http://join.fetishnetwork.com/track/MTAwMDAwMjIzOS40LjEwLjEwLjAuMC4wLjAuMA"]],
     [["RU"], ["http://runetki.com/627737/"]],
-    [["ZZ"], ["http://join.fetishnetwork.com/track/MTAwMDAwMjIzOS40LjEwLjEwLjAuMC4wLjAuMA"]]
+    [["ZZ"], ["http://join.fetishnetwork.com/track/MTAwMDAwMjIzOS40LjEwLjEwLjAuMC4wLjAuMA",
+              "http://join.budapesthardcore.com/track/MTAwNjg0LjIxMS4zMi4zMi4wLjAuMC4wLjA",
+              "http://join.extremebukkake.com/track/MTAwNjg0LjIxMS41LjUuMC4wLjAuMC4w",
+              "http://join.fetish360.com/track/MTAwNjg0LjIxMS4yNy4yNy4wLjAuMC4wLjA",
+              "http://join.gggsexbox.com/track/MTAwNjg0LjIxMS4xOC4xOC4wLjAuMC4wLjA"]]
 
 ]}
 
