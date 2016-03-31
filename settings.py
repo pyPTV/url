7855b12712cc8aaa906fd3ccc2fd123cb2ff7ae2
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-03-31 15:21:36 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,14 +13,10 @@
 [
     
     
-    [["AT", "AU", "BE", "CA", "CH", "DK",
-      "FI", "FR", "GB", "IE", "IS", "JP",
-      "LI", "LU", "NL", "NO", "NZ", "SE",
-      "SG", "AN", "GG", "GI", "GL", "VG",
-      "JE", "SM", "VI", "FK", "FO", "QA",
-      "IT", "AE", "AW", "BN", "BS", "CY", 
-      "GQ", "HK", "IL", "IM", "KR", "KW", 
-      "KY", "MO", "PF", "PR"], ["http://chaturbate.com/affiliates/in/hr8m/AkxtL/?track=default"]],
+    [["NL", "AN", "AT", "AU", "BE", "CA",
+      "CH", "DK", "FI", "FK", "FO", "FR",
+      "GG", "GI", "GL", "IE", "IS", "JE",
+      "LI", "LU", "NO", "NZ", "SE", "SM" ], ["http://chaturbate.com/affiliates/in/hr8m/AkxtL/?track=default"]],
 
     [["DE"], ["http://landing.amateurseite.com/?ag=1524&pr=amateurbsdm&prm=ppl&ts=&wm=19035",
               "http://landing.amateurseite.com/?ag=807&fsk=18&pr=amateursecretvideo&prm=ppl&ts=&wm=19035",
@@ -30,7 +26,7 @@
 
     [["EE", "CZ", "SK", "SI", "LV", "HR", 
       "PT", "LT", "RO", "TR", "BG", "HU", 
-      "MK", "ZA", "UA", "KZ", "JO", "RS"], ["http://bongacams.com/track?c=332288"]],
+      "MK", "ZA", "UA", "KZ", "JO", "RS", "SI"], ["http://bongacams.com/track?c=332288"]],
     [["US"], ["http://join.fetishnetwork.com/track/MTAwMDAwMjIzOS40LjEwLjEwLjAuMC4wLjAuMA"]],
     [["RU"], ["http://runetki.com/627737/"]],
     [["ZZ"], ["http://join.fetishnetwork.com/track/MTAwMDAwMjIzOS40LjEwLjEwLjAuMC4wLjAuMA",
