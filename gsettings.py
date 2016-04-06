bf154528b7d3f4bbb2d7e16892e209eb710c7ad6
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-04-06 10:04:56 +0400

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -2,7 +2,7 @@
     "mobile":
 [
     [["US"], ["http://join.bukkakeboysmobile.com/track/MTAwNjg0LjIxNC42OC42OC4wLjAuMC4wLjA",
-              "http://join.groupsexgays.com/track/MjAwMTM4LjEuNDkuNTAuMC4wLjAuMC4w"]],
+              "http://join.groupsexgays.com/track/MjAwMTM4LjMuNDkuMTc1LjAuMC4wLjAuMA"]],
     [["ZZ"], ["http://traffic.trafficcompany.com/?p=2478&media_type=adult&gay=1&source_type=redirect"]]
 
 ],
@@ -10,24 +10,12 @@
     "desktop":
 [
 
-    [["US"], ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w",
-              "http://join.groupsexgays.com/track/MjAwMTM4LjEuNDkuNTAuMC4wLjAuMC4w"]],
 
-    [["AT", "AU", "BE", "CA", "CH", "DK",
-      "FI", "FR", "GB", "IE", "IS", "JP",
-      "LI", "LU", "NL", "NO", "NZ", "SE",
-      "SG", "AN", "GG", "GI", "GL", "VG",
-      "JE", "SM", "VI", "FK", "FO", "QA",
-      "IT", "AE", "AW", "BN", "BS", "CY", 
-      "GQ", "HK", "IL", "IM", "KR", "KW", 
-      "KY", "MO", "PF", "PR", "DE"], ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w",
-                                      "http://join.groupsexgays.com/track/MjAwMTM4LjEuNDkuNTAuMC4wLjAuMC4w"]],
 
-    [["EE", "CZ", "SK", "SI", "LV", "HR", 
-      "PT", "LT", "RO", "TR", "BG", "HU", 
-      "MK", "ZA", "UA", "KZ", "JO", "RS"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&page=random_model&g=male"]],
+    [["EE", "CZ", "SK", "SI", "LV", "HR", "PT", "LT", "RO", "TR", "BG", "HU", "MK", "ZA", "UA", "KZ", "JO", "RS"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&page=random_model&g=male"]],
     
-    [["ZZ"], ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w"]]
+    [["ZZ"], ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w",
+              "http://join.groupsexgays.com/track/MjAwMTM4LjEuNDkuNTAuMC4wLjAuMC4w"]]
 
 
 ]
