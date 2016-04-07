16b0d7fab3d35634f8e80850db40ac4642320ea2
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-04-07 12:04:45 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -40,7 +40,11 @@
               "http://secure.arabuncovered.com/track/adultkonxxx.1.13.20.0.0.0.0.0"]],
     [["PK"], ["http://secure.fuckmypakistanigf.com/track/adultkonxxx.1.24.55.0.0.0.0.0"]],
     [["ZZ"], ["http://join.hellofbdsm.com/track/MjAwMTM4LjEuNTcuNTguMC4wLjAuMC4w",
-              "http://join.topgrl.com/track/MzAwMTMxOS4xLjQuNC4wLjAuMC4wLjA"]]
+              "http://join.topgrl.com/track/MzAwMTMxOS4xLjQuNC4wLjAuMC4wLjA",
+              "http://secure.cumeatingtraining.com/track/MjAyLjUuOS4xMy4wLjAuMC4wLjA",
+              "http://secure.dirtytalkingblowjobs.com/track/MjAyLjUuNTAuNzAuMC4wLjAuMC4w",
+              "http://secure.dirtytalkingbitches.com/track/MjAyLjUuMTEuMTUuMC4wLjAuMC4w",
+              "http://secure.peggingnewbies.com/track/MjAyLjUuNjIuOTUuMC4wLjAuMC4w"]]
 
 ]}
 
