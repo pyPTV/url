9c9145b3983138017960fbadc089257b1c7030ef
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-04-17 00:35:43 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,8 +1,7 @@
 {
     "mobile":
 [
-    [["US"], ["http://join.bukkakeboysmobile.com/track/MTAwNjA1LjIxNC42OC42OC4wLjAuMC4wLjA",
-              "http://join.groupsexgays.com/track/MjAwMTM4LjMuNDkuMTc1LjAuMC4wLjAuMA"]],
+    [["US"], ["http://join.bukkakeboysmobile.com/track/MTAwNjA1LjIxNC42OC42OC4wLjAuMC4wLjA"]],
     [["ZZ"], ["http://traffic.trafficcompany.com/?p=2478&media_type=adult&gay=1&source_type=redirect"]]
 
 ],
@@ -12,10 +11,8 @@
 
 
 
-    [["US", "GB"], ["http://join.groupsexgays.com/track/MjAwMTM4LjEuNDkuNTAuMC4wLjAuMC4w"]],
-    
-    [["ZZ"], ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w",
-              "http://join.groupsexgays.com/track/MjAwMTM4LjEuNDkuNTAuMC4wLjAuMC4w"]]
+    [["US", "GB"], ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w"]],
+    [["ZZ"], ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w"]]
 
 
 ]
