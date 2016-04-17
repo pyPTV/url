a99d1e674fde0c99c2f80d96e4b502345f65713f
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-04-18 00:34:28 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
 {
     "mobile":
 [
-    [["US"], ["http://secure.dirtytalkingblowjobs.com/track/MjAyLjUuNTAuNzAuMC4wLjAuMC4w"]], 
+    [["US"], ["http://enter.javhd.com/track/MjIxNTIuMi4yLjkuMC4wLjAuMC4w", 
+              "http://enter.shiofuky.com/track/MjIxNTIuMi4zLjExLjAuMC4wLjAuMA"]], 
     [["ZZ"], ["http://traffic.trafficcompany.com/?p=2478&media_type=adult&source_type=redirect"]]
 
 ],
@@ -20,26 +21,12 @@
 
     [["DE"], ["http://www.visit-x.net/?w=21022"]],
 
-    [["EE", "CZ", "SK", "SI", "LV", "HR", 
-      "PT", "LT", "RO", "TR", "BG", "HU", 
-      "MK", "ZA", "UA", "KZ", "RS", "SI"], ["http://bongacams.com/track?c=332288"]],
       
     [["RU"], ["http://runetki.com/627737/"]],
     
-    [["IN"], ["http://secure.babesofindia.com/track/adultkonxxx.1.8.14.0.0.0.0.0", 
-              "http://secure.indiansex247.com/track/adultkonxxx.1.7.13.0.0.0.0.0",
-              "http://secure.indiansexmassage.com/track/adultkonxxx.1.36.45.0.0.0.0.0",
-              "http://secure.indianwhippedass.com/track/adultkonxxx.1.21.44.0.0.0.0.0"]],
-    [["EG", "MA", "DZ", "SD", "SA", "YA",
-      "SY", "TN", "JO", "AE", "LB", "PS",
-      "MR", "OM", "KW", "QA", "KM", "BH"], [
-              "http://secure.araborgasm.com/track/adultkonxxx.1.14.30.0.0.0.0.0",
-              "http://secure.araborgasm.com/track/adultkonxxx.1.14.32.0.0.0.0.0",
-              "http://secure.arabuncovered.com/track/adultkonxxx.1.13.31.0.0.0.0.0",
-              "http://secure.arabuncovered.com/track/adultkonxxx.1.13.20.0.0.0.0.0"]],
-    [["PK"], ["http://secure.fuckmypakistanigf.com/track/adultkonxxx.1.24.55.0.0.0.0.0"]],
     
-    [["ZZ"], [
+    [["ZZ"], ["http://enter.javhd.com/track/MjIxNTIuMi4yLjI2LjAuMC4wLjAuMA",
+              "http://enter.shiofuky.com/track/MjIxNTIuMi4zLjI3LjAuMC4wLjAuMA",
               "http://join.topgrl.com/track/MzAwMTMxOS4xLjQuNC4wLjAuMC4wLjA",
               "http://secure.cumeatingtraining.com/track/MjAyLjUuOS4xMy4wLjAuMC4wLjA",
               "http://secure.dirtytalkingblowjobs.com/track/MjAyLjUuNTAuNzAuMC4wLjAuMC4w",
