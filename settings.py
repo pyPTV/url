2c6c5ed07c7231ef874dea702f95f7c033155578
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-04-26 11:13:56 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -19,9 +19,7 @@
       "CH", "DK", "FI", "FK", "FO", "FR",
       "GG", "GI", "GL", "IE", "IS", "JE",
       "LI", "LU", "NO", "SE", "SM"], ["http://chaturbate.com/affiliates/in/hr8m/AkxtL/?track=default",
-                                      "http://chaturbate.com/affiliates/in/hr8m/AkxtL/?track=default",
-                                      "http://enter.javhd.com/track/MjIxNTIuMi4yLjI2LjAuMC4wLjAuMA",
-                                      "http://enter.shiofuky.com/track/MjIxNTIuMi4zLjI3LjAuMC4wLjAuMA"]],
+                                      "http://enter.javhd.com/track/MjIxNTIuMi4yLjI2LjAuMC4wLjAuMA"]],
       
     [["ES", "PL", "RO", "CZ", "PT", "HU", 
       "BG", "RS", "SK", "HR", "BA", "AL", 
