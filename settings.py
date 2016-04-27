9e1376304e509361ff02897e5d35727fc5ee1fef
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-04-27 10:16:41 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,31 +14,24 @@
     
     
     [["AU", "CA", "NZ", "GB",
-    
       "NL", "AN", "AT", "BE", 
-      "CH", "DK", "FI", "FK", "FO", "FR",
-      "GG", "GI", "GL", "IE", "IS", "JE",
-      "LI", "LU", "NO", "SE", "SM"], ["http://chaturbate.com/affiliates/in/hr8m/AkxtL/?track=default"]],
+      "CH", "DK", "FI", "FK", 
+      "FO", "FR", "GG", "GI", 
+      "GL", "IE", "IS", "JE",
+      "LI", "LU", "NO", "SE", 
+      "SM"], ["http://chaturbate.com/affiliates/in/hr8m/AkxtL/?track=default"]],
       
-    [["ES", "PL", "RO", "CZ", "PT", "HU", 
-      "BG", "RS", "SK", "HR", "BA", "AL", 
-      "MK", "SI", "ME", "MT", "AD", "LT", 
-      "LV", "EE", "MC", 
-
-      "RU", "KZ", "AZ", "BY", "GE", "MD", "AM", "UA",
-      
-      "IL", "IT"], ["http://bongacams.com/track?c=332288",
-                    "http://bongacams.com/track?c=332288",
-                    "http://enter.javhd.com/track/MjIxNTIuMi4yLjI2LjAuMC4wLjAuMA",
-                    "http://enter.shiofuky.com/track/MjIxNTIuMi4zLjI3LjAuMC4wLjAuMA"]],
+    [["ES", "PL", "RO", "CZ", 
+      "PT", "HU", "BG", "RS", 
+      "SK", "HR", "BA", "AL", 
+      "MK", "SI", "ME", "MT", 
+      "AD", "LT", "LV", "EE",
+      "RU", "KZ", "AZ", "BY", 
+      "GE", "MD", "AM", "UA",
+      "IL", "IT"], ["http://bongacams.com/track?c=332288"]],
 
     [["DE"], ["http://www.saboom.com/?ag=755&pr=&prm=ppl&ts=&wm=19035",
-              "http://chaturbate.com/affiliates/in/hr8m/AkxtL/?track=default",
-              
-              ]],
-
-      
-    
+              "http://chaturbate.com/affiliates/in/hr8m/AkxtL/?track=default"]],
     
     
     [["ZZ"], ["http://enter.javhd.com/track/MjIxNTIuMi4yLjI2LjAuMC4wLjAuMA",
