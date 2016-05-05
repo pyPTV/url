28583465b684cb1585cc428d02b25dcdb90227fe
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-05-05 09:46:23 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -22,9 +22,7 @@
       "SM"], ["http://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],
       
 
-    [["DE"], ["http://www.saboom.com/?ag=754&pr=cam&prm=ppl&ts=&wm=19035",
-              "http://www.saboom.com/?ag=758&pr=static&prm=ppl&ts=&wm=19035",
-              "http://thumbcon.mydirtyhobby.com/?queueId=40041&customImageSize=1&itemCount=36&shuffleResults=0&cacheBuster=1446200721&naff=N3Z-AbsU"]],
+    [["DE"], ["http://thumbcon.mydirtyhobby.com/?queueId=40041&customImageSize=1&itemCount=36&shuffleResults=0&cacheBuster=1446200721&naff=N3Z-AbsU"]],
     
     
     [["US"], ["http://join.hellofbdsm.com/track/adultkonxxx.1.57.58.0.0.0.0.0"]],
