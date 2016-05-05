981000c73ec35d0b6b738c03388f17e80e584087
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-05-05 11:28:44 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -29,6 +29,7 @@
       
 
     [["DE"], ["http://www.saboom.com/?ag=758&pr=&prm=rev&ts=&wm=19035",
+              "http://in.mydirtyhobby.com/track/N3Z-AbsU/topamateurs",
               "http://thumbcon.mydirtyhobby.com/?queueId=40041&customImageSize=1&itemCount=36&shuffleResults=0&cacheBuster=1446200721&naff=N3Z-AbsU"]],
     
     
