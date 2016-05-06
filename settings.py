545ac65b1433bc6206013a8d07755b1ebb860a68
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-05-06 09:32:40 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,9 +2,7 @@
     "mobile":
 [
     [["US"], ["http://join.hellofbdsm.com/track/adultkonxxx.3.57.183.0.0.0.0.0"]], 
-    [["ZZ"], ["http://t0pm0b1l3.com/?id=170",
-              "http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect",
-              "http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]]
+    [["ZZ"], ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]]
 
 ],
  
@@ -21,8 +19,7 @@
       "FO", "FR", "GG", "GI", 
       "GL", "IE", "IS", "JE",
       "LI", "LU", "NO", "SE", 
-      "SM"], ["http://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz",
-              "http://join.hellofbdsm.com/track/adultkonxxx.1.57.58.0.0.0.0.0"]],
+      "SM"], ["http://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],
       
 
     [["DE"], ["http://join.hellofbdsm.com/track/adultkonxxx.1.57.58.0.0.0.0.0",
