d08b7cdd28f9b553579377467754089cc5cf5705
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-05-08 11:38:17 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 {
     "mobile":
 [
-    [["US"], ["http://join.hellofbdsm.com/track/adultkonxxx.3.57.183.0.0.0.0.0"]], 
+    [["US"], ["http://join.allpornsitespass.com/track/MjAwMjAwLjMuNC4xMjkuMC4wLjAuMC4w"]], 
     [["ZZ"], ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]]
 
 ],
@@ -22,9 +22,11 @@
       ], ["http://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],
       
     [["DE"], ["http://in.mydirtyhobby.com/track/N3Z-AbsU/topamateurs"]],
+    
     [["ZZ"], ["http://enter.javhd.com/track/MjIwOTIuMi4yLjI2LjAuMC4wLjAuMA",
               "http://join.fetishnetwork.com/track/MTAwMDAwMjMxNy40LjEwLjEwLjAuMC4wLjAuMA",
-              "http://join.hellofbdsm.com/track/adultkonxxx.1.57.58.0.0.0.0.0"]]
+              "http://join.allpornsitespass.com/track/MjAwMjAwLjEuNC41LjAuMC4wLjAuMA",
+              "http://join.hellofbdsm.com/track/MjAwMjAwLjEuNTcuNTguMC4wLjAuMC4w"]]
 
 ]}
 
