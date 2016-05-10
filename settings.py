d483155a7516394fdca65f50edf374fcb6d4b8d4
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-05-10 12:36:32 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -23,7 +23,8 @@
       
     [["DE", "AT"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=tube&prm=ppl&ts=&wm=19035",
                     "http://landing.amateurseite.com/?ag=1526&pr=milf&prm=ppl&ts=&wm=19035",
-                    "http://landing.amateurseite.com/?ag=819&prd=10&pr=secret&prm=ppl&ts=&wm=19035"]],
+                    "http://landing.amateurseite.com/?ag=819&prd=10&pr=secret&prm=ppl&ts=&wm=19035",
+                    "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=1697913&ts=deeplink&wm=19035&pr=&prm=ppl&tsd=atcrm113&fsk=18"]],
     
     [["ZZ"], ["http://join.fetishnetwork.com/track/MTAwMDAwMjMxNy40LjEwLjEwLjAuMC4wLjAuMA",
               "http://join.hellofbdsm.com/track/MjAwMjAwLjEuNTcuNTguMC4wLjAuMC4w"]]
