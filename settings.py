873941f454f3298439234fd0d68f9564f31a093a
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-05-17 22:42:57 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -19,7 +19,8 @@
       "FO", "FR", "GG", "GI", 
       "GL", "IE", "IS", "JE",
       "LI", "LU", "NO", "SE"
-      ], ["http://chaturbate.com/affiliates/in/?track=default&tour=ZmU7&campaign=AQTqz"]],
+      ], ["http://chaturbate.com/affiliates/in/?track=default&tour=ZmU7&campaign=AQTqz",
+          "http://join.hellofbdsm.com/track/MjAwMjAwLjEuNTcuNTguMC4wLjAuMC4w"]],
       
     [["DE", "AT"], ["http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2348762&ts=deeplink&wm=19035&pr=angelika&prm=ppl&tsd=atcrm113&fsk=18",
                     "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2364468&ts=deeplink&wm=19035&pr=alisha&prm=ppl&tsd=atcrm113&fsk=18",
