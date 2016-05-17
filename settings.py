52c0930cf19749cf3d51911f4dff01daba0f7fde
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-05-17 23:03:31 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -20,7 +20,8 @@
       "GL", "IE", "IS", "JE",
       "LI", "LU", "NO", "SE"
       ], ["http://chaturbate.com/affiliates/in/?track=default&tour=ZmU7&campaign=AQTqz",
-          "http://join.hellofbdsm.com/track/MjAwMjAwLjEuNTcuNTguMC4wLjAuMC4w"]],
+          "http://join.hellofbdsm.com/track/MjAwMjAwLjEuNTcuNTguMC4wLjAuMC4w",
+          "http://join.allpornsitespass.com/track/MjAwMjAwLjEuNC41LjAuMC4wLjAuMA"]],
       
     [["DE", "AT"], ["http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2348762&ts=deeplink&wm=19035&pr=angelika&prm=ppl&tsd=atcrm113&fsk=18",
                     "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2364468&ts=deeplink&wm=19035&pr=alisha&prm=ppl&tsd=atcrm113&fsk=18",
@@ -29,8 +30,7 @@
                     "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2374159&ts=deeplink&wm=19035&pr=sasha&prm=ppl&tsd=atcrm113&fsk=18"
                     ]],
     
-    [["ZZ"], ["http://join.allpornsitespass.com/track/MjAwMjAwLjEuNC41LjAuMC4wLjAuMA",
-              "http://join.hellofbdsm.com/track/MjAwMjAwLjEuNTcuNTguMC4wLjAuMC4w"]]
+    [["ZZ"], ["http://join.allpornsitespass.com/track/MjAwMjAwLjEuNC41LjAuMC4wLjAuMA"]]
 
 ]}
 
