e97283d33e6acab7c0c98bc4786225a774f45c13
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-05-17 22:34:49 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -19,14 +19,9 @@
       "FO", "FR", "GG", "GI", 
       "GL", "IE", "IS", "JE",
       "LI", "LU", "NO", "SE"
-      ], ["http://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz",
-          "http://join.allpornsitespass.com/track/MjAwMjAwLjEuNC41LjAuMC4wLjAuMA",
-          "http://join.hellofbdsm.com/track/MjAwMjAwLjEuNTcuNTguMC4wLjAuMC4w"]],
+      ], ["http://chaturbate.com/affiliates/in/?track=default&tour=ZmU7&campaign=AQTqz"]],
       
-    [["DE", "AT"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=tube&prm=ppl&ts=&wm=19035",
-                    "http://landing.amateurseite.com/?ag=1526&pr=milf&prm=ppl&ts=&wm=19035",
-                    "http://landing.amateurseite.com/?ag=819&prd=10&pr=secret&prm=ppl&ts=&wm=19035",
-                    "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2348762&ts=deeplink&wm=19035&pr=angelika&prm=ppl&tsd=atcrm113&fsk=18",
+    [["DE", "AT"], ["http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2348762&ts=deeplink&wm=19035&pr=angelika&prm=ppl&tsd=atcrm113&fsk=18",
                     "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2364468&ts=deeplink&wm=19035&pr=alisha&prm=ppl&tsd=atcrm113&fsk=18",
                     "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2348762&ts=deeplink&wm=19035&pr=sophia&prm=ppl&tsd=atcrm113&fsk=18",
                     "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=1697913&ts=deeplink&wm=19035&pr=machine&prm=ppl&tsd=atcrm113&fsk=18",
