b38ad3823160ebe31e49c7d0ae771d96690f5cda
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-05-19 19:55:12 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -25,11 +25,12 @@
 [
     
     
-    [["DE", "CH"], ["http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2348762&ts=deeplink&wm=19035&pr=angelika&prm=ppl&tsd=atcrm113&fsk=18",
-                    "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2364468&ts=deeplink&wm=19035&pr=alisha&prm=ppl&tsd=atcrm113&fsk=18",
-                    "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2348762&ts=deeplink&wm=19035&pr=sophia&prm=ppl&tsd=atcrm113&fsk=18",
-                    "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=1697913&ts=deeplink&wm=19035&pr=machine&prm=ppl&tsd=atcrm113&fsk=18",
-                    "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2374159&ts=deeplink&wm=19035&pr=sasha&prm=ppl&tsd=atcrm113&fsk=18"
+    [["DE"], ["http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2348762&ts=deeplink&wm=19035&pr=angelika&prm=ppl&tsd=atcrm113&fsk=18",
+              "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2364468&ts=deeplink&wm=19035&pr=alisha&prm=ppl&tsd=atcrm113&fsk=18",
+              "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2348762&ts=deeplink&wm=19035&pr=sophia&prm=ppl&tsd=atcrm113&fsk=18",
+              "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=1697913&ts=deeplink&wm=19035&pr=machine&prm=ppl&tsd=atcrm113&fsk=18",
+              "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2374159&ts=deeplink&wm=19035&pr=sasha&prm=ppl&tsd=atcrm113&fsk=18",
+              "http://thumbcon.mydirtyhobby.com/?queueId=40041&customImageSize=1&itemCount=36&shuffleResults=0&cacheBuster=1446200721&naff=N3Z-AbsU"
                     ]],
     
     [["RU"], ["http://runetki.com/627737/"]],
