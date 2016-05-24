9529a2addd5ab8d08cfe7d7eecfa4c183a67dca8
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-05-24 22:59:32 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -28,15 +28,12 @@
               "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2374159&ts=deeplink&wm=19035&pr=sasha&prm=ppl&tsd=atcrm113&fsk=18"
              ]],
     
-    [["RU", "UA", "KZ", "BY", "AM", "AZ"], ["http://runetki.com/627737/"]],
+    [["RU", "UA", "KZ", "BY", "AM", "AZ", "EE", "LT", "LV", "CZ", "SI", "SK"], ["http://bongacams.com/track?c=332288"]],
     
     [["JP"], ["http://enter.javhd.com/track/MjIwOTIuMi4yLjk5LjAuMC4wLjAuMA"]],
-    
     [["CN"], ["http://enter.javhd.com/track/MjIwOTIuMi4yLjY4LjAuMC4wLjAuMA"]],
-    
     [["ID", "MY", "TH", "SG", "KR", "PH"], ["http://enter.javhd.com/track/MjIwOTIuMi4yLjg3LjAuMC4wLjAuMA"]],
     [["BA", "BG", "HR", "GR", "HU", "MK", "PL", "RO", "RS"], ["http://ads.itmcash.com/a/14820"]],
-     
     [["IN", "PK", "PH", "EG", "MA", "IR"], ["http://tracking.payoneer.com/SH9T"]],
 
        
