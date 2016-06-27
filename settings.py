036fa712565b4a6183ecbc855aa415ba5b1f706b
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-06-27 17:32:04 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -40,8 +40,8 @@
       
     [["IN", "PK", "PH", "EG", "MA", "IR"], ["http://tracking.payoneer.com/SH9T"]],
     
-    [["US", "MX", "AR", "ES", "GB"], ["http://join.allpornsitespass.com/track/MjAwMjYyLjEuNC41LjAuMC4wLjAuMA",
-                                      "http://join.hellofbdsm.com/track/MjAwMjYyLjEuNTcuNTguMC4wLjAuMC4w"]],
+    [["US", "GB"], ["http://join.allpornsitespass.com/track/MjAwMjYyLjEuNC41LjAuMC4wLjAuMA",
+                    "http://join.hellofbdsm.com/track/MjAwMjYyLjEuNTcuNTguMC4wLjAuMC4w"]],
     [["ID", "TH", "MY", "SG", "KR"], ["http://enter.javhd.com/track/MjIwOTIuMi4yLjEyMC4wLjAuMC4wLjA"]],
     [["JP"], ["http://enter.javhd.com/track/MjIwOTIuMi4yLjExOS4wLjAuMC4wLjA"]],
     [["CN"], ["http://enter.javhd.com/track/MjIwOTIuMi4yLjEwOC4wLjAuMC4wLjA"]],
