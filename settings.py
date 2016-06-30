498a0f28d68fd4828f62177bfd534a76bccbcd45
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-06-30 10:05:54 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -19,16 +19,20 @@
   "desktop":
 [
     
-    [["NL", "AN", "AT", "AU", "BE", "CA",
+    [["NL", "AN", "AU", "BE", "CA", "IT",
       "CH", "DK", "FI", "FK", "FO", "FR",
       "GG", "GI", "GL", "IE", "IS", "JE",
-      "LI", "LU", "NO", "NZ", "SE", "SM"], ["http://join.allpornsitespass.com/track/MjAwMjYyLjEuNC41LjAuMC4wLjAuMA"]],
+      "LI", "LU", "NO", "NZ", "SE", "SM"], ["http://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],
     
-    [["DE", "AT"], ["http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2348762&ts=deeplink&wm=19035&pr=angelika&prm=ppl&tsd=atcrm113&fsk=18",
-                    "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2364468&ts=deeplink&wm=19035&pr=alisha&prm=ppl&tsd=atcrm113&fsk=18",
-                    "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=1697913&ts=deeplink&wm=19035&pr=machine&prm=ppl&tsd=atcrm113&fsk=18",
-                    "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2374159&ts=deeplink&wm=19035&pr=sasha&prm=ppl&tsd=atcrm113&fsk=18"
-                   ]],
+    [["DE"], ["http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2348762&ts=deeplink&wm=19035&pr=angelika&prm=ppl&tsd=atcrm113&fsk=18",
+              "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2364468&ts=deeplink&wm=19035&pr=alisha&prm=ppl&tsd=atcrm113&fsk=18",
+              "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=1697913&ts=deeplink&wm=19035&pr=machine&prm=ppl&tsd=atcrm113&fsk=18",
+              "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2374159&ts=deeplink&wm=19035&pr=sasha&prm=ppl&tsd=atcrm113&fsk=18"
+              ]],
+
+    [["AT"], ["http://in.mydirtyhobby.com/track/N3Z-AbsU/topamateurs",
+              "http://thumbcon.mydirtyhobby.com/?queueId=40041&customImageSize=1&itemCount=36&shuffleResults=0&cacheBuster=1446200721&naff=N3Z-AbsU"
+              ]],
     
     [["EE", "LT", "LV", "CZ", "SI", "SK", 
       "TR", "AM", "AZ", "BY", "KG", "KZ", 
