96e496187c852ef63f324fba3f958c82ee3093f9
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-07-18 19:11:33 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -11,8 +11,8 @@
 
 
 
-    [["US"], ["http://join.allgaysitespass.com/track/MjAwMjk4LjEuMy40LjAuMC4wLjAuMA"]],
-    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwMjk4LjEuMy40LjAuMC4wLjAuMA"]]
+    [["US"], ["http://1empiredirect.com/redirect?aff_id=1621&at=2"]],
+    [["ZZ"], ["http://1empiredirect.com/redirect?aff_id=1621&at=2"]]
 
 
 ]
