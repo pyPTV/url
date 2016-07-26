80991b7efc6e4f880e032ef47498d1a731155486
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-07-26 22:44:53 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -18,15 +18,15 @@
   "desktop":
 [
     
-    [["NL", "AN", "BE", "IT", "DK", 
-      "FI", "FK", "FO", "FR", "GG", "GI", 
-      "GL", "IE", "IS", "JE", "LI", "LU", 
-      "NO", "SE", "SM"], ["http://chaturbate.com/affiliates/in/?track=default&tour=ZmU7&campaign=AQTqz"]],
+    [["NL", "AN", "BE", "IT", "DK", "FI", 
+      "FK", "FO", "FR", "GG", "GI", "GL", 
+      "IE", "IS", "JE", "LI", "LU", "NO", 
+      "SE", "SM"], ["http://bongacams.com/track?c=332288"]],
     
     [["DE"],      ["http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2348762&ts=deeplink&wm=19035&pr=angelika&prm=ppl&tsd=atcrm113&fsk=18",
-                    "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2364468&ts=deeplink&wm=19035&pr=alisha&prm=ppl&tsd=atcrm113&fsk=18",
-                    "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=1697913&ts=deeplink&wm=19035&pr=machine&prm=ppl&tsd=atcrm113&fsk=18",
-                    "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2374159&ts=deeplink&wm=19035&pr=sasha&prm=ppl&tsd=atcrm113&fsk=18"
+                   "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2364468&ts=deeplink&wm=19035&pr=alisha&prm=ppl&tsd=atcrm113&fsk=18",
+                   "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=1697913&ts=deeplink&wm=19035&pr=machine&prm=ppl&tsd=atcrm113&fsk=18",
+                   "http://www.amateurseite.com/index.php?main=userprofile&page=profile&user_id=2374159&ts=deeplink&wm=19035&pr=sasha&prm=ppl&tsd=atcrm113&fsk=18"
                    ]],
 
     [["AT"], ["http://in.mydirtyhobby.com/track/N3Z-AbsU/topamateurs",
