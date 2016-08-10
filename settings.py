c9701b73642ff0b5e7d63f804821037f92eb334e
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-08-10 10:59:39 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -45,7 +45,7 @@
 
     
     
-    [["US", "GB", "CA", "AU", "NZ", "CH"], ["http://enter.javhd.com/track/MjI1NjguMi4yLjEyOS4wLjAuMC4wLjA"]],
+    [["US", "GB", "CA", "AU", "NZ", "CH"], ["http://join.allpornsitespass.com/track/MjAwMzUwLjEuNC41LjAuMC4wLjAuMA"]],
     [["ZZ"], ["http://join.sislovesme.com/track/MjM4MjIuNy43NC4xNjMyLjAuMC4wLjAuMA"]]
    
    
