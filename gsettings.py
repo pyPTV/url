f6879858b42e1f389c58809f5d8a510c4b7279bf
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-10-03 07:52:59 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -14,7 +14,7 @@
       "FI", "FK", "FO", "FR", "GG", "GI", 
       "GL", "IE", "IS", "JE", "LI", "LU", 
       "NO", "SE", "SM", "US", "GB", "CA", 
-      "AU", "NZ", "CH"], ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w"]],
+      "AU", "NZ", "CH"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]],
       
       
     [["ZZ"], ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w"]]
