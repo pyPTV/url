0a0d6d3b935cc27f27babbf4dbc02fa16a2b4a58
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-10-17 22:48:47 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@
     [["DE", "IN", "IT", "GB", "FR", "CH", 
       "AU", "NL", "AT", "MX", "BR", "ES", 
       "GR", "PT", "JP", "BE", "SE", "AR", 
-      "DK", "NO", "TH", "TR"], ["http://delivery.brokerbabe.com/16223", "http://t0pm0b1l3.com/?id=170"]],
+      "DK", "NO", "TH", "TR"], ["http://t0pm0b1l3.com/?id=170"]],
       
     [["ZZ"], ["http://t0pm0b1l3.com/?id=170"]]
 
