eeb8ee7abc3355830fb85f71f02a729d78f73856
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-12-19 15:32:50 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -12,7 +12,7 @@
 
       
     [["US", "CA", "GB", "AU"], ["http://join.allgaysitespass.com/track/MjAwMzc0LjEuMy40LjAuMC4wLjAuMA"]],  
-    [["ZZ"], ["http://t.frtyi.com/aff_c?offer_id=3006&aff_id=40075&bo=2779,2777,2776,2775"]]
+    [["ZZ"], ["http://t.frtyi.com/aff_c?offer_id=171&aff_id=40075&bo=2772,2771,2770,2769,2768"]]
 
 
 ]
