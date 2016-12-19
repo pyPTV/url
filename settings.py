fb0560fbf9fc9883c27f6c22a1ecf582621f9a46
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-12-19 06:07:50 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -42,7 +42,9 @@
     [["ID", "TH", "MY", "CN", "VN"], ["http://enter.javhd.com/track/MjI1NjguMi4yLjEyOS4wLjAuMC4wLjA"]],
 
     
-    [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=tubechannel&prm=ppl&ts=&wm=19035"]],   
+    [["DE"], ["http://landing.amateurseite.com/?ag=768&fsk=18&pr=tubechannel&prm=ppl&ts=&wm=19035",
+              "http://landing.amateurseite.com/?ag=1526&pr=milf&prm=ppl&ts=&wm=19035",
+              "http://landing.amateurseite.com/?ag=1524&pr=bdsm&prm=ppl&ts=&wm=19035"]],   
     
 
     [["ZZ"], ["http://t.frtyi.com/aff_c?offer_id=765&aff_id=40075&bo=2779,2777,2776,2775"]]
