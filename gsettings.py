67aaa214a1447688afc94377af3da22745daa42b
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2016-12-24 15:44:44 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,7 +1,7 @@
 {
     "mobile":
 [
-    [["US", "CA", "GB", "AU"], ["http://t.frtyi.com/aff_c?offer_id=361&aff_id=40075&bo=2772,2771,2770,2769,2768"]],
+    [["US", "CA", "GB", "AU"], ["http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA"]],
     [["ZZ"], ["http://t.frtyi.com/aff_c?offer_id=361&aff_id=40075&bo=2772,2771,2770,2769,2768"]]
 
 ],
