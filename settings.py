642325d362715878ebb06c4e5f271075685fff41
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-01-03 10:48:14 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@
 
   "desktop":
 [
-    [["US", "GB", "CA"], ["http://porn.com"]],    
+    [["US", "GB", "CA"], ["http://join.allpornsitespass.com/track/MjAwNDQyLjEuNC41LjAuMC4wLjAuMA"]],    
     
     [["AM", "AZ", "KG", "TJ", "GE", "UZ", "TM"], ["http://bongacams.com/track?c=332288"]],    
     [["CZ", "EE", "IL", "SK", "RU", "SI", "PL", 
