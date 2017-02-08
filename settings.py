082669c04c9f678a7dffd009146417a03af7ab8a
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-02-08 16:50:44 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -50,15 +50,14 @@
               "http://landing.amateurseite.com/?ag=1524&pr=bdsm&prm=ppl&ts=&wm=19035"]],   
     
 
-    [["CA"], ["http://t.frtyq.com/slmqnevibk?offer_id=765&aff_id=43372&aff_sub=MyFreeCams&nopop=1&bo=2779,2777,2778,2776,2775"]],
-    [["NZ"], ["http://t.frtyq.com/slmqnevibk?offer_id=765&aff_id=43372&aff_sub=MyFreeCams&nopop=1&bo=2779,2777,2778,2776,2775"]],
-    [["AU"], ["http://t.frtyq.com/slmqnevibk?offer_id=765&aff_id=43372&aff_sub=MyFreeCams&nopop=1&bo=2779,2777,2778,2776,2775"]],
+    [["AU", "CA", "NZ", "SG"], ["http://t.frtyn.com/hi0p94ea68?offer_id=223&aff_id=43372&aff_sub=exposed&nopop=1&bo=2779,2777,2778,2776,2775"]],
+    
     [["NL"], ["http://t.frtyh.com/1c4nw7xn28?offer_id=347&aff_id=43372&aff_sub=XClub&nopop=1&bo=2753,2754,2755,2756F"]],
-    [["IT"], ["http://t.frtyh.com/e544544m9s?offer_id=3404&aff_id=43372&nopop=1&bo=2753,2754,2755,2756"]],
+    [["IT"], ["http://t.frtyh.com/q1jxiuh7b4?offer_id=3404&aff_id=43372&aff_sub=imnaughty&nopop=1&bo=2753,2754,2755,2756"]],
     [["CH", "AT"], ["http://t.frtyi.com/g5wtkc8ao0?offer_id=645&aff_id=43372&aff_sub=VisitX&nopop=1&bo=2779,2777,2778,2776,2775"]],
         
     
-    [["ZZ"], ["http://t.frtyq.com/slmqnevibk?offer_id=765&aff_id=43372&aff_sub=MyFreeCams&nopop=1&bo=2779,2777,2778,2776,2775"]]
+    [["ZZ"], ["http://t.frtyn.com/hi0p94ea68?offer_id=223&aff_id=43372&aff_sub=exposed&nopop=1&bo=2779,2777,2778,2776,2775"]]
    
    
    
