b594d0ff9005e5265218a34e332a38728d2756d2
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-03-02 00:40:28 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,8 +1,8 @@
 {
     "mobile":
 [
-    [["US", "CA", "GB", "AU", "NZ"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]],
-    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]]
+    [["US", "CA", "GB", "AU", "NZ"], ["http://join.allgaysitespass.com/track/MjAwNTgyLjEuMy40LjAuMC4wLjAuMA"]],
+    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNTgyLjEuMy40LjAuMC4wLjAuMA"]]
                
 ],
  
@@ -11,8 +11,8 @@
 
 
       
-    [["US", "GB"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]],  
-    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]]
+    [["US", "GB"], ["http://join.allgaysitespass.com/track/MjAwNTgyLjEuMy40LjAuMC4wLjAuMA"]],  
+    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNTgyLjEuMy40LjAuMC4wLjAuMA"]]
 
 
 ]
