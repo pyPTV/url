26e5e040e7febd10a74856ecd3099d06adb0f3d1
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-03-18 07:03:03 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,8 +1,8 @@
 {
     "mobile":
 [
-    [["US", "CA", "GB", "AU", "NZ"], ["http://join.bukkakeboysmobile.com/track/MTAwNjA1LjIxNC42OC42OC4wLjAuMC4wLjA"]],
-    [["ZZ"],                         ["http://join.bukkakeboysmobile.com/track/MTAwNjA1LjIxNC42OC42OC4wLjAuMC4wLjA"]]
+    [["US"], ["http://join.allgaysitespass.com/track/MjAwMzUwLjEuMy40LjAuMC4wLjAuMA"]],
+    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwMzUwLjEuMy40LjAuMC4wLjAuMA"]]
                
 ],
  
@@ -11,8 +11,8 @@
 
 
       
-    [["US", "GB"], ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w"]],  
-    [["ZZ"],       ["http://join.bukkakeboys.com/track/MTAwNjA1LjIxMS42Mi42MTIuMC4wLjAuMC4w"]]
+    [["US"], ["http://join.allgaysitespass.com/track/MjAwMzUwLjEuMy40LjAuMC4wLjAuMA"]],  
+    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwMzUwLjEuMy40LjAuMC4wLjAuMA"]]
 
 
 ]
