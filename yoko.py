52911134dea72297ad2250f9b4f55657ef829e95
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-03-24 13:39:59 +0400

    Create yoko.py

---
+++ yoko.py
@@ -0,0 +1,24 @@
+
+
+{
+    "mobile":
+[
+    [["US"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]],
+    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]]
+               
+],
+ 
+    "desktop":
+[
+
+
+      
+    [["US"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]],  
+    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]]
+
+
+]
+}
+
+
+
