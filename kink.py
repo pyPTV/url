d5f8a4a3f0c4fd30126769c82149f706304fa4fd
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-03-24 13:39:27 +0400

    Update kink.py

--- kink.py
+++ kink.py
@@ -1,1 +1,21 @@
 
+{
+    "mobile":
+[
+    [["US"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]],
+    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]]
+               
+],
+ 
+    "desktop":
+[
+
+
+      
+    [["US"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]],  
+    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]]
+
+
+]
+}
+
