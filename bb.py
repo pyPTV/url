e395ff7cb9aed3808ed55b0ffd49e204ff6c9189
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-03-24 16:49:00 +0400

    Create bb.py

---
+++ bb.py
@@ -0,0 +1,22 @@
+
+{
+    "mobile":
+[
+    [["US"], ["http://join.allpornsitespass.com/track/MjAwNDQyLjEuNC41LjAuMC4wLjAuMA"]],
+    [["ZZ"], ["http://join.allpornsitespass.com/track/MjAwNDQyLjEuNC41LjAuMC4wLjAuMA"]]
+               
+],
+ 
+    "desktop":
+[
+
+
+      
+    [["US"], ["http://join.allpornsitespass.com/track/MjAwNDQyLjEuNC41LjAuMC4wLjAuMA"]],  
+    [["ZZ"], ["http://join.allpornsitespass.com/track/MjAwNDQyLjEuNC41LjAuMC4wLjAuMA"]]
+
+
+]
+}
+
+
