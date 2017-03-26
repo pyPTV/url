fb54991051189832c7cf820ff2f34cf235da1e87
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-03-26 11:15:05 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -18,43 +18,24 @@
   "desktop":
 [
 
-    [["US", "GB", "AU", "FR",
-      "CA", "NZ", "BE", "IT",
-      "NL", "CH", "AT", "IE",
-      "NO", "SE", "DK", "NZ",
-      "FI", "IS", "LI", "LU", "DE"], ["http://join.allpornsitespass.com/track/MjAwNDQyLjEuNC41LjAuMC4wLjAuMA"]],
-    
-    [["JP"], ["http://enter.javhd.com/track/MjI1NjguMi4yLjIuMC4wLjAuMC4w/ja"]], 
-    [["SG", "TW"], ["http://enter.javhd.com/track/MjI1NjguMi4yLjg0LjAuMC4wLjAuMA"]],
-    
+    [["US", "GB", "AU", "FR", "CA", "NZ", 
+      "BE", "IT", "NL", "CH", "AT", "IE", 
+      "NO", "SE", "DK", "NZ", "FI", "IS", 
+      "LI", "LU", "DE"], ["http://join.allpornsitespass.com/track/MjAwNDQyLjEuNC41LjAuMC4wLjAuMA"]],
 
-    
-    
 
-    [["CZ", 
-      "IL",
-      "SK",
-      "RU", 
-      "SI", "PL", "LV", 
-      "GR", 
-      "LT", "AE",
-      "EE", "EG", "RO", "HU", "TR", "CY", "BG", 
-      "HR", "PT", "MK", "KZ", "BA", "JO", "RS", 
+    [["CZ", "IL", "SK", "RU", "SI", "PL", 
+      "LV", "GR", "LT", "AE", "EE", "EG", 
+      "RO", "HU", "TR", "CY", "BG", "HR", 
+      "PT", "MK", "KZ", "BA", "JO", "RS", 
       "AR", "UA", "BY", "MD", "BA", "AM", 
-      "AZ", "KG", "TJ", "GE", "UZ", "TM"], ["http://bongacams.com/track?c=332289"]],    
-    
-     
+      "AZ", "KG", "TJ", "GE", "UZ", "TM"], ["http://bongacams.com/track?c=332289"]], 
    
 
-    [["PR", "AN", "FK", "FO", "GG", "GI", "GL", 
-      "JE", "MX", "NZ", "HT", "JM", "TT", "DM", 
-      "DO", "LC", "CW", "AG", "BB", "VC", "VG", 
-      "VI", "GD", "KY", "KN", "AW", "AI", "MS", 
-      "MF", "BL", "BS", "QA", "MV", "MT", "MH", 
-      "NR", "SC", "SB", "FJ", "PF", "BH", "HN",
-      "DJ", "KW", "OM", "BM", "MO", "AD", "BN", 
-      "KR"], ["http://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]], 
-    [["ZZ"], ["http://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]]
+    [["JP"], ["http://enter.javhd.com/track/MjI1NjguMi4yLjIuMC4wLjAuMC4w/ja"]], 
+    [["SG"], ["http://enter.javhd.com/track/MjI1NjguMi4yLjg0LjAuMC4wLjAuMA"]],
+    [["TW"], ["http://enter.javhd.com/track/MjI1NjguMi4yLjg0LjAuMC4wLjAuMA"]],
+    [["ZZ"], ["http://join.fetishnetwork.com/track/MTAwMDAwMjkxOC40LjEwLjEwLjAuMC4wLjAuMA"]]
    
    
    
