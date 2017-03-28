d30fcab6f4c22a481a395cca1ce3540af4e30eea
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-03-28 20:11:40 +0300

    Update bb.py

--- bb.py
+++ bb.py
@@ -13,7 +13,7 @@
 
       
     [["US"], ["http://join.fetishnetwork.com/track/MTAwMDAwMjkxOC40LjEwLjEwLjAuMC4wLjAuMA"]],  
-    [["ZZ"], ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]]
+    [["ZZ"], ["http://join.fetishnetwork.com/track/MTAwMDAwMjkxOC40LjEwLjEwLjAuMC4wLjAuMA"]]
 
 
 ]
