de06677b04e10ea0e021b884b6ace9af6636af5a
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-03-28 18:53:53 +0300

    Update yoko.py

--- yoko.py
+++ yoko.py
@@ -4,7 +4,7 @@
     "mobile":
 [
     [["US"], ["http://enter.javhd.com/track/MjI1NjguMi4yLjg0LjAuMC4wLjAuMA"]],
-    [["ZZ"], ["http://enter.javhd.com/track/MjI1NjguMi4yLjg0LjAuMC4wLjAuMA"]]
+    [["ZZ"], ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]]
                
 ],
  
