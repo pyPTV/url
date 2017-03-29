98e2f469bc4542dea3c5a990acb11c789ff6e381
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-03-29 07:40:33 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -2,7 +2,7 @@
     "mobile":
 [
     [["US"], ["http://join.allgaysitespass.com/track/MjAwMjk4LjEuMy40LjAuMC4wLjAuMA"]],
-    [["ZZ"], ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&pi=gay&gay=1&source_type=redirect"]]
+    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwMjk4LjEuMy40LjAuMC4wLjAuMA"]]
                
 ],
  
