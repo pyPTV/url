60b938070ccd76389c2bd2cf528f4db5ac56ff4d
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-03-29 07:41:17 +0300

    Update kink.py

--- kink.py
+++ kink.py
@@ -3,7 +3,7 @@
     "mobile":
 [
     [["US"], ["http://join.fetishnetwork.com/track/MTAwMDAwMjkxOC40LjEwLjEwLjAuMC4wLjAuMA"]],
-    [["ZZ"], ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]]
+    [["ZZ"], ["http://join.fetishnetwork.com/track/MTAwMDAwMjkxOC40LjEwLjEwLjAuMC4wLjAuMA"]]
                
 ],
  
