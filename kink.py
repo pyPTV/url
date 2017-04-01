bcddd6a5bf2b11c0cfe8c7fb06efd2e482437a47
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-04-01 03:45:12 -0400

    Update kink.py

--- kink.py
+++ kink.py
@@ -3,7 +3,7 @@
     "mobile":
 [
     [["US", "GB", "CA", "AU", "FR", 
-      "DE", "NO", "SE", "DK", "CH"], ["http://join.allpornsitespass.com/track/MjAwNjg0LjEuNC41LjAuMC4wLjAuMA"]],
+      "DE", "NO", "SE", "DK", "CH"], ["http://join.allpornsitespass.com/track/MjAwNTgyLjEuNC41LjAuMC4wLjAuMA"]],
     
     [["ZZ"], ["http://t.irtya.com/wzbm9r4u8?offer_id=3788&aff_id=40075"]]
                
@@ -15,7 +15,7 @@
 
       
     [["US", "GB", "CA", "AU", "FR", 
-      "DE", "NO", "SE", "DK", "CH"], ["http://join.allpornsitespass.com/track/MjAwNjg0LjEuNC41LjAuMC4wLjAuMA"]],
+      "DE", "NO", "SE", "DK", "CH"], ["http://join.allpornsitespass.com/track/MjAwNTgyLjEuNC41LjAuMC4wLjAuMA"]],
     
     [["ZZ"], ["http://t.irtya.com/wzbm9r4u8?offer_id=3788&aff_id=40075"]]
 
