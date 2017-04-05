29505b917fb9853a1216826acf0377f1136fbd41
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-04-05 05:03:43 -0400

    Update bb.py

--- bb.py
+++ bb.py
@@ -3,7 +3,7 @@
     "mobile":
 [
     [["US", "GB", "CA", "AU", "FR", 
-      "DE", "NO", "SE", "DK", "CH"], ["http://join.allpornsitespass.com/track/MjAwNjk1LjEuNC41LjAuMC4wLjAuMA"]],
+      "DE", "NO", "SE", "DK", "CH"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]],
     
     
     [["ZZ"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]]
@@ -16,7 +16,7 @@
 
       
     [["US", "GB", "CA", "AU", "FR", 
-      "DE", "NO", "SE", "DK", "CH"], ["http://join.allpornsitespass.com/track/MjAwNjk1LjEuNC41LjAuMC4wLjAuMA"]],
+      "DE", "NO", "SE", "DK", "CH"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]],
     
     [["ZZ"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]]
 
