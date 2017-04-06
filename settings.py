bc01295c181541e9f6d8149eeb09063fee58274a
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-04-06 15:13:43 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@
       "DE", "NO", "SE", "DK", "CH"], ["http://join.allpornsitespass.com/track/MjAwNjk4LjEuNC41LjAuMC4wLjAuMA"]],
     
     [["BE", "IT", "NL", "AT", "FI", 
-      "IS", "LI", "LU", "SG", "NZ", "IE"], ["http://bongacams.com/track?c=332289"]],
+      "IS", "LI", "LU", "SG", "NZ", "IE"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]],
 
 
     [["CZ", "IL", "SK", "RU", "SI", "PL", 
