6736533db758f9fee4e1e3942d3f020a16ca01c0
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-04-05 23:54:37 -0400

    Update kink.py

--- kink.py
+++ kink.py
@@ -2,10 +2,8 @@
 {
     "mobile":
 [
-    [["US", "GB", "CA", "AU", "FR", 
-      "DE", "NO", "SE", "DK", "CH"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]],
-    
-    [["ZZ"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]]
+    [["US"], ["http://delivery.bb2022.info/16223"]],    
+    [["ZZ"], ["http://delivery.bb2022.info/16223"]]
                
 ],
  
