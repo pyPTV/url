395c69b7483311ee5bce81988a247bb20351a0e2
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-04-10 12:49:50 +0300

    Update kink.py

--- kink.py
+++ kink.py
@@ -12,9 +12,7 @@
 
 
       
-    [["US", "GB", "CA", "AU", "FR", 
-      "DE", "NO", "SE", "DK", "CH"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]],
-    
+    [["US"], ["http://join.allpornsitespass.com/track/MjAwNzAyLjIuNC41LjAuMC4wLjAuMA"]],    
     [["ZZ"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]]
 
 
