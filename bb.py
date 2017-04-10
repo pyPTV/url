bab9900a4595a328fdbb14daa98d8fc56c5a7b00
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-04-10 12:47:08 +0300

    Update bb.py

--- bb.py
+++ bb.py
@@ -2,24 +2,15 @@
 {
     "mobile":
 [
-    [["US", "GB", "CA", "AU", "FR", 
-      "DE", "NO", "SE", "DK", "CH"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]],
-    
-    
+    [["US"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]],   
     [["ZZ"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]]
                
 ],
  
     "desktop":
-[
-
-
-      
-    [["US", "GB", "CA", "AU", "FR", 
-      "DE", "NO", "SE", "DK", "CH"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]],
-    
+[      
+    [["US"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]],    
     [["ZZ"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]]
-
 
 ]
 }
