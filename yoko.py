84fb61179b9db794d5e42c21258101392675a49e
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-04-11 09:29:02 +0300

    Update yoko.py

--- yoko.py
+++ yoko.py
@@ -3,8 +3,8 @@
 {
     "mobile":
 [
-    [["US"], ["http://delivery.bb2022.info/16223"]],
-    [["ZZ"], ["http://delivery.bb2022.info/16223"]]
+    [["US"], ["http://enter.javhd.com/track/MjI1NjguMi4yLjg0LjAuMC4wLjAuMA"]],
+    [["ZZ"], ["http://enter.javhd.com/track/MjI1NjguMi4yLjg0LjAuMC4wLjAuMA"]]
                
 ],
  
