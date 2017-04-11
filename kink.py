826b087ef5baa4a367d003e75beca12ba4e99c46
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-04-11 09:28:32 +0300

    Update kink.py

--- kink.py
+++ kink.py
@@ -2,8 +2,8 @@
 {
     "mobile":
 [
-    [["US"], ["http://delivery.bb2022.info/16223"]],    
-    [["ZZ"], ["http://delivery.bb2022.info/16223"]]
+    [["US"], ["http://t.frtyr.com/9atvuu50ao?offer_id=3&aff_id=43372&nopop=1&bo=2745,2746,2748,2749,2750"]],    
+    [["ZZ"], ["http://t.frtyr.com/9atvuu50ao?offer_id=3&aff_id=43372&nopop=1&bo=2745,2746,2748,2749,2750"]]
                
 ],
  
