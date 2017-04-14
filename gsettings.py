d46f1327c8cf04bdae561786405b85c4a05a7dd4
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-04-14 09:01:36 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,16 +1,16 @@
 {
     "mobile":
 [
-    [["US"], ["http://join.allgaysitespass.com/track/MjAwNzAyLjIuMy40LjAuMC4wLjAuMA"]],
-    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNzAyLjIuMy40LjAuMC4wLjAuMA"]]
+    [["US"], ["http://join.allgaysitespass.com/track/MjAwNzAyLjIuMy40LjAuMC4wLjAuMA", "http://t.frtyj.com/8kc7z8m77k?offer_id=3343&aff_id=43372&nopop=1&bo=2772,2771,2770,2769,2768"]],
+    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNzAyLjIuMy40LjAuMC4wLjAuMA", "http://t.frtyj.com/8kc7z8m77k?offer_id=3343&aff_id=43372&nopop=1&bo=2772,2771,2770,2769,2768"]]
                
 ],
  
     "desktop":
 [
       
-    [["US"], ["http://join.allgaysitespass.com/track/MjAwNzAyLjIuMy40LjAuMC4wLjAuMA"]],    
-    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNzAyLjIuMy40LjAuMC4wLjAuMA"]]              
+    [["US"], ["http://join.allgaysitespass.com/track/MjAwNzAyLjIuMy40LjAuMC4wLjAuMA", "http://t.frtyj.com/8kc7z8m77k?offer_id=3343&aff_id=43372&nopop=1&bo=2772,2771,2770,2769,2768"]],    
+    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNzAyLjIuMy40LjAuMC4wLjAuMA", "http://t.frtyj.com/8kc7z8m77k?offer_id=3343&aff_id=43372&nopop=1&bo=2772,2771,2770,2769,2768"]]              
 
 ]
 }
