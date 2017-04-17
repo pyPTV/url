b1fa83fbd7f5cbb5826f360407104af33ec92cdb
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-04-17 06:21:43 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@
   "desktop":
 [
 
-    [["US", "GB", "CA", "AU", "FR", "CH", "DE"], ["http://join.allpornsitespass.com/track/MjAwMjYyLjEuNC41LjAuMC4wLjAuMA"]],
+    [["US", "GB", "CA", "AU", "FR", "CH", "DE"], ["http://join.allpornsitespass.com/track/MjAwMjk4LjEuNC41LjAuMC4wLjAuMA"]],
 
     [["ZZ"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]]
    
