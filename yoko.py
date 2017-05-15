133d6a14edd2bc117cb98892911070a128c5bb15
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-05-15 21:40:46 +0300

    Update yoko.py

--- yoko.py
+++ yoko.py
@@ -3,8 +3,8 @@
 {
     "mobile":
 [
-    [["US"], ["http://join.allpornsitespass.com/track/MjAwNjk4LjEuNC41LjAuMC4wLjAuMA"]],
-    [["ZZ"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]]
+    [["US"], ["http://delivery.bb2022.info/16223"]],
+    [["ZZ"], ["http://delivery.bb2022.info/16223"]]
                
 ],
  
@@ -13,7 +13,7 @@
 
 
       
-    [["US", "GB", "CA", "AU", "FR", "CH", "DE"], ["http://join.allpornsitespass.com/track/MjAwNjk4LjEuNC41LjAuMC4wLjAuMA"]],
+    [["US", "GB", "CA", "AU", "FR", "CH", "DE"], ["http://join.allpornsitespass.com/track/MjAwNDQyLjEuNC41LjAuMC4wLjAuMA"]],
     [["ZZ"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]]
 
 
