b925dab15e1db8cf3074718e9440e5531dbd4e38
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-05-19 07:17:10 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@
   "desktop":
 [
 
-    [["US", "GB", "CA"], ["http://join.allpornsitespass.com/track/MjAwMzc0LjEuNC41LjAuMC4wLjAuMA"]],
+    [["US", "GB", "CA"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],
 
     [["ZZ"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]]
    
