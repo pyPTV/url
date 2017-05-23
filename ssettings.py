d59ffbaa5e7d88a82dd087064f9c8626093e0f5b
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-05-23 23:40:13 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,16 +2,22 @@
 {
     "mobile":
 [
-    [["US"], ["http://join.punishshemale.com/track/NjU4LjE1LjE2LjI4LjAuMC4wLjAuMA", "http://join.trans500.com/track/NjU4LjIuMy4xOS4wLjAuMC4wLjA"]],
-    [["ZZ"], ["http://join.punishshemale.com/track/NjU4LjE1LjE2LjI4LjAuMC4wLjAuMA", "http://join.trans500.com/track/NjU4LjIuMy4xOS4wLjAuMC4wLjA"]]
+    [["US"], ["http://join.punishshemale.com/track/NjU4LjE1LjE2LjI4LjAuMC4wLjAuMA", 
+              "http://join.trans500.com/track/NjU4LjIuMy4xOS4wLjAuMC4wLjA"]],
+    
+    [["ZZ"], ["http://join.punishshemale.com/track/NjU4LjE1LjE2LjI4LjAuMC4wLjAuMA", 
+              "http://join.trans500.com/track/NjU4LjIuMy4xOS4wLjAuMC4wLjA"]]
                
 ],
  
     "desktop":
 [
       
-    [["US"], ["http://join.punishshemale.com/track/NjU4LjE1LjE2LjI4LjAuMC4wLjAuMA", "http://join.trans500.com/track/NjU4LjIuMy4xOS4wLjAuMC4wLjA"]],    
-    [["ZZ"], ["http://join.punishshemale.com/track/NjU4LjE1LjE2LjI4LjAuMC4wLjAuMA", "http://join.trans500.com/track/NjU4LjIuMy4xOS4wLjAuMC4wLjA"]]              
+    [["US"], ["http://join.punishshemale.com/track/NjU4LjE1LjE2LjI4LjAuMC4wLjAuMA", 
+              "http://join.trans500.com/track/NjU4LjIuMy4xOS4wLjAuMC4wLjA"]],  
+    
+    [["ZZ"], ["http://join.punishshemale.com/track/NjU4LjE1LjE2LjI4LjAuMC4wLjAuMA", 
+              "http://join.trans500.com/track/NjU4LjIuMy4xOS4wLjAuMC4wLjA"]]              
 
 ]
 }
