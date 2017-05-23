01700317f404dbaf2e279ae00434f57fbc3de3c6
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-05-23 23:04:37 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -1,1 +1,18 @@
 
+{
+    "mobile":
+[
+    [["US"], ["http://join.punishshemale.com/track/NjU4LjE1LjE2LjI4LjAuMC4wLjAuMA", "http://join.trans500.com/track/NjU4LjIuMy4xOS4wLjAuMC4wLjA"]],
+    [["ZZ"], ["http://join.punishshemale.com/track/NjU4LjE1LjE2LjI4LjAuMC4wLjAuMA", "http://join.trans500.com/track/NjU4LjIuMy4xOS4wLjAuMC4wLjA"]]
+               
+],
+ 
+    "desktop":
+[
+      
+    [["US"], ["http://join.punishshemale.com/track/NjU4LjE1LjE2LjI4LjAuMC4wLjAuMA", "http://join.trans500.com/track/NjU4LjIuMy4xOS4wLjAuMC4wLjA"]],    
+    [["ZZ"], ["http://join.punishshemale.com/track/NjU4LjE1LjE2LjI4LjAuMC4wLjAuMA", "http://join.trans500.com/track/NjU4LjIuMy4xOS4wLjAuMC4wLjA"]]              
+
+]
+}
+
