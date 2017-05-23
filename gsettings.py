ed03e72c17d9048ae5d61f17ff0a302fb1d61f5c
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-05-23 23:55:41 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,16 +1,16 @@
 {
     "mobile":
 [
-    [["US"], ["http://gay.com"]],
-    [["ZZ"], ["http://gay.com"]]
+    [["US"], ["https://t.frtyi.com/c6psh9zf28?offer_id=361&aff_id=40075&bo=2772,2771,2770,2769,2768&nopop=1"]],
+    [["ZZ"], ["https://t.frtyi.com/c6psh9zf28?offer_id=361&aff_id=40075&bo=2772,2771,2770,2769,2768&nopop=1"]]
                
 ],
  
     "desktop":
 [
       
-    [["US"], ["http://gay.com"]],    
-    [["ZZ"], ["http://gay.com"]]              
+    [["US"], ["https://t.frtyi.com/1u87gw8fr4?offer_id=171&aff_id=40075&bo=2772,2771,2770,2769,2768&nopop=1"]],    
+    [["ZZ"], ["https://t.frtyi.com/1u87gw8fr4?offer_id=171&aff_id=40075&bo=2772,2771,2770,2769,2768&nopop=1"]]              
 
 ]
 }
