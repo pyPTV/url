7b4055b85e26b39e42373201b4b786e4b692798a
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-06-06 22:18:37 +0400

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,22 +2,22 @@
 {
     "mobile":
 [
-    [["US"], ["https://t.frtyi.com/jrbupjgc00?offer_id=4175&aff_id=40075&bo=2779,2778,2777,2776,2775&nopop=1", 
-              "https://t.frtyi.com/jrbupjgc00?offer_id=4175&aff_id=40075&bo=2779,2778,2777,2776,2775&nopop=1"]],
+    [["US"], ["https://t.frtyj.com/r4p8vn0wjk?offer_id=2902&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1", 
+              "https://t.frtyj.com/s63l7lgrpc?offer_id=3341&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1"]],
     
-    [["ZZ"], ["https://t.frtyi.com/jrbupjgc00?offer_id=4175&aff_id=40075&bo=2779,2778,2777,2776,2775&nopop=1", 
-              "https://t.frtyi.com/jrbupjgc00?offer_id=4175&aff_id=40075&bo=2779,2778,2777,2776,2775&nopop=1"]]
+    [["ZZ"], ["https://t.frtyj.com/r4p8vn0wjk?offer_id=2902&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1", 
+              "https://t.frtyj.com/s63l7lgrpc?offer_id=3341&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1"]]
                
 ],
  
     "desktop":
 [
       
-    [["US"], ["https://t.frtyi.com/jrbupjgc00?offer_id=4175&aff_id=40075&bo=2779,2778,2777,2776,2775&nopop=1", 
-              "https://t.frtyi.com/jrbupjgc00?offer_id=4175&aff_id=40075&bo=2779,2778,2777,2776,2775&nopop=1"]],  
+    [["US"], ["https://t.frtyj.com/r4p8vn0wjk?offer_id=2902&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1", 
+              "https://t.frtyj.com/s63l7lgrpc?offer_id=3341&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1"]],
     
-    [["ZZ"], ["https://t.frtyi.com/jrbupjgc00?offer_id=4175&aff_id=40075&bo=2779,2778,2777,2776,2775&nopop=1", 
-              "https://t.frtyi.com/jrbupjgc00?offer_id=4175&aff_id=40075&bo=2779,2778,2777,2776,2775&nopop=1"]]              
+    [["ZZ"], ["https://t.frtyj.com/r4p8vn0wjk?offer_id=2902&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1", 
+              "https://t.frtyj.com/s63l7lgrpc?offer_id=3341&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1"]]             
 
 ]
 }
