183147b4e998d7db4dda7d50502e165390d299b3
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-06-11 17:07:19 +0300

    Create gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,16 +1,16 @@
 {
     "mobile":
 [
-    [["US"], ["http://join.allgaysitespass.com/track/MjAwNjk4LjEuMy40LjAuMC4wLjAuMA"]],
-    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNjk4LjEuMy40LjAuMC4wLjAuMA"]]
+    [["US"], ["http://join.allgaysitespass.com/track/MjAwNTgyLjEuMy40LjAuMC4wLjAuMA"]],
+    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNTgyLjEuMy40LjAuMC4wLjAuMA"]]
                
 ],
  
     "desktop":
 [
       
-    [["US"], ["http://join.allgaysitespass.com/track/MjAwNjk4LjEuMy40LjAuMC4wLjAuMA"]],    
-    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNjk4LjEuMy40LjAuMC4wLjAuMA"]]              
+    [["US"], ["http://join.allgaysitespass.com/track/MjAwNTgyLjEuMy40LjAuMC4wLjAuMA"]],    
+    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNTgyLjEuMy40LjAuMC4wLjAuMA"]]              
 
 ]
 }
