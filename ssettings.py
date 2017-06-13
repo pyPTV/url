f29468bb6270eea8ce2e193a3f07fba669b31c26
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-06-13 15:27:55 +0300

    Create ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,22 +2,18 @@
 {
     "mobile":
 [
-    [["US"], ["https://t.frtyj.com/r4p8vn0wjk?offer_id=2902&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1", 
-              "https://t.frtyj.com/s63l7lgrpc?offer_id=3341&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1"]],
+    [["US"], ["http://www.youtrannytube.com/"]],
     
-    [["ZZ"], ["https://t.frtyj.com/r4p8vn0wjk?offer_id=2902&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1", 
-              "https://t.frtyj.com/s63l7lgrpc?offer_id=3341&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1"]]
+    [["ZZ"], ["http://www.youtrannytube.com/"]]
                
 ],
  
     "desktop":
 [
       
-    [["US"], ["https://t.frtyj.com/r4p8vn0wjk?offer_id=2902&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1", 
-              "https://t.frtyj.com/s63l7lgrpc?offer_id=3341&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1"]],
+    [["US"], ["http://www.youtrannytube.com/"]],
     
-    [["ZZ"], ["https://t.frtyj.com/r4p8vn0wjk?offer_id=2902&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1", 
-              "https://t.frtyj.com/s63l7lgrpc?offer_id=3341&aff_id=40075&bo=2745,2746,2748,2749,2750&nopop=1"]]             
+    [["ZZ"], ["http://www.youtrannytube.com/"]]             
 
 ]
 }
