b9bb6d901a0dda1cd46739b55986c9219cdaea4f
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-06-14 20:21:55 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,7 +13,6 @@
 [
 
     [["US", "GB", "CA"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],
-
     [["ZZ"], ["http://t.irtya.com/n0u3mhinnk?offer_id=3788&aff_id=43372"]]
    
    
