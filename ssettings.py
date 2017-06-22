09ad3e6edb74b11cc6619fa076e9a81f5981ae69
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-06-22 23:13:08 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,16 +2,16 @@
 {
     "mobile":
 [
-    [["US"], ["https://t.frtyj.com/9k05kuurwg?offer_id=3341&aff_id=50870&nopop=1"]],    
-    [["ZZ"], ["https://t.frtyj.com/9k05kuurwg?offer_id=3341&aff_id=50870&nopop=1"]]
+    [["US"], ["http://www.trannymaids.com/en/"]],    
+    [["ZZ"], ["http://www.trannymaids.com/en/"]]
                
 ],
  
     "desktop":
 [
       
-    [["US"], ["https://t.frtyj.com/9k05kuurwg?offer_id=3341&aff_id=50870&nopop=1"]],    
-    [["ZZ"], ["https://t.frtyj.com/9k05kuurwg?offer_id=3341&aff_id=50870&nopop=1"]]             
+    [["US"], ["http://www.trannymaids.com/en/"]],    
+    [["ZZ"], ["http://www.trannymaids.com/en/"]]             
 
 ]
 }
