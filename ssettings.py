4b7da448e4ed802e0bc32f6219b4ee91d988431c
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-07-05 08:41:08 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,18 +2,22 @@
 {
     "mobile":
 [
-    [["US"], ["https://t.frtyj.com/bdvujmzj28?offer_id=3341&aff_id=51626&nopop=1"]],  
+    [["US"], ["http://join.shemalesexzone.com/track/MjAwNzg5LjEuMTA0LjEwNS4wLjAuMC4wLjA",
+              "http://join.shemalemovieworld.com/track/MjAwNzg5LjEuMTAyLjEwMy4wLjAuMC4wLjA"]],  
     
-    [["ZZ"], ["https://t.frtyj.com/bdvujmzj28?offer_id=3341&aff_id=51626&nopop=1"]]
+    [["ZZ"], ["http://join.shemalesexzone.com/track/MjAwNzg5LjEuMTA0LjEwNS4wLjAuMC4wLjA",
+              "http://join.shemalemovieworld.com/track/MjAwNzg5LjEuMTAyLjEwMy4wLjAuMC4wLjA"]]
                
 ],
  
     "desktop":
 [
       
-    [["US"], ["https://t.frtyj.com/bdvujmzj28?offer_id=3341&aff_id=51626&nopop=1"]], 
+    [["US"], ["http://join.shemalesexzone.com/track/MjAwNzg5LjEuMTA0LjEwNS4wLjAuMC4wLjA",
+              "http://join.shemalemovieworld.com/track/MjAwNzg5LjEuMTAyLjEwMy4wLjAuMC4wLjA"]], 
     
-    [["ZZ"], ["https://t.frtyj.com/bdvujmzj28?offer_id=3341&aff_id=51626&nopop=1"]]        
+    [["ZZ"], ["http://join.shemalesexzone.com/track/MjAwNzg5LjEuMTA0LjEwNS4wLjAuMC4wLjA",
+              "http://join.shemalemovieworld.com/track/MjAwNzg5LjEuMTAyLjEwMy4wLjAuMC4wLjA"]]        
 
 ]
 }
