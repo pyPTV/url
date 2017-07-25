e67d149d2122c8c649770b6a1d849e5d531ba8fe
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-07-25 08:13:15 +0300

    Update bb.py

--- bb.py
+++ bb.py
@@ -3,14 +3,14 @@
     "mobile":
 [
     [["US"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],   
-    [["ZZ"], ["https://t.irtya.com/nzszahfav4?offer_id=3788&aff_id=40075"]]
+    [["ZZ"], ["https://t.irtya.com/ewkh0ihh8g?offer_id=3788&aff_id=53280"]]
                
 ],
  
     "desktop":
 [      
     [["US"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],    
-    [["ZZ"], ["https://t.irtya.com/nzszahfav4?offer_id=3788&aff_id=40075"]]
+    [["ZZ"], ["https://t.irtya.com/ewkh0ihh8g?offer_id=3788&aff_id=53280"]]
 
 ]
 }
