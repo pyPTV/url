9f2b99b47ae02438121413dd0a0a4786e0a1a279
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-08-01 00:04:08 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,16 +2,16 @@
 {
     "mobile":
 [
-    [["US"], ["https://chaturbate.com/affiliates/in/?track=default&tour=sxJR&campaign=AQTqz"]],      
-    [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=default&tour=sxJR&campaign=AQTqz"]]
+    [["US"], ["https://t.frtyj.com/9k05kuurwg?offer_id=3341&aff_id=50870&nopop=1"]],      
+    [["ZZ"], ["https://t.frtyj.com/9k05kuurwg?offer_id=3341&aff_id=50870&nopop=1"]]
                
 ],
  
     "desktop":
 [
       
-    [["US"], ["https://chaturbate.com/affiliates/in/?track=default&tour=sxJR&campaign=AQTqz"]],     
-    [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=default&tour=sxJR&campaign=AQTqz"]]        
+    [["US"], ["https://t.frtyj.com/9k05kuurwg?offer_id=3341&aff_id=50870&nopop=1"]],     
+    [["ZZ"], ["https://t.frtyj.com/9k05kuurwg?offer_id=3341&aff_id=50870&nopop=1"]]        
 
 ]
 }
