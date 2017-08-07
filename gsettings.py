ca4381d71a5f7e8dc80fa50734db59d360a937ca
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-08-07 14:29:02 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,16 +1,26 @@
 {
     "mobile":
 [
-    [["US"], ["http://join.allgaysitespass.com/track/MjAwNjk1LjEuMy40LjAuMC4wLjAuMA"]],
-    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNjk1LjEuMy40LjAuMC4wLjAuMA"]]
+[["CA", "US", "AU", "NZ", "GB", "DE", 
+  "CH", "NL", "RU", "SE", "RO", "IT", 
+  "FR", "ES", "PL", "BE", "GR", "CZ", 
+  "AT", "PT", "HU", "SK", "IE", "DK", 
+  "NO", "FI", "SI", "LU", "CY", "IS"], ["http://join.allgaysitespass.com/track/MjAwNjk1LjEuMy40LjAuMC4wLjAuMA"]],
+    
+[["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNjk1LjEuMy40LjAuMC4wLjAuMA"]]
                
 ],
  
     "desktop":
 [
       
-    [["US"], ["http://join.allgaysitespass.com/track/MjAwNjk1LjEuMy40LjAuMC4wLjAuMA"]],    
-    [["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNjk1LjEuMy40LjAuMC4wLjAuMA"]]              
+[["CA", "US", "AU", "NZ", "GB", "DE", 
+  "CH", "NL", "RU", "SE", "RO", "IT", 
+  "FR", "ES", "PL", "BE", "GR", "CZ", 
+  "AT", "PT", "HU", "SK", "IE", "DK", 
+  "NO", "FI", "SI", "LU", "CY", "IS"], ["http://join.allgaysitespass.com/track/MjAwNjk1LjEuMy40LjAuMC4wLjAuMA"]],    
+    
+[["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNjk1LjEuMy40LjAuMC4wLjAuMA"]]              
 
 ]
 }
