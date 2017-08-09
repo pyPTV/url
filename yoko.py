d1e5f999d87124db703870846ec66589c1be47cf
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-08-09 08:11:40 +0300

    Update yoko.py

--- yoko.py
+++ yoko.py
@@ -13,7 +13,7 @@
 
 
       
-    [["US", "FR"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],
+    [["FR"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],
     [["ZZ"], ["https://t.irtya.com/zmun567lj4?offer_id=3788&aff_id=53837"]]
 
 
