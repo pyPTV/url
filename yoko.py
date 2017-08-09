59986c22e629f089afe600474fce68a33dbec300
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-08-09 23:57:12 +0300

    Update yoko.py

--- yoko.py
+++ yoko.py
@@ -3,8 +3,8 @@
 {
     "mobile":
 [
-    [["US"], ["https://t.irtya.com/zmun567lj4?offer_id=3788&aff_id=53837"]],
-    [["ZZ"], ["https://t.irtya.com/zmun567lj4?offer_id=3788&aff_id=53837"]]
+    [["US"], ["https://t.irtya.com/ko6top2klc?offer_id=3788&aff_id=54455"]],
+    [["ZZ"], ["https://t.irtya.com/ko6top2klc?offer_id=3788&aff_id=54455"]]
                
 ],
  
@@ -14,7 +14,7 @@
 
       
     [["FR"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],
-    [["ZZ"], ["https://t.irtya.com/zmun567lj4?offer_id=3788&aff_id=53837"]]
+    [["ZZ"], ["https://t.irtya.com/ko6top2klc?offer_id=3788&aff_id=54455"]]
 
 
 ]
