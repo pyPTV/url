afd1da71479f0dda9c65d8e1ebb52061c807e5d5
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-08-10 23:01:23 +0300

    Update kink.py

--- kink.py
+++ kink.py
@@ -12,7 +12,7 @@
 
 
       
-    [["FR"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],    
+    [["FR"], ["https://t.frtyr.com/z7b2fgrts0?offer_id=3&aff_id=54455&nopop=1"]],    
     [["ZZ"], ["https://t.frtyr.com/z7b2fgrts0?offer_id=3&aff_id=54455&nopop=1"]]
 
 
