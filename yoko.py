0e21d403be58809ec705423fb50b6e7296eb4a2a
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-08-10 21:23:05 +0300

    Update yoko.py

--- yoko.py
+++ yoko.py
@@ -3,8 +3,8 @@
 {
     "mobile":
 [
-    [["US"], ["https://t.irtya.com/ko6top2klc?offer_id=3788&aff_id=54455"]],
-    [["ZZ"], ["https://t.irtya.com/ko6top2klc?offer_id=3788&aff_id=54455"]]
+    [["US"], ["https://t.mobtya.com/k7e1jy2kn4?offer_id=2104&aff_id=54455"]],
+    [["ZZ"], ["https://t.mobtya.com/k7e1jy2kn4?offer_id=2104&aff_id=54455"]]
                
 ],
  
