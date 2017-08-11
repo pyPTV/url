e6954819cff61c9225003e81f1dee902bca6844c
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-08-12 00:27:32 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -5,9 +5,9 @@
   "CH", "NL", "LI", "SE", "RO", "IT", 
   "FR", "ES", "PL", "BE", "GR", "CZ", 
   "AT", "PT", "HU", "SK", "IE", "DK", 
-  "NO", "FI", "SI", "LU", "CY", "IS"], ["https://t.frtyj.com/tp9bz5c2tc?offer_id=3343&aff_id=54455&nopop=1"]],
+  "NO", "FI", "SI", "LU", "CY", "IS"], ["https://t.frtyj.com/2pc06i47k0?offer_id=3343&aff_id=54557&nopop=1", "https://t.frtyr.com/1vowxbv3wg?offer_id=4184&aff_id=54557&nopop=1"]],
     
-[["ZZ"], ["https://t.irtya.com/pdyswbals0?offer_id=4080&aff_id=54455"]]
+[["ZZ"], ["https://t.irtya.com/smeg6r934?offer_id=4080&aff_id=54557"]]
                
 ],
  
@@ -18,9 +18,9 @@
   "CH", "NL", "LI", "SE", "RO", "IT", 
   "FR", "ES", "PL", "BE", "GR", "CZ", 
   "AT", "PT", "HU", "SK", "IE", "DK", 
-  "NO", "FI", "SI", "LU", "CY", "IS"], ["https://t.frtyj.com/tp9bz5c2tc?offer_id=3343&aff_id=54455&nopop=1"]],    
+  "NO", "FI", "SI", "LU", "CY", "IS"], ["https://t.frtyj.com/2pc06i47k0?offer_id=3343&aff_id=54557&nopop=1", "https://t.frtyr.com/1vowxbv3wg?offer_id=4184&aff_id=54557&nopop=1"]],    
     
-[["ZZ"], ["https://t.irtya.com/pdyswbals0?offer_id=4080&aff_id=54455"]]              
+[["ZZ"], ["https://t.irtya.com/smeg6r934?offer_id=4080&aff_id=54557"]]              
 
 ]
 }
