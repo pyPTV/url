eaea365e4bd21bf0bf02bf57b1fcd446be560593
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-08-12 00:30:58 +0300

    Update kink.py

--- kink.py
+++ kink.py
@@ -1,21 +1,22 @@
-
 {
     "mobile":
 [
-    [["US"], ["https://t.frtyr.com/rlux03137k?offer_id=11&aff_id=54455&nopop=1"]],    
-    [["ZZ"], ["https://t.frtyr.com/rlux03137k?offer_id=11&aff_id=54455&nopop=1"]]
-               
+    [["US"], ["https://t.mobtya.com/e16iiq0z7k?offer_id=2104&aff_id=54557"]],       
+    [["ZZ"], ["https://t.mobtya.com/e16iiq0z7k?offer_id=2104&aff_id=54557"]]
+
 ],
  
-    "desktop":
+
+ 
+
+  "desktop":
 [
 
+    [["FR"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],
+    [["ZZ"], ["https://t.irtya.com/banlyczidc?offer_id=3788&aff_id=54557", "https://t.frtyr.com/fwjhzjaxmo?offer_id=3&aff_id=54557&nopop=1"]]
+   
+   
+   
+]}
 
-      
-    [["FR"], ["https://t.frtyr.com/z7b2fgrts0?offer_id=3&aff_id=54455&nopop=1"]],    
-    [["ZZ"], ["https://t.frtyr.com/z7b2fgrts0?offer_id=3&aff_id=54455&nopop=1"]]
-
-
-]
-}
 
