f32a4128ee278ae7b18fd77a4c1472b5c7a32fd2
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-08-12 16:27:45 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -5,7 +5,7 @@
   "CH", "NL", "LI", "SE", "RO", "IT", 
   "FR", "ES", "PL", "BE", "GR", "CZ", 
   "AT", "PT", "HU", "SK", "IE", "DK", 
-  "NO", "FI", "SI", "LU", "CY", "IS"], ["https://t.frtyj.com/2pc06i47k0?offer_id=3343&aff_id=54557&nopop=1", "https://t.frtyr.com/1vowxbv3wg?offer_id=4184&aff_id=54557&nopop=1"]],
+  "NO", "FI", "SI", "LU", "CY", "IS"], ["https://t.frtyj.com/2pc06i47k0?offer_id=3343&aff_id=54557&nopop=1"]],
     
 [["ZZ"], ["https://t.irtya.com/smeg6r934?offer_id=4080&aff_id=54557"]]
                
@@ -18,7 +18,7 @@
   "CH", "NL", "LI", "SE", "RO", "IT", 
   "FR", "ES", "PL", "BE", "GR", "CZ", 
   "AT", "PT", "HU", "SK", "IE", "DK", 
-  "NO", "FI", "SI", "LU", "CY", "IS"], ["https://t.frtyj.com/2pc06i47k0?offer_id=3343&aff_id=54557&nopop=1", "https://t.frtyr.com/1vowxbv3wg?offer_id=4184&aff_id=54557&nopop=1"]],    
+  "NO", "FI", "SI", "LU", "CY", "IS"], ["https://t.frtyj.com/2pc06i47k0?offer_id=3343&aff_id=54557&nopop=1"]],    
     
 [["ZZ"], ["https://t.irtya.com/smeg6r934?offer_id=4080&aff_id=54557"]]              
 
