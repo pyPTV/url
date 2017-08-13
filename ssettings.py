9ae51ecd05efb3b94e5d60094e385c6bef9c9040
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-08-13 18:04:24 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,16 +2,16 @@
 {
     "mobile":
 [
-    [["US"], ["https://t.frtyj.com/5sx56ki96o?offer_id=3341&aff_id=54557&nopop=1"]],      
-    [["ZZ"], ["https://t.frtyj.com/5sx56ki96o?offer_id=3341&aff_id=54557&nopop=1"]]
+    [["US"], ["http://www.youtrannytube.com/"]],      
+    [["ZZ"], ["http://www.youtrannytube.com/"]]
                
 ],
  
     "desktop":
 [
       
-    [["US"], ["https://t.frtyj.com/5sx56ki96o?offer_id=3341&aff_id=54557&nopop=1"]],     
-    [["ZZ"], ["https://t.frtyj.com/5sx56ki96o?offer_id=3341&aff_id=54557&nopop=1"]]        
+    [["US"], ["http://www.youtrannytube.com/"]],     
+    [["ZZ"], ["http://www.youtrannytube.com/"]]        
 
 ]
 }
