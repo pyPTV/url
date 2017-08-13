498c5cb41b201741bdce380fbd36257956f53971
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-08-13 18:03:30 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@
     "mobile":
 [
 
-[["US"], ["https://t.mobtya.com/e16iiq0z7k?offer_id=2104&aff_id=54557"]],    
-[["ZZ"], ["https://t.mobtya.com/e16iiq0z7k?offer_id=2104&aff_id=54557"]]
+[["US"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],    
+[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]]
                
 ],
  
@@ -11,10 +11,10 @@
 [
 
 [["CA", "US", "AU", "NZ", "GB", "DE", 
-  "CH", "NL", "SE", "AT", "DK", "NO"], ["https://t.frtyr.com/fwjhzjaxmo?offer_id=3&aff_id=54557&nopop=1"]],
+  "CH", "NL", "SE", "AT", "DK", "NO"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],
 
 [["FR"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],  
-[["ZZ"], ["https://t.irtya.com/banlyczidc?offer_id=3788&aff_id=54557"]]             
+[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]]             
 
 ]
 }
