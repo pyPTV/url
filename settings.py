4754b778f70a14d11c587751d6d9b0744aad6b7d
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-08-19 09:48:00 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,10 +4,10 @@
 
 [["CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO"], 
  
-         ["https://t.frtyr.com/scc2hqb668?offer_id=3&aff_id=51626&nopop=1"]],
+         ["https://t.irtya.com/v2rif0kgcg?offer_id=3664&aff_id=43372"]],
  
 [["FR"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],   
-[["ZZ"], ["https://t.mobtya.com/hpjij8l0qo?offer_id=2104&aff_id=51626"]]
+[["ZZ"], ["https://t.mobtya.com/m0xcqhsu2o?offer_id=2104&aff_id=43372"]]
                
 ],
  
@@ -16,10 +16,10 @@
 
 [["CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO"], 
      
-         ["https://t.frtyr.com/scc2hqb668?offer_id=3&aff_id=51626&nopop=1"]],
+         ["https://t.frtyr.com/cf66aao3k?offer_id=3&aff_id=43372&nopop=1"]],
 
 [["FR"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],  
-[["ZZ"], ["https://t.irtya.com/yn6lutre68?offer_id=3788&aff_id=51626"]]             
+[["ZZ"], ["https://t.irtya.com/v2rif0kgcg?offer_id=3664&aff_id=43372"]]             
 
 ]
 }
