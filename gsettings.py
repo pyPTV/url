5c836378a15d573ba2a613365a1a83b66dcde580
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-08-24 17:26:18 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -7,7 +7,7 @@
   "AT", "PT", "HU", "SK", "IE", "DK", 
   "NO", "FI", "SI", "LU", "CY", "IS"], ["http://join.allgaysitespass.com/track/MjAwMzc0LjEuMy40LjAuMC4wLjAuMA"]],
     
-[["ZZ"], ["https://t.irtya.com/w1tor0pjuo?offer_id=4080&aff_id=43372"]]
+[["ZZ"], ["https://t.irtya.com/pyxw1smlxc?offer_id=4080&aff_id=51626"]]
                
 ],
  
@@ -20,7 +20,7 @@
   "AT", "PT", "HU", "SK", "IE", "DK", 
   "NO", "FI", "SI", "LU", "CY", "IS"], ["http://join.allgaysitespass.com/track/MjAwMzc0LjEuMy40LjAuMC4wLjAuMA"]],    
     
-[["ZZ"], ["https://t.irtya.com/w1tor0pjuo?offer_id=4080&aff_id=43372"]]              
+[["ZZ"], ["https://t.irtya.com/pyxw1smlxc?offer_id=4080&aff_id=51626"]]              
 
 ]
 }
