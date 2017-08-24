d14487d2c889cfc55c246a764c00835fe9a27f11
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-08-24 17:29:26 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,8 +2,7 @@
     "mobile":
 [
 
-[["US"], ["https://t.irtya.com/v2rif0kgcg?offer_id=3664&aff_id=43372"]],
-    
+[["US"], ["https://t.mobtya.com/hpjij8l0qo?offer_id=2104&aff_id=51626"]],    
 [["ZZ"], ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]]
                
 ],
@@ -13,10 +12,10 @@
 
 [["CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO"], 
      
-         ["https://t.frtyr.com/cf66aao3k?offer_id=3&aff_id=43372&nopop=1"]],
+         ["https://t.frtyr.com/scc2hqb668?offer_id=3&aff_id=51626&nopop=1"]],
 
 [["FR"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]],  
-[["ZZ"], ["https://t.irtya.com/v2rif0kgcg?offer_id=3664&aff_id=43372"]]             
+[["ZZ"], ["https://t.irtya.com/f3qwuvt7eo?offer_id=3664&aff_id=51626"]]             
 
 ]
 }
