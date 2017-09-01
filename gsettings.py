25b783933b3872d82f6b867ef4d375673308e0cd
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-09-01 21:19:51 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -2,10 +2,10 @@
     "mobile":
 [
 [["US"], ["https://t.frtyj.com/i2jh13c6yo?offer_id=3343&aff_id=55688&nopop=1", 
-          "https://t.frtyj.com/64u06ec6io?offer_id=3631&aff_id=55688&nopop=1"]],  
+          "https://t.frtyj.com/4aihr9fjsw?offer_id=3632&aff_id=55688&nopop=1"]],  
     
 [["ZZ"], ["https://t.frtyj.com/i2jh13c6yo?offer_id=3343&aff_id=55688&nopop=1",
-          "https://t.frtyj.com/64u06ec6io?offer_id=3631&aff_id=55688&nopop=1"]]
+          "https://t.frtyj.com/4aihr9fjsw?offer_id=3632&aff_id=55688&nopop=1"]]
                
 ],
  
