dc40d2107deb3ba0946d8e98a5f42f8043748dee
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-09-02 07:55:48 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,20 +1,24 @@
 {
     "mobile":
 [
-[["US"], ["https://t.frtyj.com/u60htlpl34?offer_id=3343&aff_id=40075&nopop=1"]],  
-    
-[["ZZ"], ["https://t.frtyj.com/u60htlpl34?offer_id=3343&aff_id=40075&nopop=1"]]
+
+[["CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR"], 
+     
+         ["https://t.frtyj.com/u60htlpl34?offer_id=3343&aff_id=40075&nopop=1"]],
+ 
+[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=gay&tour=9rL0&campaign=AQTqz"]]  
                
 ],
  
     "desktop":
 [
-      
-[["US"], ["https://t.frtyj.com/u60htlpl34?offer_id=3343&aff_id=40075&nopop=1"]],    
-    
-[["ZZ"], ["https://t.frtyj.com/u60htlpl34?offer_id=3343&aff_id=40075&nopop=1"]]              
+
+[["CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR"], 
+     
+         ["https://t.frtyj.com/u60htlpl34?offer_id=3343&aff_id=40075&nopop=1"]],
+ 
+[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=gay&tour=9rL0&campaign=AQTqz"]]             
 
 ]
 }
-
 
