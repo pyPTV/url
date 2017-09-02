37c6f03f730e257d20e08e5621ca4d47bd8b8383
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-09-02 08:02:31 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@
      
          ["https://t.frtyr.com/c7pqbuiqtc?offer_id=3&aff_id=40075&nopop=1"]],
  
-[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]]  
+[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz"]]  
                
 ],
  
@@ -17,7 +17,7 @@
      
          ["https://t.frtyr.com/c7pqbuiqtc?offer_id=3&aff_id=40075&nopop=1"]],
  
-[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=default&tour=hr8m&campaign=AQTqz"]]             
+[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz"]]             
 
 ]
 }
