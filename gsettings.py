4882c9b90c0bbef0f8c04371077c189bfacbb6e8
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-09-02 08:00:40 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -2,7 +2,7 @@
     "mobile":
 [
 
-[["IE","CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR"]], 
+[["IE","CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR"], 
      
          ["https://t.frtyj.com/u60htlpl34?offer_id=3343&aff_id=40075&nopop=1"]],
  
@@ -13,7 +13,7 @@
     "desktop":
 [
 
-[["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR"]], 
+[["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR"], 
      
          ["https://t.frtyj.com/u60htlpl34?offer_id=3343&aff_id=40075&nopop=1"]],
  
