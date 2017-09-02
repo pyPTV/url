a54b00737aaa3436730908f040fffbd643d2aaf6
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-09-02 06:52:27 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -3,16 +3,16 @@
     "mobile":
 [
     
-[["US"], ["https://t.frtyj.com/2lxs99u1a8?offer_id=3341&aff_id=55688&nopop=1"]], 
-[["ZZ"], ["https://t.frtyj.com/2lxs99u1a8?offer_id=3341&aff_id=55688&nopop=1"]]
+[["US"], ["https://t.frtyj.com/t6bckqbm0w?offer_id=3341&aff_id=40075&nopop=1"]], 
+[["ZZ"], ["https://t.frtyj.com/t6bckqbm0w?offer_id=3341&aff_id=40075&nopop=1"]]
                
 ],
  
     "desktop":
 [
       
-[["US"], ["https://t.frtyj.com/2lxs99u1a8?offer_id=3341&aff_id=55688&nopop=1"]],
-[["ZZ"], ["https://t.frtyj.com/2lxs99u1a8?offer_id=3341&aff_id=55688&nopop=1"]]        
+[["US"], ["https://t.frtyj.com/t6bckqbm0w?offer_id=3341&aff_id=40075&nopop=1"]],
+[["ZZ"], ["https://t.frtyj.com/t6bckqbm0w?offer_id=3341&aff_id=40075&nopop=1"]]        
 
 ]
 }
