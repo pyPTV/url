1b8bb5ddd56f1e2a541c2d559cab7da501fa8820
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-09-02 06:51:00 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -1,22 +1,18 @@
 {
     "mobile":
 [
-[["US"], ["https://t.frtyj.com/i2jh13c6yo?offer_id=3343&aff_id=55688&nopop=1", 
-          "https://t.frtyj.com/4aihr9fjsw?offer_id=3632&aff_id=55688&nopop=1"]],  
+[["US"], ["https://t.frtyj.com/u60htlpl34?offer_id=3343&aff_id=40075&nopop=1"]],  
     
-[["ZZ"], ["https://t.frtyj.com/i2jh13c6yo?offer_id=3343&aff_id=55688&nopop=1",
-          "https://t.frtyj.com/4aihr9fjsw?offer_id=3632&aff_id=55688&nopop=1"]]
+[["ZZ"], ["https://t.frtyj.com/u60htlpl34?offer_id=3343&aff_id=40075&nopop=1"]]
                
 ],
  
     "desktop":
 [
       
-[["US"], ["https://t.frtyj.com/i2jh13c6yo?offer_id=3343&aff_id=55688&nopop=1",
-          "https://t.frtyj.com/64u06ec6io?offer_id=3631&aff_id=55688&nopop=1"]],    
+[["US"], ["https://t.frtyj.com/u60htlpl34?offer_id=3343&aff_id=40075&nopop=1"]],    
     
-[["ZZ"], ["https://t.frtyj.com/i2jh13c6yo?offer_id=3343&aff_id=55688&nopop=1",
-          "https://t.frtyj.com/64u06ec6io?offer_id=3631&aff_id=55688&nopop=1"]]              
+[["ZZ"], ["https://t.frtyj.com/u60htlpl34?offer_id=3343&aff_id=40075&nopop=1"]]              
 
 ]
 }
