5c503a142b69640c7d1c213bd07bf9da74c744d2
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-09-17 17:29:27 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@
      
          ["https://t.frtyr.com/jretlijqdc?offer_id=3&aff_id=50870&nopop=1"]],
  
-[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz"]]  
+[["ZZ"], ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]]  
                
 ],
  
