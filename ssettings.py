0907b47fa5d51f1e0ec231ffe7b9560ec40fb587
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-10-05 10:51:23 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -4,9 +4,9 @@
 
 [["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BR"], 
      
-         ["https://t.frtyj.com/oy4kaec8jk?offer_id=3341&aff_id=54636&nopop=1"]],
+         ["https://t.frtyj.com/r7a0dekkn4?offer_id=3341&aff_id=43372&nopop=1"]],
  
-[["ZZ"], ["https://t.frtyj.com/oy4kaec8jk?offer_id=3341&aff_id=54636&nopop=1"]]  
+[["ZZ"], ["https://t.frtyj.com/r7a0dekkn4?offer_id=3341&aff_id=43372&nopop=1"]]  
                
 ],
  
@@ -15,9 +15,9 @@
 
 [["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BR"], 
      
-         ["https://t.frtyj.com/oy4kaec8jk?offer_id=3341&aff_id=54636&nopop=1"]],
+         ["https://t.frtyj.com/r7a0dekkn4?offer_id=3341&aff_id=43372&nopop=1"]],
  
-[["ZZ"], ["https://t.frtyj.com/oy4kaec8jk?offer_id=3341&aff_id=54636&nopop=1"]]             
+[["ZZ"], ["https://t.frtyj.com/r7a0dekkn4?offer_id=3341&aff_id=43372&nopop=1"]]             
 
 ]
 }
