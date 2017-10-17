cf79702133e812bb7663bffc3c32bd9c92d77735
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-10-17 17:02:01 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -4,9 +4,9 @@
 
 [["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         ["https://chaturbate.com/affiliates/in/?track=default&tour=sxJR&campaign=AQTqz"]],
+         ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz"]],
  
-[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=default&tour=sxJR&campaign=AQTqz"]]  
+[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz"]]  
                
 ],
  
@@ -15,9 +15,9 @@
 
 [["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         ["https://chaturbate.com/affiliates/in/?track=default&tour=sxJR&campaign=AQTqz"]],
+         ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz"]],
  
-[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=default&tour=sxJR&campaign=AQTqz"]]             
+[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz"]]             
 
 ]
 }
