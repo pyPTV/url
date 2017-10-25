57ab24a450911d020aad1e25c82ad2a014dcc8d0
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-10-25 10:16:44 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@
 
 [["CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO"], 
      
-         ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]],
+         ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz"]],
  
 [["ZZ"], ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]]  
                
@@ -15,7 +15,7 @@
 
 [["CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO"], 
      
-         ["http://join.fetishnetwork.com/track/MTAwMDAwMjIzOS40LjEwLjEwLjAuMC4wLjAuMA"]],
+         ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz"]],
  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz"]]             
 
