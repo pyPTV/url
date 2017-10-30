4e9365d392a501ec4307f312f1ce150206cca2fd
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-10-30 20:00:23 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -7,7 +7,8 @@
          ["http://join.allgaysitespass.com/track/MjAwNjg0LjEuMy40LjAuMC4wLjAuMA"]],
  
 [["ZZ"], ["https://join.megagayporn.com/track/MjAzLjEuMi4yLjAuMC4wLjAuMA",
-          "http://join.doctortwink.com/track/MTAwMDAwNzQwLjEuNC40LjAuMC4wLjAuMA"]]  
+          "http://join.doctortwink.com/track/MTAwMDAwNzQwLjIuNC40LjAuMC4wLjAuMA",
+          "http://join.massivegaypass.com/track/NDIwNS4yNi40Ni40Ni4wLjAuMC4wLjA"]]  
                
 ],
  
@@ -19,7 +20,8 @@
          ["http://join.allgaysitespass.com/track/MjAwNjg0LjEuMy40LjAuMC4wLjAuMA"]],
            
 [["ZZ"], ["https://join.megagayporn.com/track/MjAzLjEuMi4yLjAuMC4wLjAuMA",
-          "http://join.doctortwink.com/track/MTAwMDAwNzQwLjEuNC40LjAuMC4wLjAuMA"]]             
+          "http://join.doctortwink.com/track/MTAwMDAwNzQwLjIuNC40LjAuMC4wLjAuMA",
+          "http://join.massivegaypass.com/track/NDIwNS4yNi40Ni40Ni4wLjAuMC4wLjA"]]             
 
 ]
 }
