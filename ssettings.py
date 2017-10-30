d7d6dea7d5304dbc69fb4ac1a2aed8ae9e769da5
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-10-30 14:09:52 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -4,11 +4,7 @@
 
 [["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         [
-"http://www.iyalc.com/tsplayground/go.php?pr=8&su=2&si=165&ad=270918&pa=index&ar=&buffer=",
-"http://www.iyalc.com/trannypros/go.php?pr=8&su=2&si=117&ad=270918&pa=index&ar=&buffer=",
-"http://www.iyalc.com/shemaleidol/go.php?pr=8&su=2&si=139&ad=270918&pa=index&ar=&buffer=",
-"http://www.iyalc.com/transsexualangel/go.php?pr=8&su=2&si=237&ad=270918&pa=index&ar=&buffer="]],
+         ["http://join.shemalefuckfest.com/track/MTAwNjA1LjI2OC4zMy4zMy4wLjAuMC4wLjA"]],
  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz"]]  
                
@@ -19,11 +15,7 @@
 
 [["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         [
-"http://www.iyalc.com/tsplayground/go.php?pr=8&su=2&si=165&ad=270918&pa=index&ar=&buffer=",
-"http://www.iyalc.com/trannypros/go.php?pr=8&su=2&si=117&ad=270918&pa=index&ar=&buffer=",
-"http://www.iyalc.com/shemaleidol/go.php?pr=8&su=2&si=139&ad=270918&pa=index&ar=&buffer=",
-"http://www.iyalc.com/transsexualangel/go.php?pr=8&su=2&si=237&ad=270918&pa=index&ar=&buffer="]],
+         ["http://join.shemalefuckfest.com/track/MTAwNjA1LjI2OC4zMy4zMy4wLjAuMC4wLjA"]],
  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz"]]             
 
