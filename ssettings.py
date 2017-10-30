ff186aed250f9945008f8cc571dccf92dc08c516
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-10-30 21:48:39 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -7,7 +7,8 @@
          ["http://join.shemalefuckfest.com/track/MTAwNjA1LjI2OC4zMy4zMy4wLjAuMC4wLjA"]],
  
 [["ZZ"], ["http://join.ladyboyglamour.com/track/MTA2MDMuMi4xOC4xMTIuMC4wLjAuMC4w",
-          "http://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA"]]  
+          "http://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA",
+          "http://join.love4ladyboys.com/track/MjYwMDE2MC4xLjI3LjM0LjAuMC4wLjAuMA"]]  
                
 ],
  
@@ -19,7 +20,8 @@
          ["http://join.shemalefuckfest.com/track/MTAwNjA1LjI2OC4zMy4zMy4wLjAuMC4wLjA"]],
  
 [["ZZ"], ["http://join.ladyboyglamour.com/track/MTA2MDMuMi4xOC4xMTIuMC4wLjAuMC4w", 
-          "http://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA"]]             
+          "http://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA",
+          "http://join.love4ladyboys.com/track/MjYwMDE2MC4xLjI3LjM0LjAuMC4wLjAuMA"]]             
 
 ]
 }
