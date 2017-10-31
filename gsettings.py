de527e5172e7cdb3ff0c0711f2b841027475015d
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-10-31 13:03:53 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,7 +4,7 @@
 
 [["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         ["http://join.allgaysitespass.com/track/MjAwNjg0LjEuMy40LjAuMC4wLjAuMA"]],
+         ["http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA"]],
  
 [["ZZ"], ["https://join.megagayporn.com/track/MjAzLjEuMi4yLjAuMC4wLjAuMA",
           "http://join.doctortwink.com/track/MTAwMDAwNzQwLjIuNC40LjAuMC4wLjAuMA",
@@ -17,7 +17,7 @@
 
 [["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         ["http://join.allgaysitespass.com/track/MjAwNjg0LjEuMy40LjAuMC4wLjAuMA"]],
+         ["http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA"]],
            
 [["ZZ"], ["https://join.megagayporn.com/track/MjAzLjEuMi4yLjAuMC4wLjAuMA",
           "http://join.doctortwink.com/track/MTAwMDAwNzQwLjIuNC40LjAuMC4wLjAuMA",
