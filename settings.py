e9edc29f660255fa110655934cfd203949a2d726
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-11-09 00:34:29 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@
 
 [["CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO"], 
      
-         ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz"]],
+         ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]],
  
 [["ZZ"], ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]]  
                
