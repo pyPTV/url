1d228d971c29a777bd0293d275d1037e1a4a1999
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-11-09 08:34:04 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -4,7 +4,7 @@
 
 [["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         ["https://t.frtyj.com/b88leg2igw?offer_id=3341&aff_id=40075&nopop=1"]],
+         ["https://t.frtyj.com/g303chkb9c?offer_id=3341&aff_id=53280&nopop=1"]],
  
 [["ZZ"], ["http://join.ladyboyglamour.com/track/MTA2MDMuMi4xOC4xMTIuMC4wLjAuMC4w",
           "http://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA",
@@ -17,7 +17,7 @@
 
 [["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         ["https://t.frtyj.com/b88leg2igw?offer_id=3341&aff_id=40075&nopop=1"]],
+         ["https://t.frtyj.com/g303chkb9c?offer_id=3341&aff_id=53280&nopop=1"]],
  
 [["ZZ"], ["http://join.ladyboyglamour.com/track/MTA2MDMuMi4xOC4xMTIuMC4wLjAuMC4w", 
           "http://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA",
