7117954341380639debefb86ff4d739db11ca65f
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-11-09 08:21:14 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,7 +4,8 @@
 
 [["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]],
+         ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA",
+          "https://t.frtyj.com/lc6zumukg0?offer_id=3343&aff_id=50870&nopop=1"]],
  
 [["ZZ"], ["https://join.megagayporn.com/track/MjAzLjEuMi4yLjAuMC4wLjAuMA",  
           "http://join.eutwinks.com/track/NDIwNS4yNi4xMS4xMS4wLjAuMC4wLjA",
@@ -18,7 +19,8 @@
 
 [["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA"]],
+         ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA",
+          "https://t.frtyj.com/lc6zumukg0?offer_id=3343&aff_id=50870&nopop=1"]],
            
 [["ZZ"], ["https://join.megagayporn.com/track/MjAzLjEuMi4yLjAuMC4wLjAuMA", 
           "http://join.eutwinks.com/track/NDIwNS4yNi4xMS4xMS4wLjAuMC4wLjA",
