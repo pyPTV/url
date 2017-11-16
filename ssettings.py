5771ac63ec87cd2f4eea4059269b4e364c256d52
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-11-16 11:13:20 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -6,9 +6,7 @@
      
          ["https://t.frtyj.com/g303chkb9c?offer_id=3341&aff_id=53280&nopop=1"]],
  
-[["ZZ"], ["http://join.ladyboyglamour.com/track/MTA2MDMuMi4xOC4xMTIuMC4wLjAuMC4w",
-          "http://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA",
-          "http://join.love4ladyboys.com/track/MjYwMDE2MC4xLjI3LjM0LjAuMC4wLjAuMA"]]  
+[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz&disable_sound=True"]]  
                
 ],
  
@@ -19,9 +17,7 @@
      
          ["https://t.frtyj.com/g303chkb9c?offer_id=3341&aff_id=53280&nopop=1"]],
  
-[["ZZ"], ["http://join.ladyboyglamour.com/track/MTA2MDMuMi4xOC4xMTIuMC4wLjAuMC4w", 
-          "http://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA",
-          "http://join.love4ladyboys.com/track/MjYwMDE2MC4xLjI3LjM0LjAuMC4wLjAuMA"]]             
+[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz&disable_sound=True"]]             
 
 ]
 }
