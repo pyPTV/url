d9c6429a38be2d62cde985d4b991ec469c109899
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-11-16 11:11:44 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -6,10 +6,7 @@
      
          ["http://join.allgaysitespass.com/track/MjAwNTgyLjEuMy40LjAuMC4wLjAuMA"]],
  
-[["ZZ"], ["https://join.megagayporn.com/track/MjAzLjEuMi4yLjAuMC4wLjAuMA",  
-          "http://join.eutwinks.com/track/NDIwNS4yNi4xMS4xMS4wLjAuMC4wLjA",
-          "http://join.ultratwinks.com/track/NDIwNS4yNi4xMi4xMi4wLjAuMC4wLjA",
-          "http://join.massivegaypass.com/track/NDIwNS4yNi40Ni40Ni4wLjAuMC4wLjA"]]  
+[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=gay&tour=9rL0&campaign=AQTqz&disable_sound=True"]]  
                
 ],
  
@@ -20,10 +17,7 @@
      
          ["http://join.allgaysitespass.com/track/MjAwNTgyLjEuMy40LjAuMC4wLjAuMA"]],
            
-[["ZZ"], ["https://join.megagayporn.com/track/MjAzLjEuMi4yLjAuMC4wLjAuMA", 
-          "http://join.eutwinks.com/track/NDIwNS4yNi4xMS4xMS4wLjAuMC4wLjA",
-          "http://join.ultratwinks.com/track/NDIwNS4yNi4xMi4xMi4wLjAuMC4wLjA",
-          "http://join.massivegaypass.com/track/NDIwNS4yNi40Ni40Ni4wLjAuMC4wLjA"]]             
+[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=gay&tour=9rL0&campaign=AQTqz&disable_sound=True"]]             
 
 ]
 }
