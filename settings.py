44511833d00a742624895aea9bf696b8cc8ad2c2
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-11-18 18:56:24 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
     "mobile":
 [
 
-[["CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO"], 
+[["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
          ["https://t.frtyr.com/mht8kgpigw?offer_id=3&aff_id=54557&nopop=1"]],
  
@@ -13,7 +13,7 @@
     "desktop":
 [
 
-[["CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO"], 
+[["IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
          ["https://t.frtyr.com/mht8kgpigw?offer_id=3&aff_id=54557&nopop=1"]],
  
