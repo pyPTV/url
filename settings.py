fc72d479ece00b3e5733053fe1fda507452ea813
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-12-01 11:53:43 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@
 
 [["IS", "IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         ["https://chaturbate.com/affiliates/in/?track=strainht&tour=hr8m&campaign=AQTqz&disable_sound=True"]],
+         ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]],
  
 [["ZZ"], ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]]  
                
