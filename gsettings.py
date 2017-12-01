63ccb991f369001469a7b3b4c1c67c3cb13ad467
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-12-01 18:56:10 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,7 +4,7 @@
 
 [["IS", "IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         ["https://t.frtyj.com/1m9ds0mo3k?offer_id=3343&aff_id=54455&nopop=1"]],
+         ["http://join.allgaysitespass.com/track/MjAwNjk1LjEuMy40LjAuMC4wLjAuMA"]],
  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=gay&tour=9rL0&campaign=AQTqz&disable_sound=True"]]  
                
@@ -15,7 +15,7 @@
 
 [["IS", "IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         ["https://t.frtyj.com/1m9ds0mo3k?offer_id=3343&aff_id=54455&nopop=1"]],
+         ["http://join.allgaysitespass.com/track/MjAwNjk1LjEuMy40LjAuMC4wLjAuMA"]],
            
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=gay&tour=9rL0&campaign=AQTqz&disable_sound=True"]]             
 
