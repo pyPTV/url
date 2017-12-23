624f11a78151105262b46b75e66ae66e1b7eab2f
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-12-23 16:58:29 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -4,7 +4,7 @@
 
 [["IS", "IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         ["https://t.frtyj.com/qtvgnjwgjk?offer_id=4779&aff_id=55688&nopop=1"]],
+         ["https://t.frtyj.com/qtvgnjwgjk?offer_id=4779&aff_id=55688&nopop=1", "https://t.frtyj.com/b4w6fk1iww?offer_id=3341&aff_id=55688&nopop=1"]],
  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz&disable_sound=True"]]  
                
@@ -15,7 +15,7 @@
 
 [["IS", "IE", "CA", "US", "AU", "NZ", "GB", "DE", "CH", "NL", "SE", "AT", "DK", "NO", "FR", "BE", "FI", "IT"], 
      
-         ["https://t.frtyj.com/qtvgnjwgjk?offer_id=4779&aff_id=55688&nopop=1"]],
+         ["https://t.frtyj.com/qtvgnjwgjk?offer_id=4779&aff_id=55688&nopop=1", "https://t.frtyj.com/b4w6fk1iww?offer_id=3341&aff_id=55688&nopop=1"]],
  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz&disable_sound=True"]]             
 
