e56438401c77ce1af460e996647dc38bbec4c60e
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-12-26 00:47:20 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -5,7 +5,10 @@
 [["IS", "IE", "CA", "US", "AU", "NZ", 
   "GB", "DE", "CH", "NL", "SE", "AT", 
   "DK", "NO", "FR", "BE", "FI", "IT", 
-  "HK", "JP", "KR", "IL", "SG", "ES"], ["http://join.allgaysitespass.com/track/MjAwNjk4LjEuMy40LjAuMC4wLjAuMA"]],
+  "HK", "JP", "KR", "IL", "SG", "ES",
+  "KW", "BH", "QA", "LI", "LU", "MC",
+  "CY", "AE", "SI", "SK", "CZ", "PT",
+  "UY", "CL", "AR", "HU", "HR", "BR"], ["http://join.allgaysitespass.com/track/MjAwNjk4LjEuMy40LjAuMC4wLjAuMA"]],
  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=gay&tour=9rL0&campaign=AQTqz&disable_sound=True"]]  
                
@@ -17,7 +20,10 @@
 [["IS", "IE", "CA", "US", "AU", "NZ", 
   "GB", "DE", "CH", "NL", "SE", "AT", 
   "DK", "NO", "FR", "BE", "FI", "IT", 
-  "HK", "JP", "KR", "IL", "SG", "ES"], ["http://join.allgaysitespass.com/track/MjAwNjk4LjEuMy40LjAuMC4wLjAuMA"]],
+  "HK", "JP", "KR", "IL", "SG", "ES",
+  "KW", "BH", "QA", "LI", "LU", "MC",
+  "CY", "AE", "SI", "SK", "CZ", "PT",
+  "UY", "CL", "AR", "HU", "HR", "BR"], ["http://join.allgaysitespass.com/track/MjAwNjk4LjEuMy40LjAuMC4wLjAuMA"]],
            
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=gay&tour=9rL0&campaign=AQTqz&disable_sound=True"]]             
 
