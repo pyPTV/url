13ffe67e3ef8631affa4586b19ecb2b744f49ada
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2017-12-28 06:50:13 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -7,7 +7,7 @@
   "DK", "NO", "FR", "BE", "FI", "IT", 
   "HK", "JP", "KR", "IL", "SG", "ES",
   "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "HU", "HR", "BR"], ["https://t.frtyj.com/k1rqyhuwqo?offer_id=4779&aff_id=40075&nopop=1", "https://t.frtyj.com/dcxgxongn4?offer_id=3341&aff_id=40075&nopop=1"]],
+  "SK", "CZ", "PT", "HU", "HR", "BR"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz&disable_sound=True"]],
  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz&disable_sound=True"]]  
                
@@ -21,7 +21,7 @@
   "DK", "NO", "FR", "BE", "FI", "IT", 
   "HK", "JP", "KR", "IL", "SG", "ES",
   "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "HU", "HR", "BR"], ["https://t.frtyj.com/k1rqyhuwqo?offer_id=4779&aff_id=40075&nopop=1", "https://t.frtyj.com/dcxgxongn4?offer_id=3341&aff_id=40075&nopop=1"]],
+  "SK", "CZ", "PT", "HU", "HR", "BR"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz&disable_sound=True"]],
  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz&disable_sound=True"]]             
 
