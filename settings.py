e7cc99bf463b9817744ee5c2fd7f75da4c6d6a23
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-01-01 09:33:48 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,10 +2,8 @@
     "mobile":
 [
 
-[["IS", "IE", "CA", "US", "AU", "NZ", 
-  "GB", "DE", "CH", "NL", "SE", "AT", 
-  "DK", "NO", "FR", "BE", "FI", "IT", 
-  "HK", "JP", "ZA", "IL", "SG", "ES"], ["https://chaturbate.com/affiliates/in/?track=strainht&tour=hr8m&campaign=AQTqz&disable_sound=True"]],
+[["CA", "US", "AU", "NZ", "GB", "DE", 
+  "CH", "NL", "SE", "AT", "DK", "NO"], ["https://t.frtyr.com/lxro53jgu8?offer_id=3&aff_id=54557&nopop=1"]],
  
 [["ZZ"], ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]]  
                
@@ -14,10 +12,8 @@
     "desktop":
 [
 
-[["IS", "IE", "CA", "US", "AU", "NZ", 
-  "GB", "DE", "CH", "NL", "SE", "AT", 
-  "DK", "NO", "FR", "BE", "FI", "IT", 
-  "HK", "JP", "ZA", "IL", "SG", "ES"], ["https://chaturbate.com/affiliates/in/?track=strainht&tour=hr8m&campaign=AQTqz&disable_sound=True"]],
+[["CA", "US", "AU", "NZ", "GB", "DE", 
+  "CH", "NL", "SE", "AT", "DK", "NO"], ["https://t.frtyr.com/lxro53jgu8?offer_id=3&aff_id=54557&nopop=1"]],
  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=strainht&tour=hr8m&campaign=AQTqz&disable_sound=True"]]             
 
