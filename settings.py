692f6dfc3007d759bbaeb89b560feb3c4cb306c9
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-01-02 17:03:41 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 [
 
 [["CA", "US", "AU", "NZ", "GB", "DE", 
-  "CH", "NL", "SE", "AT", "DK", "NO"], ["https://t.frtyr.com/ui54qstrr4?offer_id=3&aff_id=54557&nopop=1"]],
+  "CH", "NL", "SE", "AT", "DK", "NO"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]],
  
 [["ZZ"], ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]]  
                
@@ -13,9 +13,9 @@
 [
 
 [["CA", "US", "AU", "NZ", "GB", "DE", 
-  "CH", "NL", "SE", "AT", "DK", "NO"], ["https://t.frtyr.com/ui54qstrr4?offer_id=3&aff_id=54557&nopop=1"]],
+  "CH", "NL", "SE", "AT", "DK", "NO"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]],
  
-[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=strainht&tour=hr8m&campaign=AQTqz&disable_sound=True"]]             
+[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]]             
 
 ]
 }
