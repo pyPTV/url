26ba2fe273b59585ebbc0e3ee7eb234ee12016e8
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-01-02 16:55:52 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -5,9 +5,7 @@
 [["IS", "IE", "CA", "US", "AU", "NZ", 
   "GB", "DE", "CH", "NL", "SE", "AT", 
   "DK", "NO", "FR", "BE", "FI", "IT", 
-  "HK", "JP", "KR", "IL", "SG", "ES",
-  "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/gnygtk6k1s?offer_id=3343&aff_id=53837&nopop=1"]],
+  "HK", "JP", "KR", "IL", "SG", "ES"], ["https://t.frtyj.com/gnygtk6k1s?offer_id=3343&aff_id=53837&nopop=1"]],
  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=gay&tour=9rL0&campaign=AQTqz&disable_sound=True"]]  
                
@@ -19,9 +17,7 @@
 [["IS", "IE", "CA", "US", "AU", "NZ", 
   "GB", "DE", "CH", "NL", "SE", "AT", 
   "DK", "NO", "FR", "BE", "FI", "IT", 
-  "HK", "JP", "KR", "IL", "SG", "ES",
-  "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/gnygtk6k1s?offer_id=3343&aff_id=53837&nopop=1"]],
+  "HK", "JP", "KR", "IL", "SG", "ES"], ["https://t.frtyj.com/gnygtk6k1s?offer_id=3343&aff_id=53837&nopop=1"]],
            
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=gay&tour=9rL0&campaign=AQTqz&disable_sound=True"]]             
 
