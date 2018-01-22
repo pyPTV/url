19019d93bef44b25de7e642a6d47f2cd422c2620
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-01-23 00:53:31 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -7,7 +7,7 @@
   "DK", "NO", "FR", "BE", "FI", "IT", 
   "HK", "JP", "KR", "IL", "SG", "ES",
   "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "AR", "HU", "HR"], ["http://join.allgaysitespass.com/track/MjAwMzUwLjEuMy40LjAuMC4wLjAuMA"]],
+  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/5ujcskr7ls?offer_id=3343&aff_id=54636&nopop=1"]],
  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=gay&tour=9rL0&campaign=AQTqz&disable_sound=True"]]  
                
@@ -21,7 +21,7 @@
   "DK", "NO", "FR", "BE", "FI", "IT", 
   "HK", "JP", "KR", "IL", "SG", "ES",
   "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "AR", "HU", "HR"], ["http://join.allgaysitespass.com/track/MjAwMzUwLjEuMy40LjAuMC4wLjAuMA"]],
+  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/5ujcskr7ls?offer_id=3343&aff_id=54636&nopop=1"]],
            
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=gay&tour=9rL0&campaign=AQTqz&disable_sound=True"]]             
 
