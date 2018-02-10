83f96d4f9893863dfbb5ee87f971b73c2a59ca26
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-02-10 16:35:58 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,12 +2,7 @@
     "mobile":
 [
 
-[["IS", "IE", "CA", "US", "AU", "NZ", 
-  "GB", "DE", "CH", "NL", "SE", "AT", 
-  "DK", "NO", "FR", "BE", "FI", "IT", 
-  "HK", "JP", "KR", "IL", "SG", "ES",
-  "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "HU", "HR", "BR"], ["https://t.frtyj.com/fc9uyd9sqo?offer_id=3341&aff_id=55688&nopop=1"]],
+[["US", "CA", "AU", "NZ", "GB", "DE"], ["https://t.frtyj.com/fc9uyd9sqo?offer_id=3341&aff_id=55688&nopop=1"]],
  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz&disable_sound=True"]]  
                
@@ -16,12 +11,7 @@
     "desktop":
 [
 
-[["IS", "IE", "CA", "US", "AU", "NZ", 
-  "GB", "DE", "CH", "NL", "SE", "AT", 
-  "DK", "NO", "FR", "BE", "FI", "IT", 
-  "HK", "JP", "KR", "IL", "SG", "ES",
-  "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "HU", "HR", "BR"], ["https://t.frtyj.com/fc9uyd9sqo?offer_id=3341&aff_id=55688&nopop=1"]],
+[["US", "CA", "AU", "NZ", "GB", "DE"], ["https://t.frtyj.com/fc9uyd9sqo?offer_id=3341&aff_id=55688&nopop=1"]],
  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz&disable_sound=True"]]             
 
