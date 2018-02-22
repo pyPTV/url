0a3fde502df075c9339b74e50da75b4f99f2dd0d
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-02-23 02:01:08 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -9,7 +9,7 @@
   "LI", "LU", "MC", "CY", "AE", "SI", 
   "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/iwv4ymk8jk?offer_id=3343&aff_id=50870&nopop=1"]],
  
-[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=gay&tour=9rL0&campaign=AQTqz&disable_sound=True"]]  
+[["ZZ"], ["http://www.gaytube4k.com"]]  
                
 ],
  
@@ -23,7 +23,7 @@
   "LI", "LU", "MC", "CY", "AE", "SI", 
   "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/iwv4ymk8jk?offer_id=3343&aff_id=50870&nopop=1"]],
            
-[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=gay&tour=9rL0&campaign=AQTqz&disable_sound=True"]]             
+[["ZZ"], ["http://www.gaytube4k.com"]]             
 
 ]
 }
