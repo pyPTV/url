1f1a2fc2b386dbd6e734a79e8a662af130cb6eed
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-02-23 15:09:48 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -7,7 +7,7 @@
   "DK", "NO", "FR", "BE", "FI", "IT", 
   "HK", "JP", "KR", "IL", "SG", "ES",
   "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/iwv4ymk8jk?offer_id=3343&aff_id=50870&nopop=1"]],
+  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/w2eczseqf4?offer_id=3343&aff_id=54557&nopop=1"]],
  
 [["ZZ"], ["http://www.gaytube4k.com"]]  
                
@@ -21,7 +21,7 @@
   "DK", "NO", "FR", "BE", "FI", "IT", 
   "HK", "JP", "KR", "IL", "SG", "ES",
   "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/iwv4ymk8jk?offer_id=3343&aff_id=50870&nopop=1"]],
+  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/w2eczseqf4?offer_id=3343&aff_id=54557&nopop=1"]],
            
 [["ZZ"], ["http://www.gaytube4k.com"]]             
 
