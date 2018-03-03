4618c9ac455c4d0f2da9f7dd743ddac3caaebf78
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-03-03 15:19:05 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,15 +2,15 @@
     "mobile":
 [
 
-[["US", "GB", "CA", "DE", "AU"], ["https://t.frtyr.com/qsre7bh9c0?offer_id=3&aff_id=53837&nopop=1"]],
-[["ZZ"], ["http://traffic.tc-clicks.com/?p=2478&media_type=adult&source_type=redirect"]]  
+[["US"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]],
+[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]]  
                
 ],
  
     "desktop":
 [
 
-[["US", "GB", "CA", "DE", "AU"], ["https://t.frtyr.com/qsre7bh9c0?offer_id=3&aff_id=53837&nopop=1"]],  
+[["US"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]],  
 [["ZZ"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]]             
 
 ]
