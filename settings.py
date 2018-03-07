09f267953d4a764c1be82d96c3a1ad11f7da9534
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-03-07 22:42:03 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@
 [["IS", "IE", "CA", "US", "AU", "NZ", 
   "GB", "DE", "CH", "NL", "SE", "AT", 
   "DK", "NO", "FR", "BE", "FI", "IT"],  ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]],
-[["ZZ"], ["https://cnhv.co/1wf99"]]  
+[["ZZ"], ["http://kinky4k.com"]]  
                
 ],
  
@@ -15,7 +15,7 @@
 [["IS", "IE", "CA", "US", "AU", "NZ", 
   "GB", "DE", "CH", "NL", "SE", "AT", 
   "DK", "NO", "FR", "BE", "FI", "IT"],  ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]],  
-[["ZZ"], ["https://cnhv.co/1wf99"]]             
+[["ZZ"], ["http://kinky4k.com"]]             
 
 ]
 }
