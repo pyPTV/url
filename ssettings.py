fd965ff7316030a59cb26ec04f9832b3735c9cc5
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-03-07 22:40:50 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -5,7 +5,7 @@
 [["US", "CA", "AU", "NZ", "GB", "DE", "FR", 
    "IT", "DK", "SE", "NO", "CH"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz&disable_sound=True"]],
  
-[["ZZ"], ["https://cnhv.co/1wf99"]]  
+[["ZZ"], ["http://tshub4k.com"]]  
                
 ],
  
@@ -15,7 +15,7 @@
 [["US", "CA", "AU", "NZ", "GB", "DE", "FR", 
   "IT", "DK", "SE", "NO", "CH"], ["https://chaturbate.com/affiliates/in/?track=trans&tour=sxJR&campaign=AQTqz&disable_sound=True"]],
  
-[["ZZ"], ["https://cnhv.co/1wf99"]]             
+[["ZZ"], ["http://tshub4k.com"]]             
 
 ]
 }
