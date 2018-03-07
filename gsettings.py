ecfef77dd105f551d85c22635f043c96afc5a90c
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-03-07 19:21:08 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -9,7 +9,7 @@
   "LI", "LU", "MC", "CY", "AE", "SI", 
   "SK", "CZ", "PT", "AR", "HU", "HR"], ["http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA"]],
  
-[["ZZ"], ["https://chaturbate.com/in/?track=gay&tour=R2Xc&campaign=AQTqz&disable_sound=True"]]  
+[["ZZ"], ["https://cnhv.co/1weqf"]]  
                
 ],
  
@@ -23,7 +23,7 @@
   "LI", "LU", "MC", "CY", "AE", "SI", 
   "SK", "CZ", "PT", "AR", "HU", "HR"], ["http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA"]],
            
-[["ZZ"], ["https://chaturbate.com/in/?track=gay&tour=R2Xc&campaign=AQTqz&disable_sound=True"]]             
+[["ZZ"], ["https://cnhv.co/1weqf"]]             
 
 ]
 }
