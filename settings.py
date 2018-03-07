8a3fcfc7a99bc5fef3d7232037c5ae814f0203ec
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-03-07 19:32:20 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,16 +2,20 @@
     "mobile":
 [
 
-[["US"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]],
-[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]]  
+[["IS", "IE", "CA", "US", "AU", "NZ", 
+  "GB", "DE", "CH", "NL", "SE", "AT", 
+  "DK", "NO", "FR", "BE", "FI", "IT"],  ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]],
+[["ZZ"], ["https://cnhv.co/1weur"]]  
                
 ],
  
     "desktop":
 [
 
-[["US"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]],  
-[["ZZ"], ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]]             
+[["IS", "IE", "CA", "US", "AU", "NZ", 
+  "GB", "DE", "CH", "NL", "SE", "AT", 
+  "DK", "NO", "FR", "BE", "FI", "IT"],  ["https://chaturbate.com/affiliates/in/?track=straight&tour=hr8m&campaign=AQTqz&disable_sound=True"]],  
+[["ZZ"], ["https://cnhv.co/1weur"]]             
 
 ]
 }
