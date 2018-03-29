c84384da118869b8ccb80cfed13a5a9f1e596138
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-03-29 23:39:28 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -7,7 +7,7 @@
   "DK", "NO", "FR", "BE", "FI", "IT", 
   "HK", "JP", "KR", "IL", "SG", "ES",
   "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "AR", "HU", "HR"], ["http://join.allgaysitespass.com/track/MjAwNjg0LjEuMy40LjAuMC4wLjAuMA"]],
+  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/h2kpl4u8zk?offer_id=3343&aff_id=53837&nopop=1"]],
  
 [["ZZ"], ["https://t.irtye.com/j9gfnx97wg?offer_id=4080&aff_id=55931&nopop=1"]]  
                
@@ -21,7 +21,7 @@
   "DK", "NO", "FR", "BE", "FI", "IT", 
   "HK", "JP", "KR", "IL", "SG", "ES",
   "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "AR", "HU", "HR"], ["http://join.allgaysitespass.com/track/MjAwNjg0LjEuMy40LjAuMC4wLjAuMA"]],
+  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/h2kpl4u8zk?offer_id=3343&aff_id=53837&nopop=1"]],
            
 [["ZZ"], ["https://t.irtye.com/j9gfnx97wg?offer_id=4080&aff_id=55931&nopop=1"]]             
 
