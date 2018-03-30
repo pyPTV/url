52b414760fb0118a8f08f5d58bb14bf7b8860561
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-03-30 23:21:01 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@
 
 [["IS", "IE", "CA", "US", "AU", "NZ", 
   "GB", "DE", "CH", "NL", "SE", "AT", 
-  "DK", "NO", "FR", "BE", "FI", "IT"],  ["https://t.irtye.com/m9o82u72rk?offer_id=3788&aff_id=55931&nopop=1"]],
+  "DK", "NO", "FR", "BE", "FI", "IT"],  ["https://t.irtye.com/26nioknvpc?offer_id=3788&aff_id=50870&nopop=1"]],
 [["ZZ"], ["https://t.irtye.com/m9o82u72rk?offer_id=3788&aff_id=55931&nopop=1"]]  
                
 ],
@@ -14,7 +14,7 @@
 
 [["IS", "IE", "CA", "US", "AU", "NZ", 
   "GB", "DE", "CH", "NL", "SE", "AT", 
-  "DK", "NO", "FR", "BE", "FI", "IT"],  ["https://t.irtye.com/m9o82u72rk?offer_id=3788&aff_id=55931&nopop=1"]],  
+  "DK", "NO", "FR", "BE", "FI", "IT"],  ["https://t.irtye.com/26nioknvpc?offer_id=3788&aff_id=50870&nopop=1"]],  
 [["ZZ"], ["https://t.irtye.com/m9o82u72rk?offer_id=3788&aff_id=55931&nopop=1"]]             
 
 ]
