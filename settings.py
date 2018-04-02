7d341e936ee87ecb7dac168518122dae681f8d49
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-03 00:00:38 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,9 +2,9 @@
     "mobile":
 [
 
-[["IS", "IE", "CA", "US", "AU", "NZ", 
+[["IS", "IE", "CA", "AU", "NZ", "ES",
   "GB", "DE", "CH", "NL", "SE", "AT", 
-  "DK", "NO", "FR", "BE", "FI", "IT"],  ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=vCirA&disable_sound=True"]],
+  "DK", "NO", "FR", "BE", "FI", "IT"],  ["https://t.irtye.com/96bqkiaips?offer_id=3788&aff_id=50870&nopop=1"]],
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=vCirA&disable_sound=True"]]  
                
 ],
@@ -12,9 +12,9 @@
     "desktop":
 [
 
-[["IS", "IE", "CA", "US", "AU", "NZ", 
+[["IS", "IE", "CA", "AU", "NZ", "ES",
   "GB", "DE", "CH", "NL", "SE", "AT", 
-  "DK", "NO", "FR", "BE", "FI", "IT"],  ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=vCirA&disable_sound=True"]],  
+  "DK", "NO", "FR", "BE", "FI", "IT"],  ["https://t.irtye.com/96bqkiaips?offer_id=3788&aff_id=50870&nopop=1"]],  
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=vCirA&disable_sound=True"]]             
 
 ]
