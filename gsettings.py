d672ec3936f17b5b656e6cf724b395233f0a881b
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-02 13:32:16 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -7,7 +7,7 @@
   "DK", "NO", "FR", "BE", "FI", "IT", 
   "HK", "JP", "KR", "IL", "SG", "ES",
   "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/ho8seb49mo?offer_id=3343&aff_id=55688&nopop=1"]],
+  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/h3oohvr54w?offer_id=3343&aff_id=53837&nopop=1"]],
  
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=9rL0&campaign=vCirA&disable_sound=True"]]  
                
@@ -21,7 +21,7 @@
   "DK", "NO", "FR", "BE", "FI", "IT", 
   "HK", "JP", "KR", "IL", "SG", "ES",
   "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/ho8seb49mo?offer_id=3343&aff_id=55688&nopop=1"]],
+  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/h3oohvr54w?offer_id=3343&aff_id=53837&nopop=1"]],
            
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=9rL0&campaign=vCirA&disable_sound=True"]]             
 
