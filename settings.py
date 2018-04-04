563db6bcb2842067a3be3db3c305f55db3458b08
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-04 06:50:02 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -18,7 +18,7 @@
   "GB", "DE", "CH", "NL", "SE", "AT", 
   "DK", "NO", "FR", "BE", "FI", "IT",
   "JP", "SG", "HK", "SI", "SK", "CZ",
-  "IL", "ZA", "US"],  ["https://t.grtyi.com/o0ohaxjcxs?offer_id=4632&aff_id=40075&nopop=1"]],  
+  "IL", "ZA", "US"],  ["https://enter.javhd.com/track/MjI1NjguMjAuMi4xMzEuMC4wLjAuMC4w"]],  
 [["ZZ"], ["https://chaturbate.com"]]             
 
 ]
