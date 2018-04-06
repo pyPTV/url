d03d542b7877ce707aecf1f0cee8521da5c79990
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-06 23:17:28 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,20 +2,16 @@
     "mobile":
 [
 
-[["US", "CA", "AU", "NZ", "GB", "DE", "FR", 
-  "IT", "DK", "SE", "NO", "CH"], ["http://www.trannytube.tv"]],
- 
-[["ZZ"], ["http://www.trannytube.tv"]]  
+[["US"], ["http://www.trannytube.tv"]], 
+[["ZZ"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]]  
                
 ],
  
     "desktop":
 [
 
-[["US", "CA", "AU", "NZ", "GB", "DE", "FR", 
-  "IT", "DK", "SE", "NO", "CH"], ["http://www.trannytube.tv"]],
- 
-[["ZZ"], ["http://www.trannytube.tv"]]             
+[["US"], ["http://www.trannytube.tv"]], 
+[["ZZ"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]]             
 
 ]
 }
