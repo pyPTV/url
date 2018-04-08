3711c7b65e49cb29a2a16b2f66315fc061bfe520
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-09 02:12:31 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -4,7 +4,9 @@
 
 [["US"], ["http://www.trannytube.tv"]], 
 [["CA", "UK", "DE", "FR", 
-  "CH", "IT", "IE", "IS"], ["http://join.shemalemovieworld.com/track/MjAxMDE4LjEuMTAyLjEwMy4wLjAuMC4wLjA"]],
+  "CH", "IT", "IE", "IS",
+  "NO", "SE", "DK", "FI",
+  "NL", "BE", "AT", "CZ"], ["http://join.shemalemovieworld.com/track/MjAxMDE4LjEuMTAyLjEwMy4wLjAuMC4wLjA"]],
 [["ZZ"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]]  
                
 ],
@@ -14,7 +16,9 @@
 
 [["US"], ["http://www.trannytube.tv"]], 
 [["CA", "UK", "DE", "FR", 
-  "CH", "IT", "IE", "IS"], ["http://join.shemalemovieworld.com/track/MjAxMDE4LjEuMTAyLjEwMy4wLjAuMC4wLjA"]],
+  "CH", "IT", "IE", "IS",
+  "NO", "SE", "DK", "FI",
+  "NL", "BE", "AT", "CZ"], ["http://join.shemalemovieworld.com/track/MjAxMDE4LjEuMTAyLjEwMy4wLjAuMC4wLjA"]],
 [["ZZ"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]]             
 
 ]
