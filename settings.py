af7b5717a9f0528eb0e3662c1ec82ff628f351ee
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-10 06:39:05 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,26 +2,18 @@
     "mobile":
 [
 
-[["IS", "IE", "ES",
-  "DE", "CH", "NL", "SE", "AT", 
-  "DK", "NO", "BE", "FI", "IT",
-  "JP", "SG", "HK", "SI", "SK", "CZ",
-  "IL", "ZA", "PT", "SG", "HK"],  ["https://t.irtyf.com/s9nerd7u4g?offer_id=3788&aff_id=50870&nopop=1"]],
+[["US", "CA", "GB", "FR", "AU", "NZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
 [["JP"], ["https://enter.javhd.com/track/MjI1NjguMjAuMi4xMzEuMC4wLjAuMC4w"]],
-[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]]  
+[["ZZ"], ["https://t.irtyf.com/s9nerd7u4g?offer_id=3788&aff_id=50870&nopop=1"]]  
                
 ],
  
     "desktop":
 [
 
-[["IS", "IE", "ES",
-  "DE", "CH", "NL", "SE", "AT", 
-  "DK", "NO", "BE", "FI", "IT",
-  "JP", "SG", "HK", "SI", "SK", "CZ",
-  "IL", "ZA", "PT", "SG", "HK"],  ["https://t.irtyf.com/s9nerd7u4g?offer_id=3788&aff_id=50870&nopop=1"]],  
+[["US", "CA", "GB", "FR", "AU", "NZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
 [["JP"], ["https://enter.javhd.com/track/MjI1NjguMjAuMi4xMzEuMC4wLjAuMC4w"]],
-[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]]             
+[["ZZ"], ["https://t.irtyf.com/s9nerd7u4g?offer_id=3788&aff_id=50870&nopop=1"]]      
 
 ]
 }
