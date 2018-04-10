6b28ffb7d4c6c63e0b0489d0c44f295749e49d72
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-10 07:15:48 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -3,10 +3,6 @@
 [
 
 [["US"], ["http://www.trannytube.tv"]], 
-[["CA", "UK", "DE", "FR", 
-  "CH", "IT", "IE", "IS",
-  "NO", "SE", "DK", "FI",
-  "NL", "BE", "AT", "CZ"], ["http://join.shemalemovieworld.com/track/MjAxMDE4LjEuMTAyLjEwMy4wLjAuMC4wLjA"]],
 [["ZZ"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]]  
                
 ],
@@ -15,10 +11,6 @@
 [
 
 [["US"], ["http://www.trannytube.tv"]], 
-[["CA", "UK", "DE", "FR", 
-  "CH", "IT", "IE", "IS",
-  "NO", "SE", "DK", "FI",
-  "NL", "BE", "AT", "CZ"], ["http://join.shemalemovieworld.com/track/MjAxMDE4LjEuMTAyLjEwMy4wLjAuMC4wLjA"]],
 [["ZZ"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]]             
 
 ]
