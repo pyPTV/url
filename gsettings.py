dfd689fad26740a070f8cb67c5acf62fd2f64f94
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-10 06:53:06 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -7,7 +7,7 @@
   "DK", "NO", "FR", "BE", "FI", "IT", 
   "HK", "JP", "KR", "IL", "SG", "ES",
   "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/2ydx8bxao0?offer_id=3343&aff_id=53837&nopop=1"]],
+  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/c3j5647d6o?offer_id=3343&aff_id=43372&nopop=1"]],
  
 [["ZZ"], ["http://www.freegaypornaccess.com/?t=405173,11,15,0"]]  
                
@@ -21,7 +21,7 @@
   "DK", "NO", "FR", "BE", "FI", "IT", 
   "HK", "JP", "KR", "IL", "SG", "ES",
   "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/2ydx8bxao0?offer_id=3343&aff_id=53837&nopop=1"]],
+  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/c3j5647d6o?offer_id=3343&aff_id=43372&nopop=1"]],
            
 [["ZZ"], ["http://www.freegaypornaccess.com/?t=405173,11,15,0"]]             
 
