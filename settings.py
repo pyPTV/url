dad730cb4def83fd5d8554f21c14559cbc67f5b8
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-10 06:29:55 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,9 +6,9 @@
   "GB", "DE", "CH", "NL", "SE", "AT", 
   "DK", "NO", "FR", "BE", "FI", "IT",
   "JP", "SG", "HK", "SI", "SK", "CZ",
-  "IL", "ZA", "US", "PT", "SG", "HK"],  ["https://t.irtyf.com/ka3jrx5vcw?offer_id=3788&aff_id=55931&nopop=1"]],
+  "IL", "ZA", "PT", "SG", "HK"],  ["https://t.irtyf.com/ka3jrx5vcw?offer_id=3788&aff_id=55931&nopop=1"]],
 [["JP"], ["https://enter.javhd.com/track/MjI1NjguMjAuMi4xMzEuMC4wLjAuMC4w"]],
-[["ZZ"], ["https://t.irtyf.com/ka3jrx5vcw?offer_id=3788&aff_id=55931&nopop=1"]]  
+[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]]  
                
 ],
  
@@ -19,9 +19,9 @@
   "GB", "DE", "CH", "NL", "SE", "AT", 
   "DK", "NO", "FR", "BE", "FI", "IT",
   "JP", "SG", "HK", "SI", "SK", "CZ",
-  "IL", "ZA", "US", "PT", "SG", "HK"],  ["https://t.irtyf.com/ka3jrx5vcw?offer_id=3788&aff_id=55931&nopop=1"]],  
+  "IL", "ZA", "PT", "SG", "HK"],  ["https://t.irtyf.com/ka3jrx5vcw?offer_id=3788&aff_id=55931&nopop=1"]],  
 [["JP"], ["https://enter.javhd.com/track/MjI1NjguMjAuMi4xMzEuMC4wLjAuMC4w"]],
-[["ZZ"], ["https://t.irtyf.com/ka3jrx5vcw?offer_id=3788&aff_id=55931&nopop=1"]]             
+[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]]             
 
 ]
 }
