2b2f6df3b494f0b4f2a40352a8f7b67967dd65f2
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-10 07:06:19 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -2,28 +2,22 @@
     "mobile":
 [
 
-[["IS", "IE", "CA", "US", "AU", "NZ", 
-  "GB", "DE", "CH", "NL", "SE", "AT", 
-  "DK", "NO", "FR", "BE", "FI", "IT", 
-  "HK", "JP", "KR", "IL", "SG", "ES",
-  "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/c3j5647d6o?offer_id=3343&aff_id=43372&nopop=1"]],
- 
-[["ZZ"], ["http://www.freegaypornaccess.com/?t=405173,11,15,0"]]  
+[["US", "CA", "GB", "FR", "AU", "NZ", 
+  "DE", "AT", "BE", "IT", "NL", "CH",
+  "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/c3j5647d6o?offer_id=3343&aff_id=43372&nopop=1"]],
+
+[["ZZ"], ["https://t.irtyf.com/j5nhqntyo0?offer_id=4080&aff_id=50870&nopop=1"]]  
                
 ],
  
     "desktop":
 [
 
-[["IS", "IE", "CA", "US", "AU", "NZ", 
-  "GB", "DE", "CH", "NL", "SE", "AT", 
-  "DK", "NO", "FR", "BE", "FI", "IT", 
-  "HK", "JP", "KR", "IL", "SG", "ES",
-  "LI", "LU", "MC", "CY", "AE", "SI", 
-  "SK", "CZ", "PT", "AR", "HU", "HR"], ["https://t.frtyj.com/c3j5647d6o?offer_id=3343&aff_id=43372&nopop=1"]],
-           
-[["ZZ"], ["http://www.freegaypornaccess.com/?t=405173,11,15,0"]]             
+[["US", "CA", "GB", "FR", "AU", "NZ", 
+  "DE", "AT", "BE", "IT", "NL", "CH",
+  "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/c3j5647d6o?offer_id=3343&aff_id=43372&nopop=1"]],
+
+[["ZZ"], ["https://t.irtyf.com/j5nhqntyo0?offer_id=4080&aff_id=50870&nopop=1"]]              
 
 ]
 }
