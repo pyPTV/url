d8ec588b53bf11d62c5e887ef8ba6e201fd43aaf
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-11 05:32:39 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
     "mobile":
 [
 
-[["US", "CA", "GB", "FR", "AU", "NZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
+[["US", "CA", "GB", "IE", "FR", "AU", "NZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
 [["JP"], ["https://enter.javhd.com/track/MjI1NjguMjAuMi4xMzEuMC4wLjAuMC4w"]],
 [["ZZ"], ["https://t.irtyf.com/s9nerd7u4g?offer_id=3788&aff_id=50870&nopop=1"]]  
                
@@ -11,7 +11,7 @@
     "desktop":
 [
 
-[["US", "CA", "GB", "FR", "AU", "NZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
+[["US", "CA", "GB", "IE", "FR", "AU", "NZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
 [["JP"], ["https://enter.javhd.com/track/MjI1NjguMjAuMi4xMzEuMC4wLjAuMC4w"]],
 [["ZZ"], ["https://t.irtyf.com/s9nerd7u4g?offer_id=3788&aff_id=50870&nopop=1"]]      
 
