26cf8704bef3266f8fd11df7657ff6aeea04f266
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-11 05:07:09 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -6,7 +6,7 @@
 [["FR", "DE", "AT", "BE", 
   "IT", "NL", "CH", "IS", 
   "IE", "NO", "SE", "DK", 
-  "FI", "NZ"], 
+  "FI", "NZ", "UK", "AU"], 
 ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]],                 
 
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=sxJR&campaign=mHsP9&disable_sound=True"]]  
