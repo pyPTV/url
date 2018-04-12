af3dd8ec8797e25be1f80575b98cd46492d00bbe
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-12 15:52:05 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@
 [["RU", "UA", "BY", "KZ", "PL", "HU", "SI", 
   "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
   "RS", "IL", "JO", "AE", "AM", "GE", "AZ"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female&pt=http"]],
-[["JP"], ["https://enter.javhd.com/track/MjI1NjguMjAuMi4xMzEuMC4wLjAuMC4w"]],
+[["JP", "TW", "KR", "CN"], ["https://enter.javhd.com/track/MjI1NjguMjAuMi4xMzEuMC4wLjAuMC4w"]],
 [["ZZ"], ["https://t.irtyf.com/s9nerd7u4g?offer_id=3788&aff_id=50870&nopop=1"]]  
                
 ],
@@ -18,7 +18,7 @@
 [["RU", "UA", "BY", "KZ", "PL", "HU", "SI", 
   "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
   "RS", "IL", "JO", "AE", "AM", "GE", "AZ"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female&pt=http"]],
-[["JP"], ["https://enter.javhd.com/track/MjI1NjguMjAuMi4xMzEuMC4wLjAuMC4w"]],
+[["JP", "TW", "KR", "CN"], ["https://enter.javhd.com/track/MjI1NjguMjAuMi4xMzEuMC4wLjAuMC4w"]],
 [["ZZ"], ["https://t.irtyf.com/s9nerd7u4g?offer_id=3788&aff_id=50870&nopop=1"]]      
 
 ]
