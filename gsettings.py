ecd81bf93112c692cc2259fe57e848a233b13292
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-13 12:00:56 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,7 +4,7 @@
 
 [["US", "CA", "GB", "FR", "AU", "NZ", 
   "DE", "AT", "BE", "IT", "NL", "CH",
-  "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/c3j5647d6o?offer_id=3343&aff_id=43372&nopop=1"]],
+  "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/fr6aryhh1c?offer_id=3343&aff_id=59764&nopop=1"]],
 
 [["ZZ"], ["https://t.irtyf.com/j5nhqntyo0?offer_id=4080&aff_id=50870&nopop=1"]]  
                
@@ -15,7 +15,7 @@
 
 [["US", "CA", "GB", "FR", "AU", "NZ", 
   "DE", "AT", "BE", "IT", "NL", "CH",
-  "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/c3j5647d6o?offer_id=3343&aff_id=43372&nopop=1"]],
+  "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/fr6aryhh1c?offer_id=3343&aff_id=59764&nopop=1"]],
 
 [["ZZ"], ["https://t.irtyf.com/j5nhqntyo0?offer_id=4080&aff_id=50870&nopop=1"]]              
 
