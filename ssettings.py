3e392eea5f1adbd8765083a47189ae475d1e9547
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-16 06:29:21 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -7,7 +7,7 @@
 ["https://t.frtyj.com/pra2u2662o?offer_id=2902&aff_id=40075&nopop=1"]],    
     
     
-[["DE", "NL", "UK", "AU", "ZA", "SG", "TW", "ES", "PT"],                          ], 
+[["DE", "NL", "UK", "AU", "ZA", "SG", "TW", "ES", "PT", "CZ", "SK", "SI", "HU"], 
 ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]], 
     
 [["ZZ"], ["http://www.youtrannytube.com"]]  
@@ -22,7 +22,7 @@
 ["https://t.frtyj.com/pra2u2662o?offer_id=2902&aff_id=40075&nopop=1"]],    
     
     
-[["DE", "NL", "UK", "AU", "ZA", "SG", "TW", "ES", "PT"],], 
+[["DE", "NL", "UK", "AU", "ZA", "SG", "TW", "ES", "PT", "CZ", "SK", "SI", "HU"], 
 ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]], 
  
 [["ZZ"], ["http://www.youtrannytube.com"]]            
