2ab63103a80d5a559534e6ffc29dbcbc31a0eda6
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-04-18 15:36:32 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
     "mobile":
 [
 
-[["US", "CA", "GB", "IE", "FR", "AU", "NZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
+[["US", "CA", "GB", "IE", "FR", "AU", "NZ", "DE"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
 [["RU", "UA", "BY", "KZ", "PL", "HU", "SI", 
   "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
   "RS", "IL", "JO", "AE", "AM", "GE", "AZ"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female&pt=http"]],
@@ -15,7 +15,7 @@
     "desktop":
 [
 
-[["US", "CA", "GB", "IE", "FR", "AU", "NZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
+[["US", "CA", "GB", "IE", "FR", "AU", "NZ", "DE"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
 [["RU", "UA", "BY", "KZ", "PL", "HU", "SI", 
   "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
   "RS", "IL", "JO", "AE", "AM", "GE", "AZ"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female&pt=http"]],
