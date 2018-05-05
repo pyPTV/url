5ebc8bfbf53e470df87d1253902d7c008bc2ba7e
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-05-05 07:10:48 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -3,7 +3,7 @@
 [
 
 [["FR", "AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "NZ"], 
-["https://t.frtyj.com/vyk9f0n3nk?offer_id=2902&aff_id=56817&nopop=1"]],    
+["https://t.frtyj.com/vyk9f0n3nk?offer_id=2902&aff_id=56817&nopop=1", "https://t.frtyj.com/yyg68b2740?offer_id=3341&aff_id=56817&nopop=1"]],    
     
     
 [["DE", "NL", "UK", "AU", "ZA", "SG", "TW", "ES", "PT", "CZ", "SK", "SI", "HU", "LI", "LU"], 
@@ -17,7 +17,7 @@
 [
 
 [["FR", "AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "NZ"], 
-["https://t.frtyj.com/vyk9f0n3nk?offer_id=2902&aff_id=56817&nopop=1"]],    
+["https://t.frtyj.com/vyk9f0n3nk?offer_id=2902&aff_id=56817&nopop=1", "https://t.frtyj.com/yyg68b2740?offer_id=3341&aff_id=56817&nopop=1"]],    
     
     
 [["DE", "NL", "UK", "AU", "ZA", "SG", "TW", "ES", "PT", "CZ", "SK", "SI", "HU", "LI", "LU"], 
