574074a7aee19d368bfb119d2e87042439f7295e
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-05-07 08:57:25 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -6,7 +6,7 @@
   "DE", "AT", "BE", "IT", "NL", "CH",
   "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/sw71zu8jsw?offer_id=3343&aff_id=40075&nopop=1"]],
 [["IN", "PH", "CN", "ID"], ["http://www.me-gay.com"]],  
-[["ZZ"], ["https://t.frtyj.com/y761gz0sqo?offer_id=3343&aff_id=40075&nopop=1"]]  
+[["ZZ"], ["https://t.frtyj.com/2m7e2lyiio?offer_id=3343&aff_id=56817&nopop=1"]]  
                
 ],
  
@@ -17,7 +17,7 @@
   "DE", "AT", "BE", "IT", "NL", "CH",
   "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/sw71zu8jsw?offer_id=3343&aff_id=40075&nopop=1"]],
 [["IN", "PH", "CN", "ID"], ["http://www.me-gay.com"]],
-[["ZZ"], ["https://t.frtyj.com/y761gz0sqo?offer_id=3343&aff_id=40075&nopop=1"]]              
+[["ZZ"], ["https://t.frtyj.com/2m7e2lyiio?offer_id=3343&aff_id=56817&nopop=1"]]              
 
 ]
 }
