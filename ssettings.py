c17398a8d5d0547d2c037af5478e7ba29ce40d70
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-05-07 16:58:52 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,11 +2,11 @@
     "mobile":
 [
 
-[["FR", "AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "NZ"], 
-["https://chaturbate.com/in/?track=default&tour=sxJR&campaign=NOak5&disable_sound=True"]],    
+[["FR", "AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "NZ",
+  "ZA", "SG", "TW", "ES", "PT", "CZ", "SK", "SI", "HU", "LI", "LU", "BR"], 
+["https://chaturbate.com/in/?track=default&tour=sxJR&campaign=NOak5&disable_sound=True"]],     
     
-    
-[["DE", "NL", "UK", "AU", "ZA", "SG", "TW", "ES", "PT", "CZ", "SK", "SI", "HU", "LI", "LU"], 
+[["DE", "NL", "UK", "AU"], 
 ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]], 
     
 [["ZZ"], ["http://www.youtrannytube.com"]]  
@@ -16,11 +16,11 @@
     "desktop":
 [
 
-[["FR", "AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "NZ"], 
-["https://chaturbate.com/in/?track=default&tour=sxJR&campaign=NOak5&disable_sound=True"]],    
+[["FR", "AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "NZ",
+  "ZA", "SG", "TW", "ES", "PT", "CZ", "SK", "SI", "HU", "LI", "LU", "BR"], 
+["https://chaturbate.com/in/?track=default&tour=sxJR&campaign=NOak5&disable_sound=True"]],     
     
-    
-[["DE", "NL", "UK", "AU", "ZA", "SG", "TW", "ES", "PT", "CZ", "SK", "SI", "HU", "LI", "LU"], 
+[["DE", "NL", "UK", "AU"], 
 ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]], 
  
 [["ZZ"], ["http://www.youtrannytube.com"]]            
