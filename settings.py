1bbf5b7aa1deaa564cc2a3ab32815fe85d65129b
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-05-14 18:38:00 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,24 +2,24 @@
     "mobile":
 [
 
-[["US", "CA", "GB", "IE", "FR", "AU", "NZ", "DE"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
+[["US", "CA", "GB", "IE", "FR", "AU", "NZ", "DE"], ["https://chaturbate.com/in/?track=qum&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
 [["RU", "UA", "BY", "KZ", "PL", "HU", "SI", 
   "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
   "RS", "IL", "JO", "AE", "AM", "GE", "AZ"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female&pt=http"]],
 [["JP", "TW", "SG", "KR", "IN", "PH", "CN", "ID", "TH", "VN", "MY"], ["https://enter.javhd.com/track/MjIwOTIuMjAuMi4yLjAuMC4wLjAuMA"]],
-[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]]  
+[["ZZ"], ["https://chaturbate.com/in/?track=zzm&tour=hr8m&campaign=zKhCt&disable_sound=True"]]  
                
 ],
  
     "desktop":
 [
 
-[["US", "CA", "GB", "IE", "FR", "AU", "NZ", "DE"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
+[["US", "CA", "GB", "IE", "FR", "AU", "NZ", "DE"], ["https://chaturbate.com/in/?track=qud&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
 [["RU", "UA", "BY", "KZ", "PL", "HU", "SI", 
   "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
   "RS", "IL", "JO", "AE", "AM", "GE", "AZ"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female&pt=http"]],
 [["JP", "TW", "SG", "KR", "IN", "PH", "CN", "ID", "TH", "VN", "MY"], ["https://enter.javhd.com/track/MjIwOTIuMjAuMi4yLjAuMC4wLjAuMA"]],
-[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]]      
+[["ZZ"], ["https://chaturbate.com/in/?track=zzd&tour=hr8m&campaign=zKhCt&disable_sound=True"]]      
 
 ]
 }
