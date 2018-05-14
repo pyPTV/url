1cbfcde423148031b2eeb142f54b505b119a080f
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-05-14 18:30:18 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,8 +6,7 @@
 [["RU", "UA", "BY", "KZ", "PL", "HU", "SI", 
   "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
   "RS", "IL", "JO", "AE", "AM", "GE", "AZ"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female&pt=http"]],
-[["JP", "TW", "KR"], ["https://enter.javhd.com/track/MjIwOTIuMjAuMi4yLjAuMC4wLjAuMA"]],
-[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://ru-xvideos.tv/porno-videos/"]],
+[["JP", "TW", "KR", "IN", "PH", "CN", "ID", "TH", "VN"], ["https://enter.javhd.com/track/MjIwOTIuMjAuMi4yLjAuMC4wLjAuMA"]],
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]]  
                
 ],
@@ -19,8 +18,7 @@
 [["RU", "UA", "BY", "KZ", "PL", "HU", "SI", 
   "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
   "RS", "IL", "JO", "AE", "AM", "GE", "AZ"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female&pt=http"]],
-[["JP", "TW", "KR"], ["https://enter.javhd.com/track/MjIwOTIuMjAuMi4yLjAuMC4wLjAuMA"]],
-[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://ru-xvideos.tv/porno-videos/"]],
+[["JP", "TW", "KR", "IN", "PH", "CN", "ID", "TH", "VN"], ["https://enter.javhd.com/track/MjIwOTIuMjAuMi4yLjAuMC4wLjAuMA"]],
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=zKhCt&disable_sound=True"]]      
 
 ]
