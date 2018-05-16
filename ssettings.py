932411b2f1191028aef399452bdb3f061477191c
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-05-16 10:39:28 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,11 +2,13 @@
     "mobile":
 [
 
-[["FR", "AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "NZ",
-  "ZA", "SG", "TW", "ES", "PT", "CZ", "SK", "SI", "HU", "LI", "LU", "BR",
+[["FR", "FI", "NZ", "ZA", "SG", 
+  "TW", "ES", "PT", "CZ", "SK", 
+  "SI", "HU", "LI", "LU", "BR",
   "DE", "NL", "AU", "CA", "UK"], 
 ["https://chaturbate.com/in/?track=default&tour=sxJR&campaign=NOak5&disable_sound=True"]],
-    
+[["AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK"], 
+["https://t.frtyj.com/ioixcl6og0?offer_id=3341&aff_id=55931&nopop=1"]],    
 [["ZZ"], ["http://www.youtrannytube.com"]]  
                
 ],
@@ -14,12 +16,14 @@
     "desktop":
 [
 
-[["FR", "AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "NZ",
-  "ZA", "SG", "TW", "ES", "PT", "CZ", "SK", "SI", "HU", "LI", "LU", "BR",
+[["FR", "FI", "NZ", "ZA", "SG", 
+  "TW", "ES", "PT", "CZ", "SK", 
+  "SI", "HU", "LI", "LU", "BR",
   "DE", "NL", "AU", "CA", "UK"], 
-["https://chaturbate.com/in/?track=default&tour=sxJR&campaign=NOak5&disable_sound=True"]],    
-  
-[["ZZ"], ["http://www.youtrannytube.com"]]           
+["https://chaturbate.com/in/?track=default&tour=sxJR&campaign=NOak5&disable_sound=True"]],
+[["AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK"], 
+["https://t.frtyj.com/ioixcl6og0?offer_id=3341&aff_id=55931&nopop=1"]],    
+[["ZZ"], ["http://www.youtrannytube.com"]]          
 
 ]
 }
