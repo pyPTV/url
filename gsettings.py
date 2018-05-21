d5cedf2db9ef189ddbc573d90e53ef9ed76e7afa
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-05-21 13:20:31 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,7 +4,7 @@
 
 [["US", "CA", "GB", "FR", "AU", "NZ", 
   "DE", "AT", "BE", "IT", "NL", "CH",
-  "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/cje7lz8eo0?offer_id=3343&aff_id=50870&nopop=1"]],
+  "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/6dtg6zuybk?offer_id=3343&aff_id=54557&nopop=1"]],
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["http://www.me-gay.com"]],  
 [["ZZ"], ["https://t.frtyj.com/dhy7mh6y68?offer_id=3343&aff_id=55931&nopop=1"]]  
                
@@ -15,7 +15,7 @@
 
 [["US", "CA", "GB", "FR", "AU", "NZ", 
   "DE", "AT", "BE", "IT", "NL", "CH",
-  "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/cje7lz8eo0?offer_id=3343&aff_id=50870&nopop=1"]],
+  "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/6dtg6zuybk?offer_id=3343&aff_id=54557&nopop=1"]],
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["http://www.me-gay.com"]],
 [["ZZ"], ["https://t.frtyj.com/dhy7mh6y68?offer_id=3343&aff_id=55931&nopop=1"]]              
 
