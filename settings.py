b26262adc31272346b15163c23ec2949b30bb90c
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-06-07 10:10:17 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,11 +2,26 @@
     "mobile":
 [
 
-[["US", "CA", "GB", "IE", "FR", "AU", "NZ", "DE"], ["https://chaturbate.com/in/?track=qum&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
-[["RU", "UA", "BY", "KZ", "PL", "HU", "SI", 
-  "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
-  "RS", "IL", "JO", "AE", "AM", "GE", "AZ"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female&pt=http"]],
-[["JP", "TW", "SG", "KR", "IN", "PH", "CN", "ID", "TH", "VN", "MY"], ["https://enter.javhd.com/track/MjIwOTIuMjAuMi4yLjAuMC4wLjAuMA"]],
+[["US", "CA", "GB", "IE",
+  "FR", "AU", "NZ", "DE",
+  "NL", "CH", "NO", "SE"], ["https://chaturbate.com/in/?track=qum&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
+
+[["RU", "UA", "BY", "KZ",
+  "PL", "HU", "SI", "CZ",
+  "BG", "RO", "SK", "LV",
+  "LT", "EE", "RS", "IL",
+  "AE", "AM", "GE", "AZ",
+  "RS", "MK", "BA", "ME",
+  "HR", "MT", "CY", "AD"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female&pt=http"]],
+
+[["AT", "BE", "LI", "LU",
+  "DK", "IS", "FI", "GR",
+  "ES", "IT", "PT", "AR",
+  "JP", "TW", "SG", "KR"], ["https://t.irtyf.com/3zkqg57f9c?offer_id=3788&aff_id=53837&nopop=1"]],
+
+[["IN", "PH", "CN", "ID", 
+  "TH", "VN", "MY", "HK"], ["https://enter.javhd.com/track/MjIwOTIuMjAuMi4yLjAuMC4wLjAuMA"]],
+
 [["ZZ"], ["https://chaturbate.com/in/?track=zzm&tour=hr8m&campaign=zKhCt&disable_sound=True"]]  
                
 ],
@@ -14,12 +29,27 @@
     "desktop":
 [
 
-[["US", "CA", "GB", "IE", "FR", "AU", "NZ", "DE"], ["https://chaturbate.com/in/?track=qud&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
-[["RU", "UA", "BY", "KZ", "PL", "HU", "SI", 
-  "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
-  "RS", "IL", "JO", "AE", "AM", "GE", "AZ"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female&pt=http"]],
-[["JP", "TW", "SG", "KR", "IN", "PH", "CN", "ID", "TH", "VN", "MY"], ["https://enter.javhd.com/track/MjIwOTIuMjAuMi4yLjAuMC4wLjAuMA"]],
-[["ZZ"], ["https://chaturbate.com/in/?track=zzd&tour=hr8m&campaign=zKhCt&disable_sound=True"]]      
+[["US", "CA", "GB", "IE",
+  "FR", "AU", "NZ", "DE",
+  "NL", "CH", "NO", "SE"], ["https://chaturbate.com/in/?track=qum&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
+
+[["RU", "UA", "BY", "KZ",
+  "PL", "HU", "SI", "CZ",
+  "BG", "RO", "SK", "LV",
+  "LT", "EE", "RS", "IL",
+  "AE", "AM", "GE", "AZ",
+  "RS", "MK", "BA", "ME",
+  "HR", "MT", "CY", "AD"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female&pt=http"]],
+
+[["AT", "BE", "LI", "LU",
+  "DK", "IS", "FI", "GR",
+  "ES", "IT", "PT", "AR",
+  "JP", "TW", "SG", "KR"], ["https://t.irtyf.com/3zkqg57f9c?offer_id=3788&aff_id=53837&nopop=1"]],
+
+[["IN", "PH", "CN", "ID", 
+  "TH", "VN", "MY", "HK"], ["https://enter.javhd.com/track/MjIwOTIuMjAuMi4yLjAuMC4wLjAuMA"]],
+
+[["ZZ"], ["https://chaturbate.com/in/?track=zzm&tour=hr8m&campaign=zKhCt&disable_sound=True"]]      
 
 ]
 }
