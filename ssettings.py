930c25d1d093543dd4f8feded6eb402db5f8a871
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-06-15 22:57:54 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -5,12 +5,10 @@
 [["FR", "NZ", "ZA", "SG", "MX",
   "TW", "ES", "PT", "CZ", "SK", 
   "SI", "HU", "LI", "LU", "BR",
-  "DE", "NL", "AU", "CA", "UK"], 
-["https://t.frtyj.com/bedhdndnpc?offer_id=3341&aff_id=55688&nopop=1"]],
-[["AT", "BE", "IT", "CH", "IS", 
+  "DE", "NL", "AU", "CA", "UK", 
+  "AT", "BE", "IT", "CH", "IS", 
   "IE", "NO", "SE", "DK", "FI",
-  "SG", "TW", "JP", "AR", "ZA"],
-["https://t.frtyj.com/bedhdndnpc?offer_id=3341&aff_id=55688&nopop=1"]],    
+  "SG", "TW", "JP", "AR", "ZA"], ["https://t.frtyj.com/45aeolv8cg?offer_id=3341&aff_id=40075&nopop=1"]],    
 [["ZZ"], ["http://www.youtrannytube.com"]]  
                
 ],
@@ -21,14 +19,11 @@
 [["FR", "NZ", "ZA", "SG", "MX",
   "TW", "ES", "PT", "CZ", "SK", 
   "SI", "HU", "LI", "LU", "BR",
-  "DE", "NL", "AU", "CA", "UK"], 
-["https://t.frtyj.com/bedhdndnpc?offer_id=3341&aff_id=55688&nopop=1"]],
-[["AT", "BE", "IT", "CH", "IS", 
+  "DE", "NL", "AU", "CA", "UK", 
+  "AT", "BE", "IT", "CH", "IS", 
   "IE", "NO", "SE", "DK", "FI",
-  "SG", "TW", "JP", "AR", "ZA"], 
-["https://t.frtyj.com/bedhdndnpc?offer_id=3341&aff_id=55688&nopop=1"]],   
-[["BG"], ["https://chaturbate.com/in/?track=default&tour=sxJR&campaign=NOak5&disable_sound=True"]],
-[["ZZ"], ["http://www.youtrannytube.com"]]           
+  "SG", "TW", "JP", "AR", "ZA"], ["https://t.frtyj.com/45aeolv8cg?offer_id=3341&aff_id=40075&nopop=1"]],    
+[["ZZ"], ["http://www.youtrannytube.com"]]          
 
 ]
 }
