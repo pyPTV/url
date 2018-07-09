94904666947bdc7659c338ec4a89c40b8407b94e
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-07-09 09:59:52 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,14 +2,15 @@
     "mobile":
 [
 
-[["FR", "NZ", "ZA", "SG", "MX",
-  "TW", "ES", "PT", "CZ", "SK", 
-  "SI", "HU", "LI", "LU", "BR",
+[["FR", "NZ", "ZA", "SG", "TW",    
   "DE", "NL", "AU", "CA", "UK", 
   "AT", "BE", "IT", "CH", "IS", 
   "IE", "NO", "SE", "DK", "FI",
-  "SG", "TW", "JP", "AR", "ZA"], ["https://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA"]],    
-[["US"], ["https://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA"]],          
+  "TW", "JP",  "ZA"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],    
+[["US"], ["http://www.youtrannytube.com"]],  
+[["MX", "ES", "PT", "CZ", "SK",
+  "SI", "HU", "LI", "LU", "BR",
+  "AR"], ["https://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA"]],
 [["ZZ"], ["https://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA"]]
                
 ],
@@ -17,15 +18,17 @@
     "desktop":
 [
 
-[["FR", "NZ", "ZA", "SG", "MX",
-  "TW", "ES", "PT", "CZ", "SK", 
-  "SI", "HU", "LI", "LU", "BR",
+[["FR", "NZ", "ZA", "SG", "TW",    
   "DE", "NL", "AU", "CA", "UK", 
   "AT", "BE", "IT", "CH", "IS", 
   "IE", "NO", "SE", "DK", "FI",
-  "SG", "TW", "JP", "AR", "ZA"], ["https://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA"]],    
-[["US"], ["https://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA"]],          
+  "TW", "JP",  "ZA"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],    
+[["US"], ["http://www.youtrannytube.com"]],  
+[["MX", "ES", "PT", "CZ", "SK",
+  "SI", "HU", "LI", "LU", "BR",
+  "AR"], ["https://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA"]],
 [["ZZ"], ["https://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA"]]
+
 ]
 }
 
