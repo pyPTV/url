9f0c600db4705f5d980cb290421fae0fc56798f8
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-07-14 10:12:44 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -6,11 +6,10 @@
   "DE", "NL", "AU", "CA", "UK", 
   "AT", "BE", "IT", "CH", "IS", 
   "IE", "NO", "SE", "DK", "FI",
-  "JP",  "ZA"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],    
-[["US"], ["https://www.trannytube.tv/"]],  
-[["MX", "ES", "PT", "CZ", "SK",
-  "SI", "HU", "LI", "LU", "BR",
-  "AR"], ["https://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA"]],
+  "JP", "ZA", "MX", "ES", "PT", 
+  "CZ", "SK", "SI", "HU", "LI", 
+  "LU", "BR", "AR"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
+[["US"], ["https://www.trannytube.tv/"]],
 [["ZZ"], ["https://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA"]]
                
 ],
@@ -22,11 +21,10 @@
   "DE", "NL", "AU", "CA", "UK", 
   "AT", "BE", "IT", "CH", "IS", 
   "IE", "NO", "SE", "DK", "FI",
-  "JP",  "ZA"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],    
-[["US"], ["https://www.trannytube.tv/"]],  
-[["MX", "ES", "PT", "CZ", "SK",
-  "SI", "HU", "LI", "LU", "BR",
-  "AR"], ["https://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA"]],
+  "JP", "ZA", "MX", "ES", "PT", 
+  "CZ", "SK", "SI", "HU", "LI", 
+  "LU", "BR", "AR"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
+[["US"], ["https://www.trannytube.tv/"]],
 [["ZZ"], ["https://premium.ladyboytube.com/track/MTA2MDMuMi4xNi4xNi4wLjAuMC4wLjA"]]
 
 ]
