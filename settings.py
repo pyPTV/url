35d50111857d345dda9b891ac415ee65b23b240e
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-07-19 20:13:17 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@
 
 [["US", "CA", "GB", "IE",
   "FR", "AU", "NZ", "DE",
-  "NL", "CH", "NO", "SE"], ["https://chaturbate.com/in/?track=qum&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
+  "NL", "CH", "NO", "SE"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=y1Hau&disable_sound=True"]],
 
 [["RU", "UA", "BY", "KZ",
   "PL", "HU", "SI", "CZ",
@@ -31,7 +31,7 @@
 
 [["US", "CA", "GB", "IE",
   "FR", "AU", "NZ", "DE",
-  "NL", "CH", "NO", "SE"], ["https://chaturbate.com/in/?track=qum&tour=hr8m&campaign=zKhCt&disable_sound=True"]],
+  "NL", "CH", "NO", "SE"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=y1Hau&disable_sound=True"]],
 
 [["RU", "UA", "BY", "KZ",
   "PL", "HU", "SI", "CZ",
