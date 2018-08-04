63d4226897e2e3e0d2f92a1e332dd5d4488d0696
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-08-04 09:12:52 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,7 +4,7 @@
 
 [["US", "CA", "GB", "FR", "AU", "NZ", 
   "DE", "AT", "BE", "IT", "NL", "CH",
-  "IS", "IE", "NO", "SE", "DK", "FI"], ["http://join.allgaysitespass.com/track/MjAwMjYyLjIuMy40LjAuMC4wLjAuMA",
+  "IS", "IE", "NO", "SE", "DK", "FI"], ["http://join.allgaysitespass.com/track/MjAwMzUwLjIuMy40LjAuMC4wLjAuMA",
                                         "https://t.frtyj.com/xjqkqb8n7k?aff_id=43372&offer_id=3343&nopop=1"]],
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["http://www.me-gay.com"]],  
 [["ZZ"], ["https://t.frtyj.com/5stxzon9ds?offer_id=3343&aff_id=40075&nopop=1"]]  
@@ -16,7 +16,7 @@
 
 [["US", "CA", "GB", "FR", "AU", "NZ", 
   "DE", "AT", "BE", "IT", "NL", "CH",
-  "IS", "IE", "NO", "SE", "DK", "FI"], ["http://join.allgaysitespass.com/track/MjAwMjYyLjIuMy40LjAuMC4wLjAuMA",
+  "IS", "IE", "NO", "SE", "DK", "FI"], ["http://join.allgaysitespass.com/track/MjAwMzUwLjIuMy40LjAuMC4wLjAuMA",
                                         "https://t.frtyj.com/xjqkqb8n7k?aff_id=43372&offer_id=3343&nopop=1"]],
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["http://www.me-gay.com"]],
 [["ZZ"], ["https://t.frtyj.com/5stxzon9ds?offer_id=3343&aff_id=40075&nopop=1"]]              
