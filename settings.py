9e2029ed7c531e62bb521596e1daf9e70183d745
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-08-09 07:57:07 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,9 +2,7 @@
     "mobile":
 [
 
-[["US", "CA", "GB", "IE",
-  "FR", "AU", "NZ", "DE",
-  "NL", "CH", "NO", "SE"], ["https://enter.javhd.com/track/MjI1NjguMi4yLjIuMC4wLjAuMC4w"]],
+[["US"], ["https://enter.javhd.com/track/MjI1NjguMi4yLjIuMC4wLjAuMC4w"]],
 
 [["RU", "UA", "BY", "KZ",
   "PL", "HU", "SI", "CZ",
@@ -12,17 +10,9 @@
   "LT", "EE", "RS", "IL",
   "AE", "AM", "GE", "AZ",
   "RS", "MK", "BA", "ME",
-  "HR", "MT", "CY", "AD",
-  "AR", "MX", "BR", "CL",
-  "PE", "UY", "VE", "ZA",
-  "IT", "PT", "ES", "TR",
-  "EG", "BE", "FI", "PR"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&g=female&pt=http"]],
+  "HR", "MT", "CY", "AD"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&g=female&pt=http"]],
 
-[["IN", "PH", "CN", "ID", 
-  "TH", "VN", "MY", "HK",
-  "JP", "TW", "SG", "KR"], ["https://enter.javhd.com/track/MjI1NjguMi4yLjIuMC4wLjAuMC4w"]],
-
-[["ZZ"], ["https://enter.javhd.com/track/MjI1NjguMi4yLjIuMC4wLjAuMC4w"]]  
+[["ZZ"], ["https://bu3m0b.com/?id=44632"]]  
                
 ],
  
