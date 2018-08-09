86d349f29be22fef193928e9a80dec508d3d7437
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-08-09 08:01:29 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,10 +7,10 @@
 [["RU", "UA", "BY", "KZ",
   "PL", "HU", "SI", "CZ",
   "BG", "RO", "SK", "LV",
-  "LT", "EE", "RS", "IL",
-  "AE", "AM", "GE", "AZ",
+  "LT", "EE", "RS", 
+  "AM", "GE", "AZ",
   "RS", "MK", "BA", "ME",
-  "HR", "MT", "CY", "AD"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&g=female&pt=http"]],
+  "HR", "MT", "CY"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&g=female&pt=http"]],
 
 [["ZZ"], ["https://bu3m0b.com/?id=44632"]]  
                
