55c021bdb69ec85600076c7774c9637cf863fa6d
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-08-11 10:16:42 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,13 +5,12 @@
 [["US", "CA"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=QmETk&disable_sound=True"]],
 
 [["RU"], ["http://runetki2.com?c=533066"]],
-[["UA", "BY", "KZ",
-  "PL", "HU", "SI", "CZ",
-  "BG", "RO", "SK", "LV",
-  "LT", "EE", "RS", 
-  "AM", "GE", "AZ",
-  "RS", "MK", "BA", "ME",
-  "HR", "MT", "CY"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&g=female&pt=http"]],
+[["UA", "BY", "KZ", "PL", 
+  "HU", "SI", "CZ", "BG", 
+  "RO", "SK", "LV", "LT", 
+  "EE", "RS", "AM", "GE", 
+  "AZ", "RS", "MK", "BA", 
+  "ME", "HR", "MT", "CY"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&g=female&pt=http"]],
 
 [["ZZ"], ["https://bu3m0b.com/?id=44632"]]  
                
@@ -21,13 +20,14 @@
 [
 
 [["US", "CA"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=QmETk&disable_sound=True"]],
-[["GB", "IE", "IT",
+
+[["GB", "IE", "IT", 
   "FR", "AU", "NZ", "DE",
   "NL", "CH", "NO", "SE"], ["https://t.frtyt.com/1i6ez0rzwg?aff_id=55688&offer_id=4093&nopop=1"]],
 
 [["RU"], ["http://runetki2.com?c=533066"]], 
-[["UA", "BY", "KZ",
-  "PL", "HU", "SI", "CZ",
+ 
+[["PL", "HU", "SI", "CZ",
   "BG", "RO", "SK", "LV",
   "LT", "EE", "RS", "IL",
   "AE", "AM", "GE", "AZ",
@@ -35,8 +35,9 @@
   "HR", "MT", "CY", "AD",
   "AR", "MX", "BR", "CL",
   "PE", "UY", "VE", "ZA",
-  "PT", "ES", "TR",
-  "EG", "BE", "FI", "PR"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&g=female&pt=http"]],
+  "PT", "ES", "TR", "UA", 
+  "BY", "KZ", "EG", "BE", 
+  "FI", "PR"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&g=female&pt=http"]],
 
 [["HK", "JP", "TW", "SG", "KR"], ["https://t.frtyt.com/1i6ez0rzwg?aff_id=55688&offer_id=4093&nopop=1"]],
 
