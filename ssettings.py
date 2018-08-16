dfe68cdf56df644820db20c7cbf8d36f0e3f065f
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-08-16 18:34:47 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -8,9 +8,10 @@
   "IE", "NO", "SE", "DK", "FI",
   "JP", "ZA", "MX", "ES", "PT", 
   "CZ", "SK", "SI", "HU", "LI", 
-  "LU", "HK", "ZA"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
+  "LU", "HK", "ZA", "KR"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
-[["US"], ["https://www.trannytube.tv/"]],
+[["US", "IN", "PH", "CN", "ID", "TH", "VN"], ["https://www.trannytube.tv/"]],
+
 [["ZZ"], ["https://t.frtyj.com/2vk9iygtfk?aff_id=56817&offer_id=3341&nopop=1"]]
                
 ],
@@ -24,9 +25,10 @@
   "IE", "NO", "SE", "DK", "FI",
   "JP", "ZA", "MX", "ES", "PT", 
   "CZ", "SK", "SI", "HU", "LI", 
-  "LU", "HK", "ZA"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
+  "LU", "HK", "ZA", "KR"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
-[["US"], ["https://www.trannytube.tv/"]],
+[["US", "IN", "PH", "CN", "ID", "TH", "VN"], ["https://www.trannytube.tv/"]],
+
 [["ZZ"], ["https://t.frtyj.com/2vk9iygtfk?aff_id=56817&offer_id=3341&nopop=1"]]
 
 ]
