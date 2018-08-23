4eb5e1651714d1baecbc2ca59851f6de25b9430d
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-08-23 09:26:43 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,34 +2,13 @@
     "mobile":
 [
 
-[["US", "CA", "AU", "NZ", "GB", "IE"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=cSFs7&disable_sound=True"]],
+[["US", "CA", "AU", "NZ", "GB", "IE", "FR"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=cSFs7&disable_sound=True"]],
 
-[["IT", "FR", "DE", "NL", "CH", "NO", 
-  "SE", "AT", "BE", "IS", "ZA", "LI", 
-  "LU"], ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female",
-          "https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=couples"]],
-    
-[["RU"], ["http://runetki2.com?c=533066"]],
-[["UA", "BY", "KZ", "PL", 
-  "HU", "SI", "CZ", "BG", 
-  "RO", "SK", "LV", "LT", 
-  "EE", "RS", "AM", "GE", 
-  "AZ", "RS", "MK", "BA", 
-  "ME", "HR", "MT", "CY"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&pt=http"]],
-
-[["ZZ"], ["https://bu3m0b.com/?id=44632"]]  
-               
-],
- 
-    "desktop":
-[
-
-[["US", "CA", "AU", "NZ", "GB", "IE"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=cSFs7&disable_sound=True"]],
-
-[["IT", "FR", "DE", "NL", "CH", "NO", 
-  "SE", "AT", "BE", "IS", "ZA", "LI", 
-  "LU"], ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female",
-          "https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=couples"]],
+[["IT", "DE", "NL", 
+  "CH", "NO", "SE", 
+  "AT", "BE", "IS", 
+  "ZA", "LI", "LU"], ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female",
+                      "https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=couples"]],
 
 [["RU"], ["http://runetki2.com?c=533066"]], 
  
@@ -40,11 +19,37 @@
   "RS", "MK", "BA", "ME",
   "HR", "MT", "CY", "PT", 
   "ES", "TR", "UA", "BY", 
-  "KZ", "EG", "PT", "ES"], ["http://tools.bongacams.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&pt=http"]],
+  "KZ", "EG", "PT", "ES"], ["https://promo-bc.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&g=female"]],
+ 
+[["ZZ"], ["https://bu3m0b.com/?id=44632"]]  
+               
+],
+ 
+    "desktop":
+[
+
+[["US", "CA", "AU", "NZ", "GB", "IE", "FR"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=cSFs7&disable_sound=True"]],
+
+[["IT", "DE", "NL", 
+  "CH", "NO", "SE", 
+  "AT", "BE", "IS", 
+  "ZA", "LI", "LU"], ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female",
+                      "https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=couples"]],
+
+[["RU"], ["http://runetki2.com?c=533066"]], 
+ 
+[["PL", "HU", "SI", "CZ",
+  "BG", "RO", "SK", "LV",
+  "LT", "EE", "RS", "IL",
+  "AE", "AM", "GE", "AZ",
+  "RS", "MK", "BA", "ME",
+  "HR", "MT", "CY", "PT", 
+  "ES", "TR", "UA", "BY", 
+  "KZ", "EG", "PT", "ES"], ["https://promo-bc.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&g=female"]],
 
 [["IN", "PH", "ID", "TH", "VN"], ["https://www.ixxx.com/"]],
 [["TW", "SG", "KR", "CN", "MY"], ["https://www.ixxx.com/"]],
-[["ZZ"], ["https://www.ixxx.com/"]]      
+[["ZZ"], ["https://promo-bc.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&g=female"]]      
 
 ]
 }
