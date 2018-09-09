03f7fed181c6cb7b3c3ea54996091dfff82040ec
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-09-09 20:19:00 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -5,7 +5,6 @@
 [["US", "CA", "GB", "FR", "AU", "NZ", 
   "DE", "AT", "BE", "IT", "NL", "CH",
   "IS", "IE", "NO", "SE", "DK", "FI"], ["http://join.allgaysitespass.com/track/MjAxMDE4LjIuMy40LjAuMC4wLjAuMA",
-                                        "https://t.frtyj.com/q8607fbyps?aff_id=54636&offer_id=3343&nopop=1",
                                         "https://t.frtyj.com/q8607fbyps?aff_id=54636&offer_id=3343&nopop=1"]],
                                         
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.grtyv.com/uwimn3c5og?aff_id=54455&offer_id=5368&nopop=1"]],  
@@ -19,7 +18,6 @@
 [["US", "CA", "GB", "FR", "AU", "NZ", 
   "DE", "AT", "BE", "IT", "NL", "CH",
   "IS", "IE", "NO", "SE", "DK", "FI"], ["http://join.allgaysitespass.com/track/MjAxMDE4LjIuMy40LjAuMC4wLjAuMA",
-                                        "https://t.frtyj.com/q8607fbyps?aff_id=54636&offer_id=3343&nopop=1",
                                         "https://t.frtyj.com/q8607fbyps?aff_id=54636&offer_id=3343&nopop=1"]],
                                         
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.grtyv.com/uwimn3c5og?aff_id=54455&offer_id=5368&nopop=1"]],
