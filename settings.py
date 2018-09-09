699588a2b14c9d2f1c5339b4582854c5c34af50e
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-09-09 14:29:22 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,15 +2,6 @@
     "mobile":
 [
 
-[["US", "CA", "AU", 
-  "NZ", "GB", "IE", 
-  "FR"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=cSFs7&disable_sound=True"]],
-
-[["IT", "DE", "NL", 
-  "CH", "NO", "SE", 
-  "AT", "BE", "IS", 
-  "ZA", "LI", "LU"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=cSFs7&disable_sound=True"]],
-
 [["RU"], ["http://runetki2.com?c=533066"]], 
  
 [["PL", "HU", "SI", "CZ",
@@ -22,22 +13,13 @@
   "ES", "TR", "UA", "BY", 
   "KZ", "EG", "PT", "ES"], ["https://promo-bc.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&g=female"]],
  
-[["ZZ"], ["https://t.grtyv.com/uwimn3c5og?aff_id=54455&offer_id=5368&nopop=1"]]  
+[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=cSFs7&disable_sound=True"]]  
                
 ],
  
     "desktop":
 [
 
-[["US", "CA", "AU", 
-  "NZ", "GB", "IE", 
-  "FR"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=cSFs7&disable_sound=True"]],
-
-[["IT", "DE", "NL", 
-  "CH", "NO", "SE", 
-  "AT", "BE", "IS", 
-  "ZA", "LI", "LU"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=cSFs7&disable_sound=True"]],
-
 [["RU"], ["http://runetki2.com?c=533066"]], 
  
 [["PL", "HU", "SI", "CZ",
@@ -49,9 +31,9 @@
   "ES", "TR", "UA", "BY", 
   "KZ", "EG", "PT", "ES"], ["https://promo-bc.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&g=female"]],
 
-[["IN", "PH", "ID", "TH", "VN"], ["https://t.grtyv.com/uwimn3c5og?aff_id=54455&offer_id=5368&nopop=1"]],
-[["TW", "SG", "KR", "CN", "MY"], ["https://t.grtyv.com/uwimn3c5og?aff_id=54455&offer_id=5368&nopop=1"]],
-[["ZZ"], ["https://t.grtyv.com/uwimn3c5og?aff_id=54455&offer_id=5368&nopop=1"]]      
+[["IN", "PH", "ID", "TH", "VN",
+  "TW", "SG", "KR", "CN", "MY"], ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
+[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=cSFs7&disable_sound=True"]]      
 
 ]
 }
