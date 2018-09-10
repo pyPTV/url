dffba5d9f74eb82e3e81c93c3385e24939cf7c6c
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-09-10 13:23:20 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,18 +2,12 @@
     "mobile":
 [
 
-[["RU"], ["http://runetki2.com?c=533066"]], 
+[["US", "CA", "GB", "NZ", "AU", 
+  "FR", "DE", "NL", "SE", "NO", 
+  "DK"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=cSFs7&disable_sound=True"]],
  
-[["PL", "HU", "SI", "CZ",
-  "BG", "RO", "SK", "LV",
-  "LT", "EE", "RS", "IL",
-  "AE", "AM", "GE", "AZ",
-  "RS", "MK", "BA", "ME",
-  "HR", "MT", "CY", "PT", 
-  "ES", "TR", "UA", "BY", 
-  "KZ", "EG", "PT", "ES"], ["https://promo-bc.com/promo.php?type=direct_link&c=533066&amute=1&page=popular_chat&g=female"]],
  
-[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=cSFs7&disable_sound=True"]]  
+[["ZZ"], ["https://bu3m0b.com/?id=44632"]]  
                
 ],
  
