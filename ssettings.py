67c05fdbea88920843aeec84e1037c493e12d926
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-09-15 19:26:38 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -1,40 +1,50 @@
 {
-    "mobile":
-[
+	"mobile": [
 
-[["FR", "NZ", "ZA", "DE", "NL", 
-  "AU", "AT", "BE", "IT", "CH", 
-  "IS", "IE", "NO", "SE", "DK", 
-  "FI", "LI", "PT", "CZ", "SK", 
-  "SI", "LU"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjU2NTI5NzA3LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
-  
-[["JP", "MX", "ES", "HU", "SG", 
-  "HK", "KR", "CA", "TW", "GB"], ["https://t.frtyj.com/a33i55m79c?aff_id=55688&offer_id=3341&nopop=1"]],
+		[
+			["FR", "NZ", "ZA", "DE", "NL", "AU", "AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "LI", "PT", "CZ", "SK", "SI", "LU"],
+			["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjU2NTI5NzA3LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
+		],
 
-[["US", "IN", "PH", "CN", "ID", "TH", "VN"], ["https://www.trannytube.tv/"]],
+		[
+			["JP", "MX", "ES", "HU", "SG", "HK", "KR", "CA", "TW", "GB"],
+			["https://t.frtyj.com/a33i55m79c?aff_id=55688&offer_id=3341&nopop=1"]
+		],
 
- 
-[["ZZ"], ["https://t.frtyj.com/a33i55m79c?aff_id=55688&offer_id=3341&nopop=1"]]
-               
-],
- 
-    "desktop":
-[
+		[
+			["US", "IN", "PH", "CN", "ID", "TH", "VN"],
+			["https://www.trannytube.tv/"]
+		],
 
-[["FR", "NZ", "ZA", "DE", "NL", 
-  "AU", "AT", "BE", "IT", "CH", 
-  "IS", "IE", "NO", "SE", "DK", 
-  "FI", "LI", "PT", "CZ", "SK", 
-  "SI", "LU"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjU2NTI5NzA3LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
-  
-[["JP", "MX", "ES", "HU", "SG", 
-  "HK", "KR", "CA", "TW", "GB"], ["https://t.frtyj.com/a33i55m79c?aff_id=55688&offer_id=3341&nopop=1"]],
+		[
+			["ZZ"],
+			["https://t.frtyj.com/a33i55m79c?aff_id=55688&offer_id=3341&nopop=1"]
+		]
 
-[["US", "IN", "PH", "CN", "ID", "TH", "VN"], ["https://www.trannytube.tv/"]],
+	],
 
- 
-[["ZZ"], ["https://t.frtyj.com/a33i55m79c?aff_id=55688&offer_id=3341&nopop=1"]]
+	"desktop": [
 
-]
+		[
+			["FR", "NZ", "ZA", "DE", "NL", "AU", "AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "LI", "PT", "CZ", "SK", "SI", "LU"],
+			["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjU2NTI5NzA3LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
+		],
+
+		[
+			["JP", "MX", "ES", "HU", "SG", "HK", "KR", "CA", "TW", "GB"],
+			["https://t.frtyj.com/a33i55m79c?aff_id=55688&offer_id=3341&nopop=1"]
+		],
+
+		[
+			["US", "IN", "PH", "CN", "ID", "TH", "VN"],
+			["https://www.trannytube.tv/"]
+		],
+
+
+		[
+			["ZZ"],
+			["https://t.frtyj.com/a33i55m79c?aff_id=55688&offer_id=3341&nopop=1"]
+		]
+
+	]
 }
-
