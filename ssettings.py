baae753216b5570e3019f39fed7d2a9c7791e49e
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-09-15 19:22:53 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,16 +2,18 @@
     "mobile":
 [
 
-[["FR", "NZ", "ZA", "SG", "TW",    
-  "DE", "NL", "AU", "CA", "UK", 
-  "AT", "BE", "IT", "CH", "IS", 
-  "IE", "NO", "SE", "DK", "FI",
-  "JP", "ZA", "MX", "ES", "PT", 
-  "CZ", "SK", "SI", "HU", "LI", 
-  "LU", "HK", "ZA", "KR"], ["https://t.frtyj.com/a33i55m79c?aff_id=55688&offer_id=3341&nopop=1"]],
+[["FR", "NZ", "ZA", "DE", "NL", 
+  "AU", "AT", "BE", "IT", "CH", 
+  "IS", "IE", "NO", "SE", "DK", 
+  "FI", "LI", "PT", "CZ", "SK", 
+  "SI", "LU"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjU2NTI5NzA3LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
+  
+[["JP", "MX", "ES", "HU", "SG", 
+  "HK", "KR", "CA", "TW", "GB"], ["https://t.frtyj.com/a33i55m79c?aff_id=55688&offer_id=3341&nopop=1"]],
 
 [["US", "IN", "PH", "CN", "ID", "TH", "VN"], ["https://www.trannytube.tv/"]],
 
+ 
 [["ZZ"], ["https://t.frtyj.com/a33i55m79c?aff_id=55688&offer_id=3341&nopop=1"]]
                
 ],
@@ -19,16 +21,18 @@
     "desktop":
 [
 
-[["FR", "NZ", "ZA", "SG", "TW",    
-  "DE", "NL", "AU", "CA", "UK", 
-  "AT", "BE", "IT", "CH", "IS", 
-  "IE", "NO", "SE", "DK", "FI",
-  "JP", "ZA", "MX", "ES", "PT", 
-  "CZ", "SK", "SI", "HU", "LI", 
-  "LU", "HK", "ZA", "KR"], ["https://t.frtyj.com/a33i55m79c?aff_id=55688&offer_id=3341&nopop=1"]],
+[["FR", "NZ", "ZA", "DE", "NL", 
+  "AU", "AT", "BE", "IT", "CH", 
+  "IS", "IE", "NO", "SE", "DK", 
+  "FI", "LI", "PT", "CZ", "SK", 
+  "SI", "LU"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODY1MDYsImMiOjU2NTI5NzA3LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
+  
+[["JP", "MX", "ES", "HU", "SG", 
+  "HK", "KR", "CA", "TW", "GB"], ["https://t.frtyj.com/a33i55m79c?aff_id=55688&offer_id=3341&nopop=1"]],
 
 [["US", "IN", "PH", "CN", "ID", "TH", "VN"], ["https://www.trannytube.tv/"]],
 
+ 
 [["ZZ"], ["https://t.frtyj.com/a33i55m79c?aff_id=55688&offer_id=3341&nopop=1"]]
 
 ]
