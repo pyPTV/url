84e8a446fac9ec2d9b28716e1be7be5dd4d24f63
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-09-17 13:00:08 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -1,50 +1,42 @@
 {
-	"mobile": [
+"mobile": [
 
-		[
-			["FR", "NZ", "ZA", "DE", "NL", "AU", "AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "LI", "PT", "CZ", "SK", "SI", "LU"],
-			["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk0OTYsImMiOjU2NTQyNDM2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
-		],
-
-		[
-			["JP", "MX", "ES", "HU", "SG", "HK", "KR", "CA", "TW", "GB"],
-			["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk0OTYsImMiOjU2NTQyNDM2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
-		],
-
-		[
-			["US", "IN", "PH", "CN", "ID", "TH", "VN"],
-			["https://www.trannytube.tv/"]
-		],
-
-		[
-			["ZZ"],
-			["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk0OTYsImMiOjU2NTQyNDM2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
-		]
-
-	],
-
-	"desktop": [
-
-		[
-			["FR", "NZ", "ZA", "DE", "NL", "AU", "AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "LI", "PT", "CZ", "SK", "SI", "LU"],
-			["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk0OTYsImMiOjU2NTQyNDM2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
-		],
-
-		[
-			["JP", "MX", "ES", "HU", "SG", "HK", "KR", "CA", "TW", "GB"],
-			["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk0OTYsImMiOjU2NTQyNDM2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
-		],
-
-		[
-			["US", "IN", "PH", "CN", "ID", "TH", "VN"],
-			["https://www.trannytube.tv/"]
-		],
+[
+["FR", "NZ", "ZA", "DE", "NL", "AU", "AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "PT", "CZ", "SK", "SI", "LU"],
+["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk0OTYsImMiOjU2NTQyNDM2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
+],
 
 
-		[
-			["ZZ"],
-			["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk0OTYsImMiOjU2NTQyNDM2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
-		]
+[
+["IN", "PH", "CN", "ID", "TH", "VN"],
+["https://www.trannytube.tv/"]
+],
 
-	]
+[
+["ZZ"],
+["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
+]
+
+],
+
+"desktop": [
+
+[
+["FR", "NZ", "ZA", "DE", "NL", "AU", "AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "PT", "CZ", "SK", "SI", "LU"],
+["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk0OTYsImMiOjU2NTQyNDM2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
+],
+
+
+[
+["IN", "PH", "CN", "ID", "TH", "VN"],
+["https://www.trannytube.tv/"]
+],
+
+
+[
+["ZZ"],
+["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
+]
+
+]
 }
