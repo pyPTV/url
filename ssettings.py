c7186ea1ed19eb73b8e833e6e8f93b9f874509c5
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-10-04 23:04:53 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,52 +2,44 @@
 "mobile": [
 
 [
-["AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "PT", "CZ", "SK", "SI", "LU", "LI"],
-["https://www.trannytube.tv/"]
+["AT", "BE", "CH", "IS", "IE", "NO", "SE", "DK", "FI"],
+["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
 ],
 
 [
-["FR", "NZ", "ZA", "DE", "NL", "AU", "GB", "CA"],
-["https://www.trannytube.tv/"]
+["DE", "FR", "IT", "NL", "AU", "NZ"],
+["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
 ],
 
+[["PT", "CZ", "SK", "SI", "LU", "LI", "ZA"], ["https://www.trannytube.tv/"]],
+ 
+[["GB", "CA"], ["https://www.trannytube.tv/"]], 
+ 
+[["US", "IN", "PH", "CN", "ID", "TH", "VN"],["https://www.trannytube.tv/"]],
 
-[
-["US", "IN", "PH", "CN", "ID", "TH", "VN"],
-["https://www.trannytube.tv/"]
-],
-
-[
-["ZZ"],
-["https://www.trannytube.tv/"]
-]
+[["ZZ"],["https://www.trannytube.tv/"]]
 
 ],
 
 "desktop": [
 
 [
-["AT", "BE", "IT", "CH", "IS", "IE", "NO", "SE", "DK", "FI", "PT", "CZ", "SK", "SI", "LU", "LI"],
-["https://www.trannytube.tv/"]
+["AT", "BE", "CH", "IS", "IE", "NO", "SE", "DK", "FI"],
+["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
 ],
 
-
 [
-["FR", "NZ", "ZA", "DE", "NL", "AU", "GB", "CA"],
-["https://www.trannytube.tv/"]
+["DE", "FR", "IT", "NL", "AU", "NZ"],
+["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]
 ],
 
+[["PT", "CZ", "SK", "SI", "LU", "LI", "ZA"], ["https://www.trannytube.tv/"]],
+ 
+[["GB", "CA"], ["https://www.trannytube.tv/"]], 
+ 
+[["US", "IN", "PH", "CN", "ID", "TH", "VN"],["https://www.trannytube.tv/"]],
 
-[
-["US", "IN", "PH", "CN", "ID", "TH", "VN"],
-["https://www.trannytube.tv/"]
-],
-
-
-[
-["ZZ"],
-["https://www.trannytube.tv/"]
-]
+[["ZZ"],["https://www.trannytube.tv/"]]
 
 ]
 }
