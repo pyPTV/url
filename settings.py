e2faf64b063873fa0a8999c78b585f079152d861
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-10-10 00:38:48 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,7 +7,16 @@
   "DK"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=swH07&disable_sound=True"]],
  
  
-[["ZZ"], ["https://bu3m0b.com/?id=44632"]]  
+[["PL", "HU", "SI", "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
+  "RS", "IL", "AE", "AM", "GE", "AZ", "GR", "RS", "MK", "BA", 
+  "ME", "MX", "HR", "MT", "CY", "PT", "LU", "ES", "TR", "UA", 
+  "BY", "LI", "KZ", "EG", "PT", "ES", "RU"], ["https://promo-bc.com/promo.php?type=direct_link&c=533066&amute=1&page=random_model"]],
+
+[["IN", "PH", "ID", "TH", "VN", "TW", "SG", "KR", "CN", "MY"], ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
+[["DK"], ["https://t.hrtye.com/t4e4sfk0g0?aff_id=53280&offer_id=4973&nopop=1"]],
+[["BE"], ["https://t.hrtye.com/lvr6zty9s0?aff_id=53280&offer_id=353&nopop=1"]],
+[["AT"], ["https://t.frtyi.com/ogqo21rnls?aff_id=53280&offer_id=645&nopop=1"]],
+[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=swH07&disable_sound=True"]]  
                
 ],
  
