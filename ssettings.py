7949c358701de6f971ffb7bcf242a7501b9f157f
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-10-26 22:20:01 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -1,7 +1,7 @@
 {
 "mobile": [
  
-[["CH", "JP", "SG", "TW"], 
+[["CH", "JP", "SG", "TW", "KR"], 
 ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
 [["AT", "BE", "IS", "IE", "NO", "SE", "DK", "FI"],
@@ -23,7 +23,7 @@
 
 "desktop": [
 
-[["CH", "JP", "SG", "TW"],  
+[["CH", "JP", "SG", "TW", "KR"],  
 ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
 [["AT", "BE", "IS", "IE", "NO", "SE", "DK", "FI"],
