6520def55ef4ad078a5b1921b05559e9a222c3c7
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-11-01 11:12:47 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -13,8 +13,8 @@
   "BY", "LI", "KZ", "EG", "PT", "RU", "AR", "BR", "CL", "ZA"], 
 ["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
-[["IN", "PH", "CN", "ID", "TH", "VN"],["https://www.trannytube.tv/"]],
-[["ZZ"],["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyOTEwNjEsImMiOjU2NjgwNzE1LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]]
+
+[["ZZ"],["https://www.trannytube.tv/"]]
 
 ],
 
@@ -27,15 +27,14 @@
 ["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
  
 
- 
 [["PL", "HU", "SI", "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
   "RS", "IL", "AE", "AM", "GE", "AZ", "GR", "RS", "MK", "BA", 
   "ME", "MX", "HR", "MT", "CY", "PT", "LU", "ES", "TR", "UA", 
   "BY", "LI", "KZ", "EG", "PT", "RU", "AR", "BR", "CL", "ZA"], 
 ["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
-[["IN", "PH", "CN", "ID", "TH", "VN"],["https://www.trannytube.tv/"]],
-[["ZZ"],["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyOTEwNjEsImMiOjU2NjgwNzE1LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]]
+
+[["ZZ"],["https://www.trannytube.tv/"]]
 
 ]
 
