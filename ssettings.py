9c9481d2a82d476f40fc6a094f9e887e2c5467ae
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-11-05 14:58:11 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -5,14 +5,15 @@
 ["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyOTAwMzYsImMiOjU2NTk0NjU1LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
 
-[["AU", "NZ", "AT", "BE", "IS", "IE", "NO", "SE", "DK", "FI", "FR", "IT", "CH", "JP", "SG", "TW", "KR"],
+[["AU", "NZ", "AT", "BE", "IS", "IE", "NO", "SE", "DK", "FI", "FR", "IT", "CH"],
 ["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
  
  
 [["PL", "HU", "SI", "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
   "RS", "IL", "AE", "AM", "GE", "AZ", "GR", "RS", "MK", "BA", 
   "ME", "MX", "HR", "MT", "CY", "PT", "LU", "ES", "TR", "UA", 
-  "BY", "LI", "KZ", "EG", "PT", "RU", "AR", "BR", "CL", "ZA"], 
+  "BY", "LI", "KZ", "EG", "PT", "RU", "AR", "BR", "CL", "ZA", 
+  "JP", "SG", "TW", "KR"], 
 ["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
 
@@ -28,14 +29,15 @@
 ["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyOTAwMzYsImMiOjU2NTk0NjU1LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
 
-[["AU", "NZ", "AT", "BE", "IS", "IE", "NO", "SE", "DK", "FI", "FR", "IT", "CH", "JP", "SG", "TW", "KR"],
+[["AU", "NZ", "AT", "BE", "IS", "IE", "NO", "SE", "DK", "FI", "FR", "IT", "CH"],
 ["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
  
  
 [["PL", "HU", "SI", "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
   "RS", "IL", "AE", "AM", "GE", "AZ", "GR", "RS", "MK", "BA", 
   "ME", "MX", "HR", "MT", "CY", "PT", "LU", "ES", "TR", "UA", 
-  "BY", "LI", "KZ", "EG", "PT", "RU", "AR", "BR", "CL", "ZA"], 
+  "BY", "LI", "KZ", "EG", "PT", "RU", "AR", "BR", "CL", "ZA",
+  "JP", "SG", "TW", "KR"], 
 ["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
 
