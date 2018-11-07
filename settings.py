c8998724078d9b23effa8d610368796398f6da11
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-11-07 16:56:41 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,6 +5,8 @@
 [["IN", "PH", "ID", "TH", "VN", 
   "TW", "SG", "KR", "CN", "MY"], 
  ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
+    
+[["RU", "UA", "KZ", "BY", "AM"], ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=random_model&g=female"]],
 
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=swH07&disable_sound=True"]]   
                
@@ -16,8 +18,10 @@
 [["IN", "PH", "ID", "TH", "VN", 
   "TW", "SG", "KR", "CN", "MY"], 
  ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
+    
+[["RU", "UA", "KZ", "BY", "AM"], ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=random_model&g=female"]],
 
-[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=swH07&disable_sound=True"]]      
+[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=swH07&disable_sound=True"]]     
 
 ]
 }
