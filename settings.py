5b6daaa2adcc396261d323128c7b7f961f570abf
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-11-07 16:52:55 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,30 +2,21 @@
     "mobile":
 [
 
-[["US", "CA", "GB", "NZ", "AU", 
-  "FR", "DE", "NL", "SE", "NO", 
-  "DK"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=swH07&disable_sound=True"]],
- 
- 
-[["PL", "HU", "SI", "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
-  "RS", "IL", "AE", "AM", "GE", "AZ", "GR", "RS", "MK", "BA", 
-  "ME", "MX", "HR", "MT", "CY", "PT", "LU", "ES", "TR", "UA", 
-  "BY", "LI", "KZ", "EG", "PT", "RU"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=swH07&disable_sound=True"]],
+[["IN", "PH", "ID", "TH", "VN", 
+  "TW", "SG", "KR", "CN", "MY"], 
+ ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
 
-[["IN", "PH", "ID", "TH", "VN", "TW", "SG", "KR", "CN", "MY"], ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
-[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=swH07&disable_sound=True"]]  
+[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=swH07&disable_sound=True"]]   
                
 ],
  
     "desktop":
 [
 
-[["PL", "HU", "SI", "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
-  "RS", "IL", "AE", "AM", "GE", "AZ", "GR", "RS", "MK", "BA", 
-  "ME", "MX", "HR", "MT", "CY", "PT", "LU", "ES", "TR", "UA", 
-  "BY", "LI", "KZ", "EG", "PT", "RU"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=swH07&disable_sound=True"]],
+[["IN", "PH", "ID", "TH", "VN", 
+  "TW", "SG", "KR", "CN", "MY"], 
+ ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
 
-[["IN", "PH", "ID", "TH", "VN", "TW", "SG", "KR", "CN", "MY"], ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=swH07&disable_sound=True"]]      
 
 ]
