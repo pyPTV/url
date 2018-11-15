15c315002bb8dcb19b3fae36f9c8cdf6cc4ad711
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-11-15 09:24:12 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -1,20 +1,18 @@
 {
 "mobile": [
 
-[["CA", "GB", "DE", "NL"], 
+[["CA", "AU", "NZ",
+  "GB", "DE", "NL", "FR", "IT", "CH", 
+  "AT", "BE", "IS", "IE", "NO", "SE", "DK", "FI"], 
 ["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyOTAwMzYsImMiOjU2NTk0NjU1LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
-
-[["AU", "NZ", "AT", "BE", "IS", "IE", "NO", "SE", "DK", "FI", "FR", "IT", "CH"],
-["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
- 
  
 [["PL", "HU", "SI", "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
   "RS", "IL", "AE", "AM", "GE", "AZ", "GR", "RS", "MK", "BA", 
   "ME", "MX", "HR", "MT", "CY", "PT", "LU", "ES", "TR", "UA", 
   "BY", "LI", "KZ", "EG", "PT", "RU", "AR", "BR", "CL", "ZA", 
   "JP", "SG", "TW", "KR"], 
-["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
+["https://chaturbate.com/in/?track=default&tour=sxJR&campaign=SIJqf&disable_sound=True"]],
 
 
 [["ZZ"],["https://chaturbate.com/in/?track=default&tour=sxJR&campaign=SIJqf&disable_sound=True"]]
@@ -25,20 +23,17 @@
 
 "desktop": [
 
-[["CA", "GB", "DE", "NL"], 
+[["CA", "AU", "NZ",
+  "GB", "DE", "NL", "FR", "IT", "CH", 
+  "AT", "BE", "IS", "IE", "NO", "SE", "DK", "FI"], 
 ["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyOTAwMzYsImMiOjU2NTk0NjU1LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
-
-[["AU", "NZ", "AT", "BE", "IS", "IE", "NO", "SE", "DK", "FI", "FR", "IT", "CH"],
-["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
- 
- 
 [["PL", "HU", "SI", "CZ", "BG", "RO", "SK", "LV", "LT", "EE", 
   "RS", "IL", "AE", "AM", "GE", "AZ", "GR", "RS", "MK", "BA", 
   "ME", "MX", "HR", "MT", "CY", "PT", "LU", "ES", "TR", "UA", 
   "BY", "LI", "KZ", "EG", "PT", "RU", "AR", "BR", "CL", "ZA",
   "JP", "SG", "TW", "KR"], 
-["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
+["https://chaturbate.com/in/?track=default&tour=sxJR&campaign=SIJqf&disable_sound=True"]],
 
 
 [["ZZ"],["https://chaturbate.com/in/?track=default&tour=sxJR&campaign=SIJqf&disable_sound=True"]]
