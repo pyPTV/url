121ab48874b52b8d26c1992eaa64974b624c5c72
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-11-19 07:46:12 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,8 @@
   "TW", "SG", "KR", "CN", "MY"], 
  ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
     
-[["RU", "UA", "KZ", "BY", "AM"], ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=random_model&g=female"]],
+[["RU", "UA", "KZ", "BY", "AM",
+  "PL", "SK"], ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=random_model&g=female"]],
 
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=OT2s&campaign=Q1hGG&disable_sound=True"]]   
                
@@ -19,7 +20,8 @@
   "TW", "SG", "KR", "CN", "MY"], 
  ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
     
-[["RU", "UA", "KZ", "BY", "AM"], ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=random_model&g=female"]],
+[["RU", "UA", "KZ", "BY", "AM",
+  "PL", "SK"], ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=random_model&g=female"]],
 
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=OT2s&campaign=Q1hGG&disable_sound=True"]]     
 
