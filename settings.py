a7f041daa13bd6ed4f760fad72380f1e443d9321
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-11-29 08:57:48 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@
 [["RU", "UA", "KZ", "BY", "AM",
   "AZ", "PL", "SK", "CZ"], ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=random_model&g=female"]],
 
-[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=VPHc6&disable_sound=True"]]   
+[["ZZ"], ["https://kinosalo.com"]]   
                
 ],
  
@@ -23,7 +23,7 @@
 [["RU", "UA", "KZ", "BY", "AM",
   "AZ", "PL", "SK", "CZ"], ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=random_model&g=female"]],
 
-[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=VPHc6&disable_sound=True"]]     
+[["ZZ"], ["https://kinosalo.com"]]     
 
 ]
 }
