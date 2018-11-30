c0571b5e42c8bcf3b4d56d9087121993ee6faa82
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-11-30 09:34:16 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,11 +3,20 @@
 [
 
 [["IN", "PH", "ID", "TH", "VN", 
-  "TW", "SG", "KR", "CN", "MY", "JP"], 
+  "TW", "KR", "CN", "MY", "JP"], 
  ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
-    
+
+
 [["RU", "UA", "KZ", "BY", "AM",
-  "AZ", "PL", "SK", "CZ"], ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=random_model&g=female"]],
+  "AZ", "PL", "SK", "CZ", "PT"], 
+ ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=random_model&g=female"]],
+
+
+[["GB", "FR", "AU", "NZ", "CA", "SG",
+  "DE", "AT", "BE", "IT", "NL", "CH",
+  "IS", "IE", "NO", "SE", "DK", "FI"], 
+ ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=wtBiH&disable_sound=True"]],
+
 
 [["ZZ"], ["https://kinosalo.com"]]   
                
@@ -17,13 +26,22 @@
 [
 
 [["IN", "PH", "ID", "TH", "VN", 
-  "TW", "SG", "KR", "CN", "MY", "JP"], 
+  "TW", "KR", "CN", "MY", "JP"], 
  ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
-    
-[["RU", "UA", "KZ", "BY", "AM",
-  "AZ", "PL", "SK", "CZ"], ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=random_model&g=female"]],
 
-[["ZZ"], ["https://kinosalo.com"]]     
+
+[["RU", "UA", "KZ", "BY", "AM",
+  "AZ", "PL", "SK", "CZ", "PT"], 
+ ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=random_model&g=female"]],
+
+
+[["GB", "FR", "AU", "NZ", "CA", "SG",
+  "DE", "AT", "BE", "IT", "NL", "CH",
+  "IS", "IE", "NO", "SE", "DK", "FI"], 
+ ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=wtBiH&disable_sound=True"]],
+
+
+[["ZZ"], ["https://kinosalo.com"]]      
 
 ]
 }
