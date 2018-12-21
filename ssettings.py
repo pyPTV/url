ba3a125fc58b6d95b902d33804985bdb5ad8de22
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2018-12-21 21:33:50 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -4,7 +4,7 @@
 [["CA", "AU", "NZ", "GB", "DE", "NL", "FR",
   "IT", "CH", "AT", "BE", "IS", "IE", "NO", 
   "SE", "DK", "FI", "SG", "JP", "TW", "KR"],
-["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyODY1MDYsImMiOjU2NTA0NDcwLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
+["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
 [["ZZ"],["https://chaturbate.com/in/?track=default&tour=4auy&campaign=bwJAD&signup_notice=1&disable_sound=True"]]
 
@@ -17,7 +17,7 @@
 [["CA", "AU", "NZ", "GB", "DE", "NL", "FR",
   "IT", "CH", "AT", "BE", "IS", "IE", "NO", 
   "SE", "DK", "FI", "SG", "JP", "TW", "KR"],
-["https://www.transangels.com/landing/tgp/?ats=eyJhIjoyODY1MDYsImMiOjU2NTA0NDcwLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
+["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
 [["ZZ"],["https://chaturbate.com/in/?track=default&tour=4auy&campaign=bwJAD&signup_notice=1&disable_sound=True"]]
 
