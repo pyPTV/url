2023016d20b05bb749acb97de9cd267dc8771364
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-02-17 15:59:19 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,12 +4,13 @@
 
 [["IN", "PH", "ID", "TH", "VN", "TW", "KR", "CN", "MY", "JP"], 
  ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
+    
 [["RU", "UA", "KZ", "BY", "AM", "AZ", "PL", "SK", "CZ", "PT"], 
  ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=random_model&g=female"]],
 
 
 [["DE", "AT", "BE", "IT", "NL", "CH", "IS", "IE", "NO", "SE", "DK", "FI"], 
- ["https://t.grtya.com/xqqr9f98ow?aff_id=82882&offer_id=1081&nopop=1"]],
+ ["https://t.grtya.com/p54yxt7chs?url_id=0&aff_id=55688&offer_id=1081&nopop=1"]],
 
 [["CA", "GB", "FR", "AU", "NZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=5EcqX&disable_sound=True"]],
     
@@ -23,12 +24,13 @@
 
 [["IN", "PH", "ID", "TH", "VN", "TW", "KR", "CN", "MY", "JP"], 
  ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
+    
 [["RU", "UA", "KZ", "BY", "AM", "AZ", "PL", "SK", "CZ", "PT"], 
  ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=random_model&g=female"]],
 
 
 [["DE", "AT", "BE", "IT", "NL", "CH", "IS", "IE", "NO", "SE", "DK", "FI"], 
- ["https://t.grtya.com/xqqr9f98ow?aff_id=82882&offer_id=1081&nopop=1"]],
+ ["https://t.grtya.com/p54yxt7chs?url_id=0&aff_id=55688&offer_id=1081&nopop=1"]],
 
 
 [["CA", "GB", "FR", "AU", "NZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=5EcqX&disable_sound=True"]],
