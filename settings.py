815e41e4e6dd1e25b3968a5ac3fbf0d75e2a99e7
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-03-26 01:27:53 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,14 +2,23 @@
     "mobile":
 [
 
-[["IN", "PH", "ID", "TH", "VN", "TW", "KR", "CN", "MY", "JP"], 
+[["IN", "PH", "ID", "TH", "VN", 
+  "TW", "KR", "CN", "MY", "JP"], 
  ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
     
-[["RU", "UA", "KZ", "BY", "AM", "AZ", "PL", "SK", "CZ", "PT", "SI"], 
+[["RU", "AM", "AZ", "PL", "LI"], 
  ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female"]],
 
+[["CZ", "ES", "FI", "IL", "SK",
+  "SI", "LV", "LT", "GR", "ES",
+  "BY", "UA", "LU", "AE", "RO", 
+  "HU", "TR", "CY", "BG", "PT",
+  "SG", "ZA", "HR", "PT", "SG",
+  "MK", "BH", "JO", "RS", "AR",
+  "KZ", "MX", "MD", "BA", "BE"], ["https://bongacams2.com/track?c=332289"]],
 
-[["DE", "AT", "BE", "IT", "NL", "CH", "IS", "IE", "NO", "SE", "DK", "FI"], 
+[["DE", "AT", "IT", "NL", "CH", 
+  "IS", "IE", "NO", "SE", "DK"], 
  ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=5EcqX&disable_sound=True"]],
 
 [["CA", "GB", "FR", "AU", "NZ"], 
@@ -25,14 +34,23 @@
     "desktop":
 [
 
-[["IN", "PH", "ID", "TH", "VN", "TW", "KR", "CN", "MY", "JP"], 
+[["IN", "PH", "ID", "TH", "VN", 
+  "TW", "KR", "CN", "MY", "JP"], 
  ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
     
-[["RU", "UA", "KZ", "BY", "AM", "AZ", "PL", "SK", "CZ", "PT", "SI"], 
+[["RU", "AM", "AZ", "PL", "LI"], 
  ["https://promo-bc.com/promo.php?type=direct_link&c=332288&amute=1&page=popular_chat&g=female"]],
 
+[["CZ", "ES", "FI", "IL", "SK",
+  "SI", "LV", "LT", "GR", "ES",
+  "BY", "UA", "LU", "AE", "RO", 
+  "HU", "TR", "CY", "BG", "PT",
+  "SG", "ZA", "HR", "PT", "SG",
+  "MK", "BH", "JO", "RS", "AR",
+  "KZ", "MX", "MD", "BA", "BE"], ["https://bongacams2.com/track?c=332289"]],
 
-[["DE", "AT", "BE", "IT", "NL", "CH", "IS", "IE", "NO", "SE", "DK", "FI"], 
+[["DE", "AT", "IT", "NL", "CH", 
+  "IS", "IE", "NO", "SE", "DK"], 
  ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=5EcqX&disable_sound=True"]],
 
 [["CA", "GB", "FR", "AU", "NZ"], 
@@ -40,7 +58,7 @@
     
 [["US"], ["https://www.xxxvideos247.com"]],
 
-[["ZZ"], ["https://www.xxxvideos247.com"]] 
+[["ZZ"], ["https://www.xxxvideos247.com"]]
 
 ]
 }
