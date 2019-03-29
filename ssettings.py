3a531354f2d4301a420e3e2766e2f675ccf6f44f
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-03-29 13:16:46 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -1,12 +1,25 @@
 {
 "mobile": [
 
-[["CA", "AU", "NZ", "GB", "DE", "NL", "FR",
-  "IT", "CH", "AT", "BE", "IS", "IE", "NO", 
-  "SE", "DK", "FI", "SG", "JP", "TW", "KR"],
+[["CA", "AU", "NZ", "GB", "DE", 
+  "NL", "FR", "IT", "CH", "AT", 
+  "BE", "IS", "IE", "NO", "SE", 
+  "DK", "FI", "SG", "JP", "TW", 
+  "KR"],
 ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyOTAwMzYsImMiOjU2NTk0NjU1LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
 
+[["CZ", "ES", "IL", "SK", "SI", 
+  "LV", "LT", "GR", "ES", "LI", 
+  "LU", "RO", "HU", "CY", "BG", 
+  "PT", "ZA", "HR", "PT", "HK", 
+  "MK", "RS", "AR", "MX", "BR", 
+  "SK"], 
+["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
+
+
 [["ZZ"],["https://chaturbate.com/in/?track=default&tour=4auy&campaign=bwJAD&signup_notice=1&disable_sound=True"]]
+
+
 
 ],
 
@@ -14,13 +27,23 @@
 
 "desktop": [
 
-[["CA", "AU", "NZ", "GB", "DE", "NL", "FR",
-  "IT", "CH", "AT", "BE", "IS", "IE", "NO", 
-  "SE", "DK", "FI", "SG", "JP", "TW", "KR"],
+[["CA", "AU", "NZ", "GB", "DE", 
+  "NL", "FR", "IT", "CH", "AT", 
+  "BE", "IS", "IE", "NO", "SE", 
+  "DK", "FI", "SG", "JP", "TW", 
+  "KR"],
 ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyOTAwMzYsImMiOjU2NTk0NjU1LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
+
+[["CZ", "ES", "IL", "SK", "SI", 
+  "LV", "LT", "GR", "ES", "LI", 
+  "LU", "RO", "HU", "CY", "BG", 
+  "PT", "ZA", "HR", "PT", "HK", 
+  "MK", "RS", "AR", "MX", "BR", 
+  "SK"], 
+["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
+
 
 [["ZZ"],["https://chaturbate.com/in/?track=default&tour=4auy&campaign=bwJAD&signup_notice=1&disable_sound=True"]]
 
-]
 
-}
+]}
