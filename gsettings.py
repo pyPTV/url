0ea47f5d0d20a25a8d1d3d47f80327247b34c70b
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-05-12 15:56:07 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -8,7 +8,7 @@
  ["http://join.allgaysitespass.com/track/MjAxMDE4LjEuMy40LjAuMC4wLjAuMA"]],
                                         
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],  
-[["ZZ"], ["https://t.frtyj.com/70wbjn1s8w?url_id=0&aff_id=55931&offer_id=3343&nopop=1"]]  
+[["ZZ"], ["https://t.frtyj.com/u031d2837k?url_id=0&aff_id=55931&offer_id=3343"]]  
                
 ],
  
@@ -21,7 +21,7 @@
  ["http://join.allgaysitespass.com/track/MjAxMDE4LjEuMy40LjAuMC4wLjAuMA"]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
-[["ZZ"], ["https://t.frtyj.com/70wbjn1s8w?url_id=0&aff_id=55931&offer_id=3343&nopop=1"]]              
+[["ZZ"], ["https://t.frtyj.com/u031d2837k?url_id=0&aff_id=55931&offer_id=3343"]]              
 
 ]
 }
