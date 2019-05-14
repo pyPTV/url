4a5c9861148c554d68a2540ca24aa5d37f85554e
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-05-14 07:40:00 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,26 +5,23 @@
 [["IN", "PH", "ID", "TH", "VN", "TW", "KR", "CN", "MY", "JP"], 
  ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
     
-[["RU", "AM", "AZ", "PL", "LI"], 
- ["https://bongacams2.com/track?c=332288"]],
-
-[["CZ", "ES", "FI", "IL", "SK",
+[["RU", "AM", "AZ", "PL", "LI", 
+  "CZ", "ES", "FI", "IL", "SK",
   "SI", "LV", "LT", "GR", "ES",
   "BY", "UA", "LU", "AE", "RO", 
   "HU", "TR", "CY", "BG", "PT",
   "SG", "ZA", "HR", "PT", "SG",
   "MK", "BH", "JO", "RS", "AR",
-  "KZ", "MX", "MD", "BA", "BE"], 
- ["https://bongacams2.com/track?c=332289"]],
+  "KZ", "MX", "MD", "BA"], ["https://bongacams2.com/track?c=332289"]],
 
 
-[["AT", "DE", "CH", "NL", "IS", "IE", "CA", "GB", "FR", "AU", "NZ", "IT", "NO", "SE", "DK"], 
+[["AT", "DE", "CH", "NL", "IS", "IE", "CA", "GB", "FR", "AU", "NZ", "IT", "NO", "SE", "DK", "BE"], 
  ["https://t.hrtya.com/9zzi4p7olc?url_id=17846&aff_id=82882&offer_id=3640&nopop=1",
   "https://t.hrtya.com/4tttn8rlz4?url_id=16948&aff_id=82882&offer_id=3640&nopop=1"]],
 
 [["US"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
 
-[["ZZ"], ["https://www.xxxvideos247.com"]]
+[["ZZ"], ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]]
 
                
 ],
@@ -35,27 +32,24 @@
 [["IN", "PH", "ID", "TH", "VN", "TW", "KR", "CN", "MY", "JP"], 
  ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
     
-[["RU", "AM", "AZ", "PL", "LI"], 
- ["https://bongacams2.com/track?c=332288"]],
-
-[["CZ", "ES", "FI", "IL", "SK",
+[["RU", "AM", "AZ", "PL", "LI", 
+  "CZ", "ES", "FI", "IL", "SK",
   "SI", "LV", "LT", "GR", "ES",
   "BY", "UA", "LU", "AE", "RO", 
   "HU", "TR", "CY", "BG", "PT",
   "SG", "ZA", "HR", "PT", "SG",
   "MK", "BH", "JO", "RS", "AR",
-  "KZ", "MX", "MD", "BA", "BE"], 
- ["https://bongacams2.com/track?c=332289"]],
+  "KZ", "MX", "MD", "BA"], ["https://bongacams2.com/track?c=332289"]],
 
 
-[["AT", "DE", "CH", "NL", "IS", "IE", "CA", "GB", "FR", "AU", "NZ", "IT", "NO", "SE", "DK"], 
+[["AT", "DE", "CH", "NL", "IS", "IE", "CA", "GB", "FR", "AU", "NZ", "IT", "NO", "SE", "DK", "BE"], 
  ["https://t.hrtya.com/9zzi4p7olc?url_id=17846&aff_id=82882&offer_id=3640&nopop=1",
   "https://t.hrtya.com/4tttn8rlz4?url_id=16948&aff_id=82882&offer_id=3640&nopop=1"]],
 
     
 [["US"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
 
-[["ZZ"], ["https://www.xxxvideos247.com"]]
+[["ZZ"], ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]]
 
 ]
 }
