9f70073277191e43abed37197102b20f94736485
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-05-14 07:50:31 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,16 +6,17 @@
  ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
     
 [["RU", "AM", "AZ", "PL", "LI", 
-  "CZ", "ES", "FI", "IL", "SK",
-  "SI", "LV", "LT", "GR", "ES",
-  "BY", "UA", "LU", "AE", "RO", 
-  "HU", "TR", "CY", "BG", "PT",
-  "SG", "ZA", "HR", "PT", "SG",
+  "CZ", "IL", "SK", "SI", "LV", 
+  "LT", "GR", "ES", "BY", "UA", 
+  "AE", "RO", "HU", "TR", "CY", 
+  "IT", "SG", "ZA", "HR", "SG",
   "MK", "BH", "JO", "RS", "AR",
   "KZ", "MX", "MD", "BA"], ["https://bongacams2.com/track?c=332289"]],
 
-
-[["AT", "DE", "CH", "NL", "IS", "IE", "CA", "GB", "FR", "AU", "NZ", "IT", "NO", "SE", "DK", "BE"], 
+[["AT", "AU", "BE", "CA", "DK",
+  "FI", "FR", "DE", "IE", "IS",
+  "JP", "LU", "NL", "NZ", "NO",
+  "PT", "ES", "SE", "CH", "GB"],
  ["https://t.hrtya.com/9zzi4p7olc?url_id=17846&aff_id=82882&offer_id=3640&nopop=1",
   "https://t.hrtya.com/4tttn8rlz4?url_id=16948&aff_id=82882&offer_id=3640&nopop=1"]],
 
@@ -33,20 +34,20 @@
  ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
     
 [["RU", "AM", "AZ", "PL", "LI", 
-  "CZ", "ES", "FI", "IL", "SK",
-  "SI", "LV", "LT", "GR", "ES",
-  "BY", "UA", "LU", "AE", "RO", 
-  "HU", "TR", "CY", "BG", "PT",
-  "SG", "ZA", "HR", "PT", "SG",
+  "CZ", "IL", "SK", "SI", "LV", 
+  "LT", "GR", "ES", "BY", "UA", 
+  "AE", "RO", "HU", "TR", "CY", 
+  "IT", "SG", "ZA", "HR", "SG",
   "MK", "BH", "JO", "RS", "AR",
   "KZ", "MX", "MD", "BA"], ["https://bongacams2.com/track?c=332289"]],
 
-
-[["AT", "DE", "CH", "NL", "IS", "IE", "CA", "GB", "FR", "AU", "NZ", "IT", "NO", "SE", "DK", "BE"], 
+[["AT", "AU", "BE", "CA", "DK",
+  "FI", "FR", "DE", "IE", "IS",
+  "JP", "LU", "NL", "NZ", "NO",
+  "PT", "ES", "SE", "CH", "GB"],
  ["https://t.hrtya.com/9zzi4p7olc?url_id=17846&aff_id=82882&offer_id=3640&nopop=1",
   "https://t.hrtya.com/4tttn8rlz4?url_id=16948&aff_id=82882&offer_id=3640&nopop=1"]],
 
-    
 [["US"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
 
 [["ZZ"], ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]]
