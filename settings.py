8801255e04da3105270baad880da9a55a2b116d1
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-05-15 06:13:00 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -15,8 +15,7 @@
   "FI", "FR", "DE", "IE", "IS",
   "JP", "LU", "NL", "NZ", "NO",
   "PT", "ES", "SE", "CH", "GB"],
- ["https://t.hrtya.com/9zzi4p7olc?url_id=17846&aff_id=82882&offer_id=3640&nopop=1",
-  "https://t.frtyn.com/ak1fbury0w?url_id=15876&aff_id=82882&offer_id=1725&nopop=1"]],
+ ["https://t.hrtya.com/9zzi4p7olc?url_id=17846&aff_id=82882&offer_id=3640&nopop=1"]],
 
 [["US"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
 
@@ -41,8 +40,7 @@
   "FI", "FR", "DE", "IE", "IS",
   "JP", "LU", "NL", "NZ", "NO",
   "PT", "ES", "SE", "CH", "GB"],
- ["https://t.hrtya.com/9zzi4p7olc?url_id=17846&aff_id=82882&offer_id=3640&nopop=1",
-  "https://t.frtyn.com/ak1fbury0w?url_id=15876&aff_id=82882&offer_id=1725&nopop=1"]],
+ ["https://t.hrtya.com/9zzi4p7olc?url_id=17846&aff_id=82882&offer_id=3640&nopop=1"]],
 
 [["US"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
 
