3d7a9bf6a44a2eeeaf5f9b9bde6dc01c256d903d
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-05-20 08:46:49 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,23 +2,8 @@
     "mobile":
 [
 
-[["IN", "PH", "ID", "TH", "VN", "KR", "CN", "MY", "TW", "SG", "JP"], 
- ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
-    
-[["RU", "AM", "PL", "LI", "CZ", 
-  "SK", "SI", "LV", "LT", "BY", 
-  "UA", "AE", "RO", "HU", "TR", 
-  "ZA", "HR", "MK", "BH", "JO", 
-  "RS", "AR", "KZ", "MD", "BA"], ["https://bongacams2.com/track?c=332289"]],
-
-[["AT", "AU", "BE", "CA", "DK",
-  "FI", "FR", "DE", "IE", "IS",
-  "LU", "NL", "NZ", "NO",
-  "PT", "ES", "SE", "CH", "GB"],
- ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=AconN&disable_sound=True"]],
-
 [["US"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
-
+    
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=AconN&disable_sound=True"]]
 
                
@@ -27,23 +12,8 @@
     "desktop":
 [
 
-[["IN", "PH", "ID", "TH", "VN", "KR", "CN", "MY", "TW", "SG", "JP"], 
- ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]],
-    
-[["RU", "AM", "PL", "LI", "CZ", 
-  "SK", "SI", "LV", "LT", "BY", 
-  "UA", "AE", "RO", "HU", "TR", 
-  "ZA", "HR", "MK", "BH", "JO", 
-  "RS", "AR", "KZ", "MD", "BA"], ["https://bongacams2.com/track?c=332289"]],
-
-[["AT", "AU", "BE", "CA", "DK",
-  "FI", "FR", "DE", "IE", "IS",
-  "LU", "NL", "NZ", "NO",
-  "PT", "ES", "SE", "CH", "GB"],
- ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=AconN&disable_sound=True"]],
-
 [["US"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
-
+    
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=AconN&disable_sound=True"]]
 
 ]
