2021a4e8b64d0c6eab60b35522f458ae89a5374d
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-06-03 10:15:07 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,7 +4,9 @@
 
 [["US"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
     
-[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=AconN&disable_sound=True"]]
+[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
+    
+[["ZZ"], ["https://t.hrtya.com/siqg5cawow?url_id=16947&aff_id=82882&offer_id=3640&nopop=1"]]
 
                
 ],
@@ -14,7 +16,9 @@
 
 [["US"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
     
-[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=AconN&disable_sound=True"]]
+[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
+ 
+[["ZZ"], ["https://t.hrtya.com/siqg5cawow?url_id=16947&aff_id=82882&offer_id=3640&nopop=1"]]
 
 ]
 }
