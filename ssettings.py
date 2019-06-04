c3514b634310ea463a08240586dc92d30473eecb
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-06-04 06:48:51 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,15 +2,11 @@
 "mobile": [
 
 
-[["US"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
-
-
 [["CA", "AU", "NZ", "GB", "DE", 
   "NL", "FR", "IT", "CH", "AT", 
   "BE", "IS", "IE", "NO", "SE", 
   "DK", "FI", "SG", "JP", "TW"],
 ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk0OTYsImMiOjU2NDU3MTQ1LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
-
 
 
 [["ZZ"],["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]]
@@ -23,16 +19,11 @@
 
 "desktop": [
 
-
-[["US"], ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
-
-
 [["CA", "AU", "NZ", "GB", "DE", 
   "NL", "FR", "IT", "CH", "AT", 
   "BE", "IS", "IE", "NO", "SE", 
   "DK", "FI", "SG", "JP", "TW"],
 ["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODk0OTYsImMiOjU2NDU3MTQ1LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]],
-
 
 
 [["ZZ"],["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoyfQ=="]]
