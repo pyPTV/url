d200107cc7603f348fc6384de6a01780af6a83e8
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-06-14 15:33:25 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,11 +2,12 @@
     "mobile":
 [
 
-[["US", "FR", "GB", "AU", "CA", "NO", "CH", "NZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
+[["US", "FR", "GB", "AU", "CA", "NO", "CH", "NZ", "DE", "NL"], 
+ ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
     
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
     
-[["ZZ"], ["https://t.hrtya.com/siqg5cawow?url_id=16947&aff_id=82882&offer_id=3640&nopop=1"]]
+[["ZZ"], ["https://bongacams3.com/track?c=332289"]]
 
                
 ],
@@ -14,11 +15,12 @@
     "desktop":
 [
 
-[["US", "FR", "GB", "AU", "CA", "NO", "CH", "NZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
+[["US", "FR", "GB", "AU", "CA", "NO", "CH", "NZ", "DE", "NL"], 
+ ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
     
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
  
-[["ZZ"], ["https://t.hrtya.com/siqg5cawow?url_id=16947&aff_id=82882&offer_id=3640&nopop=1"]]
+[["ZZ"], ["https://bongacams3.com/track?c=332289"]]
 
 ]
 }
