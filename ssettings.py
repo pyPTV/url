09eaa2d34affbcfab610179a65e36813d019bb5d
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-06-20 15:53:20 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -6,10 +6,10 @@
   "NL", "FR", "IT", "CH", "AT", 
   "BE", "IS", "IE", "NO", "SE", 
   "DK", "FI", "SG", "JP", "TW"],
-["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]],
+["https://tgp.transangelsnetwork.com/?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NTQxLCJlIjo4OTkxLCJwIjoxMX0="]],
 
 
-[["ZZ"],["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]]
+[["ZZ"],["https://tgp.transangelsnetwork.com/?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NTQxLCJlIjo4OTkxLCJwIjoxMX0="]]
 
 
 
@@ -23,10 +23,10 @@
   "NL", "FR", "IT", "CH", "AT", 
   "BE", "IS", "IE", "NO", "SE", 
   "DK", "FI", "SG", "JP", "TW"],
-["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]],
+["https://tgp.transangelsnetwork.com/?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NTQxLCJlIjo4OTkxLCJwIjoxMX0="]],
 
 
-[["ZZ"],["https://www.transangels.com/landing/tgp?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjozNSwicyI6NDI1LCJlIjo3ODY2LCJwIjoxMX0="]]
+[["ZZ"],["https://tgp.transangelsnetwork.com/?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NTQxLCJlIjo4OTkxLCJwIjoxMX0="]]
 
 
 
