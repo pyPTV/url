c1ea015160ada017bfe1fe25c46a56d383151b55
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-06-26 18:56:41 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -6,10 +6,10 @@
   "NL", "FR", "IT", "CH", "AT", 
   "BE", "IS", "IE", "NO", "SE", 
   "DK", "FI", "SG", "JP", "TW"],
-["https://tgp.transangelsnetwork.com/?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NTQxLCJlIjo4OTkxLCJwIjoxMX0="]],
+["https://www.trannytube.net/"]],
 
 
-[["ZZ"],["https://tgp.transangelsnetwork.com/?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NTQxLCJlIjo4OTkxLCJwIjoxMX0="]]
+[["ZZ"],["https://www.trannytube.net/"]]
 
 
 
@@ -23,10 +23,10 @@
   "NL", "FR", "IT", "CH", "AT", 
   "BE", "IS", "IE", "NO", "SE", 
   "DK", "FI", "SG", "JP", "TW"],
-["https://tgp.transangelsnetwork.com/?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NTQxLCJlIjo4OTkxLCJwIjoxMX0="]],
+["https://www.trannytube.net/"]],
 
 
-[["ZZ"],["https://tgp.transangelsnetwork.com/?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NTQxLCJlIjo4OTkxLCJwIjoxMX0="]]
+[["ZZ"],["https://www.trannytube.net/"]]
 
 
 
