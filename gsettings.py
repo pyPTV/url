effb27bf0eccc096ff38522f4b301d5dd6eb117f
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-07-07 13:36:23 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -5,7 +5,7 @@
 [["US", "GB", "FR", "AU", "NZ", "CA", 
   "DE", "AT", "BE", "IT", "NL", "CH",
   "IS", "IE", "NO", "SE", "DK", "FI"], 
- ["https://t.frtyj.com/qslpnvzuv4?url_id=0&aff_id=54636&offer_id=3343&nopop=1"]],
+ ["https://t.frtyj.com/730agjufb4?url_id=0&aff_id=79596&offer_id=3343&nopop=1"]],
                                         
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],  
 [["ZZ"], ["https://t.frtyj.com/uab8m45br4?url_id=0&aff_id=56817&offer_id=3343&nopop=1"]]  
@@ -18,7 +18,7 @@
 [["US", "GB", "FR", "AU", "NZ", "CA", 
   "DE", "AT", "BE", "IT", "NL", "CH",
   "IS", "IE", "NO", "SE", "DK", "FI"], 
- ["https://t.frtyj.com/qslpnvzuv4?url_id=0&aff_id=54636&offer_id=3343&nopop=1"]],
+ ["https://t.frtyj.com/730agjufb4?url_id=0&aff_id=79596&offer_id=3343&nopop=1"]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 [["ZZ"], ["https://t.frtyj.com/uab8m45br4?url_id=0&aff_id=56817&offer_id=3343&nopop=1"]]              
