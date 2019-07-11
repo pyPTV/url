f45aa71cb513bd20c0d8113dda46fe7015e7e417
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-07-11 05:55:05 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,11 +2,14 @@
     "mobile":
 [
 
-[["US", "FR", "GB", "AU", "CA", "CH", "NZ", "DE", "NL"], 
- ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
-    
+[["US"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
+
+[["CA", "FR", "DE", "GB"], ["https://t.frtyr.com/4gdkl1glj4?url_id=11889&aff_id=40075&offer_id=3&nopop=1"]],
+
+[["AU", "CH", "NZ", "NL"], ["https://t.frtyr.com/4gdkl1glj4?url_id=11889&aff_id=40075&offer_id=3&nopop=1"]],
+
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
-    
+
 [["ZZ"], ["https://bongacams3.com/track?c=332289"]]
 
                
@@ -15,11 +18,14 @@
     "desktop":
 [
 
-[["US", "FR", "GB", "AU", "CA", "CH", "NZ", "DE", "NL"], 
- ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
-    
+[["US"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
+
+[["CA", "FR", "DE", "GB"], ["https://t.frtyr.com/4gdkl1glj4?url_id=11889&aff_id=40075&offer_id=3&nopop=1"]],
+
+[["AU", "CH", "NZ", "NL"], ["https://t.frtyr.com/4gdkl1glj4?url_id=11889&aff_id=40075&offer_id=3&nopop=1"]],
+
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
- 
+
 [["ZZ"], ["https://bongacams3.com/track?c=332289"]]
 
 ]
