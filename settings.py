7c7b519e5dde07c8a9c595509328c4c1399d0d6c
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-07-18 08:01:49 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -8,7 +8,8 @@
 ["https://t.grtyv.com/wqe0gp1se8?url_id=0&aff_id=40075&offer_id=5881&nopop=1",
  "https://t.frtyr.com/4gdkl1glj4?url_id=11889&aff_id=40075&offer_id=3&nopop=1"]],
 
-[["AU", "CH", "NZ", "NL", "NO", "DK", "SE"], ["https://t.frtyr.com/4gdkl1glj4?url_id=11889&aff_id=40075&offer_id=3&nopop=1"]],
+[["AU", "CH", "NZ", "NL", "NO", "DK", "SE"], 
+["https://t.frtyr.com/4gdkl1glj4?url_id=11889&aff_id=40075&offer_id=3&nopop=1"]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
 
@@ -26,7 +27,8 @@
 ["https://t.grtyv.com/wqe0gp1se8?url_id=0&aff_id=40075&offer_id=5881&nopop=1",
  "https://t.frtyr.com/4gdkl1glj4?url_id=11889&aff_id=40075&offer_id=3&nopop=1"]],
 
-[["AU", "CH", "NZ", "NL", "NO", "DK", "SE"], ["https://t.frtyr.com/4gdkl1glj4?url_id=11889&aff_id=40075&offer_id=3&nopop=1"]],
+[["AU", "CH", "NZ", "NL", "NO", "DK", "SE"], 
+["https://t.frtyr.com/4gdkl1glj4?url_id=11889&aff_id=40075&offer_id=3&nopop=1"]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
 
