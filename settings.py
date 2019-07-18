e909f806eb9d9f8a54c65cd85d8653dc101d608e
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-07-18 08:11:40 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,8 +5,7 @@
 
 
 [["US", "CA", "FR", "DE", "GB"], 
-["https://t.grtyv.com/wqe0gp1se8?url_id=0&aff_id=40075&offer_id=5881&nopop=1",
- "https://t.frtyr.com/4gdkl1glj4?url_id=11889&aff_id=40075&offer_id=3&nopop=1"]],
+["https://t.grtyv.com/wqe0gp1se8?url_id=0&aff_id=40075&offer_id=5881&nopop=1"]],
 
 [["AU", "CH", "NZ", "NL", "NO", "DK", "SE"], 
 ["https://t.frtyr.com/4gdkl1glj4?url_id=11889&aff_id=40075&offer_id=3&nopop=1"]],
@@ -24,8 +23,7 @@
 
 
 [["US", "CA", "FR", "DE", "GB"], 
-["https://t.grtyv.com/wqe0gp1se8?url_id=0&aff_id=40075&offer_id=5881&nopop=1",
- "https://t.frtyr.com/4gdkl1glj4?url_id=11889&aff_id=40075&offer_id=3&nopop=1"]],
+["https://t.grtyv.com/wqe0gp1se8?url_id=0&aff_id=40075&offer_id=5881&nopop=1"]],
 
 [["AU", "CH", "NZ", "NL", "NO", "DK", "SE"], 
 ["https://t.frtyr.com/4gdkl1glj4?url_id=11889&aff_id=40075&offer_id=3&nopop=1"]],
