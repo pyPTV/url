171c9c586180d5e457c2fe0bde5508156cfc5c33
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-07-23 09:02:46 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -5,13 +5,13 @@
 
 [["US", "GB", "FR", "AU", "NZ", "CA", 
   "DE", "AT", "BE", "IT", "NL", "CH",
-  "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/y8wut305xc?url_id=0&aff_id=53837&offer_id=3343&nopop=1"]],
+  "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/ssdgfl4kjk?url_id=0&aff_id=53280&offer_id=3343&nopop=1"]],
 
 
-[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],  
+[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
 
-[["ZZ"], ["https://t.frtyj.com/ssdgfl4kjk?url_id=0&aff_id=53280&offer_id=3343&nopop=1"]]  
+[["ZZ"], ["https://t.frtyj.com/y8wut305xc?url_id=0&aff_id=53837&offer_id=3343&nopop=1"]]  
                
 ],
  
@@ -22,13 +22,13 @@
 
 [["US", "GB", "FR", "AU", "NZ", "CA", 
   "DE", "AT", "BE", "IT", "NL", "CH",
-  "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/y8wut305xc?url_id=0&aff_id=53837&offer_id=3343&nopop=1"]],
+  "IS", "IE", "NO", "SE", "DK", "FI"], ["https://t.frtyj.com/ssdgfl4kjk?url_id=0&aff_id=53280&offer_id=3343&nopop=1"]],
 
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
 
-[["ZZ"], ["https://t.frtyj.com/ssdgfl4kjk?url_id=0&aff_id=53280&offer_id=3343&nopop=1"]]              
+[["ZZ"], ["https://t.frtyj.com/y8wut305xc?url_id=0&aff_id=53837&offer_id=3343&nopop=1"]]              
 
 ]
 }
