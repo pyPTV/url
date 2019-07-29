790afce230441008a32468800610f80389f0f439
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-07-29 09:52:56 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@
 
 
 [["US", "CA", "FR", "DE", "GB", "AU", "CH", "NZ", "NL", "NO", "DK", "SE"], 
-["https://t.frtyr.com/n72l73si68?url_id=0&aff_id=55688&offer_id=3&nopop=1"]],
+["https://t.frtyr.com/saa6nxw4ow?url_id=0&aff_id=54557&offer_id=3&nopop=1"]],
 
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
@@ -22,7 +22,7 @@
 
 
 [["US", "CA", "FR", "DE", "GB", "AU", "CH", "NZ", "NL", "NO", "DK", "SE"], 
-["https://t.frtyr.com/n72l73si68?url_id=0&aff_id=55688&offer_id=3&nopop=1"]],
+["https://t.frtyr.com/saa6nxw4ow?url_id=0&aff_id=54557&offer_id=3&nopop=1"]],
 
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
