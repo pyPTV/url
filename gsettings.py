5cb00aa1930cdaa8eecce583a44d6dddf645c612
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-08-02 08:56:48 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -5,7 +5,9 @@
 
 [["US", "GB", "FR", "AU", "NZ", "CA", 
   "DE", "AT", "BE", "IT", "NL", "CH",
-  "IS", "IE", "NO", "SE", "DK", "FI"], ["http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA"]],
+  "IS", "IE", "NO", "SE", "DK", "FI"], 
+["http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA", 
+ "https://t.frtyj.com/v1i6a93glc?url_id=0&aff_id=55931&offer_id=2852&nopop=1"]],
 
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
@@ -22,7 +24,9 @@
 
 [["US", "GB", "FR", "AU", "NZ", "CA", 
   "DE", "AT", "BE", "IT", "NL", "CH",
-  "IS", "IE", "NO", "SE", "DK", "FI"], ["http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA"]],
+  "IS", "IE", "NO", "SE", "DK", "FI"], 
+["http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA",
+ "https://t.frtyj.com/v1i6a93glc?url_id=0&aff_id=55931&offer_id=2852&nopop=1"]],
 
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
