6b60d7da99e3f952005334a64a950547cf267f0f
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-08-15 08:10:34 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -8,7 +8,9 @@
 [["GB", "FR", "DE", "AT", 
   "BE", "IT", "NL", "CH",
   "IS", "IE", "NO", "SE", 
-  "DK", "FI", "ES", "PT"], ["http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA"]],
+  "DK", "FI", "ES", "PT"], 
+["http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA",
+ "https://wct.link/click?c=eyJhIjoxOTQ4MSwibyI6NTgsInAiOjJ9"]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
@@ -27,8 +29,9 @@
 [["GB", "FR", "DE", "AT", 
   "BE", "IT", "NL", "CH",
   "IS", "IE", "NO", "SE", 
-  "DK", "FI", "ES", "PT"], ["http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA"]],
-
+  "DK", "FI", "ES", "PT"], 
+["http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA",
+ "https://wct.link/click?c=eyJhIjoxOTQ4MSwibyI6NTgsInAiOjJ9"]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
