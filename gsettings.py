65dead676cf187bae7d335e767e60f31d9d67a8a
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-08-30 08:27:46 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -12,7 +12,8 @@
   "CZ", "SK", "SI", "PL",
   "JP", "SG", "TW", "IL",
   "HU", "LI", "LU", "GR"], 
-["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&pa=index&ar=&buffer="]],
+["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&pa=index&ar=&buffer=",
+ "https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
@@ -34,7 +35,8 @@
   "CZ", "SK", "SI", "PL",
   "JP", "SG", "TW", "IL",
   "HU", "LI", "LU", "GR"], 
-["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&pa=index&ar=&buffer="]],
+["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&pa=index&ar=&buffer=",
+ "https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
