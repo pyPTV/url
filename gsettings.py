06b4ae5d2fd8aa29168b637958b9e40fd687b42a
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-09-02 01:32:19 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -12,14 +12,12 @@
   "CZ", "SK", "SI", "PL",
   "JP", "SG", "TW", "IL",
   "HU", "LI", "LU", "GR"], 
-["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&pa=index&ar=&buffer=",
- "https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]],
+["http://join.allgaysitespass.com/track/MjAxMDE4LjEuMy40LjAuMC4wLjAuMA"]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
 
-[["ZZ"], ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273894&pa=index&ar=&buffer=",
-          "https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273894&cs=tour1&ar=&buffer="]]
+[["ZZ"], ["http://join.allgaysitespass.com/track/MjAxMDE4LjEuMy40LjAuMC4wLjAuMA"]]
                
 ],
  
@@ -36,14 +34,12 @@
   "CZ", "SK", "SI", "PL",
   "JP", "SG", "TW", "IL",
   "HU", "LI", "LU", "GR"], 
-["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&pa=index&ar=&buffer=",
- "https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]],
+["http://join.allgaysitespass.com/track/MjAxMDE4LjEuMy40LjAuMC4wLjAuMA"]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
 
-[["ZZ"], ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273894&pa=index&ar=&buffer=",
-          "https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273894&cs=tour1&ar=&buffer="]]              
+[["ZZ"], ["http://join.allgaysitespass.com/track/MjAxMDE4LjEuMy40LjAuMC4wLjAuMA"]]              
 
 ]
 }
