74fd7b126153f2eb1daa90527791f55fa99b7bf5
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-09-02 13:17:47 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -13,13 +13,12 @@
   "JP", "SG", "TW", "IL",
   "HU", "LI", "LU", "GR"], 
 ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA", 
- "http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA"]],
+ "https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=270918&cs=tour1&ar=&buffer="]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
 
-[["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA",
-          "http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA"]]
+[["ZZ"], ["http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA"]]
                
 ],
  
@@ -37,13 +36,12 @@
   "JP", "SG", "TW", "IL",
   "HU", "LI", "LU", "GR"], 
 ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA",
- "http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA"]],
+ "https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=270918&cs=tour1&ar=&buffer="]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
 
-[["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNDQyLjEuMy40LjAuMC4wLjAuMA",
-          "http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA"]]              
+[["ZZ"], ["http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA"]]              
 
 ]
 }
