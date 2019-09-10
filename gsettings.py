2eef77b8bd670868100eb17e5afafc3e19ddf4d4
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-09-11 02:28:53 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,15 +4,12 @@
 
 [["US"], ["http://join.allgaysitespass.com/track/MjAxMDE4LjEuMy40LjAuMC4wLjAuMA"]],
 
+[["FR", "GB", "DE", "CA"], ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273894&cs=tour1&ar=&buffer="]], 
 
 [["BE", "IT", "AT", "IS", 
   "IE", "NO", "SE", "DK", 
-  "FI", "LI", "LU", "AU", 
-  "NZ", "FR", "CH", "NL",
-  "ES", "GB", "DE", "CA"],
-["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273894&cs=tour1&ar=&buffer="]], 
-
-
+  "FI", "AU", "NZ", "CH", 
+  "NL", "SK", "SI", "CZ"], ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
@@ -27,14 +24,12 @@
 [["US"], ["http://join.allgaysitespass.com/track/MjAxMDE4LjEuMy40LjAuMC4wLjAuMA"]],
  
 
+[["FR", "GB", "DE", "CA"], ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273894&cs=tour1&ar=&buffer="]], 
+
 [["BE", "IT", "AT", "IS", 
   "IE", "NO", "SE", "DK", 
-  "FI", "LI", "LU", "AU", 
-  "NZ", "FR", "CH", "NL",
-  "ES", "GB", "DE", "CA"],
-["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273894&cs=tour1&ar=&buffer="]], 
-
-
+  "FI", "AU", "NZ", "CH", 
+  "NL", "SK", "SI", "CZ"], ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
