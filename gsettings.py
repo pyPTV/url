dca8a4e4ed90cafb60ee69c9a85017d9cbc47f54
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-09-10 06:38:00 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -8,16 +8,13 @@
   "NZ", "FR", "CH", "NL"],
 ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]], 
 
-[["US", "CA", "GB", "IL",
-  "DE", "ES", "PT", "CZ", 
-  "SK", "SI", "PL", "JP", 
-  "SG", "TW", "HU", "GR"], 
+[["US", "CA", "GB", "DE"], 
 ["http://join.allgaysitespass.com/track/MjAxMDE4LjEuMy40LjAuMC4wLjAuMA"]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
 
-[["ZZ"], ["http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA"]]
+[["ZZ"], ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=270918&cs=tour1&ar=&buffer="]]
                
 ],
  
@@ -32,16 +29,13 @@
   "NZ", "FR", "CH", "NL"],
 ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]], 
 
-[["US", "CA", "GB", "IL",
-  "DE", "ES", "PT", "CZ", 
-  "SK", "SI", "PL", "JP", 
-  "SG", "TW", "HU", "GR"], 
+[["US", "CA", "GB", "DE"], 
 ["http://join.allgaysitespass.com/track/MjAxMDE4LjEuMy40LjAuMC4wLjAuMA"]],
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
 
-[["ZZ"], ["http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA"]]              
+[["ZZ"], ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=270918&cs=tour1&ar=&buffer="]]              
 
 ]
 }
