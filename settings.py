c96c1191fbf96d43ed9d2081eb60d5558762d1b9
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-09-10 09:38:50 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,11 +4,8 @@
 
 
 
-[["US", "CA", "FR", "DE", "GB", "AU", "CH", "NZ", "NL", "NO", "DK", "SE"], 
-["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
+[["US"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
 
-
-[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
 
 
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]]
@@ -21,11 +18,7 @@
 
 
 
-[["US", "CA", "FR", "DE", "GB", "AU", "CH", "NZ", "NL", "NO", "DK", "SE"], 
-["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
-
-
-[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
+[["US"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]],
 
 
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]]
