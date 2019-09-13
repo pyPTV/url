2b795de4d3e1cb8d0292fda9b673f8c14a9ba302
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-09-13 10:02:59 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -2,17 +2,21 @@
     "mobile":
 [
 
-[["FR", "GB", "DE", "CA"], ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273894&cs=tour1&ar=&buffer="]], 
-
+ 
 [["BE", "IT", "AT", "IS", 
   "IE", "NO", "SE", "DK", 
   "FI", "AU", "NZ", "CH", 
-  "NL", "SK", "SI", "CZ"], ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]],
+  "NL", "FR", "DE", "GB"], 
+["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]],
+
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
 
-[["ZZ"], ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=270918&cs=tour1&ar=&buffer="]]
+[["ZZ"], 
+["https://www.buddylead.com/activeduty/go.php?pr=9&su=2&si=80&ad=273894&pa=index&ar=&buffer=",
+ "https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273894&cs=tour1&ar=&buffer="]] 
+
                
 ],
  
@@ -20,17 +24,19 @@
 [
 
 
-[["FR", "GB", "DE", "CA"], ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273894&cs=tour1&ar=&buffer="]], 
-
 [["BE", "IT", "AT", "IS", 
   "IE", "NO", "SE", "DK", 
   "FI", "AU", "NZ", "CH", 
-  "NL", "SK", "SI", "CZ"], ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]],
+  "NL", "FR", "DE", "GB"], 
+["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]],
+
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
 
-[["ZZ"], ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=270918&cs=tour1&ar=&buffer="]]              
+[["ZZ"], 
+["https://www.buddylead.com/activeduty/go.php?pr=9&su=2&si=80&ad=273894&pa=index&ar=&buffer=",
+ "https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273894&cs=tour1&ar=&buffer="]]             
 
 ]
 }
