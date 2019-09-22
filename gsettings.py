c5f7f08d26c7eab489fabe1555e5643eec24c608
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-09-22 09:32:08 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -13,8 +13,7 @@
 
 
 [["ZZ"], 
-["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273913&cs=tour1&ar=&buffer=",
- "https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273913&cs=tour1&ar=&buffer="]] 
+["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273913&cs=tour1&ar=&buffer="]] 
 
                
 ],
@@ -33,8 +32,7 @@
 
 
 [["ZZ"], 
-["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273913&cs=tour1&ar=&buffer=",
- "https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273913&cs=tour1&ar=&buffer="]]             
+["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273913&cs=tour1&ar=&buffer="]]             
 
 ]
 }
