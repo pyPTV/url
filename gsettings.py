00d7b6539a1583196b8990730a3e2222f1813032
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-09-24 12:04:06 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -2,10 +2,12 @@
     "mobile":
 [
 
- 
+[["US"], ["http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA"]],
+
 [["BE", "IT", "AT", "IS", 
   "IE", "NO", "SE", "DK", 
-  "FI", "AU", "NZ", "CH"], 
+  "FI", "AU", "NZ", "CH",
+  "FR", "DE", "NL", "GB"], 
 ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]],
 
 
@@ -22,10 +24,14 @@
 [
 
 
+[["US"], ["http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA"]],
+
 [["BE", "IT", "AT", "IS", 
   "IE", "NO", "SE", "DK", 
-  "FI", "AU", "NZ", "CH"],
+  "FI", "AU", "NZ", "CH",
+  "FR", "DE", "NL", "GB"], 
 ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]],
+
 
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
