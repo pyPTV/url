d8874ed11fa6be0ef1ce3805b1da71b4c3f44b05
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-09-26 21:50:31 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -8,7 +8,8 @@
 [["BE", "IT", "AT", "IS", 
   "IE", "NO", "SE", "DK", 
   "FI", "AU", "NZ", "CH",
-  "FR", "DE", "NL", "GB"], 
+  "FR", "DE", "NL", "GB", 
+  "US", "CA"], 
 ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]],
 
 
@@ -30,7 +31,8 @@
 [["BE", "IT", "AT", "IS", 
   "IE", "NO", "SE", "DK", 
   "FI", "AU", "NZ", "CH",
-  "FR", "DE", "NL", "GB"], 
+  "FR", "DE", "NL", "GB", 
+  "US", "CA"], 
 ["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&cs=tour1&ar=&buffer="]],
 
 
