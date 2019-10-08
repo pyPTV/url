ad2e063328e7d0a1a61f1da24b19d4445d8e8453
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-10-08 05:00:40 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -7,9 +7,8 @@
 
 [["BE", "IT", "AT", "IS", 
   "IE", "NO", "SE", "DK", 
-  "FI", "AU", "NZ", "CH",
-  "FR", "DE", "NL", "GB"], 
-["http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA"]],
+  "FI", "AU", "NZ", "CH"], 
+["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&pa=index&ar=&buffer="]],
 
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
@@ -29,9 +28,8 @@
 
 [["BE", "IT", "AT", "IS", 
   "IE", "NO", "SE", "DK", 
-  "FI", "AU", "NZ", "CH",
-  "FR", "DE", "NL", "GB"], 
-["http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA"]],
+  "FI", "AU", "NZ", "CH"], 
+["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273914&pa=index&ar=&buffer="]],
 
 
 
