9a86d50cdcd3cfa3dc6be7cbf913fbb19eff0f23
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-10-08 15:45:59 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -3,7 +3,8 @@
 [
 
 
-[["US"], ["http://join.allgaysitespass.com/track/MjAwMzc0LjEuMy40LjAuMC4wLjAuMA"]],
+[["US"], ["http://join.allgaysitespass.com/track/MjAwMzc0LjEuMy40LjAuMC4wLjAuMA",
+          "https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=274113&pa=index&ar=&buffer="]],
 
 [["BE", "IT", "AT", "IS", 
   "IE", "NO", "SE", "DK", 
@@ -15,7 +16,7 @@
 
 
 [["ZZ"], 
-["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273894&pa=index&ar=&buffer="]] 
+["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=274113&pa=index&ar=&buffer="]] 
 
                
 ],
@@ -24,7 +25,8 @@
 [
 
 
-[["US"], ["http://join.allgaysitespass.com/track/MjAwMzc0LjEuMy40LjAuMC4wLjAuMA"]],
+[["US"], ["http://join.allgaysitespass.com/track/MjAwMzc0LjEuMy40LjAuMC4wLjAuMA",
+          "https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=274113&pa=index&ar=&buffer="]],
 
 [["BE", "IT", "AT", "IS", 
   "IE", "NO", "SE", "DK", 
@@ -37,7 +39,7 @@
 
 
 [["ZZ"], 
-["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=273894&pa=index&ar=&buffer="]]             
+["https://www.buddylead.com/iconmale/go.php?pr=9&su=2&si=79&ad=274113&pa=index&ar=&buffer="]]             
 
 ]
 }
