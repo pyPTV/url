a14d2a94cb625786bd1f64cccede0a917d862d11
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-10-14 13:36:30 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -3,9 +3,7 @@
 [
 
 
-[["US"], ["http://join.allgaysitespass.com/track/MjAwMzc0LjEuMy40LjAuMC4wLjAuMA",
-          "https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer=",
-          "https://www.buddylead.com/activeduty/go.php?pr=9&su=2&si=80&ad=274113&pa=index&ar=&buffer="]],
+[["US"], ["http://join.allgaysitespass.com/track/MjAwMzc0LjEuMy40LjAuMC4wLjAuMA"]],
 
 [["BE", "IT", "AT", "IS", 
   "IE", "NO", "SE", "DK", 
@@ -27,9 +25,7 @@
 [
 
 
-[["US"], ["http://join.allgaysitespass.com/track/MjAwMzc0LjEuMy40LjAuMC4wLjAuMA",
-          "https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer=",
-          "https://www.buddylead.com/activeduty/go.php?pr=9&su=2&si=80&ad=274113&pa=index&ar=&buffer="]],
+[["US"], ["http://join.allgaysitespass.com/track/MjAwMzc0LjEuMy40LjAuMC4wLjAuMA"]],
 
 [["BE", "IT", "AT", "IS", 
   "IE", "NO", "SE", "DK", 
