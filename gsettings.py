e8079cf025a2108f7ae40903f7de64d9b750498f
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-10-27 09:51:08 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -5,6 +5,10 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
+[["US"], ["https://www.buddylead.com/activeduty/go.php?pr=9&su=2&si=80&ad=273894&pa=index&ar=&buffer=",
+          "https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273894&pa=index&ar=&buffer="]],
+
+
 [["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273894&pa=index&ar=&buffer="]] 
 
                
@@ -15,7 +19,8 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
-[["US"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273894&pa=index&ar=&buffer=", 
+[["US"], ["https://www.buddylead.com/activeduty/go.php?pr=9&su=2&si=80&ad=273894&pa=index&ar=&buffer=",
+          "https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273894&pa=index&ar=&buffer=", 
           "http://join.allgaysitespass.com/track/MjAxMDE4LjEuMy40LjAuMC4wLjAuMA"]],
 
 [["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273894&pa=index&ar=&buffer="]]             
