c88d6652fc8502a4739c27478d6c55a41cf71628
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-10-31 16:05:17 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -5,10 +5,12 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
-[["US"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=270918&pa=index&ar=&buffer="]],
+[["US"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=270918&pa=index&ar=&buffer=",
+          "https://www.iconmale.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjo3NCwicyI6NTgxLCJlIjo5MzE4LCJwIjoxMX0="]],
 
 
-[["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=270918&pa=index&ar=&buffer="]] 
+[["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=270918&pa=index&ar=&buffer=",
+          "https://www.iconmale.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjo3NCwicyI6NTgxLCJlIjo5MzE4LCJwIjoxMX0="]] 
 
                
 ],
@@ -18,9 +20,11 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
-[["US"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=270918&pa=index&ar=&buffer="]],
+[["US"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=270918&pa=index&ar=&buffer=",
+          "https://www.iconmale.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjo3NCwicyI6NTgxLCJlIjo5MzE4LCJwIjoxMX0="]],
 
-[["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=270918&pa=index&ar=&buffer="]]             
+[["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=270918&pa=index&ar=&buffer=",
+          "https://www.iconmale.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjo3NCwicyI6NTgxLCJlIjo5MzE4LCJwIjoxMX0="]]             
 
 ]
 }
