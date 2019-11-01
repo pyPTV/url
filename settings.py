009d62247c31baeafe09a8973f49674d48b220c8
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-11-02 00:42:09 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@
 
 
 [["JP", "TW", "CN", "ID", "PH", "IN"], ["https://enter.javhd.com/track/MjIwOTIuMjAuMi4yLjAuMC4wLjAuMA"]],
+[["RU", "UA", "BY", "KZ", "PL", "LT", "LV", "EE"], ["https://bongacams4.com/track?v=2&c=332288"]],
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]]
 
                
@@ -14,6 +15,7 @@
 
 
 [["JP", "TW", "CN", "ID", "PH", "IN"], ["https://enter.javhd.com/track/MjIwOTIuMjAuMi4yLjAuMC4wLjAuMA"]],
+[["RU", "UA", "BY", "KZ", "PL", "LT", "LV", "EE"], ["https://bongacams4.com/track?v=2&c=332288"]],
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=UO9ud&disable_sound=True"]]
 
 ]
