8d84dc7a4630de3e812cbd3012e17441ccf7c8f8
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-11-06 12:35:38 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -8,7 +8,9 @@
 
 
 
-[["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274280&pa=index&ar=&buffer="]] 
+[["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274280&pa=index&ar=&buffer=",
+          "https://www.buddylead.com/ragingstallion/go.php?pr=9&su=2&si=48&ad=274280&pa=index&ar=&buffer=",
+          "https://www.buddylead.com/nextdoorstudios/go.php?pr=9&su=2&si=46&ad=274280&pa=index&ar=&buffer="]] 
 
                
 ],
@@ -20,7 +22,9 @@
 
 
 
-[["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274280&pa=index&ar=&buffer="]]             
+[["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274280&pa=index&ar=&buffer=",
+          "https://www.buddylead.com/ragingstallion/go.php?pr=9&su=2&si=48&ad=274280&pa=index&ar=&buffer=",
+          "https://www.buddylead.com/nextdoorstudios/go.php?pr=9&su=2&si=46&ad=274280&pa=index&ar=&buffer="]]             
 
 ]
 }
