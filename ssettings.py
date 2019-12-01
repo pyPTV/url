97606b1836d192705ccddb2ef6b91f9771d28dae
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-12-01 15:34:20 +0200

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -1,21 +1,14 @@
 {
 "mobile": [
 
-
 [["RU"], ["https://bongacams4.com/track?c=663486"]],
-
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=khMd&campaign=8juEy&disable_sound=True"]]
 
-
 ],
-
-
 
 "desktop": [
 
 [["RU"], ["https://bongacams4.com/track?c=663486"]],
-
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=khMd&campaign=8juEy&disable_sound=True"]]
-
 
 ]}
