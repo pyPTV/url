a0fa44911e8eb40410e08199dcde76467e4fff10
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-12-20 21:04:03 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -9,8 +9,7 @@
 
 
 [["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273913&pa=index&ar=&buffer=",
-          "https://www.buddylead.com/jocksstudios/go.php?pr=9&su=2&si=47&ad=273913&pa=index&ar=&buffer=",
-          "https://www.buddylead.com/nextdoortwink/go.php?pr=9&su=2&si=35&ad=273913&pa=index&ar=&buffer="]] 
+          "https://www.buddylead.com/jocksstudios/go.php?pr=9&su=2&si=47&ad=273913&pa=index&ar=&buffer="]] 
 
                
 ],
@@ -22,8 +21,7 @@
 
 
 [["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273913&pa=index&ar=&buffer=",
-          "https://www.buddylead.com/jocksstudios/go.php?pr=9&su=2&si=47&ad=273913&pa=index&ar=&buffer=",
-          "https://www.buddylead.com/nextdoortwink/go.php?pr=9&su=2&si=35&ad=273913&pa=index&ar=&buffer="]]         
+          "https://www.buddylead.com/jocksstudios/go.php?pr=9&su=2&si=47&ad=273913&pa=index&ar=&buffer="]]         
 
 ]
 }
