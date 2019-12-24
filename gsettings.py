cdfde356a032c33c40419d3b5ea6c69f041a8021
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-12-24 16:09:26 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -9,6 +9,7 @@
 
 
 [["ZZ"], ["http://join.allgaysitespass.com/track/MjAxMDE4LjEuMy40LjAuMC4wLjAuMA",
+          "https://www.buddylead.com/jocksstudios/go.php?pr=9&su=2&si=47&ad=273913&pa=index&ar=&buffer=",
           "https://wct.link/click?c=eyJhIjoxOTQ4MSwibyI6NTgsInAiOjJ9"]] 
 
                
@@ -21,6 +22,7 @@
 
 
 [["ZZ"], ["http://join.allgaysitespass.com/track/MjAxMDE4LjEuMy40LjAuMC4wLjAuMA",
+          "https://www.buddylead.com/jocksstudios/go.php?pr=9&su=2&si=47&ad=273913&pa=index&ar=&buffer=",
           "https://wct.link/click?c=eyJhIjoxOTQ4MSwibyI6NTgsInAiOjJ9"]]        
 
 ]
