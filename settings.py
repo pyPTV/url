06905295ca98fb22eadefcea10fcecd116d8a899
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2019-12-27 22:53:13 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,6 +5,7 @@
 
 
 [["RU"], ["https://bongacams4.com/track?v=2&c=332288"]],
+[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
 [["ZZ"], ["http://join.allpornsitespass.com/track/MjAxMDE4LjEuNC41LjAuMC4wLjAuMA"]]
 
                
@@ -16,6 +17,7 @@
 
 
 [["RU"], ["https://bongacams4.com/track?v=2&c=332288"]],
+[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
 [["ZZ"], ["http://join.allpornsitespass.com/track/MjAxMDE4LjEuNC41LjAuMC4wLjAuMA"]]
 
 ]
