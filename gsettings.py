fc9d4993dbb15ce94cbea7a28b50225e4216e64a
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-02-15 13:23:34 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -8,10 +8,10 @@
 
 
 [["US"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer=",
-          "http://join.allgaysitespass.com/track/MjAwNjg0LjEuMy40LjAuMC4wLjAuMA"]],
+          "http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA"]],
     
 [["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer=",
-          "http://join.allgaysitespass.com/track/MjAwNjg0LjEuMy40LjAuMC4wLjAuMA"]] 
+          "http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA"]] 
 
                
 ],
@@ -22,10 +22,10 @@
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
 [["US"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer=",
-          "http://join.allgaysitespass.com/track/MjAwNjg0LjEuMy40LjAuMC4wLjAuMA"]],
+          "http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA"]],
     
 [["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer=",
-          "http://join.allgaysitespass.com/track/MjAwNjg0LjEuMy40LjAuMC4wLjAuMA"]]      
+          "http://join.allgaysitespass.com/track/MjAwMjYyLjEuMy40LjAuMC4wLjAuMA"]]      
 
 ]
 }
