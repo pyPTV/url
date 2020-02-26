1822f1334af4fda9b77b6904d425403e53066970
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-02-26 09:05:16 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@
 
 [["RU", "UA"], ["https://bongacams5.com/track?v=2&c=332288"]],
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
-[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=8juEy&disable_sound=True"]]
+[["ZZ"], ["http://join.allpornsitespass.com/track/MjAwMjYyLjEuNC41LjAuMC4wLjAuMA"]]
 
                
 ],
@@ -18,7 +18,7 @@
 
 [["RU", "UA"], ["https://bongacams5.com/track?v=2&c=332288"]],
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://sex.com"]],
-[["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=8juEy&disable_sound=True"]]
+[["ZZ"], ["http://join.allpornsitespass.com/track/MjAwMjYyLjEuNC41LjAuMC4wLjAuMA"]]
 
 ]
 }
