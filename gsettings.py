2da96cd69820a13ad6d05fc3a6aa1ca6b17f1919
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-03-25 09:51:20 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,9 +4,9 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
-[["US"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer="]],
+[["US", "CA", "AU", "NZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer="]],
 
-[["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNjg0LjEuMy40LjAuMC4wLjAuMA"]] 
+[["ZZ"], ["http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA"]] 
 
                
 ],
@@ -16,9 +16,9 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
-[["US"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer="]],
+[["US", "CA", "AU", "NZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer="]],
 
-[["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNjg0LjEuMy40LjAuMC4wLjAuMA"]]      
+[["ZZ"], ["http://www2.euroboyxxx.com/track/NjAwMDAwMDIzNi44LjM2LjIwMy4wLjAuMC4wLjA"]]      
 
 ]
 }
