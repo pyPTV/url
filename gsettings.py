56c26f66159ddd5b7ae95a79dd475dff5119c439
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-03-25 09:44:09 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -6,7 +6,7 @@
 
 [["US"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer="]],
 
-[["ZZ"], ["https://wct.link/click?c=eyJhIjoxOTk1MSwibyI6MjA5LCJwIjoyfSAg"]] 
+[["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNjg0LjEuMy40LjAuMC4wLjAuMA"]] 
 
                
 ],
@@ -18,7 +18,7 @@
 
 [["US"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer="]],
 
-[["ZZ"], ["https://wct.link/click?c=eyJhIjoxOTk1MSwibyI6MjA5LCJwIjoyfSAg"]]      
+[["ZZ"], ["http://join.allgaysitespass.com/track/MjAwNjg0LjEuMy40LjAuMC4wLjAuMA"]]      
 
 ]
 }
