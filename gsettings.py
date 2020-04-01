f6379165758851dfc0acc39f11a628b01f28cca5
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-04-01 18:39:16 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,9 +4,9 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
-[["US", "CA", "AU", "NZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer="]],
+[["US", "CA", "AU", "NZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273913&pa=index&ar=&buffer="]],
 
-[["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer="]] 
+[["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273913&pa=index&ar=&buffer="]] 
 
                
 ],
@@ -16,9 +16,9 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
-[["US", "CA", "AU", "NZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer="]],
+[["US", "CA", "AU", "NZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273913&pa=index&ar=&buffer="]],
 
-[["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274113&pa=index&ar=&buffer="]]      
+[["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273913&pa=index&ar=&buffer="]]      
 
 ]
 }
