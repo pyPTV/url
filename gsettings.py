9d9435d3ba015fad20ad04df31944b2dc26100e2
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-04-07 23:04:14 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,7 +4,8 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
-[["US", "CA", "AU", "NZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274280&pa=index&ar=&buffer="]],
+[["US", "CA", "AU", "NZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274280&pa=index&ar=&buffer=",
+                            "https://landing.iconmale.com?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]],
 
 [["ZZ"], ["http://www2.euroboyxxx.com/track/NjAwMDAwMDI4My44LjM2LjIwMy4wLjAuMC4wLjA"]] 
 
@@ -16,7 +17,8 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
-[["US", "CA", "AU", "NZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274280&pa=index&ar=&buffer="]],
+[["US", "CA", "AU", "NZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274280&pa=index&ar=&buffer=",
+                            "https://landing.iconmale.com?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]],
 
 [["ZZ"], ["http://www2.euroboyxxx.com/track/NjAwMDAwMDI4My44LjM2LjIwMy4wLjAuMC4wLjA"]]      
 
