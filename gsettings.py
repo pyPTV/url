32e5e9015131ca9cc3536175438b77454fb22a06
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-04-13 11:01:06 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -5,7 +5,7 @@
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
 [["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273894&pa=index&ar=&buffer=",
-          "https://landing.iconmale.com?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
+          "https://landing.iconmale.com?ats=eyJhIjoyODk0OTYsImMiOjU2NDU3MTQ1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
                
 ],
  
@@ -15,7 +15,7 @@
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://gay.com"]],
 
 [["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273894&pa=index&ar=&buffer=",
-          "https://landing.iconmale.com?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
+          "https://landing.iconmale.com?ats=eyJhIjoyODk0OTYsImMiOjU2NDU3MTQ1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
 
 ]
 }
