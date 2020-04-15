1105d93871079e814a4ec7613c8a9b7a97d4ba2c
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-04-15 08:50:44 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@
 
 
 [["RU", "UA", "BY", "KZ"], ["https://bongacams5.com/track?v=2&c=332288"]],
-[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.grtyh.com/biduh6tcn4?url_id=0&aff_id=82882&offer_id=6981"]],
+[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.frtyz.com/53gzt6cqm8?url_id=0&aff_id=82882&offer_id=6132"]],
 [["ZZ"], ["https://t.bdwafx.com/rhp3wj6fcw?url_id=0&aff_id=82882&offer_id=869"]]
 
                
@@ -17,7 +17,7 @@
 
 
 [["RU", "UA", "BY", "KZ"], ["https://bongacams5.com/track?v=2&c=332288"]],
-[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.grtyh.com/biduh6tcn4?url_id=0&aff_id=82882&offer_id=6981"]],
+[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.frtyz.com/53gzt6cqm8?url_id=0&aff_id=82882&offer_id=6132"]],
 [["ZZ"], ["https://t.bdwafx.com/rhp3wj6fcw?url_id=0&aff_id=82882&offer_id=869"]]
 
 ]
