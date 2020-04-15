a240e0c736bfcbff8bd2371220d0b0c44fbca459
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-04-15 08:55:20 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -2,7 +2,7 @@
     "mobile":
 [
 
-[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.frtyz.com/4vvxtjt2o0?url_id=18543&aff_id=82882&offer_id=6132"]],
+[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
 
 [["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273894&pa=index&ar=&buffer=",
           "https://landing.iconmale.com?ats=eyJhIjoyODk0OTYsImMiOjU2NDU3MTQ1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
@@ -12,7 +12,7 @@
     "desktop":
 [
 
-[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.frtyz.com/4vvxtjt2o0?url_id=18543&aff_id=82882&offer_id=6132"]],
+[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
 
 [["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=273894&pa=index&ar=&buffer=",
           "https://landing.iconmale.com?ats=eyJhIjoyODk0OTYsImMiOjU2NDU3MTQ1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
