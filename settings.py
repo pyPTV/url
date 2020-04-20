74fd12bb70d467895caca77f8c1055e44584a7cb
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-04-20 11:18:50 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
     "mobile":
 [
 
-[["US", "CA", "AU", "NZ", "GB", "DE", "FR", "IT"], ["https://t.bdwafx.com/d9t69tjark?url_id=1827&aff_id=55931&offer_id=1639"]],
+[["US", "CA", "AU", "NZ", "GB", "DE", "FR", "IT"], ["http://join.allpornsitespass.com/track/MjAxMDE4LjEuNC41LjAuMC4wLjAuMA"]],
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.frtyz.com/4vvxtjt2o0?url_id=18543&aff_id=82882&offer_id=6132"]],
 [["ZZ"], ["https://bongacams5.com/track?c=332289"]]
 
@@ -13,7 +13,7 @@
 [
 
 
-[["US", "CA", "AU", "NZ", "GB", "DE", "FR", "IT"], ["https://t.bdwafx.com/d9t69tjark?url_id=1827&aff_id=55931&offer_id=1639"]],
+[["US", "CA", "AU", "NZ", "GB", "DE", "FR", "IT"], ["http://join.allpornsitespass.com/track/MjAxMDE4LjEuNC41LjAuMC4wLjAuMA"]],
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.frtyz.com/4vvxtjt2o0?url_id=18543&aff_id=82882&offer_id=6132"]],
 [["ZZ"], ["https://bongacams5.com/track?c=332289"]]
 
