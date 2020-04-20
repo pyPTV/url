3e605a12701111e9d3a851fd818b4d170b4a2b40
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-04-20 09:07:00 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,11 +2,9 @@
     "mobile":
 [
 
-
-
-[["RU", "UA", "BY", "KZ", "PL", "CZ"], ["https://bongacams5.com/track?v=2&c=332288"]],
+[["US", "CA", "AU", "NZ", "GB", "DE", "FR", "IT"], ["https://t.bdwafx.com/d9t69tjark?url_id=1827&aff_id=55931&offer_id=1639"]],
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.frtyz.com/4vvxtjt2o0?url_id=18543&aff_id=82882&offer_id=6132"]],
-[["ZZ"], ["https://t.bdwafx.com/d9t69tjark?url_id=1827&aff_id=55931&offer_id=1639"]]
+[["ZZ"], ["https://bongacams5.com/track?c=332288"]]
 
                
 ],
@@ -15,11 +13,12 @@
 [
 
 
-
-[["RU", "UA", "BY", "KZ", "PL", "CZ"], ["https://bongacams5.com/track?v=2&c=332288"]],
+[["US", "CA", "AU", "NZ", "GB", "DE", "FR", "IT"], ["https://t.bdwafx.com/d9t69tjark?url_id=1827&aff_id=55931&offer_id=1639"]],
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.frtyz.com/4vvxtjt2o0?url_id=18543&aff_id=82882&offer_id=6132"]],
-[["ZZ"], ["https://t.bdwafx.com/d9t69tjark?url_id=1827&aff_id=55931&offer_id=1639"]]
+[["ZZ"], ["https://bongacams5.com/track?c=332288"]]
 
+
+    
 ]
 }
 
