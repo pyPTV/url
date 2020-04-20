ee02b72056717cc1ca6fc15b28d686c4532016bc
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-04-20 09:10:44 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -1,7 +1,7 @@
 {
 "mobile": [
 
-[["RU"], ["https://bongacams4.com/track?c=663486"]],
+[["IN"], ["https://trans.com"]],
 [["ZZ"], ["https://landing.transangelsnetwork.com?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NTQxLCJlIjo5MTEzLCJwIjoxMX0=",
           "https://landing.transsensual.com?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjo3MiwicyI6NTc2LCJlIjo5MjU4LCJwIjoxMX0="]]
 
@@ -9,7 +9,7 @@
 
 "desktop": [
 
-[["RU"], ["https://bongacams4.com/track?c=663486"]],
+[["IN"], ["https://trans.com"]],
 [["ZZ"], ["https://landing.transangelsnetwork.com?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjozNSwicyI6NTQxLCJlIjo5MTEzLCJwIjoxMX0=",
           "https://landing.transsensual.com?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjo3MiwicyI6NTc2LCJlIjo5MjU4LCJwIjoxMX0="]]
 
