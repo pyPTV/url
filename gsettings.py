cc46c0a9997ed310052b40d31e61bbad722be1ee
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-04-28 12:51:11 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,7 +4,8 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
 
-[["ZZ"], ["https://landing.iconmale.com?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
+[["ZZ"], ["http://www2.tastytwink.com/track/NjAwMDAwMDI4My44LjQuNC4wLjAuMC4wLjA",
+          "http://www2.holotwink.com/track/NjAwMDAwMDI4My44LjI2LjE5OS4wLjAuMC4wLjA"]]
                
 ],
  
@@ -13,9 +14,7 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
 
-[["ZZ"], ["https://landing.iconmale.com?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0=",
-          "https://landing.iconmale.com?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0=",         
-          "http://www2.tastytwink.com/track/NjAwMDAwMDI4My44LjQuNC4wLjAuMC4wLjA",
+[["ZZ"], ["http://www2.tastytwink.com/track/NjAwMDAwMDI4My44LjQuNC4wLjAuMC4wLjA",
           "http://www2.holotwink.com/track/NjAwMDAwMDI4My44LjI2LjE5OS4wLjAuMC4wLjA"]]
 
 ]
