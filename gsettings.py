856f10572e0dc7aee59ffcf17b3c8e7b891f6b7f
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-05-03 23:52:51 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,9 +4,7 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
 
-[["ZZ"], ["https://landing.iconmale.com?ats=eyJhIjoyOTc5MjAsImMiOjU3Nzg4NDU1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0=",
-          "https://landing.iconmale.com?ats=eyJhIjoyOTc5MjAsImMiOjU3Nzg4NDU1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0=",
-          "http://www2.holotwink.com/track/NjAwMDAwMDI4My44LjI2LjE5OS4wLjAuMC4wLjA"]]
+[["ZZ"], ["https://landing.iconmale.com?ats=eyJhIjoyOTc5MjAsImMiOjU3Nzg4NDU1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
                
 ],
  
@@ -15,9 +13,7 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
 
-[["ZZ"], ["https://landing.iconmale.com?ats=eyJhIjoyOTc5MjAsImMiOjU3Nzg4NDU1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0=",
-          "https://landing.iconmale.com?ats=eyJhIjoyOTc5MjAsImMiOjU3Nzg4NDU1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0=",
-          "http://www2.holotwink.com/track/NjAwMDAwMDI4My44LjI2LjE5OS4wLjAuMC4wLjA"]]
+[["ZZ"], ["https://landing.iconmale.com?ats=eyJhIjoyOTc5MjAsImMiOjU3Nzg4NDU1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
 
 ]
 }
