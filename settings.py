3ed28104c977f069c91caf5f1a6aeacbd1d1073f
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-05-06 05:43:43 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
     "mobile":
 [
 
-[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.frtyz.com/4vvxtjt2o0?url_id=18543&aff_id=82882&offer_id=6132"]],
+[["IY"], ["https://t.frtyz.com/4vvxtjt2o0?url_id=18543&aff_id=82882&offer_id=6132"]],
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=1zwfK&disable_sound=True"]]
 
                
@@ -12,7 +12,7 @@
 [
 
 
-[["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.frtyz.com/4vvxtjt2o0?url_id=18543&aff_id=82882&offer_id=6132"]],
+[["IY"], ["https://t.frtyz.com/4vvxtjt2o0?url_id=18543&aff_id=82882&offer_id=6132"]],
 [["ZZ"], ["https://chaturbate.com/in/?track=default&tour=hr8m&campaign=1zwfK&disable_sound=True"]]
 
 
