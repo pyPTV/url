28307332cabd0d229a55bbca314d70632177debe
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-05-15 20:34:44 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,7 +4,7 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
 
-[["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274280&pa=index&ar=&buffer="]]
+[["ZZ"], ["https://landing.iconmale.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
                
 ],
  
@@ -13,7 +13,7 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
 
-[["ZZ"], ["https://www.buddylead.com/falconstudios/go.php?pr=9&su=2&si=33&ad=274280&pa=index&ar=&buffer="]]
+[["ZZ"], ["https://landing.iconmale.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
 
 ]
 }
