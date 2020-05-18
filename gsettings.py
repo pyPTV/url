3b943a51ac2a1d9a6a5e75fad8423bac74ae8b8e
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-05-18 12:26:23 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,7 +4,7 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
 
-[["ZZ"], ["https://landing.iconmale.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.iconmale.com/?ats=eyJhIjoyODk0OTYsImMiOjU2NDU3MTQ1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
                
 ],
  
@@ -13,7 +13,7 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN"], ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
 
-[["ZZ"], ["https://landing.iconmale.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.iconmale.com/?ats=eyJhIjoyODk0OTYsImMiOjU2NDU3MTQ1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]]
 
 ]
 }
