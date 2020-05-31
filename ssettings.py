26c33ebb1122366a72c9db5a53c1012fc27728a5
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-05-31 19:50:06 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,15 +2,15 @@
 "mobile": [
 
 [["RU"], ["https://trans.com"]],
-[["ZZ"], ["https://landing.transsensual.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3MiwicyI6NTc2LCJlIjo5MjU4LCJwIjoxMX0=",
-          "https://landing.transharder.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.transsensual.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo3MiwicyI6NTc2LCJlIjo5MjU4LCJwIjoxMX0=",
+          "https://landing.transharder.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
 
 ],
 
 "desktop": [
 
 [["RU"], ["https://trans.com"]],
-[["ZZ"], ["https://landing.transsensual.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3MiwicyI6NTc2LCJlIjo5MjU4LCJwIjoxMX0=",
-          "https://landing.transharder.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.transsensual.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo3MiwicyI6NTc2LCJlIjo5MjU4LCJwIjoxMX0=",
+          "https://landing.transharder.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
 
 ]}
