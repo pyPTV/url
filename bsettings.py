ddccda016d2c05659d12f742e45fe7a691abe35c
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-06-07 08:54:58 +0300

    Create bsettings.py

---
+++ bsettings.py
@@ -0,0 +1,19 @@
+{
+    "mobile":
+[
+
+[["IN", "PH", "CN", "ID", "TH", "VN", "RU"], ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
+
+[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyOTc5MjAsImMiOjU3Nzg4NDU1LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
+               
+],
+ 
+    "desktop":
+[
+
+[["IN", "PH", "CN", "ID", "TH", "VN", "RU"], ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
+
+[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyOTc5MjAsImMiOjU3Nzg4NDU1LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
+
+]
+}
