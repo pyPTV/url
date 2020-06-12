4e5e47db9ce00609b3fc17557966de9dcf5a5ea9
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-06-12 07:26:36 +0300

    Update bsettings.py

--- bsettings.py
+++ bsettings.py
@@ -4,7 +4,7 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN", "RU"], ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
 
-[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyOTc5MjAsImMiOjU3Nzg4NDU1LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
                
 ],
  
@@ -13,7 +13,7 @@
 
 [["IN", "PH", "CN", "ID", "TH", "VN", "RU"], ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
 
-[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyOTc5MjAsImMiOjU3Nzg4NDU1LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
 
 ]
 }
