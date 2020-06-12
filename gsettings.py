b0259f191a8396a99b08f9cc4784e9c41342781a
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-06-12 07:24:45 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -6,13 +6,13 @@
  ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
     
 [["CZ", "SK", "SI", "PL"], 
- ["https://landing.czechhunter.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjo2MCwicyI6NTI4LCJlIjo5MDk3LCJwIjoxMX0="]],
+ ["https://landing.czechhunter.com/?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjo2MCwicyI6NTI4LCJlIjo5MDk3LCJwIjoxMX0="]],
     
 
 [["ZZ"], 
- ["https://landing.iconmale.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0=",
-  "https://landing.maleaccess.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjo3OCwicyI6NjAyLCJlIjo5NDc3LCJwIjoxMX0=",
-  "https://landing.mennetwork.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]]
+ ["https://landing.iconmale.com/?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0=",
+  "https://landing.maleaccess.com/?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjo3OCwicyI6NjAyLCJlIjo5NDc3LCJwIjoxMX0=",
+  "https://landing.mennetwork.com/?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]]
                
 ],
  
@@ -23,13 +23,13 @@
  ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
     
 [["CZ", "SK", "SI", "PL"], 
- ["https://landing.czechhunter.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjo2MCwicyI6NTI4LCJlIjo5MDk3LCJwIjoxMX0="]],
+ ["https://landing.czechhunter.com/?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjo2MCwicyI6NTI4LCJlIjo5MDk3LCJwIjoxMX0="]],
     
 
 [["ZZ"], 
- ["https://landing.iconmale.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0=",
-  "https://landing.maleaccess.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjo3OCwicyI6NjAyLCJlIjo5NDc3LCJwIjoxMX0=",
-  "https://landing.mennetwork.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]]
+ ["https://landing.iconmale.com/?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0=",
+  "https://landing.maleaccess.com/?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjo3OCwicyI6NjAyLCJlIjo5NDc3LCJwIjoxMX0=",
+  "https://landing.mennetwork.com/?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]]
 
 ]
 }
