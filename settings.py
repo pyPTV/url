880cdb307ac91673c66d4ba8085bc603c30d4a3d
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-06-14 10:14:44 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,9 @@
 [
 
 [["RU"], ["https://bongacams7.com/track?c=332288"]],
-[["ZZ"], ["http://join.allpornsitespass.com/track/MjAwMjYyLjEuNC41LjAuMC4wLjAuMA"]]
+[["ZZ"], ["http://join.allpornsitespass.com/track/MjAwMjYyLjEuNC41LjAuMC4wLjAuMA",
+          "https://t.bdwafx.com/m2mlns29j4?url_id=0&aff_id=82882&offer_id=1639",
+          "https://t.bewafx.com/xtd6ft2c00?url_id=0&aff_id=82882&offer_id=235"]]
 
                
 ],
@@ -13,7 +15,9 @@
 
 
 [["RU"], ["https://bongacams7.com/track?c=332288"]],
-[["ZZ"], ["http://join.allpornsitespass.com/track/MjAwMjYyLjEuNC41LjAuMC4wLjAuMA"]]
+[["ZZ"], ["http://join.allpornsitespass.com/track/MjAwMjYyLjEuNC41LjAuMC4wLjAuMA",
+          "https://t.bdwafx.com/m2mlns29j4?url_id=0&aff_id=82882&offer_id=1639",
+          "https://t.bewafx.com/xtd6ft2c00?url_id=0&aff_id=82882&offer_id=235"]]
 
 
     
