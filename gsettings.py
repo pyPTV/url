ba18d774c2dc44eac9762a6687c0eedae6592ad9
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-06-28 10:10:59 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -6,13 +6,12 @@
  ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
     
 [["CZ", "SK"], 
- ["https://landing.czechhunter.com/?ats=eyJhIjoyODk0OTYsImMiOjU2NDU3MTQ1LCJuIjo2MCwicyI6NTI4LCJlIjo5MDk3LCJwIjoxMX0="]],
+ ["https://landing.czechhunter.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo2MCwicyI6NTI4LCJlIjo5MDk3LCJwIjoxMX0="]],
     
 
 [["ZZ"], 
- ["https://landing.iconmale.com/?ats=eyJhIjoyODk0OTYsImMiOjU2NDU3MTQ1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0=",
-  
-  "https://landing.mennetwork.com/?ats=eyJhIjoyODk0OTYsImMiOjU2NDU3MTQ1LCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]]
+ ["https://landing.iconmale.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0=",  
+  "https://landing.mennetwork.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]]
                
 ],
  
@@ -23,13 +22,12 @@
  ["https://t.bdwafx.com/6xz5cp05kw?url_id=5804&aff_id=82882&offer_id=2996"]],
     
 [["CZ", "SK"], 
- ["https://landing.czechhunter.com/?ats=eyJhIjoyODk0OTYsImMiOjU2NDU3MTQ1LCJuIjo2MCwicyI6NTI4LCJlIjo5MDk3LCJwIjoxMX0="]],
+ ["https://landing.czechhunter.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo2MCwicyI6NTI4LCJlIjo5MDk3LCJwIjoxMX0="]],
     
 
 [["ZZ"], 
- ["https://landing.iconmale.com/?ats=eyJhIjoyODk0OTYsImMiOjU2NDU3MTQ1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0=",
-  
-  "https://landing.mennetwork.com/?ats=eyJhIjoyODk0OTYsImMiOjU2NDU3MTQ1LCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]]
+ ["https://landing.iconmale.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0=",  
+  "https://landing.mennetwork.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]]
 
 ]
 }
