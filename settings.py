e4da8d649b144c7681625b41ec4d0eca4783ccb4
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-07-24 06:27:58 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@
 [["RU", "UA", "PL"], ["https://bongacams7.com/track?c=332288"]],
     
     
-[["ZZ"], ["http://join.allpornsitespass.com/track/MjAxMDE4LjEuNC41LjAuMC4wLjAuMA",
+[["ZZ"], [
           "https://landing.brazzersnetwork.com/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ==",
           "https://landing.fakehub.com/tgp1/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjozMCwicyI6MjYxLCJlIjoxMTcwLCJwIjoxMX0=",
           "https://landing.milfed.com/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjo5MiwicyI6NjMxLCJlIjo5ODE2LCJwIjoxMX0=",
@@ -22,7 +22,7 @@
 [["RU", "UA", "PL"], ["https://bongacams7.com/track?c=332288"]],
     
     
-[["ZZ"], ["http://join.allpornsitespass.com/track/MjAxMDE4LjEuNC41LjAuMC4wLjAuMA",
+[["ZZ"], [
           "https://landing.brazzersnetwork.com/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ==",
           "https://landing.fakehub.com/tgp1/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjozMCwicyI6MjYxLCJlIjoxMTcwLCJwIjoxMX0=",
           "https://landing.milfed.com/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjo5MiwicyI6NjMxLCJlIjo5ODE2LCJwIjoxMX0=",
