45f64634748a1df093fdd5ab722ba2477ad94140
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-07-24 23:34:40 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,8 +7,7 @@
     
 [["ZZ"], [
           "https://landing.brazzersnetwork.com/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ==",
-          "https://landing.fakehub.com/tgp1/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjozMCwicyI6MjYxLCJlIjoxMTcwLCJwIjoxMX0=",
-          "https://landing.milfed.com/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjo5MiwicyI6NjMxLCJlIjo5ODE2LCJwIjoxMX0=",
+          "https://landing.fakehub.com/tgp1/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjozMCwicyI6MjYxLCJlIjoxMTcwLCJwIjoxMX0=",          
           "https://landing.sexyhub.com/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjozMSwicyI6MjczLCJlIjo5MTkwLCJwIjoxMX0="          
          ]]
 
@@ -24,8 +23,7 @@
     
 [["ZZ"], [
           "https://landing.brazzersnetwork.com/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ==",
-          "https://landing.fakehub.com/tgp1/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjozMCwicyI6MjYxLCJlIjoxMTcwLCJwIjoxMX0=",
-          "https://landing.milfed.com/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjo5MiwicyI6NjMxLCJlIjo5ODE2LCJwIjoxMX0=",
+          "https://landing.fakehub.com/tgp1/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjozMCwicyI6MjYxLCJlIjoxMTcwLCJwIjoxMX0=",          
           "https://landing.sexyhub.com/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjozMSwicyI6MjczLCJlIjo5MTkwLCJwIjoxMX0="          
          ]]
 
