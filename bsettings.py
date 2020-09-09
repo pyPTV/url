61e6a5ebf498d83fd8708eb34cbd7a963fc4877d
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-09-09 17:44:18 +0300

    Update bsettings.py

--- bsettings.py
+++ bsettings.py
@@ -2,18 +2,18 @@
     "mobile":
 [
 
-[["IY"], ["https://landing.biempire.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]],
+[["IY"], ["https://landing.biempire.com/?ats=eyJhIjozMDc4OTcsImMiOjU5MDIzMTM1LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]],
 
-[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjozMDc4OTcsImMiOjU5MDIzMTM1LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
                
 ],
  
     "desktop":
 [
 
-[["IY"], ["https://landing.biempire.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]],
+[["IY"], ["https://landing.biempire.com/?ats=eyJhIjozMDc4OTcsImMiOjU5MDIzMTM1LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]],
 
-[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjozMDc4OTcsImMiOjU5MDIzMTM1LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
 
 ]
 }
