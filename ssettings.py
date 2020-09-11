eb276bfbb67fa71a1a9891ca6506bc3802fd0cc5
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-09-11 03:54:16 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -2,13 +2,13 @@
 "mobile": [
 
 [["RU"], ["https://trans.com"]],
-[["ZZ"], ["https://landing.transsensual.com/?ats=eyJhIjozMDc4OTcsImMiOjU5MDIzMTM1LCJuIjo3MiwicyI6NTc2LCJlIjo5MjU4LCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.transsensual.com/?ats=eyJhIjozMDYzMTIsImMiOjU4OTMwNjY5LCJuIjo3MiwicyI6NTc2LCJlIjo5MjU4LCJwIjoxMX0="]]
 
 ],
 
 "desktop": [
 
 [["RU"], ["https://trans.com"]],
-[["ZZ"], ["https://landing.transsensual.com/?ats=eyJhIjozMDc4OTcsImMiOjU5MDIzMTM1LCJuIjo3MiwicyI6NTc2LCJlIjo5MjU4LCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.transsensual.com/?ats=eyJhIjozMDYzMTIsImMiOjU4OTMwNjY5LCJuIjo3MiwicyI6NTc2LCJlIjo5MjU4LCJwIjoxMX0="]]
 
 ]}
