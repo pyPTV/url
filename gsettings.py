46cb48e8fb0788769cc40188759e3d82f3cb1853
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-09-21 07:15:51 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,7 +4,8 @@
 
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.mennetwork.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]],  
+ ["https://landing.mennetwork.com/?ats=eyJhIjozMDcxMDQsImMiOjU4OTc3MDkzLCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0=",
+  "https://landing.iconmale.com/?ats=eyJhIjozMDcxMDQsImMiOjU4OTc3MDkzLCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]],  
   
 
 [["ZZ"], 
@@ -18,7 +19,8 @@
 
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.mennetwork.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]],  
+ ["https://landing.mennetwork.com/?ats=eyJhIjozMDcxMDQsImMiOjU4OTc3MDkzLCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0=",
+  "https://landing.iconmale.com/?ats=eyJhIjozMDcxMDQsImMiOjU4OTc3MDkzLCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="]],  
   
 
 [["ZZ"], 
