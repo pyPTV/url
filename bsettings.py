efd1ec6f61f80179bb35f5656d69cd6ce8bdb2cc
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-09-22 16:46:27 +0300

    Update bsettings.py

--- bsettings.py
+++ bsettings.py
@@ -2,7 +2,7 @@
     "mobile":
 [
 
-[["RU"], ["https://bongacams7.com/track?c=332288"]],
+[["RU"], ["https://bongacams7.com/track?v=2&c=332289"]],
 
 [["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
                
@@ -11,7 +11,7 @@
     "desktop":
 [
 
-[["RU"], ["https://bongacams7.com/track?c=332288"]],
+[["RU"], ["https://bongacams7.com/track?v=2&c=332289"]],
 
 [["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
 
