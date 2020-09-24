dc57c32ee181de48b9f08f7baa5bf7752839eac0
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-09-24 05:27:56 +0300

    Update bsettings.py

--- bsettings.py
+++ bsettings.py
@@ -2,18 +2,22 @@
     "mobile":
 [
 
-[["RU"], ["https://bongacams7.com/track?v=2&c=332289"]],
-
-[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
+[["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
+  "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
+ ["https://landing.biempire.com/?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]],
+        
+[["ZZ"], ["https://bongacams7.com/track?v=2&c=332289"]]
                
 ],
  
     "desktop":
 [
 
-[["RU"], ["https://bongacams7.com/track?v=2&c=332289"]],
-
-[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
+[["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
+  "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
+ ["https://landing.biempire.com/?ats=eyJhIjoyODk4NTYsImMiOjU2NTQyNDk0LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]],
+        
+[["ZZ"], ["https://bongacams7.com/track?v=2&c=332289"]]
 
 ]
 }
