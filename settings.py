abb1da4ec1e41740f78835b8f5b1ee5a42a8a214
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-11-06 09:58:42 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,8 +4,7 @@
 
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.brazzersnetwork.com/?ats=eyJhIjozMDkxNTgsImMiOjU5MTAxMTgwLCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ==",
-  "http://join.allpornsitespass.com/track/MjAwMzc0LjEuNC41LjAuMC4wLjAuMA"]],     
+ ["http://join.allpornsitespass.com/track/MjAwMzc0LjEuNC41LjAuMC4wLjAuMA"]],     
 
     
 [["ZZ"], ["https://bongacams7.com/track?v=2&c=332289"]]
@@ -18,8 +17,7 @@
 
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.brazzersnetwork.com/?ats=eyJhIjozMDkxNTgsImMiOjU5MTAxMTgwLCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ==",
-  "http://join.allpornsitespass.com/track/MjAwMzc0LjEuNC41LjAuMC4wLjAuMA"]],     
+ ["http://join.allpornsitespass.com/track/MjAwMzc0LjEuNC41LjAuMC4wLjAuMA"]],     
 
     
 [["ZZ"], ["https://bongacams7.com/track?v=2&c=332289"]]
