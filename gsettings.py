863ff9bc7ad44a0b7d10254b831fdfb89b1f7cc2
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-11-22 13:56:38 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -6,7 +6,11 @@
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
  ["https://landing.mennetwork.com/?ats=eyJhIjozMDc4OTcsImMiOjU5MDIzMTM1LCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]],  
   
-
+[["BR", "AR", "CL", "MX", "PE", "BO", "VE", "GT", "DO", "CO",
+  "CR", "CU", "PY", "UY", "EC", "ES", "PT"],
+ ["https://wct.link/click?c=eyJhIjoxOTk1MSwibyI6NjYsInAiOjIsImMiOnt9fSAg"]],
+  
+  
 [["ZZ"], 
  ["https://wct.link/click?c=eyJhIjoxOTQ4MSwibyI6NTgsInAiOjJ9",
   "https://wct.link/click?c=eyJhIjoxOTQ4MSwibyI6MjA5LCJwIjoyfSAg"]]
@@ -20,7 +24,13 @@
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
  ["https://landing.mennetwork.com/?ats=eyJhIjozMDc4OTcsImMiOjU5MDIzMTM1LCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]],  
   
-
+    
+[["BR", "AR", "CL", "MX", "PE", "BO", "VE", "GT", "DO", "CO",
+  "CR", "CU", "PY", "UY", "EC", "ES", "PT"],
+ ["https://wct.link/click?c=eyJhIjoxOTk1MSwibyI6NjYsInAiOjIsImMiOnt9fSAg"]],
+    
+    
+    
 [["ZZ"], 
  ["https://wct.link/click?c=eyJhIjoxOTQ4MSwibyI6NTgsInAiOjJ9",
   "https://wct.link/click?c=eyJhIjoxOTQ4MSwibyI6MjA5LCJwIjoyfSAg"]]
