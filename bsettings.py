4148254b6cc7d99a8c547bfbbaf60edd5139d7ae
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-12-16 19:05:12 +0300

    Update bsettings.py

--- bsettings.py
+++ bsettings.py
@@ -4,7 +4,7 @@
 
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.biempire.com/?ats=eyJhIjozMDY5ODksImMiOjU4OTcwNDEyLCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]],
+ ["https://landing.biempire.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]],
         
 [["ZZ"], ["https://bongacams7.com/track?v=2&c=332289"]]
                
@@ -15,7 +15,7 @@
 
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.biempire.com/?ats=eyJhIjozMDY5ODksImMiOjU4OTcwNDEyLCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]],
+ ["https://landing.biempire.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]],
         
 [["ZZ"], ["https://bongacams7.com/track?v=2&c=332289"]]
 
