575ce8b1e74fa8aac1afd678a92a7786467eaf09
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2020-12-16 19:05:50 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -3,7 +3,7 @@
 
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.transharder.com/?ats=eyJhIjozMDY5ODksImMiOjU4OTcwNDEyLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]],
+ ["https://landing.transsensual.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo3MiwicyI6NTc2LCJlIjo5MjU4LCJwIjoxMX0="]],
   
 [["ZZ"], ["https://trans.com"]]
 
@@ -13,7 +13,7 @@
 
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.transharder.com/?ats=eyJhIjozMDY5ODksImMiOjU4OTcwNDEyLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]],
+ ["https://landing.transsensual.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo3MiwicyI6NTc2LCJlIjo5MjU4LCJwIjoxMX0="]],
   
 [["ZZ"], ["https://trans.com"]]
 ]}
