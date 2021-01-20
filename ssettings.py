971e1a0fbbdfee498fe7381a182263f9dc318c5b
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-01-20 13:56:17 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -5,6 +5,8 @@
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
  ["https://landing.transangelsnetwork.com/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjozNSwicyI6NTQxLCJlIjo5MTEzLCJwIjoxMX0="]],
   
+[["RU"], ["https://trans.com/"]],
+  
 [["ZZ"], ["https://trans.com"]]
 
 ],
@@ -14,6 +16,8 @@
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
  ["https://landing.transangelsnetwork.com/?ats=eyJhIjozMDU2ODQsImMiOjU4ODkxNzk0LCJuIjozNSwicyI6NTQxLCJlIjo5MTEzLCJwIjoxMX0="]],
+  
+[["RU"], ["https://trans.com/"]],
   
 [["ZZ"], ["https://trans.com"]]
 ]}
