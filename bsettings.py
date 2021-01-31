b5f8136d65124e98a498e0c128bad8bdeb7267b6
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-01-31 15:53:54 +0300

    Update bsettings.py

--- bsettings.py
+++ bsettings.py
@@ -5,7 +5,9 @@
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
  ["https://landing.biempire.com/?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]],
-        
+
+[["RU"], ["https://transdh.com/"]],
+    
 [["ZZ"], ["https://bongacams7.com/track?v=2&c=332289"]]
                
 ],
@@ -16,7 +18,9 @@
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
  ["https://landing.biempire.com/?ats=eyJhIjoyODY1MDYsImMiOjUzMjU1MTM5LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]],
-        
+
+[["RU"], ["https://transdh.com/"]],
+    
 [["ZZ"], ["https://bongacams7.com/track?v=2&c=332289"]]
 
 ]
