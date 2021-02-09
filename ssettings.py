d0a578604cc760f972cf3e80cb2c983d78d5028f
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-02-09 05:54:01 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -3,11 +3,11 @@
 
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.transharder.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]],
+ ["https://landing.transharder.com/?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]],
   
 [["RU"], ["https://transdh.com/"]],
   
-[["ZZ"], ["https://landing.transharder.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.transharder.com/?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
 
 ],
 
@@ -15,9 +15,9 @@
 
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.transharder.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]],
+ ["https://landing.transharder.com/?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]],
   
 [["RU"], ["https://transdh.com/"]],
   
-[["ZZ"], ["https://landing.transharder.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.transharder.com/?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
 ]}
