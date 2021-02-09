39068ff559660e9066117836831b113bae84a070
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-02-09 05:53:21 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@
 
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.brazzersnetwork.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]],     
+ ["https://landing.brazzersnetwork.com/?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]],     
     
     
 [["ZZ"], ["https://bongacams7.com/track?v=2&c=332289"]]
@@ -17,7 +17,7 @@
 
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.brazzersnetwork.com/?ats=eyJhIjoyOTY5OTcsImMiOjU3NjY3NjgzLCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]],     
+ ["https://landing.brazzersnetwork.com/?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]],     
 
     
 [["ZZ"], ["https://bongacams7.com/track?v=2&c=332289"]]
