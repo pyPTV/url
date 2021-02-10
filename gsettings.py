39eaf9024d32c6cd237738f4bf013521b439f066
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-02-10 07:03:41 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -4,7 +4,7 @@
 
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.mennetwork.com/?ats=eyJhIjozMDYzMTIsImMiOjU4OTMwNjY5LCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]],  
+ ["https://landing.mennetwork.com/?ats=eyJhIjozMDkxNTgsImMiOjU5MTAxMTgwLCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]],  
   
 [["BR", "AR", "CL", "MX", "PE", "BO", "VE", "GT", "DO", "CO",
   "CR", "CU", "PY", "UY", "EC", "ES", "PT"],
@@ -27,7 +27,7 @@
 
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.mennetwork.com/?ats=eyJhIjozMDYzMTIsImMiOjU4OTMwNjY5LCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]],  
+ ["https://landing.mennetwork.com/?ats=eyJhIjozMDkxNTgsImMiOjU5MTAxMTgwLCJuIjoyMiwicyI6NTQyLCJlIjo5MDk0LCJwIjoxMX0="]],  
   
     
 [["BR", "AR", "CL", "MX", "PE", "BO", "VE", "GT", "DO", "CO",
