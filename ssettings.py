1074df54db51ae7c668ffb8892f5e269e8867e81
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-02-16 05:42:20 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -1,23 +1,19 @@
 {
 "mobile": [
 
-[["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
-  "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.transharder.com/?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]],
+
   
 [["RU"], ["https://transdh.com/"]],
   
-[["ZZ"], ["https://landing.transharder.com/?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.transharder.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
 
 ],
 
 "desktop": [
 
-[["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
-  "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.transharder.com/?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]],
+
   
 [["RU"], ["https://transdh.com/"]],
   
-[["ZZ"], ["https://landing.transharder.com/?ats=eyJhIjoyODkwNDIsImMiOjU2MzQ2MzQxLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.transharder.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
 ]}
