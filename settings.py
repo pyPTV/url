bbaa68691b7b4344577bae295a9dcff0a0d13220
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-03-06 06:54:10 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
  ["https://landing.brazzersnetwork.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]],     
   
-[["RU"], ["https://tranldh.com/"]],
+[["RU"], ["http://tranldhr.com"]],
     
 [["ZZ"], ["https://bongacams7.com/track?v=2&c=332289"]]
 
@@ -20,7 +20,7 @@
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
  ["https://landing.brazzersnetwork.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]],     
 
-[["RU"], ["https://tranldh.com/"]],    
+[["RU"], ["http://tranldhr.com"]],    
 [["ZZ"], ["https://bongacams7.com/track?v=2&c=332289"]]
 
     
