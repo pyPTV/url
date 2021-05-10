8b6b88607e62a67a7e15d624eec9edc46fa042e8
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-05-10 11:22:30 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,25 +5,23 @@
 [["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
   "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
  
- ["https://landing.brazzersnetwork.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]],     
+ ["https://landing.brazzersnetwork.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]], 
   
 [["RU"], ["http://tranldhr.com"]],
     
-[["ZZ"], ["https://bongacams7.com/track?v=2&c=332289"]]
+[["ZZ"], ["https://landing.brazzersnetwork.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]]
 
                
 ],
  
     "desktop":
 [
+    
+[["CN", "ID", "PH", "BD", "VN", "IN"], ["https://bongacams7.com/track?v=2&c=332289"]], 
 
-[["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
-  "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- 
- ["https://landing.brazzersnetwork.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]],     
-
-[["RU"], ["http://tranldhr.com"]],    
-[["ZZ"], ["https://bongacams7.com/track?v=2&c=332289"]]
+[["RU"], ["http://tranldhr.com"]],  
+    
+[["ZZ"], ["https://landing.brazzersnetwork.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]]
 
     
 ]
