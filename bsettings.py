5bc147ff6957eac1bfa96705141a916bf8b7b2f1
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-05-16 07:02:40 +0300

    Update bsettings.py

--- bsettings.py
+++ bsettings.py
@@ -3,7 +3,7 @@
 [
 
 
-
+[["CN", "ID", "PH", "BD", "VN", "IN"], ["https://bongacams7.com/track?v=2&c=332289"]], 
 [["RU"], ["http://galturgg.com"]],
     
 [["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
@@ -13,7 +13,7 @@
     "desktop":
 [
 
-
+[["CN", "ID", "PH", "BD", "VN", "IN"], ["https://bongacams7.com/track?v=2&c=332289"]], 
 [["RU"], ["http://galturgg.com"]],
     
 [["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
