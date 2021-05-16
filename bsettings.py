b2fe8312d96faae92b2e46f393b2b28252299aba
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-05-16 07:00:16 +0300

    Update bsettings.py

--- bsettings.py
+++ bsettings.py
@@ -2,26 +2,21 @@
     "mobile":
 [
 
-[["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
-  "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.biempire.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]],
+
 
 [["RU"], ["http://galturgg.com"]],
     
-[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
                
 ],
  
     "desktop":
 [
 
-[["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
-  "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- ["https://landing.biempire.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]],
 
 [["RU"], ["http://galturgg.com"]],
     
-[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyOTc3MjYsImMiOjU3NzY3NzI0LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
 
 ]
 }
