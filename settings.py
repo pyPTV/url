980cc27001f2daa30fa02688c4ba594cf650784b
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-07-18 09:53:48 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@
   
 [["RU"], ["http://tranldhr.com"]],
     
-[["ZZ"], ["https://1261a046afdc.nobhere.com/?p=25321&media_type=adult"]]
+[["ZZ"], ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]]
 
                
 ],
@@ -17,11 +17,14 @@
     "desktop":
 [
     
-[["CN", "ID", "PH", "BD", "VN", "IN", "JP", "TW"], ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]], 
-
-[["RU"], ["http://tranldhr.com"]],  
+[["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
+  "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
+ 
+ ["https://landing.brazzersnetwork.com/?ats=eyJhIjozMDkxNTgsImMiOjU5MTAxMTgwLCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]], 
+  
+[["RU"], ["http://tranldhr.com"]],
     
-[["ZZ"], ["https://landing.brazzersnetwork.com/?ats=eyJhIjozMDkxNTgsImMiOjU5MTAxMTgwLCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]]
+[["ZZ"], ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]]
 
     
 ]
