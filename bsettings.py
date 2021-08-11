461cc42ad7908a199568c4c52d32d8f1615f13b7
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-08-11 05:20:24 +0300

    Update bsettings.py

--- bsettings.py
+++ bsettings.py
@@ -6,7 +6,7 @@
 [["CN", "ID", "PH", "BD", "VN", "IN"], ["https://bongacams7.com/track?v=2&c=332289"]], 
 [["RU"], ["http://galturgg.com"]],
     
-[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjozMDYzMTIsImMiOjU4OTMwNjY5LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
                
 ],
  
@@ -16,7 +16,7 @@
 [["CN", "ID", "PH", "BD", "VN", "IN"], ["https://bongacams7.com/track?v=2&c=332289"]], 
 [["RU"], ["http://galturgg.com"]],
     
-[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjoyODgxMzgsImMiOjU1ODIwMDAxLCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.biempire.com/?ats=eyJhIjozMDYzMTIsImMiOjU4OTMwNjY5LCJuIjo3MSwicyI6NTc1LCJlIjo5MjcwLCJwIjoxMX0="]]
 
 ]
 }
