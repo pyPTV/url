f3b8a30aff7a1b66902ff84cfc0536cfc6498e4b
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-09-02 19:09:39 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -5,7 +5,7 @@
 [["CN", "ID", "PH", "BD", "VN", "IN"], ["https://bongacams7.com/track?v=2&c=332289"]],   
 [["RU"], ["http://merytgddm.com"]],
   
-[["ZZ"], ["https://landing.transharder.com/?ats=eyJhIjozMDcxMDQsImMiOjU4OTc3MDkzLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.transharder.com/?ats=eyJhIjozMDkxNTgsImMiOjU5MTAxMTgwLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
 
 ],
 
@@ -15,5 +15,5 @@
   
 [["RU"], ["http://merytgddm.com"]],
   
-[["ZZ"], ["https://landing.transharder.com/?ats=eyJhIjozMDcxMDQsImMiOjU4OTc3MDkzLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
+[["ZZ"], ["https://landing.transharder.com/?ats=eyJhIjozMDkxNTgsImMiOjU5MTAxMTgwLCJuIjo5MCwicyI6NjI5LCJlIjo5ODAyLCJwIjoxMX0="]]
 ]}
