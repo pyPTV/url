be1c741e7be80c5d3efe5788f020a89c4bfa517c
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-09-06 07:51:57 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -3,32 +3,37 @@
 [
 
    
-[["CN", "ID", "PH", "BD", "VN", "IN"], ["https://wct.link/click?c=eyJhIjoxOTk1MSwibyI6NjUsInAiOjIsImMiOnt9fSAg"]],    
+[["CN", "ID", "PH", "BD", "VN", "IN"], 
+ ["https://wct.link/click?c=eyJhIjoxOTk1MSwibyI6NjUsInAiOjIsImMiOnt9fSAg"]],    
 
 [["RU"], ["http://pylasdfghh.com"]],
     
-[["ZZ"], ["https://landing.twinkpop.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo5NywicyI6NjQ0LCJlIjo5ODk4LCJwIjoxMX0=",
-          "https://landing.noirmale.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo4OCwicyI6NjI2LCJlIjo5NzgwLCJwIjoxMX0=",
-          "https://landing.seancodynetwork.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjoyNywicyI6NTM4LCJlIjo5MDk2LCJwIjoxMX0=",
-          "https://landing.iconmale.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="
-         ]]
+[["ZZ"], 
+ ["https://landing.twinkpop.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo5NywicyI6NjQ0LCJlIjo5ODk4LCJwIjoxMX0=",
+  "https://landing.maleaccess.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3OCwicyI6NjAyLCJlIjo5NDc3LCJwIjoxMX0="
+  ]]
+          
+          
+         
  
                
 ],
- 
+
     "desktop":
 [
 
 
-[["CN", "ID", "PH", "BD", "VN", "IN"], ["https://wct.link/click?c=eyJhIjoxOTk1MSwibyI6NjUsInAiOjIsImMiOnt9fSAg"]],    
+[["CN", "ID", "PH", "BD", "VN", "IN"], 
+ ["https://wct.link/click?c=eyJhIjoxOTk1MSwibyI6NjUsInAiOjIsImMiOnt9fSAg"]],    
 
 [["RU"], ["http://pylasdfghh.com"]],
     
-[["ZZ"], ["https://landing.twinkpop.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo5NywicyI6NjQ0LCJlIjo5ODk4LCJwIjoxMX0=",
-          "https://landing.noirmale.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo4OCwicyI6NjI2LCJlIjo5NzgwLCJwIjoxMX0=",
-          "https://landing.seancodynetwork.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjoyNywicyI6NTM4LCJlIjo5MDk2LCJwIjoxMX0=",
-          "https://landing.iconmale.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="
-         ]]
+[["ZZ"],
+ ["https://landing.twinkpop.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo5NywicyI6NjQ0LCJlIjo5ODk4LCJwIjoxMX0=",
+  "https://landing.maleaccess.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjo3OCwicyI6NjAyLCJlIjo5NDc3LCJwIjoxMX0="
+        
+          
+  ]]
 
 ]
 }
