7053eafd78fcaec6b562c6aba462173e7e8feaec
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-09-21 10:09:29 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -9,7 +9,7 @@
 [["RU"], ["http://pylasdfghh.com"]],
     
 [["ZZ"], 
- ["https://landing.iconmale.com/?ats=eyJhIjoyOTc5MjAsImMiOjU3Nzg4NDU1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="
+ ["https://landing.iconmale.com/?ats=eyJhIjozMDcxMDQsImMiOjU4OTc3MDkzLCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="
   
   
 
@@ -32,7 +32,7 @@
     
 [["ZZ"],
  [
-  "https://landing.iconmale.com/?ats=eyJhIjoyOTc5MjAsImMiOjU3Nzg4NDU1LCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="
+  "https://landing.iconmale.com/?ats=eyJhIjozMDcxMDQsImMiOjU4OTc3MDkzLCJuIjo3NCwicyI6NTgxLCJlIjo5NDM3LCJwIjoxMX0="
   
         
           
