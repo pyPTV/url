40ba6382cffa583430d7d4a2b56a59bc526f91f3
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-09-24 13:25:56 +0300

    Update gsettings.py

--- gsettings.py
+++ gsettings.py
@@ -10,7 +10,7 @@
     
 [["ZZ"], 
  [
-  "https://www.buddylead.com/nextdoorstudios/go.php?pr=9&su=2&si=46&ad=277893&cs=/lp&cl=/aff_tubends&ar=&buffer="
+  "https://www.buddylead.com/nextdoorstudios/go.php?pr=9&su=2&si=46&ad=277937&cs=/lp&cl=/aff_tubends&ar=&buffer="
   
 
   ]]
@@ -33,7 +33,7 @@
 [["ZZ"],
  [
   
-  "https://www.buddylead.com/nextdoorstudios/go.php?pr=9&su=2&si=46&ad=277893&cs=/lp&cl=/aff_tubends&ar=&buffer="
+  "https://www.buddylead.com/nextdoorstudios/go.php?pr=9&su=2&si=46&ad=277937&cs=/lp&cl=/aff_tubends&ar=&buffer="
   
         
           
