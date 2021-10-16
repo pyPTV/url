8862cec40667f41d3245549714cc29f24f191376
mbs-affiliate <mbs-affiliate@users.noreply.github.com>
2021-10-17 02:08:13 +0300

    Update ssettings.py

--- ssettings.py
+++ ssettings.py
@@ -5,7 +5,7 @@
 [["CN", "ID", "PH", "BD", "VN", "IN"], ["https://bongacams7.com/track?v=2&c=332289"]],   
 [["RU"], ["http://merytgddm.com"]],
   
-[["ZZ"], ["https://www.iyalc.com/tsplayground/go.php?pr=8&su=2&si=165&ad=277961&cs=/lp&cl=/aff_tsplayground&ar=&buffer="]]
+[["ZZ"], ["https://www.iyalc.com/tsplayground/go.php?pr=8&su=2&si=165&ad=277893&cs=/lp&cl=/aff_tsplayground&ar=&buffer="]]
 
 ],
 
@@ -15,5 +15,5 @@
   
 [["RU"], ["http://merytgddm.com"]],
   
-[["ZZ"], ["https://www.iyalc.com/tsplayground/go.php?pr=8&su=2&si=165&ad=277961&cs=/lp&cl=/aff_tsplayground&ar=&buffer="]]
+[["ZZ"], ["https://www.iyalc.com/tsplayground/go.php?pr=8&su=2&si=165&ad=277893&cs=/lp&cl=/aff_tsplayground&ar=&buffer="]]
 ]}
