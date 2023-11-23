9e309be7dee34683ea832405e69e7bfec383108d
Kurt <11032241+pyPTV@users.noreply.github.com>
2023-11-23 18:21:03 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,34 +1,4 @@
-{
-    "mobile":
-[
-
-[["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
-  "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- 
- ["https://landing.brazzersnetwork.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]], 
-  
-[["RU"], ["http://tranldhr.com"]],
-    
-[["ZZ"], ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]]
-
-               
-],
- 
-    "desktop":
-[
-    
-[["CA", "US", "AU", "NZ", "GB", "IE", "DK", "NO", "SE", 
-  "FR", "DE", "AT", "BE", "IT", "CH", "NL", "IS", "FI"],
- 
- ["https://landing.brazzersnetwork.com/?ats=eyJhIjoyODg4MzMsImMiOjU2Mjk2NzE2LCJuIjoxNCwicyI6OTAsImUiOjg4MDMsInAiOjExfQ=="]], 
-  
-[["RU"], ["http://tranldhr.com"]],
-    
-[["ZZ"], ["https://enter.javhd.com/track/MjIwOTIuMi4yLjIuMC4wLjAuMC4w"]]
-
-    
-]
-}
+fgh
 
 
 
